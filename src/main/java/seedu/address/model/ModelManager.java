64ca2430e528c499fb5795d654e151c9f47e81cf
bannified <freedomzelda_dc@hotmail.com>
2018-09-01 23:59:47 +0800

    ModelManager.java: Add deleteTag method

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -15,6 +15,7 @@ import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -78,6 +79,12 @@ public class ModelManager extends ComponentManager implements Model {
         requireAllNonNull(target, editedPerson);
 
         versionedAddressBook.updatePerson(target, editedPerson);
+        indicateAddressBookChanged();
+    }
+
+    public void deleteTag(Tag tag) throws DuplicatePersonException {
+
+        versionedAddressBook.removeTagFromAll(tag);
         indicateAddressBookChanged();
     }
 
