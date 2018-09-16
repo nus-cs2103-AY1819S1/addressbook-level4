946a7821aa81cef676cf4e5992d07345fb4e6b18
zioul123 <zioul.rsmod123@gmail.com>
2018-09-16 14:30:39 +0800

    Person cards: Change spacing between fields and enable text wrapping.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -48,6 +48,10 @@ public class PersonCard extends UiPart<Region> {
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        
+        name.setWrapText(true);
+        address.setWrapText(true);
+        email.setWrapText(true);
     }
 
     @Override
