1862a544301f506c430fd42fb5797ecc0f5063ad
dioptrique <h0910139@gmail.com>
2018-09-19 04:34:23 +0800

    modify test for remark command parsing in address book parser

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.commands.CommandTestUtil.REMARK_DESC_SAMPLE_1;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
@@ -106,7 +107,8 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_remark() throws Exception {
-        Command remarkCommand = parser.parseCommand(RemarkCommand.COMMAND_WORD);
+        Command remarkCommand = parser.parseCommand(RemarkCommand.COMMAND_WORD + " " +
+                INDEX_FIRST_PERSON.getOneBased() + REMARK_DESC_SAMPLE_1);
         assertTrue(remarkCommand instanceof RemarkCommand);
     }
 
