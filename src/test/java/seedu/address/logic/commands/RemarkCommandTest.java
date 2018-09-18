e4504a259fff2cb5597a8a50d5b65a9e1fde6858
dioptrique <h0910139@gmail.com>
2018-09-19 02:56:38 +0800

    add test for RemarkCommand#equals()

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -6,8 +6,12 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.testutil.TypicalPersons;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 public class RemarkCommandTest {
     Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(),new UserPrefs());
@@ -16,5 +20,29 @@ public class RemarkCommandTest {
     public void execute_invalidCommandFormat_failure() {
         RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, "test remark");
         assertCommandFailure(remarkCommand, model, INDEX_FIRST_PERSON +" "+"test remark");
+    }
+
+    @Test
+    public void equals() {
+        final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON, "test remark");
+        final RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON,"test remark");
+
+        // same values -> returns true
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON, "test remark")));
+
+        // different remark -> returns false
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, "test remark 2")));
     }
 }
