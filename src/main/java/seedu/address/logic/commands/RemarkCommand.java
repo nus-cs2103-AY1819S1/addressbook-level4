68cb18383ccab9b2ef53ebc5ef590367986f7ab2
dioptrique <h0910139@gmail.com>
2018-09-18 22:15:20 +0800

    modify AddressBookParser to accept remark command

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -5,7 +5,8 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
-public class RemarkCommand extends Command{
+public class RemarkCommand extends Command {
+    public static final String COMMAND_WORD = "remark";
     @Override
     public CommandResult execute() throws CommandException {
         throw new CommandException(Messages.MESSAGE_INVALID_COMMAND_FORMAT);
