0d233d2ef4d900f45f9595f920c6eb908d2f36bb
dioptrique <h0910139@gmail.com>
2018-09-19 09:53:33 +0800

    add remark class

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ */
+public class Remark {
+    public static final String MESSAGE_REMARK_CONSTRAINTS =
+            "Remarks can take any values, and they can be blank";
+
+    public String value;
+
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        this.value = remark;
+    }
+
+    @Override
+    public String toString() { return value; }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Remark)
+                && this.value.equals(((Remark) other).value);
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+}
