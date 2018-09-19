4aad9b6b57541fa6a70545fa5dfaaca0bfd2bc01
bannified <freedomzelda_dc@hotmail.com>
2018-09-19 15:25:24 +0800

    Remove unused imports

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -10,7 +10,6 @@ import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 
 
