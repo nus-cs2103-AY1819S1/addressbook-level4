ddf4f3cfc0b166e2e4058fdb0c80ed0ec4dbb2c8
Jack Wong <jackwong3101@yahoo.com>
2018-09-22 00:49:22 +0800

    add enqueue test in AddCommandTest method stub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -119,6 +119,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void enqueue(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
