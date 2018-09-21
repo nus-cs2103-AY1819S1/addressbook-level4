906d99ec1b07a870717e3410879c2739a705ce91
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-09-22 00:05:04 +0800

    Refactor LogicManagerTest
    - getFilteredPersonList_modifyList_throwsUnsupportedOperationException ->
    getFilteredTaskList_modifyList_throwsUnsupportedOperationException

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -47,7 +47,7 @@ public class LogicManagerTest {
     }
 
     @Test
-    public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
+    public void getFilteredTaskList_modifyList_throwsUnsupportedOperationException() {
         thrown.expect(UnsupportedOperationException.class);
         logic.getFilteredTaskList().remove(0);
     }
