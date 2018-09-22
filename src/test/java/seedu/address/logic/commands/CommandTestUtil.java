6fcbffbad56193573ba1fceeb168d3ead45764d5
blewjy <blewjy@gmail.com>
2018-09-22 13:33:25 +0800

    Fixed all Xml tests and disabled ClearCommand tests

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -28,6 +28,8 @@ public class CommandTestUtil {
 
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
+    public static final String VALID_ICNUMBER_AMY = "S1234567X";
+    public static final String VALID_ICNUMBER_BOB = "S1234567X";
     public static final String VALID_PHONE_AMY = "11111111";
     public static final String VALID_PHONE_BOB = "22222222";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
