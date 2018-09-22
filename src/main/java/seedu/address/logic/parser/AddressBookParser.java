55161030ca6061fbac6c611849e77cc47cf9e2f4
KJ-LT\kengj <kengji.c>
2018-09-22 09:38:49 +0800

    fix style

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -11,7 +11,6 @@ import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.eventcommands.AddEventCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
@@ -21,6 +20,8 @@ import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
 
+
+import seedu.address.logic.commands.eventcommands.AddEventCommand;
 import seedu.address.logic.commands.eventcommands.SetDateCommand;
 import seedu.address.logic.parser.eventparsers.AddEventCommandParser;
 import seedu.address.logic.parser.eventparsers.SetDateCommandParser;
--- src/main/java/seedu/address/logic/parser/eventparsers/SetDateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/eventparsers/SetDateCommandParser.java
@@ -6,9 +6,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import java.util.Date;
 
 import seedu.address.logic.commands.eventcommands.SetDateCommand;
-import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -16,6 +16,12 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses inputs arguments and creates a new SetDateCommand object.
  */
 public class SetDateCommandParser implements Parser<SetDateCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the SetDateCommand
+     * and returns an SetDateCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public SetDateCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_DATE);
