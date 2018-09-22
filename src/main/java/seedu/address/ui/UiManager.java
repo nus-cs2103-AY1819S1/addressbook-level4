38ce789b92a51eababdd3ed98ed3ffb9abd9b210
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-09-22 22:22:43 +0800

    UIManager.java: Add author
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -52,6 +52,7 @@ public class UiManager extends ComponentManager implements Ui {
         //Set the application icon.
         primaryStage.getIcons().add(getImage(ICON_APPLICATION));
         
+        //@@author jjlee050
         //Set full screen in exclusive mode
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
