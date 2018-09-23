32880c4a01d96c0e35fe971a384435edc6d8afab
Kok Yuan <warfear@hotmail.com>
2018-09-23 22:58:51 +0800

    Added compose command to perform list

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.ComposeCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
@@ -83,6 +84,9 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
+        case ComposeCommand.COMMAND_WORD:
+            return new ComposeCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
