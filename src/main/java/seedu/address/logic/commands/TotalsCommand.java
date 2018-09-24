5973375f924501b6132252551add892014ec1881
Arsaln Cheema <arsalanc.v2@gmail.com>
2018-09-24 16:01:00 +0800

    standardize statistics command classes

--- src/main/java/seedu/address/logic/commands/TotalsCommand.java
+++ src/main/java/seedu/address/logic/commands/TotalsCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.TotalStatistics;
@@ -16,7 +15,6 @@ public class TotalsCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays the following statistics as a bulleted list\n"
             + TotalStatistics.displayTypes()
             + "Example: " + COMMAND_WORD;
-    public static final String MESSAGE_SUCCESS = "totals:\n%1$s";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
