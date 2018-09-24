7e391b4d54741f3175597b7380233d6bddc6dc67
kengwoon <shannalimkengwoon@gmail.com>
2018-09-24 14:32:48 +0800

    Differentiated clear command success messages.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -12,14 +12,19 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_CLEAR_ALL_SUCCESS = "Hallper has been cleared!";
+    public static final String MESSAGE_CLEAR_SPECIFIC_SUCCESS = "Cleared contacts under %1$s in Hallper";
 
+    private final String target;
+
+    public ClearCommand(String target) {this.target = target;}
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
         model.resetData(new AddressBook());
         model.commitAddressBook();
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult( target.toLowerCase().equals("all") ?
+                MESSAGE_CLEAR_ALL_SUCCESS : String.format(MESSAGE_CLEAR_SPECIFIC_SUCCESS, target));
     }
 }
