c5a689e4112cf484644cf042f99420b79c9a8321
kengwoon <shannalimkengwoon@gmail.com>
2018-09-24 14:53:24 +0800

    Added tag check predicate. Modified clear command case check.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.person.exceptions.ContactContainsTagPredicate;
 
 /**
  * Clears the address book.
@@ -15,16 +16,25 @@ public class ClearCommand extends Command {
     public static final String MESSAGE_CLEAR_ALL_SUCCESS = "Hallper has been cleared!";
     public static final String MESSAGE_CLEAR_SPECIFIC_SUCCESS = "Cleared contacts under %1$s in Hallper";
 
+    private final ContactContainsTagPredicate predicate;
+
     private final String target;
 
-    public ClearCommand(String target) {this.target = target;}
+    public ClearCommand(String target, ContactContainsTagPredicate predicate) {
+        this.target = target;
+        this.predicate = predicate;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.resetData(new AddressBook());
-        model.commitAddressBook();
-        return new CommandResult( target.toLowerCase().equals("all") ?
-                MESSAGE_CLEAR_ALL_SUCCESS : String.format(MESSAGE_CLEAR_SPECIFIC_SUCCESS, target));
+        if (target.toLowerCase().equals("all")) {
+            model.resetData(new AddressBook());
+            model.commitAddressBook();
+            return new CommandResult(MESSAGE_CLEAR_ALL_SUCCESS);
+        } else {
+            model.updateFilteredPersonList(predicate);
+            return new CommandResult(String.format(MESSAGE_CLEAR_SPECIFIC_SUCCESS, target));
+        }
     }
 }
