35aca54e446ed8e142a62a6495c03fd984641326
Jonathan Tan <e0032273@u.nus.edu>
2018-09-25 15:13:20 +0800

    Fix style for DateTest

--- src/test/java/seedu/address/model/expense/DateTest.java
+++ src/test/java/seedu/address/model/expense/DateTest.java
@@ -1,10 +1,11 @@
 package seedu.address.model.expense;
 
-import org.junit.Test;
-import seedu.address.testutil.Assert;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
 
 //@@author jonathantjm
 public class DateTest {
