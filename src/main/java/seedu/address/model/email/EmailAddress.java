3c1d2c9c8a7cb88bb346b68591fcc5d72a438b5c
Kok Yuan <warfear@hotmail.com>
2018-09-26 01:51:49 +0800

    Added Subject in model.email package

--- src/main/java/seedu/address/model/email/EmailAddress.java
+++ src/main/java/seedu/address/model/email/EmailAddress.java
@@ -5,7 +5,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 
 /**
- * Represents a Person's email address in Email.
+ * Represents an email address in Email.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmailAddress(String)}
  */
 public class EmailAddress {
