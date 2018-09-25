c3ef76a6c7840e729f3d85ec5ed173d16cd48c6c
Jonathan Tan <e0032273@u.nus.edu>
2018-09-25 12:38:09 +0800

    Edit Date class

--- src/main/java/seedu/address/model/expense/Date.java
+++ src/main/java/seedu/address/model/expense/Date.java
@@ -3,16 +3,21 @@ package seedu.address.model.expense;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+//@@author jonathantjm
 /**
  * Represents the date the Expense was added into the Expense tracker.
  * Guarantees: immutable; is valid as declared in {@link #isValidDate(String)}
  */
 public class Date {
-    public static final String MESSAGE_DATE_CONSTRAINTS =
-            "Date should only contain numbers and slashes, and it should not be blank";
+    public static final String MESSAGE_FORMAT_CONSTRAINTS =
+            "Date should be valid, only contain numbers and slashes, and it should not be blank";
 
     public static final String DATE_VALIDATION_REGEX = "(\\d{1,2})(\\-)(\\d{1,2})(\\-)(\\d{4})";
-    public final String fullDate;
+    public final Calendar fullDate = new GregorianCalendar();
 
 
     /**
@@ -22,16 +27,39 @@ public class Date {
      */
     public Date(String date) {
         requireNonNull(date);
-        checkArgument(isValidDate(date), MESSAGE_DATE_CONSTRAINTS);
-        fullDate = date;
+        checkArgument(isValidDate(date), MESSAGE_FORMAT_CONSTRAINTS);
+        String [] parsedDate = date.split("-");
+        fullDate.set(Integer.parseInt(parsedDate[2]),
+                Integer.parseInt(parsedDate[1]) - 1,
+                Integer.parseInt(parsedDate[0]));
     }
 
 
     /**
-     * return true is the given date is a valid date.
+     * return true is the given date is in the valid format.
      * */
-    private boolean isValidDate(String test) {
-        return test.matches(DATE_VALIDATION_REGEX);
+    public static boolean isValidDate(String test) {
+        if (test.matches(DATE_VALIDATION_REGEX)) {
+            Calendar date = new GregorianCalendar();
+            String [] parsedDate = test.split("-");
+            date.setLenient(false);
+            date.set(Integer.parseInt(parsedDate[2]),
+                    Integer.parseInt(parsedDate[1]) - 1,
+                    Integer.parseInt(parsedDate[0]));
+            try {
+                date.getTime();
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        return false;
     }
 
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(fullDate.getTime());
+    }
 }
