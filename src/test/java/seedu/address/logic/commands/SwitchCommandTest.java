bbe45d5e654e47d502b3557d515268890b0621ed
iMarbles <amabel_yeo@hotmail.com>
2018-09-26 04:48:17 +0800

    tests: Added test for switch

---
+++ src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+import static seedu.address.model.Context.EVENT_CONTEXT_ID;
+import static seedu.address.model.Context.EVENT_CONTEXT_NAME;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Context;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class SwitchCommandTest {
+    private Model model = new ModelManager();
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_switch_success() {
+        SwitchCommand switchCommand = new SwitchCommand(Context.EVENT_CONTEXT_ID);
+
+        String expectedMessage = String.format(SwitchCommand.MESSAGE_SUCCESS, EVENT_CONTEXT_NAME);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setCurrentContext(EVENT_CONTEXT_ID);
+
+        assertCommandSuccess(switchCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/SwitchCommandParserTest.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.CONTEXT_INVALID_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.CONTEXT_VALID_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SwitchCommand;
+import seedu.address.model.Context;
+
+public class SwitchCommandParserTest {
+    private SwitchCommandParser parser = new SwitchCommandParser();
+    
+    @Test
+    public void parse_invalidValue_failure() {
+        // invalid contextId
+        assertParseFailure(parser, CONTEXT_INVALID_DESC, Context.MESSAGE_CONTEXT_CONSTRAINTS);
+
+        // non-empty preamble
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + CONTEXT_VALID_DESC,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchCommand.MESSAGE_USAGE));
+    }
+}
