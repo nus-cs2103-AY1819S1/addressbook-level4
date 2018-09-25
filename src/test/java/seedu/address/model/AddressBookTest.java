bab1fb24b9b01ee29591b8a08de40093215b6d5d
iMarbles <amabel_yeo@hotmail.com>
2018-09-25 12:29:27 +0800

    test: AddressBookTest.java with records

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_HOUR_H2;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_R2;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalRecords.R1;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,7 +25,9 @@ import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.record.Record;
+import seedu.address.model.record.exceptions.DuplicateRecordException;
 import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.RecordBuilder;
 
 public class AddressBookTest {
 
@@ -34,6 +39,7 @@ public class AddressBookTest {
     @Test
     public void constructor() {
         assertEquals(Collections.emptyList(), addressBook.getPersonList());
+        assertEquals(Collections.emptyList(), addressBook.getRecordList());
     }
 
     @Test
@@ -49,13 +55,14 @@ public class AddressBookTest {
         assertEquals(newData, addressBook);
     }
 
+    //// Person Tests
     @Test
     public void resetData_withDuplicatePersons_throwsDuplicatePersonException() {
         // Two persons with the same identity fields
         Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
                 .build();
         List<Person> newPersons = Arrays.asList(ALICE, editedAlice);
-        AddressBookStub newData = new AddressBookStub(newPersons);
+        AddressBookStub newData = new AddressBookStub(newPersons, null);
 
         thrown.expect(DuplicatePersonException.class);
         addressBook.resetData(newData);
@@ -92,6 +99,51 @@ public class AddressBookTest {
         addressBook.getPersonList().remove(0);
     }
 
+
+    //// Record Tests
+    @Test
+    public void resetData_withDuplicateRecords_throwsDuplicateRecordException() {
+        // Two records with the same identity fields
+        Record editedRecord = new RecordBuilder(R1).withHour(VALID_HOUR_H2).withRemark(VALID_REMARK_R2)
+                .build();
+        List<Record> newRecords = Arrays.asList(R1, editedRecord);
+        AddressBookStub newData = new AddressBookStub(null, newRecords);
+
+        thrown.expect(DuplicateRecordException.class);
+        addressBook.resetData(newData);
+    }
+
+    @Test
+    public void hasRecord_nullRecord_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        addressBook.hasRecord(null);
+    }
+
+    @Test
+    public void hasRecord_recordNotInAddressBook_returnsFalse() {
+        assertFalse(addressBook.hasRecord(R1));
+    }
+
+    @Test
+    public void hasRecord_recordInAddressBook_returnsTrue() {
+        addressBook.addRecord(R1);
+        assertTrue(addressBook.hasRecord(R1));
+    }
+
+    @Test
+    public void hasRecord_recordWithSameIdentityFieldsInAddressBook_returnsTrue() {
+        addressBook.addRecord(R1);
+        Record editedRecord = new RecordBuilder(R1).withHour(VALID_HOUR_H2).withRemark(VALID_REMARK_R2)
+                .build();
+        assertTrue(addressBook.hasRecord(editedRecord));
+    }
+
+    @Test
+    public void getRecordList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        addressBook.getRecordList().remove(0);
+    }
+
     /**
      * A stub ReadOnlyAddressBook whose persons list can violate interface constraints.
      */
@@ -99,8 +151,13 @@ public class AddressBookTest {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
         private final ObservableList<Record> records = FXCollections.observableArrayList();
 
-        AddressBookStub(Collection<Person> persons) {
-            this.persons.setAll(persons);
+        AddressBookStub(Collection<Person> persons, Collection<Record> records) {
+            if (persons != null) {
+                this.persons.setAll(persons);
+            }
+            if (records != null) {
+                this.records.setAll(records);
+            }
         }
 
         @Override
