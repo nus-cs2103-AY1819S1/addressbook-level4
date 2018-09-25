92174330f83f36c63c1857d7e0fdc87aca603d7a
KJ-LT\kengj <kengji.c>
2018-09-25 23:39:18 +0800

    fix style

--- src/main/java/seedu/address/ui/PollDisplayPanel.java
+++ src/main/java/seedu/address/ui/PollDisplayPanel.java
@@ -1,6 +1,9 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
+
 import com.google.common.eventbus.Subscribe;
+
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -10,8 +13,9 @@ import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.DisplayPollEvent;
 
-import java.util.logging.Logger;
-
+/**
+ * Panel displaying the poll.
+ */
 public class PollDisplayPanel extends UiPart<Region> {
     private static final Logger logger = LogsCenter.getLogger(ResultDisplay.class);
     private static final String FXML = "PollDisplayPanel.fxml";
