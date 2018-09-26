ecbf35d7da169bf94791b90670b6cf5dc6d5b4a8
bannified <freedomzelda_dc@hotmail.com>
2018-09-26 21:03:32 +0800

    checkstyle: XmlUtilTest.java import order

--- src/test/java/seedu/address/commons/util/XmlUtilTest.java
+++ src/test/java/seedu/address/commons/util/XmlUtilTest.java
@@ -16,8 +16,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.model.WishBook;
-import seedu.address.storage.XmlAdaptedWish;
 import seedu.address.storage.XmlAdaptedTag;
+import seedu.address.storage.XmlAdaptedWish;
 import seedu.address.storage.XmlSerializableWishBook;
 import seedu.address.testutil.TestUtil;
 import seedu.address.testutil.WishBookBuilder;
