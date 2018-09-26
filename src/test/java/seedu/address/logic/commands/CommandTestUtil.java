c94cecf77f0015fdf8047baf4434f0ea3497d894
bannified <freedomzelda_dc@hotmail.com>
2018-09-26 20:36:03 +0800

    checkstyle: CommandTestUtil.java import order

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -16,11 +16,11 @@ import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.WishBook;
 import seedu.address.model.Model;
+import seedu.address.model.WishBook;
 import seedu.address.model.wish.NameContainsKeywordsPredicate;
-import seedu.address.model.wish.Wish;
 import seedu.address.model.wish.Remark;
+import seedu.address.model.wish.Wish;
 import seedu.address.testutil.EditWishDescriptorBuilder;
 
 /**
