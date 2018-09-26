cdef3523dae39524d53089611802924363623fa8
Jun Rong <jun_rong5@hotmail.com>
2018-09-27 01:11:28 +0800

    Refactor person to recipe for classes in parser

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_RECIPE;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,9 +29,9 @@ import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.recipe.NameContainsKeywordsPredicate;
 import seedu.address.model.recipe.Recipe;
-import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.address.testutil.EditRecipeDescriptorBuilder;
 import seedu.address.testutil.RecipeBuilder;
-import seedu.address.testutil.PersonUtil;
+import seedu.address.testutil.RecipeUtil;
 
 public class AddressBookParserTest {
     @Rule
@@ -42,7 +42,7 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_add() throws Exception {
         Recipe recipe = new RecipeBuilder().build();
-        AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(recipe));
+        AddCommand command = (AddCommand) parser.parseCommand(RecipeUtil.getAddCommand(recipe));
         assertEquals(new AddCommand(recipe), command);
     }
 
@@ -55,17 +55,17 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_delete() throws Exception {
         DeleteCommand command = (DeleteCommand) parser.parseCommand(
-                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_RECIPE.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_RECIPE), command);
     }
 
     @Test
     public void parseCommand_edit() throws Exception {
         Recipe recipe = new RecipeBuilder().build();
-        EditCommand.EditRecipeDescriptor descriptor = new EditPersonDescriptorBuilder(recipe).build();
+        EditCommand.EditRecipeDescriptor descriptor = new EditRecipeDescriptorBuilder(recipe).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
+                + INDEX_FIRST_RECIPE.getOneBased() + " " + RecipeUtil.getEditRecipeDescriptorDetails(descriptor));
+        assertEquals(new EditCommand(INDEX_FIRST_RECIPE, descriptor), command);
     }
 
     @Test
@@ -110,8 +110,8 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_select() throws Exception {
         SelectCommand command = (SelectCommand) parser.parseCommand(
-                SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-        assertEquals(new SelectCommand(INDEX_FIRST_PERSON), command);
+                SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_RECIPE.getOneBased());
+        assertEquals(new SelectCommand(INDEX_FIRST_RECIPE), command);
     }
 
     @Test
