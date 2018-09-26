c3b9c1aa8680492dd9050eee971b0447b8f40f14
bannified <freedomzelda_dc@hotmail.com>
2018-09-26 20:49:17 +0800

    checkstyle: SampleDataTest.java import order

--- src/test/java/systemtests/SampleDataTest.java
+++ src/test/java/systemtests/SampleDataTest.java
@@ -9,8 +9,8 @@ import java.nio.file.Path;
 import org.junit.Test;
 
 import seedu.address.model.WishBook;
-import seedu.address.model.wish.Wish;
 import seedu.address.model.util.SampleDataUtil;
+import seedu.address.model.wish.Wish;
 import seedu.address.testutil.TestUtil;
 
 public class SampleDataTest extends WishBookSystemTest {
