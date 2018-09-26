8d6ac6f9adf47acf1ff1b79b5e8713ddb926275d
bannified <freedomzelda_dc@hotmail.com>
2018-09-26 20:23:03 +0800

    checkstyle: SampleDataUtil.java import order

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.ReadOnlyWishBook;
 import seedu.address.model.WishBook;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.wish.Address;
 import seedu.address.model.wish.Email;
 import seedu.address.model.wish.Name;
 import seedu.address.model.wish.Phone;
 import seedu.address.model.wish.Remark;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.wish.Wish;
 
 /**
