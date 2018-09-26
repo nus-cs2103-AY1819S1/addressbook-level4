97ade25d62f15642900f7e40edac3332c4a59c54
Jason Chong <jasontjakra@gmail.com>
2018-09-26 17:39:51 +0800

    Add methods for MainWindow UI to show/hide components based on logged in state

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -1,9 +1,6 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
-
 import com.google.common.eventbus.Subscribe;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
@@ -16,10 +13,14 @@ import javafx.stage.Stage;
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.model.UserLoggedInEvent;
 import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
 import seedu.address.logic.Logic;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.exceptions.NoUserSelectedException;
+
+import java.util.logging.Logger;
 
 /**
  * The Main Window. Provides the basic application layout containing
@@ -92,6 +93,7 @@ public class MainWindow extends UiPart<Stage> {
 
     /**
      * Sets the accelerator of a MenuItem.
+     *
      * @param keyCombination the KeyCombination value of the accelerator
      */
     private void setAccelerator(MenuItem menuItem, KeyCombination keyCombination) {
@@ -127,17 +129,27 @@ public class MainWindow extends UiPart<Stage> {
         browserPanel = new BrowserPanel();
         browserPlaceholder.getChildren().add(browserPanel.getRoot());
 
-        personListPanel = new PersonListPanel(logic.getFilteredPersonList());
-        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
-
         ResultDisplay resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(prefs.getAddressBookFilePath());
-        statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
-
         CommandBox commandBox = new CommandBox(logic);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
+        hideLoggedInUI();
+    }
+
+    private void hideLoggedInUI() {
+        splitPane.setManaged(false);
+        getPrimaryStage().setHeight(225);
+        getPrimaryStage().setMaxHeight(225);
+        getPrimaryStage().setMinHeight(225);
+    }
+
+    private void showLoggedInUI() {
+        splitPane.setManaged(true);
+        getPrimaryStage().setHeight(600);
+        getPrimaryStage().setMaxHeight(Integer.MAX_VALUE);
+        getPrimaryStage().setMinHeight(600);
     }
 
     void hide() {
@@ -204,5 +216,19 @@ public class MainWindow extends UiPart<Stage> {
     private void handleShowHelpEvent(ShowHelpRequestEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         handleHelp();
+    }
+
+    @Subscribe
+    public void handleLoggedInEvent(UserLoggedInEvent event) {
+        try {
+            personListPanel = new PersonListPanel(logic.getFilteredPersonList());
+        } catch (NoUserSelectedException e) {
+            //TODO fix
+            throw new IllegalStateException();
+        }
+        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+        StatusBarFooter statusBarFooter = new StatusBarFooter(prefs.getAddressBookFilePath());
+        statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
+        showLoggedInUI();
     }
 }
