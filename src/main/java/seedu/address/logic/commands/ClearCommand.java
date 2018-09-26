e649b56746bd0a5ffec0985621ffee51d2c52c24
bannified <freedomzelda_dc@hotmail.com>
2018-09-26 19:50:33 +0800

    checkstyle: ClearCommand.java import order

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,8 +3,8 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.WishBook;
 import seedu.address.model.Model;
+import seedu.address.model.WishBook;
 
 /**
  * Clears the wish book.
