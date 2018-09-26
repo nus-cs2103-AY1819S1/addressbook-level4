6489b7298d1ca76e61f37a9aafd11f20187e00e2
DESKTOP-NSV5A3C\Lee Yu Cong <yu_cong96@hotmail.com>
2018-09-27 01:19:13 +0800

    Create Medicine class

---
+++ src/main/java/seedu/address/model/Medicine.java
@@ -0,0 +1,21 @@
+package seedu.address.model;
+
+import java.util.ArrayList;
+
+public class Medicine {
+    private static ArrayList<Medicine> medicineLowStockList;
+    private String name;
+    private int minimumQuantityInStock;
+    private int stock;
+    private int serialNumber;
+    private long pricePerUnit;
+
+    public Medicine(String name, int minimumQuantityInStock, int stock, int serialNumber, long pricePerUnit) {
+        this.name = name;
+        this.minimumQuantityInStock = minimumQuantityInStock;
+        this.stock = stock;
+        this.serialNumber = serialNumber;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+}
