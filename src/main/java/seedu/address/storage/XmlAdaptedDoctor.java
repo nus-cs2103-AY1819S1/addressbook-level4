5489cd58db9350837617a877431ddba09944e4c9
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-09-28 00:35:52 +0800

    Remove unused imports.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/address/storage/XmlAdaptedDoctor.java
+++ src/main/java/seedu/address/storage/XmlAdaptedDoctor.java
@@ -1,22 +1,15 @@
 package seedu.address.storage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 import javax.xml.bind.annotation.XmlElement;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.doctor.Doctor;
 import seedu.address.model.doctor.Id;
 import seedu.address.model.doctor.Password;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 
 //@@author jjlee050
 /**
