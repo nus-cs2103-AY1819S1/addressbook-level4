f9e0983ab7f61402737efe873dd020dad7fe8e86
Reuben Tan <reubentjl@gmail.com>
2018-09-27 14:33:32 +0800

    Add generate command and update parser

---
+++ src/main/java/seedu/address/logic/commands/GenerateCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ *
+ */
+public class GenerateCommand extends Command {
+
+    public static final String COMMAND_WORD = "generate";
+    public static final String MESSAGE_SUCCESS = "Generate success!";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
