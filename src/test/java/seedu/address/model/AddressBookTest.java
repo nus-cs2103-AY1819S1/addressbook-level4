596933d8bc5080391c9c6b20a0491c59e1d2c0b9
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-09-27 19:18:08 +0800

    AddressBookTest.java: Add test method for doctors.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -5,22 +5,27 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.TypicalPersons.ADAM;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import javax.print.Doc;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.doctor.Doctor;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.testutil.DoctorBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddressBookTest {
@@ -33,6 +38,8 @@ public class AddressBookTest {
     @Test
     public void constructor() {
         assertEquals(Collections.emptyList(), addressBook.getPersonList());
+        //@@author jjlee050
+        assertEquals(Collections.emptyList(), addressBook.getDoctorList());
     }
 
     @Test
@@ -54,7 +61,20 @@ public class AddressBookTest {
         Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
                 .build();
         List<Person> newPersons = Arrays.asList(ALICE, editedAlice);
-        AddressBookStub newData = new AddressBookStub(newPersons);
+        //@@author jjlee050
+        AddressBookStub newData = new AddressBookStub(newPersons, new ArrayList<>());
+
+        thrown.expect(DuplicatePersonException.class);
+        addressBook.resetData(newData);
+    }
+
+    //@@author jjlee050
+    @Test
+    public void resetData_withDuplicateDoctors_throwsDuplicatePersonException() {
+        // Two doctors with the same identity fields
+        Doctor editedAdam = new DoctorBuilder(ADAM).withName("Adam Bell").build();
+        List<Doctor> newDoctors = Arrays.asList(ADAM, editedAdam);
+        AddressBookStub newData = new AddressBookStub(new ArrayList<>(), newDoctors);
 
         thrown.expect(DuplicatePersonException.class);
         addressBook.resetData(newData);
@@ -65,16 +85,36 @@ public class AddressBookTest {
         thrown.expect(NullPointerException.class);
         addressBook.hasPerson(null);
     }
+    
+    //@@author jjlee050
+    @Test
+    public void hasDoctor_nullDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        addressBook.hasDoctor(null);
+    }
 
     @Test
     public void hasPerson_personNotInAddressBook_returnsFalse() {
         assertFalse(addressBook.hasPerson(ALICE));
     }
 
+    //@@author jjlee050
+    @Test
+    public void hasDoctor_doctorNotInAddressBook_returnsFalse() {
+        assertFalse(addressBook.hasDoctor(ADAM));
+    }
+    
     @Test
     public void hasPerson_personInAddressBook_returnsTrue() {
         addressBook.addPerson(ALICE);
         assertTrue(addressBook.hasPerson(ALICE));
+    }
+
+    //@@author jjlee050
+    @Test
+    public void hasDoctor_doctorInAddressBook_returnsTrue() {
+        addressBook.addDoctor(ADAM);
+        assertTrue(addressBook.hasDoctor(ADAM));
     }
 
     @Test
@@ -85,10 +125,25 @@ public class AddressBookTest {
         assertTrue(addressBook.hasPerson(editedAlice));
     }
 
+    //@@author jjlee050
+    @Test
+    public void hasDoctor_doctorWithSameIdentityFieldsInAddressBook_returnsTrue() {
+        addressBook.addDoctor(ADAM);
+        Doctor editedAdam = new DoctorBuilder(ADAM).withPassword("doctor1").build();
+        assertTrue(addressBook.hasDoctor(editedAdam));
+    }
+
     @Test
     public void getPersonList_modifyList_throwsUnsupportedOperationException() {
         thrown.expect(UnsupportedOperationException.class);
         addressBook.getPersonList().remove(0);
+    }
+
+    //@@author jjlee050
+    @Test
+    public void getDoctorList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        addressBook.getDoctorList().remove(0);
     }
 
     /**
@@ -96,14 +151,21 @@ public class AddressBookTest {
      */
     private static class AddressBookStub implements ReadOnlyAddressBook {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
+        private final ObservableList<Doctor> doctors = FXCollections.observableArrayList();
 
-        AddressBookStub(Collection<Person> persons) {
+        AddressBookStub(Collection<Person> persons, Collection<Doctor> doctors) {
             this.persons.setAll(persons);
+            this.doctors.setAll(doctors);
         }
 
         @Override
         public ObservableList<Person> getPersonList() {
             return persons;
+        }
+        
+        @Override
+        public ObservableList<Doctor> getDoctorList() {
+            return doctors;
         }
     }
 
