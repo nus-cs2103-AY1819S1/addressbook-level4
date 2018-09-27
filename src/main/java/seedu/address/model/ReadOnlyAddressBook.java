e9a13716e81b76620a267b629575bf735ae12c11
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-09-27 17:25:35 +0800

    ReadOnlyAddressBook.java: Add getDoctorList.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -1,6 +1,7 @@
 package seedu.address.model;
 
 import javafx.collections.ObservableList;
+import seedu.address.model.doctor.Doctor;
 import seedu.address.model.person.Person;
 
 /**
@@ -13,5 +14,11 @@ public interface ReadOnlyAddressBook {
      * This list will not contain any duplicate persons.
      */
     ObservableList<Person> getPersonList();
+
+    /**
+     * Returns an unmodifiable view of the doctors list.
+     * This list will not contain any duplicate doctors.
+     */
+    ObservableList<Doctor> getDoctorList();
 
 }
