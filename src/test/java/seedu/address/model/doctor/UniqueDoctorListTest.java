615e3608d605e33b2d0cb67b99933bd3104b3fa6
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-09-27 20:53:20 +0800

    AddressBookTest.java: Add test cases
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

---
+++ src/test/java/seedu/address/model/doctor/UniqueDoctorListTest.java
@@ -0,0 +1,183 @@
+package seedu.address.model.doctor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalPersons.ADAM;
+import static seedu.address.testutil.TypicalPersons.BEN;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.testutil.DoctorBuilder;
+
+//@@author jjlee050
+public class UniqueDoctorListTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private final UniqueDoctorList uniqueDoctorList = new UniqueDoctorList();
+
+    @Test
+    public void contains_nullDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.contains(null);
+    }
+
+    @Test
+    public void contains_doctorNotInList_returnsFalse() {
+        assertFalse(uniqueDoctorList.contains(ADAM));
+    }
+
+    @Test
+    public void contains_doctorInList_returnsTrue() {
+        uniqueDoctorList.add(ADAM);
+        assertTrue(uniqueDoctorList.contains(ADAM));
+    }
+
+    @Test
+    public void contains_doctorWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueDoctorList.add(ADAM);
+        Doctor editedAdam = new DoctorBuilder(ADAM).withPassword("doctor1")
+                .build();
+        assertTrue(uniqueDoctorList.contains(editedAdam));
+    }
+
+    @Test
+    public void add_nullDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.add(null);
+    }
+
+    @Test
+    public void add_duplicateDoctor_throwsDuplicatePersonException() {
+        uniqueDoctorList.add(ADAM);
+        thrown.expect(DuplicatePersonException.class);
+        uniqueDoctorList.add(ADAM);
+    }
+
+    @Test
+    public void setDoctor_nullTargetDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.setDoctor(null, ADAM);
+    }
+
+    @Test
+    public void setDoctor_nullEditedDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.setDoctor(ADAM, null);
+    }
+
+    @Test
+    public void setDoctor_targetDoctorNotInList_throwsPersonNotFoundException() {
+        thrown.expect(PersonNotFoundException.class);
+        uniqueDoctorList.setDoctor(ADAM, ADAM);
+    }
+
+    @Test
+    public void setDoctor_editedDoctorIsSameDoctor_success() {
+        uniqueDoctorList.add(ADAM);
+        uniqueDoctorList.setDoctor(ADAM, ADAM);
+        UniqueDoctorList expectedUniqueDoctorList = new UniqueDoctorList();
+        expectedUniqueDoctorList.add(ADAM);
+        assertEquals(expectedUniqueDoctorList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctor_editedDoctorHasSameIdentity_success() {
+        uniqueDoctorList.add(ADAM);
+        Doctor editedAdam = new DoctorBuilder(ADAM).withPassword("doctor1")
+                .build();
+        uniqueDoctorList.setDoctor(ADAM, editedAdam);
+        UniqueDoctorList expectedUniqueDoctorList = new UniqueDoctorList();
+        expectedUniqueDoctorList.add(editedAdam);
+        assertEquals(expectedUniqueDoctorList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctor_editedDoctorHasDifferentIdentity_success() {
+        uniqueDoctorList.add(ADAM);
+        uniqueDoctorList.setDoctor(ADAM, BEN);
+        UniqueDoctorList expectedUniqueDoctorList = new UniqueDoctorList();
+        expectedUniqueDoctorList.add(BEN);
+        assertEquals(expectedUniqueDoctorList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctor_editedDoctorHasNonUniqueIdentity_throwsDuplicatePersonException() {
+        uniqueDoctorList.add(ADAM);
+        uniqueDoctorList.add(BEN);
+        thrown.expect(DuplicatePersonException.class);
+        uniqueDoctorList.setDoctor(ADAM, BEN);
+    }
+
+    @Test
+    public void remove_nullDoctor_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.remove(null);
+    }
+
+    @Test
+    public void remove_doctorDoesNotExist_throwsPersonNotFoundException() {
+        thrown.expect(PersonNotFoundException.class);
+        uniqueDoctorList.remove(ADAM);
+    }
+
+    @Test
+    public void remove_existingDoctor_removesDoctor() {
+        uniqueDoctorList.add(ADAM);
+        uniqueDoctorList.remove(ADAM);
+        UniquePersonList expectedUniquePersonList = new UniquePersonList();
+        assertEquals(expectedUniquePersonList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctors_nullUniqueDoctorList_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.setDoctors((UniqueDoctorList) null);
+    }
+
+    @Test
+    public void setDoctors_uniqueDictirList_replacesOwnListWithProvidedUniqueDoctorList() {
+        uniqueDoctorList.add(ADAM);
+        UniqueDoctorList expectedUniqueDoctorList = new UniqueDoctorList();
+        expectedUniqueDoctorList.add(BEN);
+        uniqueDoctorList.setDoctors(expectedUniqueDoctorList);
+        assertEquals(expectedUniqueDoctorList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctors_nullList_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        uniqueDoctorList.setDoctors((List<Doctor>) null);
+    }
+
+    @Test
+    public void setDoctors_list_replacesOwnListWithProvidedList() {
+        uniqueDoctorList.add(ADAM);
+        List<Doctor> personList = Collections.singletonList(BEN);
+        uniqueDoctorList.setDoctors(personList);
+        UniqueDoctorList expectedUniqueDoctorList = new UniqueDoctorList();
+        expectedUniqueDoctorList.add(BEN);
+        assertEquals(expectedUniqueDoctorList, uniqueDoctorList);
+    }
+
+    @Test
+    public void setDoctors_listWithDuplicateDoctors_throwsDuplicatePersonException() {
+        List<Doctor> listWithDuplicatePersons = Arrays.asList(ADAM, ADAM);
+        thrown.expect(DuplicatePersonException.class);
+        uniqueDoctorList.setDoctors(listWithDuplicatePersons);
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        uniqueDoctorList.asUnmodifiableObservableList().remove(0);
+    }
+}
