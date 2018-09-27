8bc5c128a0c06bbceba8cf5c46b4c380dea2cf1f
Jack Wong <jackwong3101@yahoo.com>
2018-09-28 00:48:03 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -16,8 +16,7 @@ import seedu.address.model.person.Person;
 /**
  * The main LogicManager of the app.
  */
-public class
-LogicManager extends ComponentManager implements Logic {
+public class LogicManager extends ComponentManager implements Logic {
     private final Logger logger = LogsCenter.getLogger(LogicManager.class);
 
     private final Model model;
