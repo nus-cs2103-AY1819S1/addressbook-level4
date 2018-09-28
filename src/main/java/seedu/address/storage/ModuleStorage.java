d41c852be3446c46cd968bb6eeb8aa032734adb2
Reuben Tan <reubentjl@gmail.com>
2018-09-28 17:27:01 +0800

    Add interface and class for Module Storage
    The interface will make it easier to load in other modules in different
    formats in the future.

---
+++ src/main/java/seedu/address/storage/ModuleStorage.java
@@ -0,0 +1,14 @@
+package seedu.address.storage;
+
+import java.nio.file.Path;
+
+/**
+ * Represents a storage for {@link seedu.address.model.Module}.
+ */
+public interface ModuleStorage {
+
+    /**
+     * Returns the file path of the data file.
+     */
+    Path getAddressBookFilePath();
+}
---
+++ src/main/java/seedu/address/storage/XmlModuleStorage.java
@@ -0,0 +1,24 @@
+package seedu.address.storage;
+
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
+
+/**
+ * A class to access Modules data stored as an xml file on the hard disk.
+ */
+public class XmlModuleStorage implements ModuleStorage {
+    private static final Logger logger = LogsCenter.getLogger(XmlAddressBookStorage.class);
+
+    private Path filePath;
+
+    public XmlModuleStorage(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public Path getAddressBookFilePath() {
+        return null;
+    }
+}
