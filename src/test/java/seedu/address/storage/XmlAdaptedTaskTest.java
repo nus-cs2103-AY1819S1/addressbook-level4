12da69f39362cc771c36ab2ae4dbe7969de8235b
remo5000 <vigneshshankar@gmail.com>
2018-09-28 12:36:50 +0800

    Refactor a test file

--- src/test/java/seedu/address/storage/XmlAdaptedTaskTest.java
+++ src/test/java/seedu/address/storage/XmlAdaptedTaskTest.java
@@ -33,60 +33,60 @@ public class XmlAdaptedTaskTest {
             .collect(Collectors.toList());
 
     @Test
-    public void toModelType_validPersonDetails_returnsPerson() throws Exception {
+    public void toModelType_validTaskDetails_returnsTask() throws Exception {
         XmlAdaptedTask task = new XmlAdaptedTask(BENSON);
         assertEquals(BENSON, task.toModelType());
     }
 
     @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
-        XmlAdaptedTask person =
+        XmlAdaptedTask task =
                 new XmlAdaptedTask(INVALID_NAME, VALID_DUE_DATE, VALID_PRIORITY_VALUE, VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = Name.MESSAGE_NAME_CONSTRAINTS;
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        XmlAdaptedTask person = new XmlAdaptedTask(null, VALID_DUE_DATE, VALID_PRIORITY_VALUE,
+        XmlAdaptedTask task = new XmlAdaptedTask(null, VALID_DUE_DATE, VALID_PRIORITY_VALUE,
                 VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
-    public void toModelType_invalidPhone_throwsIllegalValueException() {
-        XmlAdaptedTask person =
+    public void toModelType_invalidDueDate_throwsIllegalValueException() {
+        XmlAdaptedTask task =
                 new XmlAdaptedTask(VALID_NAME, INVALID_DUE_DATE, VALID_PRIORITY_VALUE, VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = DueDate.MESSAGE_DUEDATE_CONSTRAINTS;
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
-    public void toModelType_nullPhone_throwsIllegalValueException() {
-        XmlAdaptedTask person = new XmlAdaptedTask(VALID_NAME, null, VALID_PRIORITY_VALUE,
+    public void toModelType_nullDueDate_throwsIllegalValueException() {
+        XmlAdaptedTask task = new XmlAdaptedTask(VALID_NAME, null, VALID_PRIORITY_VALUE,
                 VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, DueDate.class.getSimpleName());
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
-    public void toModelType_invalidEmail_throwsIllegalValueException() {
-        XmlAdaptedTask person =
+    public void toModelType_invalidPriorityValue_throwsIllegalValueException() {
+        XmlAdaptedTask task =
                 new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, INVALID_PRIORITY_VALUE, VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = PriorityValue.MESSAGE_PRIORITYVALUE_CONSTRAINTS;
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
-    public void toModelType_nullEmail_throwsIllegalValueException() {
-        XmlAdaptedTask person = new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, null, VALID_DESCRIPTION, VALID_LABELS);
+    public void toModelType_nullPriorityValue_throwsIllegalValueException() {
+        XmlAdaptedTask priorityValue = new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, null, VALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, PriorityValue.class.getSimpleName());
-        Assert.assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, priorityValue::toModelType);
     }
 
     @Test
-    public void toModelType_invalidAddress_throwsIllegalValueException() {
+    public void toModelType_invalidDescription_throwsIllegalValueException() {
         XmlAdaptedTask task =
                 new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, VALID_PRIORITY_VALUE, INVALID_DESCRIPTION, VALID_LABELS);
         String expectedMessage = Description.MESSAGE_DESCRIPTION_CONSTRAINTS;
@@ -94,19 +94,18 @@ public class XmlAdaptedTaskTest {
     }
 
     @Test
-    public void toModelType_nullAddress_throwsIllegalValueException() {
+    public void toModelType_nullDescription_throwsIllegalValueException() {
         XmlAdaptedTask task = new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, VALID_PRIORITY_VALUE, null, VALID_LABELS);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
     }
 
     @Test
-    public void toModelType_invalidTags_throwsIllegalValueException() {
-        List<XmlAdaptedLabel> invalidTags = new ArrayList<>(VALID_LABELS);
-        invalidTags.add(new XmlAdaptedLabel(INVALID_LABEL));
+    public void toModelType_invalidLabels_throwsIllegalValueException() {
+        List<XmlAdaptedLabel> invalidLabels = new ArrayList<>(VALID_LABELS);
+        invalidLabels.add(new XmlAdaptedLabel(INVALID_LABEL));
         XmlAdaptedTask task =
-                new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, VALID_PRIORITY_VALUE, VALID_DESCRIPTION, invalidTags);
+                new XmlAdaptedTask(VALID_NAME, VALID_DUE_DATE, VALID_PRIORITY_VALUE, VALID_DESCRIPTION, invalidLabels);
         Assert.assertThrows(IllegalValueException.class, task::toModelType);
     }
-
 }
