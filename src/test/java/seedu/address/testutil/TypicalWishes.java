eee561388b4a5095b4264ae6f679d90270f8b9ff
bannified <freedomzelda_dc@hotmail.com>
2018-09-28 19:34:31 +0800

    Update TypicalWishes price fields

--- src/test/java/seedu/address/testutil/TypicalWishes.java
+++ src/test/java/seedu/address/testutil/TypicalWishes.java
@@ -26,53 +26,54 @@ public class TypicalWishes {
     public static final Wish ALICE = new WishBuilder().withName("Alice Pauline")
             .withUrl("https://www.lazada.sg/products/ps4-092-hori-real-arcade-pron-hayabusaps4ps3pc-i223784444-s340908955.html")
             .withEmail("alice@example.com")
-            .withPhone("94351253")
+            .withPrice("94.35")
             .withTags("friends")
             .build();
     public static final Wish BENSON = new WishBuilder().withName("Benson Meier")
             .withUrl("https://www.amazon.com/Apple-iPhone-Fully-Unlocked-32GB/dp/B0731HBTZ7")
-            .withEmail("johnd@example.com").withPhone("98765432")
+            .withEmail("johnd@example.com")
+            .withPrice("98.76")
             .withTags("owesMoney", "friends")
             .build();
     public static final Wish CARL = new WishBuilder().withName("Carl Kurz")
-            .withPhone("95352563")
+            .withPrice("98.76")
             .withEmail("heinz@example.com")
             .withUrl("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV")
             .build();
     public static final Wish DANIEL = new WishBuilder().withName("Daniel Meier")
-            .withPhone("87652533")
+            .withPrice("87.65")
             .withEmail("cornelia@example.com")
             .withUrl("https://www.amazon.com/Logitech-Mechanical-Keyboard-Romer-Switches/dp/B071VHYZ62")
             .withTags("friends")
             .build();
     public static final Wish ELLE = new WishBuilder().withName("Elle Meyer")
-            .withPhone("9482224")
+            .withPrice("94.82")
             .withEmail("werner@example.com")
             .withUrl("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV")
             .build();
     public static final Wish FIONA = new WishBuilder().withName("Fiona Kunz")
-            .withPhone("9482427")
+            .withPrice("94.82")
             .withEmail("lydia@example.com")
             .withUrl("https://www.lazada.sg/products/nintendo-switch-neon-console-1-year-local-warranty-best-seller-i180040203-s230048296.html").build();
     public static final Wish GEORGE = new WishBuilder().withName("George Best")
-            .withPhone("9482442")
+            .withPrice("94.82")
             .withEmail("anna@example.com")
             .withUrl("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV").build();
 
     // Manually added
     public static final Wish HOON = new WishBuilder().withName("Hoon Meier")
-            .withPhone("8482424")
+            .withPrice("84.82")
             .withEmail("stefan@example.com")
             .withUrl("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV").build();
     public static final Wish IDA = new WishBuilder().withName("Ida Mueller")
-            .withPhone("8482131")
+            .withPrice("84.82")
             .withEmail("hans@example.com")
             .withUrl("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Wish AMY = new WishBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PRICE_AMY)
+    public static final Wish AMY = new WishBuilder().withName(VALID_NAME_AMY).withPrice(VALID_PRICE_AMY)
             .withEmail(VALID_EMAIL_AMY).withUrl(VALID_URL_AMY).withTags(VALID_TAG_FRIEND).build();
-    public static final Wish BOB = new WishBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PRICE_BOB)
+    public static final Wish BOB = new WishBuilder().withName(VALID_NAME_BOB).withPrice(VALID_PRICE_BOB)
             .withEmail(VALID_EMAIL_BOB).withUrl(VALID_URL_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
 
