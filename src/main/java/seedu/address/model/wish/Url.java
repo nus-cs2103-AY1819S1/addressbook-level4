6e239ddbe21a679c2689704d0cc6f11f62a92b85
bannified <freedomzelda_dc@hotmail.com>
2018-09-28 12:47:32 +0800

    Update Url regex

--- src/main/java/seedu/address/model/wish/Url.java
+++ src/main/java/seedu/address/model/wish/Url.java
@@ -16,7 +16,7 @@ public class Url {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String URL_VALIDATION_REGEX = "[^\\s].*";
+    public static final String URL_VALIDATION_REGEX = "\\S*";
 
     public final String value;
 
