15dc4a741b6a71858b5f430cd9c2326b6c06bfe9
bannified <freedomzelda_dc@hotmail.com>
2018-09-29 02:08:37 +0800

    SampleDataUtil.java: checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -22,35 +22,35 @@ public class SampleDataUtil {
     public static final Remark SAMPLE_REMARK_EMPTY = new Remark("");
     public static Wish[] getSampleWishes() {
         return new Wish[] {
-                new Wish(new Name("Apple iPhone 7"), new Price("87438807"), new Email("alexyeoh@example.com"),
-                        new Url("https://www.amazon.com/Apple-iPhone-Fully-Unlocked-32GB/dp/B0731HBTZ7"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("friends")),
-                new Wish(new Name("Logitech K840 Mechanical Keyboard"), new Price("99272758"),
-                        new Email("berniceyu@example.com"),
-                        new Url("https://www.amazon.com/Logitech-Mechanical-Keyboard-Romer-Switches/dp/B071VHYZ62"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("colleagues", "friends")),
-                new Wish(new Name("Nintendo Switch"), new Price("93210283"), new Email("charlotte@example.com"),
-                        new Url("https://www.lazada.sg/products/" +
-                                "nintendo-switch-neon-console-1-year-local-warranty-best-seller-i180040203-s230048296.html"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("neighbours")),
-                new Wish(new Name("PS4 Pro"), new Price("91031282"), new Email("lidavid@example.com"),
-                        new Url("https://www.lazada.sg/products/" +
-                                "sony-playstation-4-pro-1tb-console-local-stock-with-sony-warranty-i100009437-s100011973.html"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("family")),
-                new Wish(new Name("EVGA 1080 Ti Graphics Card"), new Price("92492021"),
-                        new Email("irfan@example.com"),
-                        new Url("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("classmates")),
-                new Wish(new Name("1TB SSD"), new Price("92624417"),
-                        new Email("royb@example.com"),
-                        new Url("https://www.amazon.com/gp/product/B07D998212"),
-                        new SavedAmount("0.00"),
-                        SAMPLE_REMARK_EMPTY, getTagSet("colleagues"))
+            new Wish(new Name("Apple iPhone 7"), new Price("87438807"), new Email("alexyeoh@example.com"),
+                    new Url("https://www.amazon.com/Apple-iPhone-Fully-Unlocked-32GB/dp/B0731HBTZ7"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("friends")),
+            new Wish(new Name("Logitech K840 Mechanical Keyboard"), new Price("99272758"),
+                    new Email("berniceyu@example.com"),
+                    new Url("https://www.amazon.com/Logitech-Mechanical-Keyboard-Romer-Switches/dp/B071VHYZ62"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("colleagues", "friends")),
+            new Wish(new Name("Nintendo Switch"), new Price("93210283"), new Email("charlotte@example.com"),
+                    new Url("https://www.lazada.sg/products/nintendo-switch-neon-console-"
+                            + "1-year-local-warranty-best-seller-i180040203-s230048296.html"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("neighbours")),
+            new Wish(new Name("PS4 Pro"), new Price("91031282"), new Email("lidavid@example.com"),
+                    new Url("https://www.lazada.sg/products/sony-playstation-4-pro-1tb-console-"
+                            + "local-stock-with-sony-warranty-i100009437-s100011973.html"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("family")),
+            new Wish(new Name("EVGA 1080 Ti Graphics Card"), new Price("92492021"),
+                    new Email("irfan@example.com"),
+                    new Url("https://www.amazon.com/EVGA-GeForce-Gaming-GDDR5X-Technology/dp/B0762Q49NV"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("classmates")),
+            new Wish(new Name("1TB SSD"), new Price("92624417"),
+                    new Email("royb@example.com"),
+                    new Url("https://www.amazon.com/gp/product/B07D998212"),
+                    new SavedAmount("0.00"),
+                    SAMPLE_REMARK_EMPTY, getTagSet("colleagues"))
         };
     }
 
