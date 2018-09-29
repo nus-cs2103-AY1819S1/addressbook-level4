5c3723be9e920c7b7c1422d4b2575d219b88e849
Reuben Tan <reubentjl@gmail.com>
2018-09-30 00:45:52 +0800

    Add a static CS1010 module for TypicalModules

--- src/test/java/seedu/address/testutil/TypicalModules.java
+++ src/test/java/seedu/address/testutil/TypicalModules.java
@@ -12,7 +12,20 @@ import seedu.address.model.ModuleList;
  */
 public class TypicalModules {
 
-
+    public static final Module CS1010 = new ModuleBuilder().withCode("CS1010")
+            .withDepartment("Computer Science").withTitle("Programming Methodology")
+            .withDescription("This module introduces the fundamental "
+                    + "concepts of problem solving by computing and programming using an imperative "
+                    + "programming language. It is the first and foremost introductory course to computing. "
+                    + "It is also the first part of a three-part series on introductory programming and "
+                    + "problem solving by computing, which also includes CS1020 and CS2010. Topics covered "
+                    + "include problem solving by computing, writing pseudo-codes, basic problem formulation "
+                    + "and problem solving, program development, coding, testing and debugging, fundamental "
+                    + "programming constructs (variables, types, expressions, assignments, functions, control"
+                    + " structures, etc.), fundamental data structures: arrays, strings and structures, "
+                    + "simple file processing, and basic recursion. This module is appropriate for SoC "
+                    + "students.").withCredit(4).withIsAvailableInSem1(true).withIsAvailableInSem2(true)
+            .withisAvailableInSpecialTerm1(false).withisAvailableInSpecialTerm2(false).build();
 
     private TypicalModules() {} // prevents instantiation
 
