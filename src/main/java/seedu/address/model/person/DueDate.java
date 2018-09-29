86ac61368cf54dba1b53c6d3e3a3f9d68bfe89e5
pengnam <seanngpengnam@gmail.com>
2018-09-29 23:28:48 +0800

    Remove unecessary functions from DueDate
    functions migrated to DateFormatUtil

--- src/main/java/seedu/address/model/person/DueDate.java
+++ src/main/java/seedu/address/model/person/DueDate.java
@@ -2,11 +2,13 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.model.util.DateFormatUtil.isValidDateFormat;
+import static seedu.address.model.util.DateFormatUtil.parseDate;
 
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- * Represents a Task's due date number in the task manager.
+ * Represents a due date in the {@link Task}.
  * Guarantees: immutable; is valid as declared in {@link #isValidDueDate(String)}
  */
 public class DueDate {
@@ -15,10 +17,10 @@ public class DueDate {
             "DueDate should only contain numbers, and it should be in one of the following formats:\n"
                     + " dd-mm-yy, dd-mm-yyyy, dd-mm-yy HHmm, dd-mm-yyyy HHmm\n"
                     + "Note: 24h time format";
-    public static final String DUEDATE_FORMAT_MINIMAL = "dd-MM-yy";
-    public static final String DUEDATE_FORMAT = "dd-MM-yy HHmm";
+
     public static final String DUEDATE_REGEX = "\\d{1,2}-\\d{1,2}-\\d{2,4}( \\d{4})?";
     public final String value;
+    public final Date valueDate;
 
     /**
      * Constructs a {@code DueDate}.
@@ -29,12 +31,13 @@ public class DueDate {
         requireNonNull(dueDate);
         checkArgument(isValidDueDate(dueDate), MESSAGE_DUEDATE_CONSTRAINTS);
         value = dueDate;
+        valueDate = parseDate(value);
     }
 
     /**
      * Returns true if a given string is a valid due date.
-     * Checks if due date is either one of two possible formats.
-     * {@link #isValidDueDateMinimalFormat(String)}.{@link #isValidDueDateStandardFormat(String)}
+     *
+     * @param test date to be checked
      */
     public static boolean isValidDueDate(String test) {
         if (test == null) {
@@ -43,37 +46,14 @@ public class DueDate {
         if (!test.matches(DUEDATE_REGEX)) {
             return false;
         }
-        return isValidDueDateMinimalFormat(test) || isValidDueDateStandardFormat(test);
+        return isValidDateFormat(test);
     }
 
     /**
-     * Returns true if a given string is a valid due date minimal format.
-     * Minimal format specified in {@link #DUEDATE_FORMAT_MINIMAL}
+     * Returns true if a given {@code DueDate} is overdue.
      */
-    private static boolean isValidDueDateMinimalFormat(String test) {
-        try {
-            SimpleDateFormat formatMinimal = new SimpleDateFormat(DUEDATE_FORMAT_MINIMAL);
-            formatMinimal.setLenient(false);
-            formatMinimal.parse(test);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns true if a given string is a valid due date standard format.
-     * Standard format specified in {@link #DUEDATE_FORMAT}
-     */
-    private static boolean isValidDueDateStandardFormat(String test) {
-        try {
-            SimpleDateFormat format = new SimpleDateFormat(DUEDATE_FORMAT);
-            format.setLenient(false);
-            format.parse(test);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+    public boolean isOverDue() {
+        return new Date().compareTo(valueDate) > 0;
     }
 
     @Override
