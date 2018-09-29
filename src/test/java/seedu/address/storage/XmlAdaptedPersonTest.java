c5d7551818c2cc57320c59fee3cbca4f6aaaa11a
Jeffrey <jeffrey950310@gmail.com>
2018-09-29 23:59:20 +0800

    Fix Travis checkstyleTest failed

--- src/test/java/seedu/address/storage/XmlAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/XmlAdaptedPersonTest.java
@@ -114,7 +114,8 @@ public class XmlAdaptedPersonTest {
         List<XmlAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
         invalidTags.add(new XmlAdaptedTag(INVALID_TAG));
         XmlAdaptedPerson person =
-                new XmlAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, invalidTags, VALID_GROUP_TAGS);
+                new XmlAdaptedPerson(
+                        VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, invalidTags, VALID_GROUP_TAGS);
         Assert.assertThrows(IllegalValueException.class, person::toModelType);
     }
 
