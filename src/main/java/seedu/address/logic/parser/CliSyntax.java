d30410732b656e48b32465c46bb51f9ae7dfa874
Ng Zhen Wei Alec <alecngzw@gmail.com>
2018-09-30 04:07:17 +0800

    CliSyntax.java : add prefix for healthplans

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,4 +12,16 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
+
+    //prefix for healthplan
+    public static final Prefix PREFIX_HPNAME = new Prefix("n/");
+    public static final Prefix PREFIX_TWEIGHT= new Prefix("t/");
+    public static final Prefix PREFIX_CWEIGHT = new Prefix("w/");
+    public static final Prefix PREFIX_CHEIGHT = new Prefix("h/");
+    public static final Prefix PREFIX_AGE = new Prefix("a/");
+    public static final Prefix PREFIX_DURATION = new Prefix("d/");
+    public static final Prefix PREFIX_SCHEME = new Prefix("s/");
+
+
+
 }
