c9583f145e289852808b51ba866d29814ece1776
Ng Zhen Wei Alec <alecngzw@gmail.com>
2018-09-29 16:56:41 +0800

    Model/healthplan folder: Contains the models for Healthplan and related functionality

---
+++ src/main/java/seedu/address/model/healthplan/TargetWeight.java
@@ -0,0 +1,45 @@
+package seedu.address.model.healthplan;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+
+public class TargetWeight {
+
+    public static final String MESSAGE_WEIGHT_CONSTRAINTS =
+            "Weights should only contain numbers";
+    public static final String WEIGHT_VALIDATION_REGEX = "\\d{2,}";
+    public final String value;
+
+
+    public TargetWeight(String weight) {
+        requireNonNull(weight);
+        checkArgument(isValidWeight(weight), MESSAGE_WEIGHT_CONSTRAINTS);
+        value = weight;
+    }
+
+    public static boolean isValidWeight(String test) {
+        return test.matches(WEIGHT_VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TargetWeight // instanceof handles nulls
+                && value.equals(((TargetWeight) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+
+}
