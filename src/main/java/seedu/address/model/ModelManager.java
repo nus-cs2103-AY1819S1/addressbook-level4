21cc11610d029fbd076bb4f9081073c80fbe56db
Jack Wong <jackwong3101@yahoo.com>
2018-09-29 22:40:24 +0800

    disable my optimize import on the fly option

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -1,5 +1,10 @@
 package seedu.address.model;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.function.Predicate;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -9,11 +14,8 @@ import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.model.doctor.Doctor;
 import seedu.address.model.person.Person;
 
-import java.util.function.Predicate;
-import java.util.logging.Logger;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 
 /**
  * Represents the in-memory model of the address book data.
