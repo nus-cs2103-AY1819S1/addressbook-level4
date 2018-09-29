ce6d2f54cdaf561c7b827601e97f43064808e41b
Ng Zhen Wei Alec <alecngzw@gmail.com>
2018-09-29 16:56:41 +0800

    Model/healthplan folder: Contains the models for Healthplan and related functionality

---
+++ src/main/java/seedu/address/model/healthplan/Scheme.java
@@ -0,0 +1,21 @@
+package seedu.address.model.healthplan;
+
+public enum Scheme {
+    //this enum describes the health plan scheme that the user has chosen
+
+
+    GAIN,LOSS,MAINTAIN;
+
+
+     Scheme(){
+
+
+    }
+
+
+
+}
+
+
+
+
