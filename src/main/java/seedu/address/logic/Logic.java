53107a145dd28cbc9d89d86d7398ff373dc17329
Ng Zhen Wei Alec <alecngzw@gmail.com>
2018-09-29 16:51:21 +0800

    Logic.java : extended functionality for healthPlan

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.healthplan.HealthPlan;
 import seedu.address.model.recipe.Recipe;
 
 /**
@@ -21,6 +22,9 @@ public interface Logic {
 
     /** Returns an unmodifiable view of the filtered list of recipes */
     ObservableList<Recipe> getFilteredRecipeList();
+
+    ObservableList<HealthPlan> getFilteredPlans();
+
 
     /** Returns the list of input entered by the user, encapsulated in a {@code ListElementPointer} object */
     ListElementPointer getHistorySnapshot();
