38de0beec34f86630fe6df95ac365eaed584c387
CrimsonJacket <rysienakao@gmail.com>
2018-09-29 16:10:31 +0800

    Credential: incl. Object equals(), hashcode() & toString() methods

--- src/main/java/seedu/address/model/credential/Credential.java
+++ src/main/java/seedu/address/model/credential/Credential.java
@@ -1,5 +1,9 @@
 package seedu.address.model.credential;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
 public class Credential {
 
     // Identity Field
@@ -9,7 +13,11 @@ public class Credential {
     private final String password;
     private final String key; // TODO Awaiting Encryption function
 
+    /**
+     * Every field must be present and not null.
+     */
     public Credential(String username, String password, String key) {
+        requireAllNonNull(username, password, key);
         this.username = username;
         this.password = password;
         this.key = key;
@@ -25,4 +33,41 @@ public class Credential {
 
     public String getKey() { return key; }
 
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Credential)) {
+            return false;
+        }
+
+        Credential otherCredential = (Credential) other;
+        return otherCredential.getUsername().equals(getUsername())
+            && otherCredential.getPassword().equals(getPassword())
+            && otherCredential.getKey().equals(getKey());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(username, password, key);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Username: ")
+            .append(getUsername())
+            .append(" Password: ")
+            .append(getPassword())
+            .append(" Key: ")
+            .append(getKey());
+        return builder.toString();
+    }
 }
