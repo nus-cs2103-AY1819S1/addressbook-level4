510223d5720f08eb999406c9faae26ae670a83ba
Jack Wong <jackwong3101@yahoo.com>
2018-09-29 22:44:19 +0800

    fix import line arrangement

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -5,9 +5,11 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.function.Predicate;
 import java.util.logging.Logger;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
