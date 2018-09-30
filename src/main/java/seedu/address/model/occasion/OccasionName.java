c53570cf1cdac3a24658d850eb70fac02c0e2644
KongZijin <e0253754@u.nus.edu>
2018-09-30 12:02:50 +0800

    fix typo

--- src/main/java/seedu/address/model/occasion/OccasionName.java
+++ src/main/java/seedu/address/model/occasion/OccasionName.java
@@ -5,7 +5,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents an Occasion's name in the address book.
- * Gurantees: immutable, is valid as declared in {@link #isValidName(String)}
+ * Guarantees: immutable, is valid as declared in {@link #isValidName(String)}
  *
  * @author KongZijin
  */
