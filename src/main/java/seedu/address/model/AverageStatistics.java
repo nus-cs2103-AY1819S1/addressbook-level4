9d7b22bb9650f8a4f929d7d00ebff8f3b6c8f792
Arsaln Cheema <arsalanc.v2@gmail.com>
2018-09-30 11:18:05 +0800

    replace statistic type constant variables with enum

--- src/main/java/seedu/address/model/AverageStatistics.java
+++ src/main/java/seedu/address/model/AverageStatistics.java
@@ -2,15 +2,15 @@ package seedu.address.model;
 
 public class AverageStatistics extends Statistics {
 
-    public static final String STATISTICS_TYPE = "Average";
+    public static final StatisticType statisticType = StatisticType.AVERAGE;
     public static final int NUMBER_STATISTICS = 4;
     private static final String AVERAGE_TYPE_DAY = "per day";
     private static final String AVERAGE_TYPE_PATIENT = "per patient";
 
-    private static final String NUMBER_PATIENTS_DAY = STATISTICS_TYPE + " number of patients " + AVERAGE_TYPE_DAY;
-    private static final String TIME_WAITING_PATIENT = STATISTICS_TYPE + " waiting time " + AVERAGE_TYPE_PATIENT;
-    private static final String NUMBER_MEDICINES_DAY = STATISTICS_TYPE + " number of medicines dispensed " + AVERAGE_TYPE_DAY;
-    private static final String NUMBER_MEDICINES_PATIENT = STATISTICS_TYPE + " number of medicines dispensed " + AVERAGE_TYPE_PATIENT;
+    private static final String NUMBER_PATIENTS_DAY = statisticType + " number of patients " + AVERAGE_TYPE_DAY;
+    private static final String TIME_WAITING_PATIENT = statisticType + " waiting time " + AVERAGE_TYPE_PATIENT;
+    private static final String NUMBER_MEDICINES_DAY = statisticType + " number of medicines dispensed " + AVERAGE_TYPE_DAY;
+    private static final String NUMBER_MEDICINES_PATIENT = statisticType + " number of medicines dispensed " + AVERAGE_TYPE_PATIENT;
 
     public AverageStatistics() {
         super();
--- src/main/java/seedu/address/model/TotalStatistics.java
+++ src/main/java/seedu/address/model/TotalStatistics.java
@@ -2,11 +2,11 @@ package seedu.address.model;
 
 public class TotalStatistics extends Statistics {
 
-    public static final String STATISTICS_TYPE = "Total";
+    public static final StatisticType statisticType = StatisticType.TOTAL;
     public static final int NUMBER_STATISTICS = 3;
-    private static final String NUMBER_DAYS = STATISTICS_TYPE + " number of days of operation (inclusive)";
-    private static final String NUMBER_PATIENTS = STATISTICS_TYPE + " number of patients to date";
-    private static final String NUMBER_MEDICINES = STATISTICS_TYPE + " number of medicines dispensed to date";
+    private static final String NUMBER_DAYS = statisticType + " number of days of operation (inclusive)";
+    private static final String NUMBER_PATIENTS = statisticType + " number of patients to date";
+    private static final String NUMBER_MEDICINES = statisticType + " number of medicines dispensed to date";
 
     public TotalStatistics() {
         super();
