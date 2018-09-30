50df8665b21f91815ca875a33c1742263dadd174
KongZijin <e0253754@u.nus.edu>
2018-09-30 14:05:07 +0800

    update indentation style

--- src/main/java/seedu/address/model/occasion/Occasion.java
+++ src/main/java/seedu/address/model/occasion/Occasion.java
@@ -17,88 +17,88 @@ import seedu.address.model.tag.Tag;
  */
 public class Occasion extends Inanimate {
 
-  // Identity fields
-  private final OccasionName occasionName;
-  private final OccasionDate occasionDate;
-  private final Person organiser;
+    // Identity fields
+    private final OccasionName occasionName;
+    private final OccasionDate occasionDate;
+    private final Person organiser;
 
-  // Date fields
-  private final Set<Tag> tags = new HashSet<>();
+    // Date fields
+    private final Set<Tag> tags = new HashSet<>();
 
-  /**
-   * Every field must be present and not null.
-   */
-  public Occasion(OccasionName occasionName, OccasionDate occasionDate,
-      Person organiser, Set<Tag> tags) {
-    requireAllNonNull(occasionName, occasionDate, organiser, tags);
-    this.occasionName = occasionName;
-    this.occasionDate = occasionDate;
-    this.organiser = organiser;
-    this.tags.addAll(tags);
-  }
-
-  public OccasionName getOccasionName() {
-    return occasionName;
-  }
-
-  public OccasionDate getOccasionDate() {
-    return occasionDate;
-  }
-
-  public Person getOrganiser() {
-    return organiser;
-  }
-
-  /**
-   * Get the set of Tags.
-   *
-   * @return An immutable tag set, which throws {@code
-   * UnsupportedOperationException} if modification is attempted.
-   */
-  @Override
-  public Set<Tag> getTags() { return Collections.unmodifiableSet(tags); }
-
-  /**
-   * Check the equality of two occasions. This defines a stronger notion of
-   * equality between two occasions.
-   *
-   * @return Return true if both occasion have the same identity and data
-   * fields.
-   */
-  @Override
-  public boolean equals(Object other) {
-    if (other == this) {
-      return true;
+    /**
+     * Every field must be present and not null.
+     */
+    public Occasion(OccasionName occasionName, OccasionDate occasionDate,
+        Person organiser, Set<Tag> tags) {
+        requireAllNonNull(occasionName, occasionDate, organiser, tags);
+        this.occasionName = occasionName;
+        this.occasionDate = occasionDate;
+        this.organiser = organiser;
+        this.tags.addAll(tags);
     }
 
-    if (!(other instanceof Occasion)) {
-      return false;
+    public OccasionName getOccasionName() {
+        return occasionName;
     }
 
-    Occasion otherOccasion = (Occasion) other;
-    return otherOccasion.getOccasionName().equals(this.getOccasionName())
-        && otherOccasion.getOccasionDate().equals(this.getOccasionDate())
-        && otherOccasion.getOrganiser().equals(this.getOrganiser())
-        && otherOccasion.getTags().equals(this.getTags());
-  }
+    public OccasionDate getOccasionDate() {
+        return occasionDate;
+    }
 
-  @Override
-  public int hashCode(){
-    // Use this method for custom fields hashing instead of implementing one.
-    return Objects.hash(occasionName, occasionDate, organiser, tags);
-  }
+    public Person getOrganiser() {
+        return organiser;
+    }
+    
+    /**
+     * Get the set of Tags.
+     *
+     * @return An immutable tag set, which throws {@code
+     * UnsupportedOperationException} if modification is attempted.
+     */
+    @Override
+    public Set<Tag> getTags() { return Collections.unmodifiableSet(tags); }
 
-  @Override
-  public String toString() {
-    final StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(getOccasionName())
-        .append(" ")
-        .append(getOccasionDate())
-        .append(" Organiser: ")
-        .append(getOrganiser().getName()) // use the person's name to
-        // represent this Peron object.
-        .append(" Tags: ");
-    getTags().forEach(stringBuilder::append);
-    return stringBuilder.toString();
-  }
+    /**
+     * Check the equality of two occasions. This defines a stronger notion of
+     * equality between two occasions.
+     *
+     * @return Return true if both occasion have the same identity and data
+     * fields.
+     */
+    @Override
+    public boolean equals(Object other) {
+      if (other == this) {
+          return true;
+      }
+      
+      if (!(other instanceof Occasion)) {
+          return false;
+      }
+      
+      Occasion otherOccasion = (Occasion) other;
+      return otherOccasion.getOccasionName().equals(this.getOccasionName())
+          && otherOccasion.getOccasionDate().equals(this.getOccasionDate())
+          && otherOccasion.getOrganiser().equals(this.getOrganiser())
+          && otherOccasion.getTags().equals(this.getTags());
+    }
+
+    @Override
+    public int hashCode(){
+        // Use this method for custom fields hashing instead of implementing one.
+        return Objects.hash(occasionName, occasionDate, organiser, tags);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getOccasionName())
+            .append(" ")
+            .append(getOccasionDate())
+            .append(" Organiser: ")
+            .append(getOrganiser().getName()) // use the person's name to represent this Peron
+            // object.
+            .append(" Tags: ");
+        getTags().forEach(stringBuilder::append);
+        return stringBuilder.toString();
+    }
 }
