0c9499f83868a79ada85cc9d2eb20ded025d245e
denzelchung <denzelchung@hotmail.com>
2018-10-01 00:19:46 +0800

    Update AddressBookParserTest.java
    Fix checkstyle.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE_LOCATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
@@ -230,7 +227,8 @@ public class AddressBookParserTest {
     public void parseCommand_pictureCommandAlias_returnsPictureCommand() throws Exception {
         assertTrue(parser.parseCommand("p n/johndoe l/johndoe.jpg") instanceof PictureCommand);
     }
-      
+    
+    @Test
     public void parseCommandWord_tag() throws Exception {
         List<String> keywords = Arrays.asList("foo", "friends", "colleagues");
         TagCommand command = (TagCommand) parser.parseCommand(
