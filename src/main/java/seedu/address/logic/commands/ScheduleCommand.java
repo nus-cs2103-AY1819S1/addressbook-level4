4615f6e6bc068a1fe3ee124093e58311b692a417
AyushChatto <ayush.chattoraj@u.nus.edu>
2018-09-30 14:33:17 +0800

    Fixing formatting issues

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -2,21 +2,28 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.meeting.Meeting;
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
-
 import java.util.List;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.meeting.Meeting;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Address;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Schedules a meeting with a person.
+ */
 public class ScheduleCommand extends Command {
 
     public static final String COMMAND_WORD = "schedule";
