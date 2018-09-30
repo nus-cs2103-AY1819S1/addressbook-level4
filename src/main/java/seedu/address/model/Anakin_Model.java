733a65895c2846385090b5d9e744747fbfd80295
tomforge <fooguowei3@gmail.com>
2018-09-30 15:02:30 +0800

    Remove unused import from Anakin_Model

--- src/main/java/seedu/address/model/Anakin_Model.java
+++ src/main/java/seedu/address/model/Anakin_Model.java
@@ -3,7 +3,6 @@ package seedu.address.model;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
-import seedu.address.model.Anakin_deck.Anakin_Card;
 import seedu.address.model.Anakin_deck.Anakin_Deck;
 
 /**
