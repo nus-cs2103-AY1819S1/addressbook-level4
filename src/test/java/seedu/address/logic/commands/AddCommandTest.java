b57e8c9a3a5551ca8b8f184fcfa48b573c9b7d92
JACK\Jack <cjiefeng@live.com>
2018-10-01 01:06:02 +0800

    AddCommandTest: add importContacts method

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -152,6 +152,11 @@ public class AddCommandTest {
         public void commitAddressBook() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void importContacts(String filePath)  {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
