21b572c3b3b5a7c97db1654c8f34c53ef67317fd
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-01 22:19:44 +0800

    Fixed methods in Appointment class. Date and Time class added.

--- src/main/java/seedu/address/model/appointment/Appointment.java
+++ src/main/java/seedu/address/model/appointment/Appointment.java
@@ -2,38 +2,40 @@ package seedu.address.model.appointment;
 
 import seedu.address.model.person.Person;
 
+import java.util.Objects;
+
 /**
  * Contains details regarding appointment.
  */
 public class Appointment {
 
     //fields used for making appointment
-    private static int appointmentDate;
-    private static int appointmentTime;
+    private static Date appointmentDate;
+    private static Time appointmentTime;
     private static int appointmentStatus;
 
     private static final int APPROVED = 1;
     private static final int CANCELLED = 0;
 
-    //to replace with patient later on
+    //to replace with patient class later on
     private static Person patient;
 
     /**
      * Constructs appointment.
      * appointmentStatus is set to APPROVED as default.
      */
-    public Appointment(int date, int time, Person patient) {
+    public Appointment(Date date, Time time, Person patient) {
         this.appointmentDate = date;
         this.appointmentTime = time;
         this.patient = patient;
         this.appointmentStatus = APPROVED;
     }
 
-    public int getAppointmentDate() {
+    public Date getAppointmentDate() {
         return appointmentDate;
     }
 
-    public static int getAppointmentTime() {
+    public static Time getAppointmentTime() {
         return appointmentTime;
     }
 
@@ -46,24 +48,63 @@ public class Appointment {
     }
 
     /**
-     * Returns true if appointments have the same time, date, patient and status.
-     *
+     * Cancels appointment.
      */
-    public boolean isSameAppointment() {
-        return false;
+    public void cancelAppointment() {
+        this.appointmentStatus = CANCELLED;
     }
 
     /**
      * Returns true if the appointment has the same date and time slot.
      */
-    public boolean isSameSlot() {
-        return false;
+    public boolean isSameSlot(Appointment other) {
+        return other.getAppointmentDate().equals(getAppointmentDate())
+                && other.getAppointmentTime().equals(getAppointmentTime());
+    }
+
+    /**
+     * Returns true if the appointment has the same patient.
+     */
+    public boolean isSamePatient(Appointment other) {
+        return other.getPatient().equals(getPatient());
     }
 
     /**
      * Returns true if appointment is cancelled.
      */
     public boolean isCancelled() {
+        if (appointmentStatus == APPROVED) {
+            return true;
+        }
         return false;
+    }
+
+    /**
+     * Returns true if appointments have the same date, time, patient and status.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other instanceof Appointment) {
+            return false;
+        }
+        Appointment otherAppointment = (Appointment) other;
+        return otherAppointment.getAppointmentDate().equals(getAppointmentDate())
+                && otherAppointment.getAppointmentTime().equals(getAppointmentTime())
+                && otherAppointment.getPatient().equals(getPatient())
+                && otherAppointment.getAppointmentStatus(),equals(getAppointmentStatus());
+    }
+
+    @Override
+    public int hashCode() {
+        return Object.hash(appointmentDate, appointmentTime, patient, appointmentStatus);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        return builder.toString();
     }
 }
