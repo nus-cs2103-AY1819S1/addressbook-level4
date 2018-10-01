3281fae6eb407ac1ea170d92f4bc4bbab8814471
LAPTOP-C1KL05RT\lionel <e0203420@u.nus.edu>
2018-10-01 17:31:24 +0800

    ListCommandParserTest: add test for ListCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import seedu.address.logic.commands.ListCommand;
+
+public class ListCommandParserTest {
+    private ListCommandParser parser = new ListCommandParser();
+
+    @Test
+    public void parse_emptyArgs_returnsListAll() {
+        assertParseSuccess(parser, "   ", new ListCommand(ListCommand.ListType.SHOW_ALL));
+    }
+
+    @Test
+    public void parse_validArgs_returnsCorrectList() {
+        ListCommand completedListCommand = new ListCommand(ListCommand.ListType.SHOW_COMPLETED);
+        ListCommand uncompletedListCommand = new ListCommand(ListCommand.ListType.SHOW_UNCOMPLETED);
+
+        assertParseSuccess(parser, "-c", completedListCommand);
+        assertParseSuccess(parser, "-u", uncompletedListCommand);
+    }
+}
