f8c83e97bf10774a3fcd7ec776bf8b0e2a4aacf9
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-02 02:40:30 +0800

    Added test cases to TimeTest. Minor fixes to DateTest.

--- src/test/java/seedu/address/model/appointment/DateTest.java
+++ src/test/java/seedu/address/model/appointment/DateTest.java
@@ -60,5 +60,6 @@ public class DateTest {
 
         //different
         assertFalse(date3.equals(date2));
+        assertFalse(date2.equals(null));
     }
 }
--- src/test/java/seedu/address/model/appointment/TimeTest.java
+++ src/test/java/seedu/address/model/appointment/TimeTest.java
@@ -1,4 +1,54 @@
 package seedu.address.model.appointment;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
 public class TimeTest {
+
+    @Test
+    public void isValidHour() {
+        Time time = new Time(23, 17);
+
+        //valid hour
+        assertTrue(time.isValidHour(23));
+
+        //invalid hour
+        assertFalse(time.isValidHour(-100));
+        assertFalse(time.isValidHour(100));
+        assertFalse(time.isValidHour(24));
+    }
+
+    @Test
+    public void isValidMinute() {
+        Time time = new Time(23, 17);
+
+        //valid minute
+        assertTrue(time.isValidMinute(23));
+
+        //invalid minute
+        assertFalse(time.isValidMinute(-100));
+        assertFalse(time.isValidMinute(100));
+        assertFalse(time.isValidMinute(9022324));
+    }
+
+    @Test
+    public void equals() {
+        Time time = new Time(23, 17);
+        Time time2 = new Time(23, 17);
+        Time time3 = new Time(2, 19);
+
+        //same obj
+        assertTrue(time.equals(time));
+
+        //same fields
+        assertTrue(time.equals(time2));
+
+        //diff
+        assertFalse(time2.equals(time3));
+        assertFalse(time2.equals(null));
+    }
 }
