b2619b56f6c3e054db8566273a3ea7f59f266d46
denzelchung <denzelchung@hotmail.com>
2018-10-02 00:11:14 +0800

    Update PictureCommand.java
    Modify constructor to take in PictureCommand(Index, Path).

--- src/main/java/seedu/address/logic/commands/PictureCommand.java
+++ src/main/java/seedu/address/logic/commands/PictureCommand.java
@@ -2,12 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE_LOCATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
+import java.nio.file.Path;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Name;
 
 /**
  * Adds a picture to a contact in the address book.
@@ -19,32 +20,31 @@ public class PictureCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a picture to a contact in the address book "
         + "by the name used in the displayed person list.\n"
-        + "Parameters: " + PREFIX_NAME + "NAME "
+        + "Parameters: INDEX (must be a positive integer) "
         + PREFIX_FILE_LOCATION + "FILE_LOCATION\n"
-        + "Example: " + COMMAND_WORD + " "
-        + PREFIX_NAME + "johndoe "
+        + "Example: " + COMMAND_WORD + " 1 "
         + PREFIX_FILE_LOCATION + "/images/johndoe.jpg";
 
     public static final String MESSAGE_SUCCESS = "Added picture for Person: %1$s";
     public static final String MESSAGE_INVALID_PICTURE = "The image file could not be found.";
 
-    private final Name name;
-    private final String fileLocation;
+    private final Index index;
+    private final Path fileLocation;
 
     /**
-     * @param name of the person in the filtered person list to edit
+     * @param index of the person in the filtered person list to edit
      * @param fileLocation of the profile picture
      */
-    public PictureCommand(Name name, String fileLocation) {
-        requireNonNull(name);
+    public PictureCommand(Index index, Path fileLocation) {
+        requireNonNull(index);
         requireNonNull(fileLocation);
 
-        this.name = name;
+        this.index = index;
         this.fileLocation = fileLocation;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        return new CommandResult(String.format(MESSAGE_SUCCESS, name));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, index));
     }
 }
