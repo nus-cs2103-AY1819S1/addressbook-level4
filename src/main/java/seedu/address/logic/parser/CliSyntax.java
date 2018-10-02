2a04b92575fe197708a843de60eb2926ea4924a0
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-02 15:45:47 +0800

    Add login prefix definitions for CliSyntax.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    
+    /* Login Prefix definitions */
+    public static final Prefix PREFIX_ROLE = new Prefix("role/");
+    public static final Prefix PREFIX_PASSWORD = new Prefix("pass/");
 }
