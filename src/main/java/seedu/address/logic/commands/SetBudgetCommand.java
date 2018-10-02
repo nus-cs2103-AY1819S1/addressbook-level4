b6e4b1e7c004661c741b1bb2947c5f01a3f8a182
winson <winsonheng@hotmail.com>
2018-10-03 02:10:04 +0800

    Added setBudget command parser

--- src/main/java/seedu/address/logic/commands/SetBudgetCommand.java
+++ src/main/java/seedu/address/logic/commands/SetBudgetCommand.java
@@ -1,4 +1,44 @@
-package seedu.address.logic.commands.exceptions;
+package seedu.address.logic.commands;
+//@author winsonhys
 
-public class SetBudgetCommand {
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.budget.Budget;
+import seedu.address.model.exceptions.NoUserSelectedException;
+
+/**
+ * Sets the budget cap for the expense tracker.
+ */
+
+public class SetBudgetCommand extends Command {
+    public static final String COMMAND_WORD = "setBudget";
+    public static final String COMMAND_ALIAS = "sb";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sets a budget cap for the expense tracker "
+        + "Parameters: MONEY (Must be a positive float with 2 decimal places)\n"
+        + "Example " + COMMAND_WORD + " 130.00";
+
+    public static final String MESSAGE_SUCCESS = "Budget set to %1$s";
+    private final Budget toSet;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Budget}
+     */
+
+    public SetBudgetCommand(Budget budget) {
+        requireNonNull(budget);
+        this.toSet = budget;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws NoUserSelectedException {
+        requireNonNull(model);
+
+        model.modifyMaximumBudget(this.toSet);
+        model.commitAddressBook();
+        return new CommandResult(String.format(MESSAGE_SUCCESS, this.toSet));
+    }
+
 }
--- src/main/java/seedu/address/logic/parser/SetBudgetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SetBudgetCommandParser.java
@@ -1,4 +1,27 @@
 package seedu.address.logic.parser;
 
-public class SetBudgetCommandParser {
+import seedu.address.logic.commands.SetBudgetCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.budget.Budget;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+//@author winsonhys
+public class SetBudgetCommandParser implements Parser<SetBudgetCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the LoginCommand
+     * and returns an LoginCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public SetBudgetCommand parse(String userInput) throws ParseException {
+        try {
+            Budget budget = new Budget(userInput.trim());
+            return new SetBudgetCommand(budget);
+        } catch (IllegalArgumentException iae) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetBudgetCommand.MESSAGE_USAGE));
+        }
+    }
 }
