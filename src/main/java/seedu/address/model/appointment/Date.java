bdb07dc8b9a3812d5971c6f5267e18625995f6c3
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-02 20:40:08 +0800

    Edits to pass checkstyle.

--- src/main/java/seedu/address/model/appointment/Date.java
+++ src/main/java/seedu/address/model/appointment/Date.java
@@ -1,7 +1,7 @@
 package seedu.address.model.appointment;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Objects;
 
