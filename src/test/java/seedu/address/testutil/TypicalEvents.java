ea92c0ebf28a64b1327adcdbc9cc6d6b6e55dba1
kaitingpeck <kaiting_95@hotmail.com>
2018-10-02 17:26:30 +0800

    fix travis errors

--- src/test/java/seedu/address/testutil/TypicalEvents.java
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -1,33 +1,17 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_MEETING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_MEETING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESC_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESC_MEETING;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_MEETING;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TIME_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TIME_MEETING;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import seedu.address.model.AddressBook;
 import seedu.address.model.event.Event;
-import seedu.address.model.person.Person;
 
 /**
  * A utility class containing a list of {@code Event} objects to be used in tests.
@@ -47,8 +31,8 @@ public class TypicalEvents {
     public static final Event MEETING =
             new ScheduledEventBuilder()
                     .withName(VALID_NAME_MEETING)
-                    .withDescription(VALID_DESC_MEETING).
-                    withDate(VALID_DATE_MEETING)
+                    .withDescription(VALID_DESC_MEETING)
+                    .withDate(VALID_DATE_MEETING)
                     .withTime(VALID_TIME_MEETING)
                     .withAddress(VALID_ADDRESS_MEETING)
                     .build();
