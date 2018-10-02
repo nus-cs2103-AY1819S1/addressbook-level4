6ea027256f7ee5c7eacc0734c063ed33bf4a985e
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-02 21:18:16 +0800

    Edit doctor builder.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/address/testutil/DoctorBuilder.java
+++ src/test/java/seedu/address/testutil/DoctorBuilder.java
@@ -1,36 +1,41 @@
 package seedu.address.testutil;
 
+import java.util.HashSet;
 import seedu.address.model.doctor.Doctor;
 import seedu.address.model.doctor.Id;
 import seedu.address.model.doctor.Password;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 //@@author jjlee050
 /**
  * A utility class to help with building Doctor objects.
  */
-public class DoctorBuilder {
+public class DoctorBuilder extends PersonBuilder {
 
     public static final int DEFAULT_ID = 1;
     public static final String DEFAULT_NAME = "Alice Pauline";
     public static final String DEFAULT_PASSWORD = "alicepaul";
 
     private Id id;
-    private Name name;
     private Password password;
 
     public DoctorBuilder() {
+        super(new Person(new Name(DEFAULT_NAME), new Phone(""), new Email(""), new Address(""), new HashSet<>()));
         id = new Id(DEFAULT_ID);
-        name = new Name(DEFAULT_NAME);
         password = new Password(DEFAULT_PASSWORD);
     }
 
     /**
-     * Initializes the DoctorBuilder with the data of {@code personToCopy}.
+     * Initializes the DoctorBuilder with the data of {@code doctorToCopy}.
      */
     public DoctorBuilder(Doctor doctorToCopy) {
+        super(new Person(doctorToCopy.getName(), new Phone(""), new Email(""), new Address(""), new HashSet<>()));
         id = doctorToCopy.getId();
-        name = doctorToCopy.getName();
         password = doctorToCopy.getPassword();
     }
 
@@ -39,14 +44,6 @@ public class DoctorBuilder {
      */
     public DoctorBuilder withId(int id) {
         this.id = new Id(id);
-        return this;
-    }
-
-    /**
-     * Sets the {@code Name} of the {@code Doctor} that we are building.
-     */
-    public DoctorBuilder withName(String name) {
-        this.name = new Name(name);
         return this;
     }
 
