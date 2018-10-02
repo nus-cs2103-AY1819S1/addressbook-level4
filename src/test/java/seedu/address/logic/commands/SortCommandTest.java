2e2e05508dc9f368ba59d69e8d8094cff3c170f5
NyxF4ll <vosk1093@gmail.com>
2018-10-02 22:01:58 +0800

    Revert "Revert "Implement sort command""

---
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+//TODO
+class SortCommandTest {
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void equals() {
+    }
+
+    @Test
+    void execute1() {
+    }
+}
