7664d8cf8ff9566d1242737210a34507398d2677
chenlu-chua <chenlu.ccl@gmail.com>
2018-10-02 20:24:42 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/parser/EarningsCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EarningsCommandParser.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.EarningsCommand;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -10,15 +8,18 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class EarningsCommandParser implements Parser<EarningsCommand> {
 
-	public EarningsCommand parse(String args) throws ParseException {
+    /**
+     * Parses the given {@code String} of arguments in the context of the EarningsCommand
+     * and returns an EarningsCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EarningsCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format("Enter error message here"));
+        }
 
-		//v1.1 args should contain name of student to retrieve his tuition fees
-		String trimmedArgs = args.trim();
-		if (trimmedArgs.isEmpty()) {
-			throw new ParseException(
-					String.format("Enter error message here"));
-		}
-
-		return new EarningsCommand(trimmedArgs);
-	}
+        return new EarningsCommand(trimmedArgs);
+    }
 }
--- src/main/java/seedu/address/model/person/Fees.java
+++ src/main/java/seedu/address/model/person/Fees.java
@@ -1,39 +1,40 @@
 package seedu.address.model.person;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import static java.util.Objects.requireNonNull;
 
 import java.text.DecimalFormat;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's tuition fees per hour.
  */
 public class Fees {
 
-	public static final String MESSAGE_FEES_CONSTRAINTS =
-			"Fees should only contain integer or double values, and it should not be blank";
+    public static final String MESSAGE_FEES_CONSTRAINTS =
+            "Fees should only contain integer or double values, and it should not be blank";
 
-	public static final String FEES_VALIDATION_REGEX = "\\d+|\\d+\\.\\d+";
-	public static DecimalFormat df = new DecimalFormat("#.00");
+    public static final String FEES_VALIDATION_REGEX = "\\d+|\\d+\\.\\d+";
 
-	public Fees(double amount) {
-		requireNonNull(amount);
-		checkArgument(isValidFee(amount), MESSAGE_FEES_CONSTRAINTS);
-		feesPerHr = amount;
-	}
+    private static DecimalFormat df = new DecimalFormat("#.00");
 
 	public final double feesPerHr;
 
-	/**
-	 * Returns true if a given amount has valid format.
-	 */
-	public static boolean isValidFee(double feeAmount) {
-		String fees = String.valueOf(feeAmount);
-		return fees.matches(FEES_VALIDATION_REGEX);
+    public Fees(double amount) {
+        requireNonNull(amount);
+        checkArgument(isValidFee(amount), MESSAGE_FEES_CONSTRAINTS);
+        feesPerHr = amount;
+    }
 
-	}
+    /**
+     * Returns true if a given amount has valid format.
+     */
+    public static boolean isValidFee(double feeAmount) {
+        String fees = String.valueOf(feeAmount);
+        return fees.matches(FEES_VALIDATION_REGEX);
 
-	@Override
-	public String toString() { return "$" + df.format(feesPerHr) + "/hour"; }
+    }
+
+    @Override
+    public String toString() {
+    	return "$" + df.format(feesPerHr) + "/hour"; }
 }
