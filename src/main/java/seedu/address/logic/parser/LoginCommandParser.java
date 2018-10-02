3acda26b3f3aca314e98dfd10ba799475b5a3b3c
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-02 19:59:11 +0800

    Create LoginCommandParser to parse login command.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

---
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.LoginCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+
+/**
+ * Parses input arguments and creates a new LoginCommand object
+ */
+public class LoginCommandParser implements Parser<LoginCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the LoginCommand
+     * and returns an LoginCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public LoginCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_ROLE, PREFIX_NAME, PREFIX_PASSWORD);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_ROLE, PREFIX_NAME, PREFIX_PASSWORD)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+
+        //Person person = new Person(name, phone, email, address, tagList);
+
+        return new LoginCommand();
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
