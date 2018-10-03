24f49cd42b9bbb9172e9fffaffab4e676ef04f6e
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2018-10-03 22:51:00 +0800

    Port content of Messages to AnakinMessages

--- src/main/java/seedu/address/commons/core/AnakinMessages.java
+++ src/main/java/seedu/address/commons/core/AnakinMessages.java
@@ -9,5 +9,6 @@ public class AnakinMessages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DECK_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_INVALID_CARD_DISPLAYED_INDEX = "The card index provided is invalid";
 
 }
