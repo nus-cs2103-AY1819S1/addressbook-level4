772fdb6d346c28808ff331c5329bffa2fc5652f0
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2018-10-04 01:55:37 +0800

    Edit further for checkstyles

--- src/main/java/seedu/address/model/transformation/TransformationSet.java
+++ src/main/java/seedu/address/model/transformation/TransformationSet.java
@@ -1,10 +1,10 @@
 package seedu.address.model.transformation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ImageMagickUtil;
-
-import java.util.LinkedList;
-import java.util.ArrayList;
 
 //@@author j-lum
 
