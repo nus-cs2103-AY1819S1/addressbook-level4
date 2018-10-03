f263064f44a6c08a08ffb6ec57c9155ee1105917
Ong Jun Jie <>
2018-10-03 14:11:24 +0800

    add education class

---
+++ src/main/java/seedu/address/model/person/Subject.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's subject(s) taught in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidSubject(String)}
+ */
+public class Subject {
+    public static final String MESSAGE_SUBJECT_CONSTRAINTS =
+            "Subject contain only alphanumerical characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the subject must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String SUBJECT_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Subject}.
+     *
+     * @param subject A valid subject.
+     */
+    public Subject(String subject) {
+        requireNonNull(subject);
+        checkArgument(isValidSubject(subject), MESSAGE_SUBJECT_CONSTRAINTS);
+        value = subject;
+    }
+
+    /**
+     * Returns true if a given string is a valid subject.
+     */
+    public static boolean isValidSubject(String test) {
+        return test.matches(SUBJECT_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Subject // instanceof handles nulls
+                && value.equals(((Subject) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
