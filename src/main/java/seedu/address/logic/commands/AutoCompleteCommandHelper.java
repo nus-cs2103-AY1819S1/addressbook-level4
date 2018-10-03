4da24bf454a963736302148cf1a5f3ec2160c753
Kok Jian Yu <kokjianyu@gmail.com>
2018-10-03 22:09:26 +0800

    Adjusted identation

--- src/main/java/seedu/address/logic/commands/AutoCompleteCommandHelper.java
+++ src/main/java/seedu/address/logic/commands/AutoCompleteCommandHelper.java
@@ -5,18 +5,18 @@ package seedu.address.logic.commands;
  */
 public class AutoCompleteCommandHelper {
     private static String[] commandWordList = {
-            AddCommand.COMMAND_WORD,
-            ClearCommand.COMMAND_WORD,
-            DeleteCommand.COMMAND_WORD,
-            EditCommand.COMMAND_WORD,
-            ExitCommand.COMMAND_WORD,
-            FindCommand.COMMAND_WORD,
-            HelpCommand.COMMAND_WORD,
-            HistoryCommand.COMMAND_WORD,
-            ListCommand.COMMAND_WORD,
-            RedoCommand.COMMAND_WORD,
-            SelectCommand.COMMAND_WORD,
-            UndoCommand.COMMAND_WORD
+        AddCommand.COMMAND_WORD,
+        ClearCommand.COMMAND_WORD,
+        DeleteCommand.COMMAND_WORD,
+        EditCommand.COMMAND_WORD,
+        ExitCommand.COMMAND_WORD,
+        FindCommand.COMMAND_WORD,
+        HelpCommand.COMMAND_WORD,
+        HistoryCommand.COMMAND_WORD,
+        ListCommand.COMMAND_WORD,
+        RedoCommand.COMMAND_WORD,
+        SelectCommand.COMMAND_WORD,
+        UndoCommand.COMMAND_WORD
     };
 
     /**
