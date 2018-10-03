d31b1769b4faf256346aef7b9c64db52a30f1357
NEW-DGU39GCO4DB\teoji <e0175930@u.nus.edu>
2018-10-03 15:23:53 +0800

    Fix checkstyle violations

--- src/test/java/seedu/address/logic/commands/ViewAllCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewAllCommandTest.java
@@ -30,12 +30,14 @@ public class ViewAllCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ViewAllCommand(), model, commandHistory, ViewAllCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ViewAllCommand(), model, commandHistory,
+                ViewAllCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ViewAllCommand(), model, commandHistory, ViewAllCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ViewAllCommand(), model, commandHistory,
+                ViewAllCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
