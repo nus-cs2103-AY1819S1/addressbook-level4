7dea94e3929654d0e975b53ecafcc41089a7dc1d
chivent <e0191350@u.nus.edu>
2018-10-03 22:56:35 +0800

    Added feature for google authentication - Added classes GoogleClientInstance.java, GoogleClientFactory.java, LoginCommand.java and client_credentials.json to help with authentication - AddressBookParser.java: Added call for LoginCommand - Model.java/ModelManager.java: Added GoogleClientInstance and its accessors/mutators - build.gradle: Added dependencies

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -12,6 +12,7 @@ import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
+import seedu.address.model.google.GoogleClientInstance;
 import seedu.address.model.person.Person;
 
 /**
@@ -22,6 +23,7 @@ public class ModelManager extends ComponentManager implements Model {
 
     private final VersionedAddressBook versionedAddressBook;
     private final FilteredList<Person> filteredPersons;
+    public GoogleClientInstance photoLibrary = null;
 
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
@@ -98,6 +100,18 @@ public class ModelManager extends ComponentManager implements Model {
     public void updateFilteredPersonList(Predicate<Person> predicate) {
         requireNonNull(predicate);
         filteredPersons.setPredicate(predicate);
+    }
+
+    //=========== GoogleClient Accessors =============================================================
+
+    @Override
+    public void setGoogleClientInstance(GoogleClientInstance instance) {
+        photoLibrary = instance;
+    }
+
+    @Override
+    public GoogleClientInstance getGoogleClientInstance() {
+        return photoLibrary;
     }
 
     //=========== Undo/Redo =================================================================================
