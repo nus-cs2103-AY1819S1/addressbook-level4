e4053508b634faf606978beb4517c553169769de
Shengmin <leongshengmin@gmail.com>
2018-10-03 16:49:30 +0800

    fix issue in ListCommand with unfound model

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_WISHES;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
