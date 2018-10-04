35063a05a5250c561081b704eee0186c8624ad46
elroyhaw <elroyhaw@gmail.com>
2018-10-04 21:33:19 +0800

    Edit indentations in CommandBox

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -50,22 +50,22 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private void handleKeyPress(KeyEvent keyEvent) {
         switch (keyEvent.getCode()) {
-            case UP:
-                // As up and down buttons will alter the position of the caret,
-                // consuming it causes the caret's position to remain unchanged
-                keyEvent.consume();
-                navigateToPreviousInput();
-                break;
-            case DOWN:
-                keyEvent.consume();
-                navigateToNextInput();
-                break;
-            case TAB:
-                keyEvent.consume();
-                filterCommands(commandTextField.getText());
-                break;
-            default:
-                // let JavaFx handle the keypress
+        case UP:
+            // As up and down buttons will alter the position of the caret,
+            // consuming it causes the caret's position to remain unchanged
+            keyEvent.consume();
+            navigateToPreviousInput();
+            break;
+        case DOWN:
+            keyEvent.consume();
+            navigateToNextInput();
+            break;
+        case TAB:
+            keyEvent.consume();
+            filterCommands(commandTextField.getText());
+            break;
+        default:
+            // let JavaFx handle the keypress
         }
     }
 
