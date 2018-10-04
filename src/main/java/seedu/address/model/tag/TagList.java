a8ff9b766e2b5af844076498d55d886ffc797327
Jiacheng Wu <>
2018-10-04 23:21:39 +0800

    Partial Implementation on TagBook storage.

---
+++ src/main/java/seedu/address/model/tag/TagList.java
@@ -0,0 +1,103 @@
+package seedu.address.model.tag;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.event.exceptions.EventNotFoundException;
+
+/**
+ * A list of events that does not allow nulls.
+ *
+ * Supports a minimal set of list operations.
+ */
+public class TagList implements Iterable<Tag> {
+
+    private final ObservableList<Tag> internalList = FXCollections.observableArrayList();
+
+    /**
+     * Returns true if the list contains the same event as the given argument.
+     */
+    public boolean contains(Tag toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::equals);
+    }
+
+    /**
+     * Adds an event to the list.
+     */
+    public void add(Tag toAdd) {
+        requireNonNull(toAdd);
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the event {@code target} in the list with {@code editedEvent}.
+     * {@code target} must exist in the list.
+     * The event identity of {@code editedEvent} must not be the same as another existing event in the list.
+     */
+    public void setTag(Tag target, Tag editedTag) {
+        requireAllNonNull(target, editedTag);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new EventNotFoundException();
+        }
+
+        internalList.set(index, editedTag);
+    }
+
+    /**
+     * Removes the equivalent event from the list.
+     * The event must exist in the list.
+     */
+    public void remove(Tag toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new EventNotFoundException();
+        }
+    }
+
+    public void setTags(TagList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code events}.
+     */
+    public void setTags(List<Tag> tags) {
+        requireAllNonNull(tags);
+        internalList.setAll(tags);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Tag> asUnmodifiableObservableList() {
+        return FXCollections.unmodifiableObservableList(internalList);
+    }
+
+    @Override
+    public Iterator<Tag> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TagList // instanceof handles nulls
+                && internalList.equals(((TagList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+}
---
+++ src/main/java/seedu/address/storage/XmlSerializableTagBook.java
@@ -0,0 +1,1 @@
+package seedu.address.storage;
