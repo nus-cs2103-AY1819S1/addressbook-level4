47b04787ca5abe3e0d94d00e1483c3842afc02e7
Shengmin <leongshengmin@gmail.com>
2018-10-04 21:09:22 +0800

    update equals method in XmlAdaptedWishWrapper

--- src/main/java/seedu/address/storage/XmlAdaptedWishWrapper.java
+++ src/main/java/seedu/address/storage/XmlAdaptedWishWrapper.java
@@ -1,5 +1,6 @@
 package seedu.address.storage;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import javax.xml.bind.annotation.XmlElementWrapper;
@@ -21,5 +22,27 @@ public class XmlAdaptedWishWrapper {
 
     public LinkedList<XmlAdaptedWish> getXmlAdaptedWishes() {
         return xmlAdaptedWishes;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof XmlAdaptedWishWrapper) || hasSameNumberOfWishes((XmlAdaptedWishWrapper) obj)) {
+            return false;
+        }
+        Iterator<XmlAdaptedWish> otherIterator = ((XmlAdaptedWishWrapper) obj).getXmlAdaptedWishes().listIterator();
+        Iterator<XmlAdaptedWish> thisIterator = xmlAdaptedWishes.listIterator();
+        while (otherIterator.hasNext()) {
+            if (!thisIterator.next().equals(otherIterator.next())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks if this has the same number of {@code XmlAdaptedWish} objects as the other {@code XmlAdaptedWishWrapper}.
+     */
+    private boolean hasSameNumberOfWishes(XmlAdaptedWishWrapper other) {
+        return xmlAdaptedWishes.size() == other.getXmlAdaptedWishes().size();
     }
 }
