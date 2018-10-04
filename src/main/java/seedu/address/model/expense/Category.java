68a9062cb98e598706fb0ae5050c988edb4a4f35
DESKTOP-GJ61B8F\xiangyu <1141108909@qq.com>
2018-10-04 21:53:09 +0800

    fix style issue

--- src/main/java/seedu/address/model/expense/Category.java
+++ src/main/java/seedu/address/model/expense/Category.java
@@ -3,8 +3,6 @@ package seedu.address.model.expense;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-import java.util.ArrayList;
-
 /**
  * Represents a Expense's category in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidCategory(String)}
