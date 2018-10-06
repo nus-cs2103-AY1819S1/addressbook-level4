4b32969ca2a0b1acbacbc1d734a9a96ed457b200
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-10-06 09:53:19 +0800

    Complete SortCommandTest.java

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,11 +1,8 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.CARL;
@@ -15,19 +12,18 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonPropertyComparator;
 import seedu.address.testutil.AddressBookBuilder;
-import seedu.address.testutil.TypicalPersons;
 
-//TODO
+/**
+ * Contains integration tests (interaction with the Model) for {@code SortCommand}.
+ */
 class SortCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    Model model = new ModelManager(new AddressBookBuilder().withPerson(CARL).withPerson(BENSON)
+            .withPerson(ALICE).build(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
     private PersonPropertyComparator nameComparator = PersonPropertyComparator.NAME;
@@ -60,15 +56,39 @@ class SortCommandTest {
     }
 
     @Test
+    public void testOrderOfSortedListOfModel() {
+        assertEquals(Arrays.asList(CARL, BENSON, ALICE), model.getSortedPersonList());
+    }
+
+    @Test
     public void execute_sortByName() {
-        Model testModel = new ModelManager(new AddressBookBuilder().withPerson(CARL).withPerson(BENSON)
-                .withPerson(ALICE).build(), new UserPrefs());
-        assertEquals(Arrays.asList(CARL, BENSON, ALICE), testModel.getSortedPersonList());
         String expectedMessage = String.format(MESSAGE_PERSONS_SORTED_OVERVIEW, nameComparator.toString());
-        Model expectedModel = new ModelManager(new AddressBookBuilder().withPerson(ALICE).withPerson(BENSON)
-                .withPerson(CARL).build(), new UserPrefs());
-        testModel.updateSortedPersonList(nameComparator);
-        assertCommandSuccess(sortByNameCommand, testModel, commandHistory, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(ALICE, BENSON, CARL), testModel.getSortedPersonList());
+        model.updateSortedPersonList(nameComparator);
+        assertCommandSuccess(sortByNameCommand, model, commandHistory, expectedMessage, model);
+        assertEquals(Arrays.asList(ALICE, BENSON, CARL), model.getSortedPersonList());
+    }
+
+    @Test
+    public void execute_sortByPhone() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_SORTED_OVERVIEW, phoneComparator.toString());
+        model.updateSortedPersonList(phoneComparator);
+        assertCommandSuccess(sortByPhoneCommand, model, commandHistory, expectedMessage, model);
+        assertEquals(Arrays.asList(ALICE, CARL, BENSON), model.getSortedPersonList());
+    }
+
+    @Test
+    public void execute_sortByEmail() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_SORTED_OVERVIEW, emailComparator.toString());
+        model.updateSortedPersonList(emailComparator);
+        assertCommandSuccess(sortByEmailCommand, model, commandHistory, expectedMessage, model);
+        assertEquals(Arrays.asList(ALICE, CARL, BENSON), model.getSortedPersonList());
+    }
+
+    @Test
+    public void execute_sortByAddress() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_SORTED_OVERVIEW, addressComparator.toString());
+        model.updateSortedPersonList(addressComparator);
+        assertCommandSuccess(sortByAddressCommand, model, commandHistory, expectedMessage, model);
+        assertEquals(Arrays.asList(ALICE, BENSON, CARL), model.getSortedPersonList());
     }
 }
