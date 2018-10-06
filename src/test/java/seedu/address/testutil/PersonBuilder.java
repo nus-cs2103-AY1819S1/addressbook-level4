3c05e60126e0360dc53a23ad6784f107eec8bddc
CrimsonAng <e0176842@u.nus.edu>
2018-10-06 23:11:47 +0800

    fix checksum no.6

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -7,8 +7,8 @@ import seedu.address.model.tag.Tag;
 import seedu.address.model.task.Address;
 import seedu.address.model.task.Email;
 import seedu.address.model.task.Name;
-import seedu.address.model.task.Task;
 import seedu.address.model.task.Phone;
+import seedu.address.model.task.Task;
 import seedu.address.model.util.SampleDataUtil;
 
 /**
