25c1564a8f88e41cfca70f860f7330e2206d754a
Gongjie <e0007878@u.nus.edu>
2018-10-07 17:32:33 +0800

    add librarySerializer

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -15,11 +15,9 @@ public class CliSyntax {
     public static final Prefix PREFIX_INDEX = new Prefix("i/");
     // todo remove AddressBook prefixes when its dependencies are resolved
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_PHONE = new Prefix("ph/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_TAG = new Prefix("ta/");
 
 }
--- src/main/java/seedu/address/storage/LibrarySerializer.java
+++ src/main/java/seedu/address/storage/LibrarySerializer.java
@@ -1,4 +1,26 @@
 package seedu.address.storage;
 
-public class LibrarySerializer {
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import seedu.address.model.Library;
+
+/**
+ * Serializes the library into json text.
+ */
+public class LibrarySerializer implements JsonSerializer<Library> {
+
+    @Override
+    public JsonElement serialize(final Library library, final Type typeOfSrc, final JsonSerializationContext context) {
+        final JsonObject jsonObject = new JsonObject();
+
+        final JsonElement jsonPlaylists = context.serialize(library.getPlaylists());
+        jsonObject.add("playlists", jsonPlaylists);
+
+        return jsonObject;
+    }
 }
