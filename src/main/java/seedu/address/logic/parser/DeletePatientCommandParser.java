8b8a0cab28567de9911ba29ea591ea9fd63bd282
elroyhaw <elroyhaw@gmail.com>
2018-10-07 20:02:14 +0800

    Separate import statements

--- src/main/java/seedu/address/logic/parser/DeletePatientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePatientCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import java.util.stream.Stream;
+
 import seedu.address.logic.commands.DeletePatientCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Name;
