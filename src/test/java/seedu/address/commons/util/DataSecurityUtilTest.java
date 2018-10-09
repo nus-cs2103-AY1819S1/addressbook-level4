9eb88e76af9be18a0f8fc78398e301f7a038ca36
etjk <edricteojk@hotmail.com>
2018-10-09 23:22:53 +0800

    Add test case for decrypt

--- src/test/java/seedu/address/commons/util/DataSecurityUtilTest.java
+++ src/test/java/seedu/address/commons/util/DataSecurityUtilTest.java
@@ -3,27 +3,30 @@ package seedu.address.commons.util;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static seedu.address.commons.util.DataSecurityUtil.encrypt;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Formatter;
+import javax.xml.bind.DatatypeConverter;
 
+import com.google.common.base.Charsets;
 import org.junit.Test;
-import seedu.address.testutil.Assert;
+import seedu.address.commons.exceptions.CorruptedFileException;
+import seedu.address.commons.exceptions.InvalidPasswordException;
 
 public class DataSecurityUtilTest {
 
     private String PASSWORD = "p@ssW0Rd";
+    private String WRONG_PASSWORD = "1";
 
-    private byte[] TEST_DATA = "1qazXSW@1qazXSW@".getBytes();
-    private String EXPECTED_ENCRYPTION = "0d2989d11f8a7e8acf7c518335651dc0";
-    private String EXPECTED_DECRYPTION = "1qazXSW@1qazXSW@";
+    private byte[] TEST_DATA_ENCRYPTION = "TEST DATA".getBytes();
+    private String EXPECTED_ENCRYPTION = "D91B9E39621EACE5877DCD6158393F0F";
+
+    private byte[] TEST_DATA_DECRYPTION = convertStringToByteArray("D91B9E39621EACE5877DCD6158393F0F");
+    private String EXPECTED_DECRYPTION = "TEST DATA";
 
     // Encryption test case
     @Test
     public void testEncryptingBytes(){
-        byte[] encryptedData = DataSecurityUtil.encrypt(TEST_DATA, PASSWORD);
-        String encryptedDataString = byteArrayToHex(encryptedData);
+        byte[] encryptedData = DataSecurityUtil.encrypt(TEST_DATA_ENCRYPTION, PASSWORD);
+        String encryptedDataString = convertByteArrayToHexString(encryptedData);
 
         assertEquals(EXPECTED_ENCRYPTION, encryptedDataString);
     }
@@ -34,22 +37,49 @@ public class DataSecurityUtilTest {
     }
 
     @Test(expected = NullPointerException.class)
+    public void testEncryptingValidBytesAndNullPassword(){
+        DataSecurityUtil.encrypt(TEST_DATA_ENCRYPTION, null);
+    }
+
+    @Test(expected = NullPointerException.class)
     public void testEncryptingNullBytesAndNullPassword(){
         DataSecurityUtil.encrypt(null, null);
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testEncryptingValidBytesAndNullPassword(){
-        DataSecurityUtil.encrypt(TEST_DATA, null);
+    // Decryption test case
+    @Test
+    public void testDecryptingBytes() throws CorruptedFileException, InvalidPasswordException {
+        byte[] decryptedData = DataSecurityUtil.decrypt(TEST_DATA_DECRYPTION, PASSWORD);
+        String decryptedDataString = new String(decryptedData, Charsets.UTF_8);
+
+        assertEquals(EXPECTED_DECRYPTION, decryptedDataString);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testDecryptingNullBytesValidPassword() throws CorruptedFileException, InvalidPasswordException {
+        DataSecurityUtil.decrypt(null, PASSWORD);
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void testDecryptingValidBytesAndNullPassword(){
+        DataSecurityUtil.encrypt(TEST_DATA_DECRYPTION, null);
+    }
 
-    private String byteArrayToHex(byte[] data) {
-        Formatter formatter = new Formatter();
-        for (byte eachByte : data) {
-            formatter.format("%02x", eachByte);
-        }
-        return formatter.toString();
+    @Test(expected = NullPointerException.class)
+    public void testDecryptingNullBytesAndNullPassword(){
+        DataSecurityUtil.encrypt(null, null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void testDecryptingBytesWrongPassword() throws CorruptedFileException, InvalidPasswordException {
+        DataSecurityUtil.decrypt(TEST_DATA_DECRYPTION, WRONG_PASSWORD);
+    }
+
+    public static String convertByteArrayToHexString(byte[] data) {
+        return DatatypeConverter.printHexBinary(data);
+    }
+
+    public static byte[] convertStringToByteArray(String s) {
+        return DatatypeConverter.parseHexBinary(s);
     }
 }
