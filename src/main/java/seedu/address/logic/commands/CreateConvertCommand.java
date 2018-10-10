36474f65521edf3e9eb57c44e5f80799b4f6a006
lancelotwillow <lancelotwillow@gmail.com>
2018-10-10 23:38:28 +0800

    add arg validation and transformation convert

--- src/main/java/seedu/address/logic/commands/CreateConvertCommand.java
+++ src/main/java/seedu/address/logic/commands/CreateConvertCommand.java
@@ -5,6 +5,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.storage.JsonConvertArgsStorage;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class CreateConvertCommand extends Command {
@@ -17,6 +18,26 @@ public class CreateConvertCommand extends Command {
         } else {
             this.cmds = cmds;
             this.name = name;
+        }
+    }
+
+    private void checkSigleValidation(String arg) throws IllegalArgumentException {
+        //just a template, not only this
+        if(arg == "") {
+            throw new IllegalArgumentException();
+        }
+    };
+
+
+
+    private void checkValidation() {
+        Iterator<String> iter = cmds.iterator();
+        while(iter.hasNext()) {
+            try{
+                checkSigleValidation(iter.next());
+            } catch(IllegalArgumentException e) {
+                System.err.println(e);
+            }
         }
     }
 
