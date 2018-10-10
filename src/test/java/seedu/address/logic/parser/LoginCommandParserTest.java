edd8b09e13f4abd058091fb6c2d88df106611506
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-10 09:16:46 +0800

    LoginCommandParserTest.java: Add test case for empty arguments.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

---
+++ src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+import org.junit.Test;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.LoginCommand;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+public class LoginCommandParserTest {
+    
+    private LoginCommandParser parser = new LoginCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsLoginCommand() {
+        // no leading and trailing whitespaces
+        //
+        //assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        //assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+    }
+}
