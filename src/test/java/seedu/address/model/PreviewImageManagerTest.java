c29fd0e146fd8ac9ca33fcac257d2dd8015ea406
ihwk1996 <ihwk1996@gmail.com>
2018-10-10 22:41:57 +0800

    PreviewImageManager.java: change order

--- src/test/java/seedu/address/model/PreviewImageManagerTest.java
+++ src/test/java/seedu/address/model/PreviewImageManagerTest.java
@@ -2,8 +2,9 @@ package seedu.address.model;
 
 // import org.junit.Test;
 // import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 
 import java.util.List;
 
