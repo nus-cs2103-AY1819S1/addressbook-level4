453e170d10bc28ce520ac49710107fc2abd50960
ihwk1996 <ihwk1996@gmail.com>
2018-10-10 22:15:59 +0800

    PreviewImage.java: change access modifier

--- src/main/java/seedu/address/model/PreviewImage.java
+++ src/main/java/seedu/address/model/PreviewImage.java
@@ -12,6 +12,12 @@ public class PreviewImage implements PreviewableImage {
     private final Image image;
     private final TransformationSet transformationSet;
 
+
+    PreviewImage() {
+        this.image = new Image("https://via.placeholder.com/500x500");
+        this.transformationSet = new TransformationSet();
+    }
+    
     public PreviewImage(Image image, TransformationSet transformationSet) {
         this.image = image;
         this.transformationSet = transformationSet;
@@ -25,11 +31,6 @@ public class PreviewImage implements PreviewableImage {
     public PreviewImage(PreviewableImage previewableImage) {
         this.image = previewableImage.getImage();
         this.transformationSet = previewableImage.getTransformationSet();
-    }
-    
-    public PreviewImage() {
-        this.image = new Image("https://via.placeholder.com/500x500");
-        this.transformationSet = new TransformationSet();
     }
 
     public Image getImage() {
