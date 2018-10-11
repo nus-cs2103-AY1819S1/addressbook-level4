95171bcea221cc4c8df62705736924f0747e7dbc
BennTay <benntayguobin@gmail.com>
2018-10-12 02:23:45 +0800

    RecipeCard.java: Modified to extend GenericCard.java

--- src/main/java/seedu/souschef/ui/RecipeCard.java
+++ src/main/java/seedu/souschef/ui/RecipeCard.java
@@ -10,17 +10,9 @@ import seedu.souschef.model.recipe.Recipe;
 /**
  * An UI component that displays information of a {@code Recipe}.
  */
-public class RecipeCard extends UiPart<Region> {
+public class RecipeCard extends GenericCard<Recipe> {//UiPart<Region> {
 
     private static final String FXML = "RecipeListCard.fxml";
-
-    /**
-     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
-     * As a consequence, UI elements' variable names cannot be set to such keywords
-     * or an exception will be thrown by JavaFX during runtime.
-     *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AppContent level 4</a>
-     */
 
     public final Recipe recipe;
 
