9bfa695cb4641edd36aafb46f1a5828ace5c842c
hzf1174 <35621762+hzf1174@users.noreply.github.com>
2018-10-11 19:02:04 +0800

    Update ModuleTest.java

--- src/test/java/seedu/address/model/module/ModuleTest.java
+++ src/test/java/seedu/address/model/module/ModuleTest.java
@@ -47,8 +47,8 @@ public class ModuleTest {
 
         // is not the prefix -> returns false
         assertFalse(cs1010Prefix.isPrefixModule(ACC1002));
-        }
-        
+    }
+    
     @Test
     public void equals() {
         // same values -> returns true
