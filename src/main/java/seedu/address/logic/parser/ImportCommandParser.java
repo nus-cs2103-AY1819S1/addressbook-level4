d7a9f54a281efb2009627d608f6a51c11ffd15e4
kengwoon <shannalimkengwoon@gmail.com>
2018-10-12 14:53:09 +0800

    Modified ModelStub in AddCommandTest

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -16,13 +16,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class ImportCommandParser implements Parser<ImportCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * Parses the given {@code String} of arguments in the context of the ImportCommand
      * and returns an ImportCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public ImportCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args);
+                ArgumentTokenizer.tokenize(args, PREFIX_FILE);
 
         if (!arePrefixesPresent(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
