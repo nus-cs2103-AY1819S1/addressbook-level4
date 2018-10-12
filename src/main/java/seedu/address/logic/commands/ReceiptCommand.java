62c8ca21044e95e56c5438a335dbe450b14e7c1d
LAPTOP-E3JOI16D\clint <clawyq@hotmail.com>
2018-10-12 10:35:30 +0800

    Update execute method in ReceiptCommand. Add getServedPatientListLength for ServedPatientList.

--- src/main/java/seedu/address/logic/commands/ReceiptCommand.java
+++ src/main/java/seedu/address/logic/commands/ReceiptCommand.java
@@ -2,8 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.ArrayList;
-
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
@@ -11,14 +9,16 @@ import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.PatientQueue;
 import seedu.address.model.Receipt;
+import seedu.address.model.ServedPatientList;
 import seedu.address.model.person.ServedPatient;
 
 //integrate select command
 /**
  * Generates a receipt for {@code Patient} specified by {@code index} that appears in the GUI and in a pdf.
  */
-public class ReceiptCommand extends Command {
+public class ReceiptCommand extends QueueCommand {
     public static final String COMMAND_WORD = "receipt";
     public static final String COMMAND_ALIAS = "rct";
 
@@ -29,6 +29,7 @@ public class ReceiptCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Receipt generated for patient!";
 
     private final Index index;
+    private String generatedResult;
 
     /**
      * Creates a ReceiptCommand for the {@code servedPatient} specified by {@code index}
@@ -39,22 +40,21 @@ public class ReceiptCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+    public CommandResult execute(Model model, PatientQueue patientQueue, ServedPatient currentPatient,
+                                 ServedPatientList servedPatientList, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        ArrayList<ServedPatient> servedPatientList = new ArrayList<>(); //where is my served patient list? :(
-
-        if (index.getZeroBased() >= servedPatientList.size()) {
+        if (index.getZeroBased() >= servedPatientList.getServedPatientListLength()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
         final Receipt receipt;
 
-        ServedPatient servedPatient = servedPatientList.get(index.getZeroBased());
+        ServedPatient servedPatient = servedPatientList.selectServedPatient(index);
         receipt = new Receipt(servedPatient);
-        receipt.generate();
+        generatedResult = receipt.generate();
 
         EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
-        return new CommandResult(String.format(MESSAGE_SUCCESS));
+        return new CommandResult(String.format(generatedResult, MESSAGE_SUCCESS));
 
     }
 
