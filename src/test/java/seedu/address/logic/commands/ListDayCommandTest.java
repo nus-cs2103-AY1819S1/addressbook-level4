87599ee816bd50d1b51b1af17dd2dcb167e1efba
CrimsonAng <e0176842@u.nus.edu>
2018-10-13 22:31:50 +0800

    add junit test for 'listday'

--- src/test/java/seedu/address/logic/commands/ListDayCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListDayCommandTest.java
@@ -1,7 +1,10 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.ListDayCommand.MESSAGE_FAILURE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalTasks.AMY;
 import static seedu.address.testutil.TypicalTasks.getTypicalSchedulePlanner;
 
 import org.junit.Test;
@@ -10,16 +13,27 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.task.DateSamePredicate;
+import seedu.address.model.task.Task;
+import seedu.address.testutil.TaskBuilder;
+
+
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListDayCommand.
  */
 public class ListDayCommandTest {
-    private Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
 
-    /*
+    //private Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
+
     @Test
-    public void execute() {
-        assertCommandFailure(new ListDayCommand(), model, new CommandHistory(), MESSAGE_FAILURE);
+    public void task_remain_afterFilter() {
+        //after update the filteredtasklist with a specific date predicate, model would have that task remaining
+        Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
+        Task validTask = new TaskBuilder().withDate("111111").build();
+        model.addTask(validTask);
+        model.updateFilteredTaskList(new DateSamePredicate(validTask.getDate().value));
+        assertTrue(model.hasTask(validTask));
     }
-    */
+    
 }
