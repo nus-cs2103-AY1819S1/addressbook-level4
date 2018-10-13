4dd4e9ee500740ab97f1cefc8fb2e494bb00bae4
dan-delion <sram-b@comp.nus.edu.sg>
2018-10-14 05:11:12 +0800

    Add name field in finder and owner panels

--- src/main/java/seedu/lostandfound/MainApp.java
+++ src/main/java/seedu/lostandfound/MainApp.java
@@ -50,7 +50,6 @@ public class MainApp extends Application {
     protected Config config;
     protected UserPrefs userPrefs;
 
-
     @Override
     public void init() throws Exception {
         logger.info("=============================[ Initializing ArticleList ]===========================");
