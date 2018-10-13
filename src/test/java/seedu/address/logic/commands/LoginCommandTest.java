47e96a015610eb374b4b442be8209fb1075c4206
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-13 15:49:09 +0800

    LoginCommand: Add test cases for equals()
    - Test for similar object and values.
    - Add @@author
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

---
+++ src/test/java/seedu/address/logic/commands/LoginCommandTest.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import static seedu.address.testutil.TypicalPersons.ADAM;
+import static seedu.address.testutil.TypicalPersons.BEN;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.doctor.Doctor;
+
+//@@author jjlee050
+public class LoginCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void equals() {
+        LoginCommand loginFirstCommand = new LoginCommand(new Doctor(ADAM.getId(), ADAM.getName(), ADAM.getPassword()));
+        LoginCommand loginSecondCommand = new LoginCommand(new Doctor(BEN.getId(), BEN.getName(), BEN.getPassword()));
+        
+        // same object -> returns true
+        assertTrue(loginFirstCommand.equals(loginFirstCommand));
+
+        // same values -> returns true
+        LoginCommand loginFirstCommandCopy = new LoginCommand(new Doctor(ADAM.getId(), ADAM.getName(), ADAM.getPassword()));
+        assertTrue(loginFirstCommand.equals(loginFirstCommandCopy));
+        
+        // different types -> returns false
+        assertFalse(loginFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(loginFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(loginFirstCommand.equals(loginSecondCommand));
+    }
+}
