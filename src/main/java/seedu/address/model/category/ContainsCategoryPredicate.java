e2cfc7e3372c9b00c7414d91676a8654811665ab
ongspxm <ongspxm@gmail.com>
2018-10-13 13:10:57 +0800

    add containCategoryPredicate

---
+++ src/main/java/seedu/address/model/category/ContainsCategoryPredicate.java
@@ -0,0 +1,28 @@
+package seedu.address.model.category;
+
+import java.util.function.Predicate;
+import seedu.address.model.entry.MajorResumeEntry;
+
+/**
+ * Tests that a {@code MajorResumeEntry}'s matches the keyword given.
+ */
+public class ContainsCategoryPredicate implements Predicate<MajorResumeEntry> {
+    private final Category category;
+
+    public ContainsCategoryPredicate(String keyword) {
+        category = new Category(keyword);
+    }
+
+    @Override
+    public boolean test(MajorResumeEntry entry) {
+        return category.equals(entry.getCategory());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ContainsCategoryPredicate // instanceof handles nulls
+                && category.equals(((ContainsCategoryPredicate) other).category)); // state check
+    }
+
+}
