9c71865768739a967aaf8fe97b067e054375da18
jjlee050 <josephlee050@gmail.com>
2018-10-13 19:31:43 +0800

    LoginCommandTest.java: Add test cases.
    - Add test cases for valid login credentials.
    - Add test cases for invalid login credentials.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/address/logic/commands/LoginCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LoginCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.ADAM;
 import static seedu.address.testutil.TypicalPersons.BEN;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -13,7 +14,9 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.analytics.Analytics;
 import seedu.address.model.doctor.Doctor;
+import seedu.address.model.doctor.Password;
 
 //@@author jjlee050
 public class LoginCommandTest {
@@ -21,6 +24,31 @@ public class LoginCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
+    private Analytics analytics = new Analytics();
+
+    @Test
+    public void execute_validCredentials_returnTrue() {
+        LoginCommand command = new LoginCommand(
+                new Doctor(ADAM.getId(), ADAM.getName(), new Password("doctor1", false)));
+        String expectedMessage = LoginCommand.MESSAGE_SUCCESS;
+
+        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
+    }
+
+    @Test
+    public void execute_invalidCredentials_returnFalse() {
+        String expectedMessage = LoginCommand.MESSAGE_FAILURE;
+
+        // different name
+        LoginCommand command = new LoginCommand(
+                new Doctor(ADAM.getId(), BEN.getName(), new Password("doctor1", false)));
+        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
+
+        // different password
+        command = new LoginCommand(
+                new Doctor(ADAM.getId(), ADAM.getName(), new Password("doctor2", false)));
+        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
+    }
 
     @Test
     public void equals() {
