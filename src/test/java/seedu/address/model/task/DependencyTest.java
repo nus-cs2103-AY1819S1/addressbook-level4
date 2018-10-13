410a2b60d7c874a5e62f10448f97ec120e93d2eb
pengnam <seanngpengnam@gmail.com>
2018-10-14 00:14:19 +0800

    Add inital test for Dependency Field
    only added for null exception

---
+++ src/test/java/seedu/address/model/task/DependencyTest.java
@@ -0,0 +1,16 @@
+package seedu.address.model.task;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import seedu.address.testutil.Assert;
+
+public class DependencyTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Dependency(null));
+    }
+    
+}
