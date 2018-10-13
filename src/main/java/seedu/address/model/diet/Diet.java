b6a50f2acc5393d19e3302ccd5c12d999c3ad187
yuntongzhang <ang.unong@gmail.com>
2018-10-13 20:03:04 +0800

    Complete Diet class and test

--- src/main/java/seedu/address/model/diet/Diet.java
+++ src/main/java/seedu/address/model/diet/Diet.java
@@ -1,40 +1,75 @@
 package seedu.address.model.diet;
 
 //@@author yuntongzhang
+
+import java.util.Objects;
+
 /**
- * Represents a Dietary requirement for a Person.
- * Guarantees: field values are validated and immutable.
+ * Represents dietary requirement for a person.
  */
 public class Diet {
-    private final Allegy allergy;
-    private final CulturalRequriement cuturalRequirement;
-    private final PhysicalDifficulty physicalDifficulty;
+    private final String detail;
+    private final DietType type;
 
-    // Every field must be present
-    public Diet(Allegy allergy, CulturalRequriement cuturalRequirement, PhysicalDifficulty physicalDifficulty) {
-        this.allergy = allergy;
-        this.cuturalRequirement = cuturalRequirement;
-        this.physicalDifficulty = physicalDifficulty;
-    }
-
-    public Allergy getAllergy() {
-        return allergy;
-    }
-
-    public CulturalRequirement getCulturalRequirement() {
-        return culturalRequirement;
-    }
-
-    public PhysicalDifficulty getPhysicalDifficulty() {
-        return physicalDifficulty;
+    /**
+     * Constructor for a Diet Object.
+     * All fields must be non-null.
+     *
+     * @param detail The detail of this Diet requirement.
+     * @param type The type of this Diet requirement.
+     */
+    public Diet(String detail, DietType type) {
+        Objects.requireNonNull(detail);
+        Objects.requireNonNull(type);
+        this.detail = detail;
+        this.type = type;
     }
 
     /**
-     * Returns true if the two diet requirements have the same allergy requirement,
-     * cultural requirement and physical difficulty requirement.
-     *
-     * @param other The other Object to be compared with
-     * @return Whether this Diet is the same as the other Object
+     * Get the detail if this Diet requirement.
+     * @return The detail of this Diet requirement.
+     */
+    public String getDetail() {
+        return this.detail;
+    }
+
+    /**
+     * Get the type of this Diet requirement.
+     * @return The type of this Diet requirement.
+     */
+    public DietType getType() {
+        return this.type;
+    }
+
+    /**
+     * Check whether this diet requirement is allergy.
+     * @return True if this diet requirement is an allergy.
+     */
+    public boolean isAllergy() {
+        return this.type == DietType.ALLERGY;
+    }
+
+    /**
+     * Check whether this diet requirement is cultural requirement.
+     * @return True if this diet requirement is a cultural requirement.
+     */
+    public boolean isCulturalRequirement() {
+        return this.type == DietType.CULTURAL;
+    }
+
+    /**
+     * Check whether this diet requirement is physical difficulty.
+     * @return True if this diet requirement is a physical difficulty.
+     */
+    public boolean isPhysicalDifficulty() {
+        return this.type == DietType.PHYSICAL;
+    }
+
+
+    /**
+     * Check whether this diet is equal to the given object.
+     * @param other The other object to compare with
+     * @return Whether this diet is equal to the given object.
      */
     @Override
     public boolean equals(Object other) {
@@ -47,20 +82,22 @@ public class Diet {
         }
 
         Diet otherDiet = (Diet) other;
-        return otherDiet.getAllergy().equals(getAllergy())
-               && otherDiet.getCulturalRequirement().equals(getCulturalRequirement())
-               && otherDiet.getPhysicalDifficulty().equals(getPhysicalDifficulty());
+        return otherDiet.getDetail().equals(getDetail())
+                && (otherDiet.getType() == getType());
     }
 
+    /**
+     * Convert the diet to String for representation.
+     * @return The string representation for this diet.
+     */
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.appen(" Allegies: ")
-                .append(getAllergy())
-                .append(" Cultural Requirements: ")
-                .append(getCulturalRequirement())
-                .append(" Physical Difficulties: ")
-                .append(getPhysicalDifficulty());
+
+        builder.append(" ")
+                .append(getDetail())
+                .append(" ");
+
         return builder.toString();
     }
 }
