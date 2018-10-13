87db41e6fa4b6c823d133cc83093f7ff89f94c14
BennTay <benntayguobin@gmail.com>
2018-10-13 21:30:00 +0800

    DayNotFoundException.java: Add exception for unable to find day

---
+++ src/main/java/seedu/souschef/model/planner/exceptions/DayNotFoundException.java
@@ -0,0 +1,21 @@
+package seedu.souschef.model.planner.exceptions;
+
+/**
+ * Signals that no recipe is found at the specified meal slot of the day.
+ */
+public class DayNotFoundException extends RuntimeException {
+    /**
+     * @param message should contain relevant information on the failed constraint(s)
+     */
+    public DayNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param message should contain relevant information on the failed constraint(s)
+     * @param cause of the main exception
+     */
+    public DayNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
