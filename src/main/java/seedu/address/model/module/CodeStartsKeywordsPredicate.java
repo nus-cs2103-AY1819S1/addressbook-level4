28814466252e163aa7590edf449c0e2be92f9965
hzf1174 <e0253687@MacBook-Pro-4.local>
2018-10-14 02:00:28 +0800

    rewrite search command

--- src/main/java/seedu/address/model/module/CodeStartsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/module/CodeStartsKeywordsPredicate.java
@@ -3,30 +3,28 @@ package seedu.address.model.module;
 import java.util.List;
 import java.util.function.Predicate;
 
-import seedu.address.commons.util.StringUtil;
-import seedu.address.model.module.Module;
 
 /**
- * Tests that a {@code Module}'s {@code Code} starts with the keyword given.
+ * Tests that a {@code Module}'s {@code Code} starts with the keywords given.
  */
-public class CodeStartsKeywordPredicate implements Predicate<Module> {
+public class CodeStartsKeywordsPredicate implements Predicate<Module> {
     private final List<String> keywords;
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
+    public CodeStartsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
-    public boolean test(Person person) {
+    public boolean test(Module module) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> new Module(keyword).isPrefixModule(module));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
-                && keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+                || (other instanceof CodeStartsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((CodeStartsKeywordsPredicate) other).keywords)); // state check
     }
 
 }
