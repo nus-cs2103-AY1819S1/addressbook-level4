f41c9c79c98dd6ca991901e4c60101f8f4b9951c
David Choo <davidchoo16@gmail.com>
2018-10-15 03:31:35 +0800

    Revert and refactor ClearCommand and its test

--- src/main/java/seedu/jxmusic/logic/commands/ClearCommand.java
+++ src/main/java/seedu/jxmusic/logic/commands/ClearCommand.java
@@ -1,24 +1,23 @@
-//package seedu.jxmusic.logic.commands;
-//
-//import static java.util.Objects.requireNonNull;
-//
-//import seedu.jxmusic.model.AddressBook;
-//import seedu.jxmusic.model.Model;
-//
-///**
-// * Clears the jxmusic book.
-// */
-//public class ClearCommand extends Command {
-//
-//    public static final String COMMAND_WORD = "clear";
-//    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-//
-//
-//    @Override
-//    public CommandResult execute(Model model, CommandHistory history) {
-//        requireNonNull(model);
-//        model.resetData(new AddressBook());
-//        model.commitAddressBook();
-//        return new CommandResult(MESSAGE_SUCCESS);
-//    }
-//}
+package seedu.jxmusic.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.jxmusic.model.Library;
+import seedu.jxmusic.model.Model;
+
+/**
+* Clears the library.
+*/
+public class ClearCommand extends Command {
+
+   public static final String COMMAND_PHRASE = "clear";
+   public static final String MESSAGE_SUCCESS = "Library has been cleared!";
+
+
+   @Override
+   public CommandResult execute(Model model) {
+       requireNonNull(model);
+       model.resetData(new Library());
+       return new CommandResult(MESSAGE_SUCCESS);
+   }
+}
--- src/test/java/seedu/jxmusic/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/jxmusic/logic/commands/ClearCommandTest.java
@@ -1,36 +1,35 @@
-//package seedu.jxmusic.logic.commands;
-//
-//import static seedu.jxmusic.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.jxmusic.testutil.TypicalPlaylists.getTypicalAddressBook;
-//
-//import org.junit.Test;
-//
-//import seedu.jxmusic.model.AddressBook;
-//import seedu.jxmusic.model.Model;
-//import seedu.jxmusic.model.ModelManager;
-//import seedu.jxmusic.model.UserPrefs;
-//
-//public class ClearCommandTest {
-//
-//    private CommandHistory commandHistory = new CommandHistory();
-//
-//    @Test
-//    public void execute_emptyAddressBook_success() {
-//        Model model = new ModelManager();
-//        Model expectedModel = new ModelManager();
-//        expectedModel.commitAddressBook();
-//
-//        assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_nonEmptyAddressBook_success() {
-//        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-//        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-//        expectedModel.resetData(new AddressBook());
-//        expectedModel.commitAddressBook();
-//
-//        assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-//    }
-//
-//}
+package seedu.jxmusic.logic.commands;
+
+import static seedu.jxmusic.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.jxmusic.testutil.TypicalPlaylists.getTypicalLibrary;
+
+import org.junit.Test;
+
+import seedu.jxmusic.logic.CommandHistory;
+import seedu.jxmusic.model.Library;
+import seedu.jxmusic.model.Model;
+import seedu.jxmusic.model.ModelManager;
+import seedu.jxmusic.model.UserPrefs;
+
+public class ClearCommandTest {
+
+   private CommandHistory commandHistory = new CommandHistory();
+
+   @Test
+   public void execute_emptyLibrary_success() {
+       Model model = new ModelManager();
+       Model expectedModel = new ModelManager();
+
+       assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+   }
+
+   @Test
+   public void execute_nonEmptyLibrary_success() {
+       Model model = new ModelManager(getTypicalLibrary(), new UserPrefs());
+       Model expectedModel = new ModelManager(getTypicalLibrary(), new UserPrefs());
+       expectedModel.resetData(new Library());
+
+       assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+   }
+
+}
