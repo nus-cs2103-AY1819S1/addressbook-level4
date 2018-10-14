78fd0730115b0fef4f67d4a6b1577668de1bdae1
yuntongzhang <ang.unong@gmail.com>
2018-10-14 16:44:20 +0800

    Add AddDietCommand class and test

--- src/main/java/seedu/address/logic/commands/AddDietCommand.java
+++ src/main/java/seedu/address/logic/commands/AddDietCommand.java
@@ -1,12 +1,25 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ALLERGY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CULTURAL_REQUIREMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHYSICAL_DIFFICULTY;
 
+import java.util.Set;
+
+import javafx.collections.ObservableList;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.diet.DietCollection;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 
 //@author yuntongzhang
@@ -16,43 +29,96 @@ import seedu.address.model.person.Person;
  * @author yuntongzhang
  */
 public class AddDietCommand extends Command {
-    // TODO: currently this command only consider allergies, add cultural requirements, physical difficulties
-
     public static final String COMMAND_WORD = "adddiet";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds dietary requirements for a patient. "
                                                + "Parameters: "
                                                + PREFIX_NRIC + "NRIC "
-                                               + PREFIX_ALLERGY + "ALLERGIES "
+                                               + PREFIX_ALLERGY + "ALLERGY1 "
+                                               + PREFIX_ALLERGY + "ALLERGY2 "
+                                               + PREFIX_CULTURAL_REQUIREMENT + "REQUIREMENT "
+                                               + PREFIX_PHYSICAL_DIFFICULTY + "DIFFICULTY\n"
                                                + "Example: " + COMMAND_WORD + " "
-                                               + PREFIX_NRIC + "S1234567M "
-                                               + PREFIX_ALLERGY + "Egg ";
+                                               + PREFIX_NRIC + "S1234567A "
+                                               + PREFIX_ALLERGY + "Egg "
+                                               + PREFIX_ALLERGY + "Crab "
+                                               + PREFIX_CULTURAL_REQUIREMENT + "Halal "
+                                               + PREFIX_PHYSICAL_DIFFICULTY + "Can't move his hands. ";
 
-    public static final String MESSAGE_SUCCESS = "Added dietary requirements for patient: %1$s";
-    public static final String MESSAGE_IN_PROGRESS = "Feature in the progress of development.";
+    public static final String MESSAGE_SUCCESS = "Dietary requirements added for patient: %1$s";
+    public static final String MESSAGE_NO_SUCH_PATIENT = "No such patient exists.";
+    public static final String MESSAGE_MULTIPLE_PATIENTS = "Multiple such patients exist. "
+                                                           + "Please contact the system administrator.";
 
-    private final Person patientToEdit;
-
-    // TODO: add new class and include the attribute here
-    // private final Diet dietToAdd;
+    private final DietCollection dietsToAdd;
+    private final Nric patientNric;
 
     /**
      * Creates an AddDietCommand to add Diet to a patient.
-     * @param patient The patient to be edited.
+     * @param patientNric NRIC of the patient to be updated.
+     * @param dietCollection The collection of dietary requirements to be added to a patient..
      */
-    public AddDietCommand(Person patient) {
-        patientToEdit = patient;
+    public AddDietCommand(Nric patientNric, DietCollection dietCollection) {
+        this.patientNric = requireNonNull(patientNric);
+        this.dietsToAdd = requireNonNull(dietCollection);
     }
 
-    // TODO: finish this method
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        return new CommandResult(MESSAGE_IN_PROGRESS);
+        requireNonNull(model);
+
+        ObservableList<Person> filteredByNric = model.getFilteredPersonList()
+                .filtered(p-> patientNric.equals(p.getNric()));
+
+        if (filteredByNric.size() < 1) {
+            throw new CommandException(MESSAGE_NO_SUCH_PATIENT);
+        }
+
+        if (filteredByNric.size() > 1) {
+            throw new CommandException(MESSAGE_MULTIPLE_PATIENTS);
+        }
+
+        Person patientToUpdate = filteredByNric.get(0);
+        Person updatedPatient = addDietsForPatient(patientToUpdate, dietsToAdd);
+
+        model.updatePerson(patientToUpdate, updatedPatient);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, patientNric));
     }
 
-    // TODO: add an override equals() method
+    /**
+     * Update a patient with the new dietary requirement.
+     *
+     * @param patientToUpdate The patient to be updated.
+     * @param dietsToAdd The diet to be added to the patient.
+     * @return An updated patient with the appropriate diet added.
+     */
+    private static Person addDietsForPatient(Person patientToUpdate, DietCollection dietsToAdd) {
+        assert patientToUpdate != null;
+
+        DietCollection updatedDiet = patientToUpdate.getDietCollection().addMoreDiets(dietsToAdd);
+
+        Nric nric = patientToUpdate.getNric();
+        Name name = patientToUpdate.getName();
+        Phone phone = patientToUpdate.getPhone();
+        Email email = patientToUpdate.getEmail();
+        Address address = patientToUpdate.getAddress();
+        Set<Tag> tags = patientToUpdate.getTags();
+
+        return new Person(nric, name, phone, email, address, tags, updatedDiet);
+    }
+
     @Override
     public boolean equals(Object other) {
-        return true;
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof AddDietCommand)) {
+            return false;
+        }
+
+        AddDietCommand otherAddDietCommand = (AddDietCommand) other;
+        return otherAddDietCommand.patientNric.equals(patientNric)
+                && otherAddDietCommand.dietsToAdd.equals(dietsToAdd);
     }
 }
--- src/test/java/seedu/address/logic/commands/AddDietCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddDietCommandTest.java
@@ -1,31 +1,146 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ALLERGY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_CULTURAL_REQUIREMENT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHYSICAL_DIFFICULTY;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.diet.Diet;
+import seedu.address.model.diet.DietCollection;
+import seedu.address.model.diet.DietType;
+import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.Assert;
 import seedu.address.testutil.PersonBuilder;
 
+//@@author yuntongzhang
+
+/**
+ * Test driver for AddDietCommand class.
+ * @author yuntongzhang
+ */
 public class AddDietCommandTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private Person patient;
+    private Nric patientNric;
+    private Set<Diet> dietSet;
+    private DietCollection dietsToAdd;
     private CommandHistory commandHistory = new CommandHistory();
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Before
+    public void setUp() {
+        patient = new PersonBuilder().build();
+        patientNric = patient.getNric();
+
+        dietSet = new HashSet<>();
+        dietSet.add(new Diet(VALID_ALLERGY, DietType.ALLERGY));
+        dietSet.add(new Diet(VALID_CULTURAL_REQUIREMENT, DietType.CULTURAL));
+        dietSet.add(new Diet(VALID_PHYSICAL_DIFFICULTY, DietType.PHYSICAL));
+        dietsToAdd = new DietCollection(dietSet);
+    }
 
     @Test
-    public void execute_successful() throws Exception {
-        Person person = new PersonBuilder().build();
-        CommandResult commandResult = new AddDietCommand(person).execute(model, commandHistory);
+    public void constructor_null_nric_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new AddDietCommand(null, dietsToAdd));
+    }
 
-        assertEquals(AddDietCommand.MESSAGE_IN_PROGRESS, commandResult.feedbackToUser);
+    @Test
+    public void constructor_null_dietsToAdd_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new AddDietCommand(patientNric, null));
+    }
+
+    @Test
+    public void execute_adddietToExisitngPatient_adddietSuccessful() throws CommandException {
+        ModelStubAcceptingAdddiet modelStub = new ModelStubAcceptingAdddiet(patient);
+        CommandResult commandResult = new AddDietCommand(patientNric, dietsToAdd)
+                .execute(modelStub, commandHistory);
+        assertEquals(commandResult.feedbackToUser, AddDietCommand.MESSAGE_SUCCESS, patientNric);
+    }
+
+    @Test
+    public void execute_adddietToNonexistentPatient_throwsCommandException() throws Exception {
+        Person patientNotInModel = new PersonBuilder().withNric(VALID_NRIC_BOB).withName(VALID_NAME_BOB).build();
+        AddDietCommand addDietCommand = new AddDietCommand(patientNotInModel.getNric(), dietsToAdd);
+        ModelStubAcceptingAdddiet modelStub = new ModelStubAcceptingAdddiet(patient);
+
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(AddDietCommand.MESSAGE_NO_SUCH_PATIENT);
+        addDietCommand.execute(modelStub, commandHistory);
+    }
+
+    @Test
+    public void equals() {
+        Person otherPatient = new PersonBuilder().withNric(VALID_NRIC_BOB).withName(VALID_NAME_BOB).build();
+        Set<Diet> otherDietSet = new HashSet<>();
+        otherDietSet.add(new Diet(VALID_ALLERGY, DietType.ALLERGY));
+        DietCollection otherDietCollection = new DietCollection(otherDietSet);
+
+        AddDietCommand adddietCommand = new AddDietCommand(patientNric, dietsToAdd);
+        AddDietCommand adddietCommandCopy = new AddDietCommand(patientNric, dietsToAdd);
+        CheckinCommand checkinCommand = new CheckinCommand(patient);
+        AddDietCommand adddietOtherDietCommand = new AddDietCommand(patientNric, otherDietCollection);
+        AddDietCommand adddietOtherPatientCommand = new AddDietCommand(otherPatient.getNric(), dietsToAdd);
+
+        // same AddDietCommand, returns true
+        assertTrue(adddietCommand.equals(adddietCommand));
+
+        // compare with a copy of the same command, returns true
+        assertTrue(adddietCommand.equals(adddietCommandCopy));
+
+        // compare with a different type of command, returns false
+        assertFalse(adddietCommand.equals(checkinCommand));
+
+        // same patientNric, different dietsToAdd, return false
+        assertFalse(adddietCommand.equals(adddietOtherDietCommand));
+
+        // different patientNric, same dietsToAdd, return false
+        assertFalse(adddietCommand.equals(adddietOtherPatientCommand));
+    }
+
+    /**
+     * A Model stub that always accepts adddiet commands for a single person.
+     */
+    private class ModelStubAcceptingAdddiet extends CommandTestUtil.ModelStub {
+        private Person patient;
+
+        public ModelStubAcceptingAdddiet(Person patient) {
+            this.patient = patient;
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            // called by {@code AddDietCommand#execute()}
+            ObservableList<Person> patients = FXCollections.observableArrayList();
+            patients.add(patient);
+
+            FilteredList<Person> filteredPatients = new FilteredList<>(patients);
+            return FXCollections.unmodifiableObservableList(filteredPatients);
+        }
+
+        @Override
+        public void updatePerson(Person personToUpdate, Person updatedPerson) {
+            requireAllNonNull(personToUpdate, updatedPerson);
+            patient = updatedPerson;
+        }
     }
 }
