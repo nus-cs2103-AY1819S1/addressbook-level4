7739f08a3c78a7196397c8878b708b6c516147ef
pengnam <seanngpengnam@gmail.com>
2018-10-14 20:55:27 +0800

    Add DependencyCommand class
    class handles logic for dependency command

---
+++ src/main/java/seedu/address/logic/commands/DependencyCommand.java
@@ -0,0 +1,64 @@
+package seedu.address.logic.commands;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
+import java.util.List;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.task.Task;
+/**
+ * Lists all the commands entered by user from the start of app launch.
+ */
+//TODO: elaborate on what is dependant and dependee
+public class DependencyCommand extends Command {
+    public static final String COMMAND_WORD = "dependency";
+    //TODO: Improve succcess message
+    public static final String MESSAGE_SUCCESS = "You have added dependency for :\n%1$s";
+    public static final String MESSAGE_ALREADY_DEPENDANT = "The dependee task is already dependent on the dependant task";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Dependency identified by hashcode of the task.\n"
+            + "Parameters: Index of task dependee, Index of task dependant\n"
+            + "Example: " + COMMAND_WORD + " 1 2";
+    private final Index dependeeIndex;
+    private final Index dependantIndex;
+    public DependencyCommand(Index dependeeIndex, Index dependantIndex) {
+        requireNonNull(dependeeIndex);
+        requireNonNull(dependantIndex);
+        this.dependeeIndex = dependeeIndex;
+        this.dependantIndex = dependantIndex;
+    }
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        List<Task> lastShownList = model.getFilteredTaskList();
+        if (dependeeIndex.getZeroBased() >= lastShownList.size() || dependantIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+        Task taskDependee = lastShownList.get(dependeeIndex.getZeroBased());
+        Task taskDependant = lastShownList.get(dependantIndex.getZeroBased());
+        if (taskDependant.getDependency().checkDependency(taskDependant)) {
+            throw new CommandException(MESSAGE_ALREADY_DEPENDANT);
+        }
+        Task updatedTask = createDependeeTask(taskDependee, taskDependant);
+
+
+        model.updateTask(taskDependee, updatedTask);
+        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        model.commitTaskManager();
+        return new CommandResult(String.format(MESSAGE_SUCCESS, updatedTask));
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else if (obj instanceof DependencyCommand) {
+            return dependeeIndex.equals(((DependencyCommand) obj).dependeeIndex);
+        } else {
+            // superclass's implementation might pass,
+            // although in this instance it's a == relationship.
+            return super.equals(obj);
+        }
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/DependencyCommandParser.java
@@ -0,0 +1,85 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DUE_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LABEL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY_VALUE;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Label;
+
+/**
+ * Parses input arguments and creates a new EditCommand object
+ */
+public class EditCommandParser implements Parser<EditCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the EditCommand
+     * and returns an EditCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EditCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_DUE_DATE,
+                    PREFIX_PRIORITY_VALUE, PREFIX_DESCRIPTION, PREFIX_LABEL);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
+        }
+
+        EditCommand.EditTaskDescriptor editTaskDescriptor = new EditCommand.EditTaskDescriptor();
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            editTaskDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+        }
+        if (argMultimap.getValue(PREFIX_DUE_DATE).isPresent()) {
+            editTaskDescriptor.setDueDate(ParserUtil.parseDueDate(argMultimap.getValue(PREFIX_DUE_DATE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_PRIORITY_VALUE).isPresent()) {
+            editTaskDescriptor.setPriorityValue(
+                    ParserUtil.parsePriorityValue(argMultimap.getValue(PREFIX_PRIORITY_VALUE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_DESCRIPTION).isPresent()) {
+            editTaskDescriptor.setDescription(
+                    ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get()));
+        }
+        parseLabelsForEdit(argMultimap.getAllValues(PREFIX_LABEL)).ifPresent(editTaskDescriptor::setLabels);
+
+        if (!editTaskDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
+        }
+
+        return new EditCommand(index, editTaskDescriptor);
+    }
+
+    /**
+     * Parses {@code Collection<String> labels} into a {@code Set<Label>} if {@code labels} is non-empty.
+     * If {@code labels} contain only one element which is an empty string, it will be parsed into a
+     * {@code Set<Label>} containing zero labels.
+     */
+    private Optional<Set<Label>> parseLabelsForEdit(Collection<String> labels) throws ParseException {
+        assert labels != null;
+
+        if (labels.isEmpty()) {
+            return Optional.empty();
+        }
+        Collection<String> labelSet = labels.size() == 1 && labels.contains("") ? Collections.emptySet() : labels;
+        return Optional.of(ParserUtil.parseLabels(labelSet));
+    }
+
+}
