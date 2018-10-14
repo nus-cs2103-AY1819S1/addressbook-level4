4be6b116628fb8d466656d4bb43b92a0d903897b
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-10-14 23:19:43 +0800

    Add new tests to CompleteCommandParser
    Since CompleteCommandParser has been updated to take in prefixed labels
    as symbols, it requires mroe regresison tests to ensure proper
    behaviour.
    
    Thus more tests has been added to take care of different combinations of
    failing and succeeding Strings to be parsed.

--- src/test/java/seedu/address/logic/parser/CompleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CompleteCommandParserTest.java
@@ -8,6 +8,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 import org.junit.Test;
 
 import seedu.address.logic.commands.CompleteCommand;
+import seedu.address.model.task.LabelMatchesKeywordPredicate;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
@@ -19,16 +20,45 @@ import seedu.address.logic.commands.CompleteCommand;
 public class CompleteCommandParserTest {
 
     private CompleteCommandParser parser = new CompleteCommandParser();
+    private final LabelMatchesKeywordPredicate FRIENDS_LABEL = new LabelMatchesKeywordPredicate("friends");
 
     @Test
     public void parse_validArgs_returnsCompleteCommand() {
+        // Arguments with no whitespaces
         assertParseSuccess(parser, "1", new CompleteCommand(INDEX_FIRST_TASK));
+
+        // Arguments with leading whitepaces
+        assertParseSuccess(parser, " 1", new CompleteCommand(INDEX_FIRST_TASK));
+        assertParseSuccess(parser, " l/friends", new CompleteCommand(FRIENDS_LABEL));
+
+        // Arguments with leading and trailing whitespaces
+        assertParseSuccess(parser, "  1  ", new CompleteCommand(INDEX_FIRST_TASK));
+        assertParseSuccess(parser, "  l/ friends  ", new CompleteCommand(FRIENDS_LABEL));
+
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
+
+        // No valid symbols
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 CompleteCommand.MESSAGE_USAGE));
+
+        // No whitespace before label
+        assertParseFailure(parser, "l/friends", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            CompleteCommand.MESSAGE_USAGE));
+
+        // Mix of Label and Index symbols
+        assertParseFailure(parser, "1 l/friends", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            CompleteCommand.MESSAGE_USAGE));
+
+        // Multiple Index symbols
+        assertParseFailure(parser, "1 2", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            CompleteCommand.MESSAGE_USAGE));
+
+        // Improper Label symbol
+        assertParseFailure(parser, " l\\friends", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            CompleteCommand.MESSAGE_USAGE));
     }
 
 }
