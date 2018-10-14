2e9357517aa79c4a81705c565a7b98f19a404149
CrimsonAng <e0176842@u.nus.edu>
2018-10-14 11:58:40 +0800

    update junit test for 'listday' and 'listweek'

--- src/test/java/seedu/address/logic/commands/ListDayCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListDayCommandTest.java
@@ -1,15 +1,11 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalTasks.AMY;
 import static seedu.address.testutil.TypicalTasks.getTypicalSchedulePlanner;
 
 import org.junit.Test;
 
-import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -17,23 +13,22 @@ import seedu.address.model.task.DateSamePredicate;
 import seedu.address.model.task.Task;
 import seedu.address.testutil.TaskBuilder;
 
-
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListDayCommand.
  */
 public class ListDayCommandTest {
-    private CommandHistory commandHistory = new CommandHistory();
-
-    //private Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
 
     @Test
     public void task_remain_afterFilter() {
         //after update the filteredtasklist with a specific date predicate, model would have that task remaining
         Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
         Task validTask = new TaskBuilder().withDate("111111").build();
+
         model.addTask(validTask);
         model.updateFilteredTaskList(new DateSamePredicate(validTask.getDate().value));
+
         assertTrue(model.hasTask(validTask));
+        assertTrue(model.getFilteredTaskList().contains(validTask));
     }
 
     @Test
@@ -43,10 +38,13 @@ public class ListDayCommandTest {
         Model modelCheck = new ModelManager();
         Task validTask = new TaskBuilder().withDate("111111").build();
         Task validTaskCheck = new TaskBuilder().withDate("121212").build();
+
         model.addTask(validTask);
         model.addTask(validTaskCheck);
         modelCheck.addTask(validTaskCheck);
         model.updateFilteredTaskList(new DateSamePredicate(validTask.getDate().value));
-        assertFalse(model.getFilteredTaskList() == modelCheck.getFilteredTaskList());
+
+        assertFalse(model.getFilteredTaskList().equals(modelCheck.getFilteredTaskList()));
+        assertFalse(model.getFilteredTaskList().contains(validTaskCheck));
     }
 }
---
+++ src/test/java/seedu/address/logic/commands/ListWeekCommandTest.java
@@ -0,0 +1,95 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalTasks.getTypicalSchedulePlanner;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.task.DateWeekSamePredicate;
+import seedu.address.model.task.Task;
+import seedu.address.testutil.TaskBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListWeekCommand.
+ */
+public class ListWeekCommandTest {
+
+    @Test
+    public void task_remain_afterFilter() {
+        //after update the filteredtasklist with a specific date predicate, model would have that task(s) from that date
+        //till the closest sunday.
+        //131018 is a saturday -> task from 131018 and 141018 will remain.
+        Model model = new ModelManager(getTypicalSchedulePlanner(), new UserPrefs());
+        Model modelCheck = new ModelManager();
+        Task validTaskSat = new TaskBuilder().withDate("131018").build();
+        Task validTaskSun = new TaskBuilder().withDate("141018").build();
+
+        List<String> dateList = new ArrayList<String>();
+        dateList.add("131018");
+        dateList.add("141018");
+
+        model.addTask(validTaskSat);
+        model.addTask(validTaskSun);
+        modelCheck.addTask(validTaskSat);
+        modelCheck.addTask(validTaskSun);
+
+        model.updateFilteredTaskList(new DateWeekSamePredicate(dateList));
+
+        assertTrue(model.getFilteredTaskList().equals(modelCheck.getFilteredTaskList()));
+        assertTrue(model.getFilteredTaskList().contains(validTaskSat));
+        assertTrue(model.getFilteredTaskList().contains(validTaskSun));
+    }
+
+    @Test
+    public void task_gone_afterFilter() {
+        //after update the filteredtasklist with a specific date predicate, model would have that task(s) from that date
+        //till the closest sunday and not other days.
+        //131018 is a saturday -> task from 131018 and 141018 will remain
+        //131018 is a saturday -> task from 081018(monday) till 121018(friday) will not remain
+        Model model = new ModelManager();
+        Model modelCheck = new ModelManager();
+        Task validTaskMon = new TaskBuilder().withDate("081018").build();
+        Task validTaskTue = new TaskBuilder().withDate("091018").build();
+        Task validTaskWed = new TaskBuilder().withDate("101018").build();
+        Task validTaskThu = new TaskBuilder().withDate("111018").build();
+        Task validTaskFri = new TaskBuilder().withDate("121018").build();
+        Task validTaskSat = new TaskBuilder().withDate("131018").build();
+        Task validTaskSun = new TaskBuilder().withDate("141018").build();
+
+        model.addTask(validTaskMon);
+        model.addTask(validTaskTue);
+        model.addTask(validTaskWed);
+        model.addTask(validTaskThu);
+        model.addTask(validTaskFri);
+        model.addTask(validTaskSat);
+        model.addTask(validTaskSun);
+        modelCheck.addTask(validTaskMon);
+        modelCheck.addTask(validTaskTue);
+        modelCheck.addTask(validTaskWed);
+        modelCheck.addTask(validTaskThu);
+        modelCheck.addTask(validTaskFri);
+
+        List<String> dateList = new ArrayList<String>();
+        dateList.add("131018");
+        dateList.add("141018");
+        model.updateFilteredTaskList(new DateWeekSamePredicate(dateList));
+
+        System.out.println(model.getFilteredTaskList());
+        assertFalse(model.getFilteredTaskList().contains(validTaskMon));
+        assertFalse(model.getFilteredTaskList().contains(validTaskTue));
+        assertFalse(model.getFilteredTaskList().contains(validTaskWed));
+        assertFalse(model.getFilteredTaskList().contains(validTaskThu));
+        assertFalse(model.getFilteredTaskList().contains(validTaskFri));
+
+        assertTrue(model.getFilteredTaskList().contains(validTaskSat));
+        assertTrue(model.getFilteredTaskList().contains(validTaskSun));
+    }
+}
