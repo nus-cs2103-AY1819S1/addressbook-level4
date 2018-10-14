9c5a99393c57f15a510c9fd9b4037be3a29dc50f
BennTay <benntayguobin@gmail.com>
2018-10-15 01:18:34 +0800

    PlanMealCommandParser.java: Parses command for PlanMealCommand.

---
+++ src/main/java/seedu/souschef/logic/parser/commandparser/PlanMealCommandParser.java
@@ -0,0 +1,56 @@
+package seedu.souschef.logic.parser.commandparser;
+
+import static seedu.souschef.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import seedu.souschef.logic.commands.PlanMealCommand;
+import seedu.souschef.logic.parser.exceptions.ParseException;
+import seedu.souschef.model.Model;
+import seedu.souschef.model.planner.Day;
+import seedu.souschef.model.planner.Meal;
+import seedu.souschef.model.recipe.Recipe;
+
+
+
+/**
+ *
+ */
+public class PlanMealCommandParser {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the PlanMealCommand
+     * and returns an PlanMealCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public PlanMealCommand parsePlan(Model mealPlannerModel, Model recipeModel, String args) throws ParseException {
+        try {
+            String[] arguments = args.split("\\s+");
+            List<Recipe> recipeList = recipeModel.getFilteredList();
+            List<Day> mealPlannerList = mealPlannerModel.getFullList().asUnmodifiableObservableList();
+            int listIndex = Integer.parseInt(arguments[0]) - 1;
+
+            if ((listIndex < 0) || listIndex >= recipeList.size()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, PlanMealCommand.MESSAGE_USAGE));
+            }
+
+            Recipe toAdd = recipeList.get(listIndex);
+            LocalDate date = LocalDate.parse(arguments[1]);
+            Day toPlan;
+            Day newDay = new Day(date);
+            if (mealPlannerList.contains(newDay)) {
+                toPlan = mealPlannerList.get(mealPlannerList.indexOf(date));
+            } else {
+                toPlan = newDay;
+                mealPlannerModel.add(toPlan);
+            }
+            Meal meal = Meal.stringToMealEnum(arguments[2]);
+
+            return new PlanMealCommand(mealPlannerModel, toAdd, toPlan, meal);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, PlanMealCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
