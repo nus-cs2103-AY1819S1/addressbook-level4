252fa367a71adc8287e4dd1f9def44a7afaf08a3
kengwoon <shannalimkengwoon@gmail.com>
2018-10-14 23:59:01 +0800

    ImportCommandTest pass + test files added

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -9,6 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.AddressBook;
 import seedu.address.model.BudgetBook;
@@ -22,6 +24,7 @@ import seedu.address.model.person.Phone;
 import seedu.address.model.person.Room;
 import seedu.address.model.person.School;
 import seedu.address.model.tag.Tag;
+import seedu.address.testutil.PersonBuilder;
 
 //@@author kengwoon
 
@@ -33,11 +36,11 @@ public class ImportCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
-    private final String PERSON_NAME = "John";
-    private final String PERSON_PHONE = "12345678";
-    private final String PERSON_EMAIL = "john@gmail.com";
-    private final String PERSON_ROOM = "C234";
-    private final String PERSON_SCHOOL = "Computing";
+    private final String VALID_PERSON_NAME = "John";
+    private final String VALID_PERSON_PHONE = "12345678";
+    private final String VALID_PERSON_EMAIL = "john@gmail.com";
+    private final String VALID_PERSON_ROOM = "C234";
+    private final String VALID_PERSON_SCHOOL = "Computing";
 
     @Test
     public void execute_nullFile_throwsFileNotFoundException() throws Exception {
@@ -45,13 +48,13 @@ public class ImportCommandTest {
 
         String expectedMessage = ImportCommand.MESSAGE_FILE_NOT_FOUND;
 
-        assertEquals(expectedMessage, importCommand.execute(model, commandHistory));
+        assertCommandFailure(importCommand, model, commandHistory, expectedMessage);
     }
 
     @Test
     public void execute_validContactsFile_success() {
         String fileName = "contactImports.xml";
-        File file = new File("./imports/" + fileName);
+        File file = new File("./src/test/data/ImportCommandTest/" + fileName);
         ImportCommand importCommand = new ImportCommand(file);
 
         String expectedMessage = String.format(ImportCommand.MESSAGE_SUCCESS, fileName);
@@ -59,8 +62,8 @@ public class ImportCommandTest {
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new BudgetBook(model.getBudgetBook()), new UserPrefs());
         Set<Tag> tags = new HashSet<>();
         tags.add(new Tag("basketball"));
-        Person newPerson = new Person(new Name(PERSON_NAME), new Phone(PERSON_PHONE), new Email(PERSON_EMAIL),
-                new Room(PERSON_ROOM), new School(PERSON_SCHOOL), tags);
+        Person newPerson = new Person(new Name(VALID_PERSON_NAME), new Phone(VALID_PERSON_PHONE), new Email(VALID_PERSON_EMAIL),
+                new Room(VALID_PERSON_ROOM), new School(VALID_PERSON_SCHOOL), tags);
         expectedModel.addPerson(newPerson);
         expectedModel.commitAddressBook();
 
@@ -70,18 +73,22 @@ public class ImportCommandTest {
     @Test
     public void execute_validCcaFile_success() {
         String fileName = "ccaImports.xml";
-        File file = new File("./imports/" + fileName);
+        File file = new File("./src/test/data/ImportCommandTest/" + fileName);
         ImportCommand importCommand = new ImportCommand(file);
 
         String expectedMessage = String.format(ImportCommand.MESSAGE_SUCCESS, fileName);
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new BudgetBook(model.getBudgetBook()), new UserPrefs());
-        Set<Tag> tags = new HashSet<>();
+        /*Set<Tag> tags = new HashSet<>();
         Person original = model.getAddressBook().getPersonList().get(0);
         tags.add((Tag)original.getTags().toArray()[0]);
         tags.add(new Tag("golf"));
         Person edited = new Person(original.getName(), original.getPhone(), original.getEmail(),
-                original.getRoom(), original.getSchool(), tags);
+                original.getRoom(), original.getSchool(), tags);*/
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        Person original = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
+        PersonBuilder personInFile = new PersonBuilder(original);
+        Person edited = personInFile.withTags("golf").build();
         expectedModel.updatePerson(original, edited);
         expectedModel.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
         expectedModel.commitAddressBook();
