a32dd15beca237f5515e7c00d5113a514e9a226b
pengnam <seanngpengnam@gmail.com>
2018-10-14 22:26:51 +0800

    Refactor checkDependency to use containDependency

--- src/main/java/seedu/address/logic/commands/DependencyCommand.java
+++ src/main/java/seedu/address/logic/commands/DependencyCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
+
 import java.util.List;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
@@ -38,7 +40,7 @@ public class DependencyCommand extends Command {
         }
         Task taskDependee = lastShownList.get(dependeeIndex.getZeroBased());
         Task taskDependant = lastShownList.get(dependantIndex.getZeroBased());
-        if (taskDependant.getDependency().checkDependency(taskDependant)) {
+        if (taskDependant.getDependency().containsDependency(taskDependant)) {
             throw new CommandException(MESSAGE_ALREADY_DEPENDANT);
         }
         Task updatedTask = createDependeeTask(taskDependee, taskDependant);
