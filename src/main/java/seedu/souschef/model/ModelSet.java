a4f2ae8ba39859bc689779a1aec0b9999d78eaa5
BennTay <benntayguobin@gmail.com>
2018-10-15 01:15:43 +0800

    ModelSet.java: Add getMealPlannerModel() method

--- src/main/java/seedu/souschef/model/ModelSet.java
+++ src/main/java/seedu/souschef/model/ModelSet.java
@@ -2,6 +2,7 @@ package seedu.souschef.model;
 
 import seedu.souschef.model.healthplan.HealthPlan;
 import seedu.souschef.model.ingredient.Ingredient;
+import seedu.souschef.model.planner.Day;
 import seedu.souschef.model.recipe.Recipe;
 import seedu.souschef.model.tag.Tag;
 
@@ -21,4 +22,7 @@ public interface ModelSet {
 
     /** Returns the model for healthplans*/
     Model<HealthPlan> getHealthPlanModel();
+
+    /** Returns the model for meal planner*/
+    Model<Day> getMealPlannerModel();
 }
