b168d50a339dfc5e1a02576aa43c50372487c275
jeffypie369 <jffersonzzz@hotmail.com>
2018-10-14 18:57:07 +0800

    Add XmlAdaptedAppointmentsListTest

---
+++ src/test/java/seedu/address/storage/XmlAdaptedAppointmentsListTest.java
@@ -0,0 +1,32 @@
+package seedu.address.storage;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.appointment.Type;
+
+public class XmlAdaptedAppointmentsListTest {
+    private XmlAdaptedAppointmentsList xmlAdaptedAppointmentsList;
+    private XmlAdaptedAppointment xmlAdaptedAppointment;
+
+    @Before
+    public void setUp() throws IllegalValueException {
+        xmlAdaptedAppointmentsList = new XmlAdaptedAppointmentsList();
+        xmlAdaptedAppointment = new XmlAdaptedAppointment(Type.SURGICAL, "Heart Bypass",
+                "12-12-2022 12:00", "Dr. Pepper");
+    }
+
+    @Test
+    public void setAppointment() {
+        List<XmlAdaptedAppointment> newList = new ArrayList<>();
+        newList.add(xmlAdaptedAppointment);
+
+        xmlAdaptedAppointmentsList.setAppt(newList);
+        assertTrue(xmlAdaptedAppointmentsList.equals(new XmlAdaptedAppointmentsList(newList)));
+    }
+}
