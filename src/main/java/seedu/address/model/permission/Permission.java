7b161bcaf63199d528e4ab80e0c442ca0c06db5a
Kok Jian Yu <kokjianyu@gmail.com>
2018-10-14 18:19:17 +0800

    Remove permission preset from Permission enum class.

--- src/main/java/seedu/address/model/permission/Permission.java
+++ src/main/java/seedu/address/model/permission/Permission.java
@@ -22,49 +22,4 @@ public enum Permission {
     ASSIGN_DEPARTMENT,
 
     ASSIGN_PERMISSION //Might need a superadmin permission to control this permission.
-    ;
-
-    /**
-     * @return a set of permission that represent the permission an admin should have.
-     */
-    public Set<Permission> getAdminPreset() {
-        Set<Permission> preset = new HashSet<>();
-        preset.add(ADD_EMPLOYEE);
-        preset.add(REMOVE_EMPLOYEE);
-        preset.add(EDIT_EMPLOYEE);
-        preset.add(VIEW_EMPLOYEE_LEAVE);
-        preset.add(APPROVE_LEAVE);
-        preset.add(CREATE_PROJECT);
-        preset.add(VIEW_PROJECT);
-        preset.add(CREATE_DEPARTMENT);
-        preset.add((ASSIGN_DEPARTMENT));
-        preset.add(ASSIGN_PERMISSION);
-        return preset;
-    }
-
-    /**
-     * @return a set of permission that represent the permission a manager should have.
-     */
-    public Set<Permission> getManagerPreset() {
-        Set<Permission> preset = new HashSet<>();
-        preset.add(ADD_EMPLOYEE);
-        preset.add(REMOVE_EMPLOYEE);
-        preset.add(EDIT_EMPLOYEE);
-        preset.add(VIEW_EMPLOYEE_LEAVE);
-        preset.add(APPROVE_LEAVE);
-        preset.add(CREATE_PROJECT);
-        preset.add(VIEW_PROJECT);
-        preset.add(CREATE_DEPARTMENT);
-        preset.add((ASSIGN_DEPARTMENT));
-        return preset;
-    }
-
-    /**
-     * @return a set of permission that represent the permission an employee should have.
-     */
-    public Set<Permission> getEmployeePreset() {
-        Set<Permission> preset = new HashSet<>();
-        return preset;
-    }
-
 }
