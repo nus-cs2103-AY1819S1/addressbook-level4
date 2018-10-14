2fdb137fb5222ede675a7716e9abfbbdfb1e5e9c
yuntongzhang <ang.unong@gmail.com>
2018-10-14 12:43:52 +0800

    Add DietCollection class and test

--- src/main/java/seedu/address/model/diet/DietCollection.java
+++ src/main/java/seedu/address/model/diet/DietCollection.java
@@ -1,4 +1,120 @@
 package seedu.address.model.diet;
 
+//@@author yuntongzhang
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Represents a Collection of dietary requirement for a patient.
+ * Guarantees: field values are validated and immutable.
+ */
 public class DietCollection {
+    private final Set<Diet> dietSet;
+
+    public DietCollection() {
+        this.dietSet = new HashSet<>();
+    }
+
+    public DietCollection(Set<Diet> dietSet) {
+        Objects.requireNonNull(dietSet);
+        this.dietSet = dietSet;
+    }
+
+    public DietCollection(Set<Diet>... dietSets) {
+        this();
+        for (Set<Diet> diets: dietSets) {
+            Objects.requireNonNull(diets);
+            this.dietSet.addAll(diets);
+        }
+    }
+
+    public List<Diet> getAllergies() {
+        List<Diet> allergies = new LinkedList<>();
+        for (Diet diet: dietSet) {
+            if (diet.isAllergy()) {
+                allergies.add(diet);
+            }
+        }
+        return allergies;
+    }
+
+    public List<Diet> getCulturalRequirements() {
+        List<Diet> culturalRequirements = new LinkedList<>();
+        for (Diet diet: dietSet) {
+            if (diet.isCulturalRequirement()) {
+                culturalRequirements.add(diet);
+            }
+        }
+        return culturalRequirements;
+    }
+
+    public List<Diet> getPhysicalDifficulties() {
+        List<Diet> physicalDifficulties = new LinkedList<>();
+        for (Diet diet: dietSet) {
+            if (diet.isPhysicalDifficulty()) {
+                physicalDifficulties.add(diet);
+            }
+        }
+        return physicalDifficulties;
+    }
+
+    /**
+     * Add more diet requirement to an existing Diet Collection.
+     * @param newDietCollection The new diet requirements to be added.
+     * @return A new copy of DietCollection which has the updated diet requirements.
+     */
+    public DietCollection addMoreDiets(DietCollection newDietCollection) {
+        Set<Diet> updatedDiets = new HashSet<>(this.dietSet);
+        updatedDiets.addAll(newDietCollection.dietSet);
+        return new DietCollection(updatedDiets);
+    }
+
+    /**
+     * Returns true if the two collections of diet requirements are the same.
+     *
+     * @param other The other Object to be compared with
+     * @return Whether this DietCollection is the same as the other Object
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof DietCollection)) {
+            return false;
+        }
+
+        DietCollection otherDietCollection = (DietCollection) other;
+        return otherDietCollection.dietSet.equals(this.dietSet);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        List<Diet> allergies = getAllergies();
+        List<Diet> culturalRequirements = getCulturalRequirements();
+        List<Diet> physicalDifficulties = getPhysicalDifficulties();
+
+        builder.append(" Allergies: ");
+        for (Diet allergy: allergies) {
+            builder.append(allergy);
+        }
+
+        builder.append(" Cultural Requirements: ");
+        for (Diet cr: culturalRequirements) {
+            builder.append(cr);
+        }
+
+        builder.append(" Physical Difficulties: ");
+        for (Diet pd: physicalDifficulties) {
+            builder.append(pd);
+        }
+
+        return builder.toString();
+    }
 }
---
+++ src/test/java/seedu/address/model/diet/DietCollectionTest.java
@@ -0,0 +1,104 @@
+package seedu.address.model.diet;
+
+//@@author yuntongzhang
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+/**
+ * Test driver for DietCollection wrapper class functionality.
+ * @author yuntongzhang
+ */
+public class DietCollectionTest {
+    private Set<Diet> dietSetOne;
+    private Set<Diet> dietSetTwo;
+    private Diet eggAllergy;
+    private Diet milkAllergy;
+    private Diet halalRequirement;
+    private Diet vegeRequirement;
+    private Diet handsDifficulty;
+
+    @Before
+    public void setUp() {
+        dietSetOne = new HashSet<>();
+        dietSetTwo = new HashSet<>();
+        eggAllergy = new Diet("Egg", DietType.ALLERGY);
+        milkAllergy = new Diet("Milk", DietType.ALLERGY);
+        halalRequirement = new Diet("Halal", DietType.CULTURAL);
+        vegeRequirement = new Diet("Vegetarian", DietType.CULTURAL);
+        handsDifficulty = new Diet("Hands cannot move", DietType.PHYSICAL);
+
+        dietSetOne.add(eggAllergy);
+        dietSetOne.add(milkAllergy);
+        dietSetOne.add(halalRequirement);
+        dietSetOne.add(handsDifficulty);
+
+        dietSetTwo.add(vegeRequirement);
+    }
+
+    @Test
+    public void constructor_nullSet_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new DietCollection(null, dietSetOne));
+    }
+
+    @Test
+    public void getAllergies() {
+        List<Diet> expectedAllergies = new LinkedList<>();
+        expectedAllergies.add(eggAllergy);
+        expectedAllergies.add(milkAllergy);
+
+        assertTrue(new DietCollection(dietSetOne).getAllergies().equals(expectedAllergies));
+    }
+
+    @Test
+    public void getCulturalRequirements() {
+        List<Diet> expectedCulturalRequirement = new LinkedList<>();
+        expectedCulturalRequirement.add(halalRequirement);
+
+        assertTrue(new DietCollection(dietSetOne).getCulturalRequirements().equals(expectedCulturalRequirement));
+    }
+
+    @Test
+    public void getPhysicalDifficulty() {
+        List<Diet> expectedPhysicalDifficulties = new LinkedList<>();
+        expectedPhysicalDifficulties.add(handsDifficulty);
+
+        assertTrue(new DietCollection(dietSetOne).getPhysicalDifficulties().equals(expectedPhysicalDifficulties));
+    }
+
+    @Test
+    public void addMoreDiets() {
+        Set<Diet> dietSet = new HashSet<>(dietSetOne);
+        dietSet.addAll(dietSetTwo);
+        DietCollection expectedCollection = new DietCollection(dietSet);
+
+        DietCollection actualCollection = new DietCollection(dietSetOne).addMoreDiets(new DietCollection(dietSetTwo));
+        assertTrue(actualCollection.equals(expectedCollection));
+    }
+
+    @Test
+    public void equals() {
+        DietCollection collectionOne = new DietCollection(dietSetOne);
+        DietCollection collectionOneCopy = new DietCollection(dietSetOne);
+        DietCollection collectionTwo = new DietCollection(dietSetTwo);
+
+        // DietCollection equals to itself
+        assertTrue(collectionOne.equals(collectionOne));
+
+        // DietCollection equals to a copy with the same content
+        assertTrue(collectionOne.equals(collectionOneCopy));
+
+        // DietCollection does not equal to another collection with different content
+        assertFalse(collectionOne.equals(collectionTwo));
+    }
+}
