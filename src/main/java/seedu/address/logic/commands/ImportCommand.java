46c582090089bec3330c4a32184d31644af6cea2
chantca95 <chantca95@gmail.com>
2018-10-14 21:04:33 +0800

    delete blank lines in ImportCommand.java

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -2,9 +2,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import java.util.ArrayList;
-
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
@@ -23,8 +21,7 @@ public class ImportCommand extends Command {
     public static final String MESSAGE_USAGE = "Please use the file browser to choose a csv file.";
     public static final String MESSAGE_INVALID_FIELD = " Contacts with invalid fields not imported.";
     public static final String MESSAGE_NAMELESS_CONTACT = " Contacts without at least a name field not imported.";
-
-
+    
     private final ArrayList<Person> personsToAdd;
     private final boolean hasContactWithInvalidField;
     private final boolean hasContactWithoutName;
