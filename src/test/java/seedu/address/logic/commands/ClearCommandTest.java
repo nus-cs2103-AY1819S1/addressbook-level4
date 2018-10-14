87c70843edeef71acb49c73b8efbee6025d17014
kengwoon <shannalimkengwoon@gmail.com>
2018-10-15 00:15:43 +0800

    Checkstyle changes

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -21,7 +21,6 @@ public class ClearCommandTest {
 
     private CommandHistory commandHistory = new CommandHistory();
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private ModelManager expectedModel = new ModelManager(model.getAddressBook(), new BudgetBook(), new UserPrefs());
 
     @Test
     public void execute_emptyAddressBook_success() {
@@ -29,27 +28,30 @@ public class ClearCommandTest {
         Model expectedModelEmpty = new ModelManager();
         List<String> target = new ArrayList<>();
         target.add("basketball");
+        ClearCommand clearCommand = new ClearCommand(target);
 
         String expectedMessage = String.format(ClearCommand.MESSAGE_CLEAR_NOTHING, '[' + target.get(0) + ']');
 
-        assertCommandSuccess(new ClearCommand(target), modelEmpty, commandHistory, expectedMessage, expectedModelEmpty);
+        assertCommandSuccess(clearCommand, modelEmpty, commandHistory, expectedMessage, expectedModelEmpty);
     }
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
         List<String> target = new ArrayList<>();
         target.add("track");
+        ClearCommand clearCommand = new ClearCommand(target);
 
         Person p = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         List<Person> persons = new ArrayList<>();
         persons.add(p);
 
+        Model expectedModel = new ModelManager(model.getAddressBook(), new BudgetBook(), new UserPrefs());
         expectedModel.clearMultiplePersons(persons);
         expectedModel.commitAddressBook();
 
         String expectedMessage = String.format(ClearCommand.MESSAGE_CLEAR_SPECIFIC_SUCCESS, '[' + target.get(0) + ']');
 
-        assertCommandSuccess(new ClearCommand(target), model, commandHistory, expectedMessage, expectedModel);
+        assertCommandSuccess(clearCommand, model, commandHistory, expectedMessage, expectedModel);
     }
 
     @Test
@@ -63,6 +65,8 @@ public class ClearCommandTest {
         Person p = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         List<Person> persons = new ArrayList<>();
         persons.add(p);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new BudgetBook(), new UserPrefs());
         expectedModel.clearMultiplePersons(persons);
         expectedModel.commitAddressBook();
 
