5449e9039500bc8ea522a80236a2ee6c13ff93e0
iMarbles <amabel_yeo@hotmail.com>
2018-10-15 23:52:00 +0800

    SampleDataUtil.java: Fix CheckStyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -29,44 +29,50 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[]{
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"),
-                        new Email("alexyeoh@example.com"), new Address("Blk 30 Geylang Street 29, #06-40"),
-                        getTagSet("friends")),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"),
-                        new Email("berniceyu@example.com"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                        getTagSet("neighbours")),
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                        getTagSet("family")),
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new Address("Blk 47 Tampines Street 20, #17-35"),
-                        getTagSet("classmates")),
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                        new Address("Blk 45 Aljunied Street 85, #11-31"),
-                        getTagSet("colleagues"))
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"),
+                    new Email("alexyeoh@example.com"),
+                    new Address("Blk 30 Geylang Street 29, #06-40"),
+                    getTagSet("friends")),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"),
+                    new Email("berniceyu@example.com"),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    getTagSet("colleagues", "friends")),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
+                    new Email("charlotte@example.com"),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    getTagSet("neighbours")),
+            new Person(new Name("David Li"), new Phone("91031282"),
+                    new Email("lidavid@example.com"),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    getTagSet("family")),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"),
+                         new Email("irfan@example.com"),
+                         new Address("Blk 47 Tampines Street 20, #17-35"),
+                         getTagSet("classmates")),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"),
+                    new Email("royb@example.com"),
+                    new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    getTagSet("colleagues"))
         };
     }
 
     public static Event[] getSampleEvents() {
         return new Event[]{
-                new Event(new seedu.address.model.event.Name("Blood Donation Drive 2018"),
-                        new Location("750E Chai Chee Road"), new Date("02-10-2018"), new Date("05-10-2018"),
-                        new Time("11:30"), new Time("17:30"), new Description("Donation drive for blood."),
-                        getTagSet("Public", "Donation")),
-                new Event(new seedu.address.model.event.Name("Youth Humanitarian Challenge"),
-                        new Location("29 Havelock Road"), new Date("28-09-2018"), new Date("28-09-2018"),
-                        new Time("10:00"), new Time("14:00"), new Description("To engage youths in humanitarianism."),
-                        getTagSet("Competition")),
+            new Event(new seedu.address.model.event.Name("Blood Donation Drive 2018"),
+                    new Location("750E Chai Chee Road"), new Date("02-10-2018"), new Date("05-10-2018"),
+                    new Time("11:30"), new Time("17:30"), new Description("Donation drive for blood."),
+                    getTagSet("Public", "Donation")),
+            new Event(new seedu.address.model.event.Name("Youth Humanitarian Challenge"),
+                    new Location("29 Havelock Road"), new Date("28-09-2018"), new Date("28-09-2018"),
+                    new Time("10:00"), new Time("14:00"), new Description("To engage youths in humanitarianism."),
+                    getTagSet("Competition")),
         };
     }
 
     public static Record[] getSampleRecord() {
         return new Record[]{
-                new Record(new EventId(1), new PersonId(1), new Hour("1"), new Remark("Emcee")),
-                new Record(new EventId(2), new PersonId(1), new Hour("1"), new Remark("Delivery Man"))
+            new Record(new EventId(1), new PersonId(1), new Hour("1"), new Remark("Emcee")),
+            new Record(new EventId(2), new PersonId(1), new Hour("1"), new Remark("Delivery Man"))
         };
     }
 
