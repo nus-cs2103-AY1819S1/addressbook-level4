5667e0b6ab011a50f52b6028703ff9a9098a193b
Elliot Tan <elliottan@gmail.com>
2018-10-15 12:09:29 +0800

    Add Model classes for LeaveApplication. Includes: LeaveApplication, LeaveId, LeaveStatus, StatusEnum, Description.

---
+++ src/main/java/seedu/address/model/leaveapplication/StatusEnum.java
@@ -0,0 +1,10 @@
+package seedu.address.model.leaveapplication;
+
+/**
+ * Represents an enumeration of possible Status values.
+ */
+public class StatusEnum {
+    public enum Status {
+        PENDING, APPROVED, REJECTED, CANCELLED;
+    }
+}
