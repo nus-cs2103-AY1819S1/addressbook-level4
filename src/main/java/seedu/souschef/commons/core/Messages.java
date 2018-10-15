e66be307e049e83a1c156d1e4e269d08060fb87a
barkhyehyeon <barkhyehyeon@gmail.com>
2018-10-15 14:20:03 +0800

    Add mistakenly deleted massage

--- src/main/java/seedu/souschef/commons/core/Messages.java
+++ src/main/java/seedu/souschef/commons/core/Messages.java
@@ -9,5 +9,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_RECIPE_DISPLAYED_INDEX = "The recipe index provided is invalid";
     public static final String MESSAGE_LISTED_OVERVIEW = "%1$d %2$ss listed!";
+    public static final String MESSAGE_DUPLICATE = "This %1$s already exists.";
     public static final String MESSAGE_NO_ELEMENT = "There is no matching result";
 }
