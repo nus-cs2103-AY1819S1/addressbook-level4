344306482ee7eeb5dd602d2d8ab52d9a3901cf54
pengnam <seanngpengnam@gmail.com>
2018-10-15 13:32:47 +0800

    Add cyclicCheck in DependencyGraph
    checks cycles in the directed graph using dfs

--- src/main/java/seedu/address/model/DependencyGraph.java
+++ src/main/java/seedu/address/model/DependencyGraph.java
@@ -19,4 +19,48 @@ public class DependencyGraph {
         }
     }
 
+    /**
+     * Checks if there will be a cycle in the graph
+     * @param task
+     */
+    public String checkCyclic() {
+        Set<String> visited = new HashSet<>();
+        Set<String> stack = new HashSet<>();
+        for (String node: adjacencyList.keySet()) {
+            if (!(visited.contains(node))){
+                String violation = depthFirstSearch(node, visited, stack, adjacencyList);
+                if (violation != null) {
+                    return violation;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Performs dfs on graph to check for cycles
+     * @param node next node to check
+     * @param visited set of visited nodes
+     * @param stack set of nodes in current path. Used to check cycles
+     * @param adjacencyList
+     * @return String hash of the violating node
+     */
+    private String depthFirstSearch(String node, Set<String> visited, Set<String> stack, Map<String, Set<String>> adjacencyList) {
+        if (stack.contains(node)) {
+            return node;
+        }
+        visited.add(node);
+        stack.add(node);
+        Set<String> edges = adjacencyList.getOrDefault(node, new HashSet<String>());
+        for (String nextNode: edges) {
+            String violation = depthFirstSearch(nextNode, visited, stack, adjacencyList);
+            if (violation != null) {
+                return violation;
+            }
+        }
+        stack.remove(node);
+        return null;
+    }
+    //TODO: Add toposort function
+
 }
