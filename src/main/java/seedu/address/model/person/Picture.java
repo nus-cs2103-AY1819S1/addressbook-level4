31bcd3aea8e76101eacd0830fd698079c32a23b8
denzelchung <denzelchung@hotmail.com>
2018-10-15 18:45:34 +0800

    Add author tag.

--- src/main/java/seedu/address/model/person/Picture.java
+++ src/main/java/seedu/address/model/person/Picture.java
@@ -3,6 +3,7 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+//@@author denzelchung
 /**
  * Represents a Person's picture in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidPicture(String)}
--- src/test/java/seedu/address/model/person/PictureTest.java
+++ src/test/java/seedu/address/model/person/PictureTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import seedu.address.testutil.Assert;
 
+//@@author denzelchung
 public class PictureTest {
 
     @Test
