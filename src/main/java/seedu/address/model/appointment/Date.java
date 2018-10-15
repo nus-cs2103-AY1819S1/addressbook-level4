cbea33a6d1fbc248100a48f656afe768ee88bb66
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-15 12:16:01 +0800

    Remove if-else from isLeapYear in Date class.

--- src/main/java/seedu/address/model/appointment/Date.java
+++ src/main/java/seedu/address/model/appointment/Date.java
@@ -82,15 +82,16 @@ public class Date {
      */
     public static boolean isLeapYear(int year) {
         //Solution below adapted from https://en.wikipedia.org/wiki/Leap_year
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 100 != 0) {
-            return true;
-        } else if (year % 400 != 0) {
-            return false;
-        } else {
+        if (year % 400 == 0) {
             return true;
         }
+        if (year % 100 == 0) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            return true;
+        }
+        return false;
     }
 
     /**
