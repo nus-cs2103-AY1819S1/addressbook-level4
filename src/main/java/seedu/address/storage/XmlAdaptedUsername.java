ce2666fd945b283dcb48b23e021735a1445f6fee
Jason Chong <jasontjakra@gmail.com>
2018-10-15 16:22:51 +0800

    Fix typo in comments of XmlAdaptedUsername

--- src/main/java/seedu/address/storage/XmlAdaptedUsername.java
+++ src/main/java/seedu/address/storage/XmlAdaptedUsername.java
@@ -37,7 +37,7 @@ public class XmlAdaptedUsername {
     }
 
     /**
-     * Converts this jaxb-friendly adapted tag object into the model's Tag object.
+     * Converts this jaxb-friendly adapted Username object into the model's Username object.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted expense
      */
