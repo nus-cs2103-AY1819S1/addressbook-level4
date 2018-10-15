0266f7c0648b0a842bb20a7e4a711795d6482417
rtjl <reubentjl@gmail.com>
2018-10-15 21:56:57 +0800

    Fix style check for test

--- src/test/java/seedu/address/logic/commands/AddOnCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddOnCommandTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
