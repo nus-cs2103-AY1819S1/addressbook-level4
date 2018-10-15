4a986443c73331a8c96f4c61405b258ae9b03079
David Choo <davidchoo16@gmail.com>
2018-10-15 16:45:28 +0800

    Change validation for Track
    As Track is translated from Tag, the original validation method is the isValidTag method.
    The method was adapted to a isValidMp3 stub in PR #42.
    
    Now the Track constructors is used as the entry point of validation.
    The constructors validates for file inexistence and javafx media support
    using the AppUtil.checkArgument method, so it throws IllegalArgumentException.
    
    Todo: move validation for javafx media support to PlayableTrack

--- src/main/java/seedu/jxmusic/model/Track.java
+++ src/main/java/seedu/jxmusic/model/Track.java
@@ -2,20 +2,25 @@ package seedu.jxmusic.model;
 
 import java.io.File;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+
 import seedu.jxmusic.commons.util.AppUtil;
 import seedu.jxmusic.commons.util.CollectionUtil;
 
 /**
  * Represents a track in JxMusic
- * Guarantees: immutable; file is valid as declared in {@link #isValidMp3(File)}
+ * Guarantees: immutable; file is supported by javafx media as declared in {@link #isSupported(File)}
  */
 public class Track {
-    public static final String MESSAGE_NAME_CONSTRAINTS =
-            "Track file should be playable by javafx media";
-    private final File file;
-    // fileName not appended with MP3_EXTENSION
-    private final String fileName;
+    public static final String MESSAGE_FILE_NOT_EXIST =
+            "Track file does not exist";
+    public static final String MESSAGE_FILE_NOT_SUPPORTED =
+            "Track file not supported by javafx media";
     public static final String MP3_EXTENSION = ".mp3";
+    private final File file;
+    // fileNameWithoutExtension not appended with MP3_EXTENSION
+    private final String fileNameWithoutExtension;
 
     /**
      * Constructs a {@code Track}.
@@ -27,22 +32,24 @@ public class Track {
         String fileNameDotMp3;
         if (trackFileName.toString().endsWith(MP3_EXTENSION)) {
             fileNameDotMp3 = trackFileName.toString();
-            fileName = removeMp3Extension(fileNameDotMp3);
+            fileNameWithoutExtension = removeMp3Extension(fileNameDotMp3);
         } else {
-            fileName = trackFileName.toString();
-            fileNameDotMp3 = fileName + MP3_EXTENSION;
+            fileNameWithoutExtension = trackFileName.toString();
+            fileNameDotMp3 = fileNameWithoutExtension + MP3_EXTENSION;
         }
         File file = new File(Library.LIBRARYDIR + fileNameDotMp3);
-        AppUtil.checkArgument(isValidMp3(file), MESSAGE_NAME_CONSTRAINTS);
+        AppUtil.checkArgument(file.exists(), MESSAGE_FILE_NOT_EXIST);
+        AppUtil.checkArgument(isSupported(file), MESSAGE_FILE_NOT_SUPPORTED);
         this.file = file;
     }
 
     public Track(File file) {
         CollectionUtil.requireAllNonNull(file);
-        AppUtil.checkArgument(isValidMp3(file), MESSAGE_NAME_CONSTRAINTS);
+        AppUtil.checkArgument(file.exists(), MESSAGE_FILE_NOT_EXIST);
+        AppUtil.checkArgument(isSupported(file), MESSAGE_FILE_NOT_SUPPORTED);
         this.file = file;
         String fileNameDotMp3 = file.getName();
-        fileName = removeMp3Extension(fileNameDotMp3);
+        fileNameWithoutExtension = removeMp3Extension(fileNameDotMp3);
     }
 
     public File getFile() {
@@ -53,13 +60,26 @@ public class Track {
         return file.getName();
     }
 
+    public String getFileNameWithoutExtension() {
+        return fileNameWithoutExtension;
+    }
+
     /**
-     * Checks for valid mp3 file
-     * @param file the mp3 file to check
-     * @return true if mp3 file is supported by javafx media, false otherwise
+     * Checks for javafx media support
+     * @param file the file to check
+     * @return true if file is supported by javafx media, false otherwise
      */
-    private static boolean isValidMp3(File file) {
-        // todo implement method to check if mp3 file is valid so that it's playable with javafx media
+    private static boolean isSupported(File file) {
+        // todo check by using PlayableTrack constructor in try block
+        AppUtil.checkArgument(file.exists(), MESSAGE_FILE_NOT_EXIST);
+        try {
+            Media media = new Media(file.toURI().toString());
+        } catch (MediaException e) {
+            // if (e.getType() == MediaException.Type.MEDIA_UNSUPPORTED) {
+            //     return false;
+            // }
+            return false;
+        }
         return true;
     }
 
@@ -71,18 +91,18 @@ public class Track {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Track // instanceof handles nulls
-                && fileName.equals(((Track) other).fileName)); // state check
+                && fileNameWithoutExtension.equals(((Track) other).fileNameWithoutExtension)); // state check
     }
 
     @Override
     public int hashCode() {
-        return fileName.hashCode();
+        return fileNameWithoutExtension.hashCode();
     }
 
     /**
      * Format state as text for viewing.
      */
     public String toString() {
-        return '[' + fileName + ']';
+        return '[' + fileNameWithoutExtension + ']';
     }
 }
