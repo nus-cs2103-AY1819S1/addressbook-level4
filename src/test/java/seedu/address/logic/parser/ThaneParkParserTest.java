9acc08920b3d343b6b8eab262fa1f2f6551d06c3
leong eng sea <leongengsea@gmail.com>
2018-10-16 05:56:21 +0800

    Update ThaneParkParserTest.java.

--- src/test/java/seedu/address/logic/parser/ThaneParkParserTest.java
+++ src/test/java/seedu/address/logic/parser/ThaneParkParserTest.java
@@ -17,8 +17,8 @@ import org.junit.rules.ExpectedException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.UpdateCommand;
+import seedu.address.logic.commands.UpdateCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
@@ -64,9 +64,9 @@ public class ThaneParkParserTest {
     public void parseCommand_edit() throws Exception {
         Ride ride = new RideBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(ride).build();
-        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
+        UpdateCommand command = (UpdateCommand) parser.parseCommand(UpdateCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
+        assertEquals(new UpdateCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
     @Test
