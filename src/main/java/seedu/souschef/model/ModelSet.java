7c2af46828bd468e93d4141832e3a7d281f7b8ff
BennTay <benntayguobin@gmail.com>
2018-10-15 21:46:54 +0800

    Fix import order

--- src/main/java/seedu/souschef/model/ModelSet.java
+++ src/main/java/seedu/souschef/model/ModelSet.java
@@ -1,8 +1,8 @@
 package seedu.souschef.model;
 
 import seedu.souschef.model.healthplan.HealthPlan;
-import seedu.souschef.model.planner.Day;
 import seedu.souschef.model.ingredient.Ingredient;
+import seedu.souschef.model.planner.Day;
 import seedu.souschef.model.recipe.Recipe;
 import seedu.souschef.model.tag.Tag;
 /**
