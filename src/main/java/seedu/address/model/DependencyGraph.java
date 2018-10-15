bacc7dc8738f279f483ba071325c14395613148c
pengnam <seanngpengnam@gmail.com>
2018-10-15 22:23:25 +0800

    Modify dfs in DependencyGraph to record visited nodes
    changed function signature and implementation so that dfs function can be used for toposort function

--- src/main/java/seedu/address/model/DependencyGraph.java
+++ src/main/java/seedu/address/model/DependencyGraph.java
@@ -1,5 +1,6 @@
 package seedu.address.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 import seedu.address.model.task.Task;
+import seedu.address.model.task.exceptions.GraphCycleException;
 
 /**
  * DependencyGraph models the dependencies of the tasks so that checks such as cyclic dependencies can be performed and
@@ -37,11 +39,12 @@ public class DependencyGraph {
      * Checks if there will be a cycle in the graph
      */
     public boolean checkPresenceOfCycle() {
-        Set<String> visited = new HashSet<>();
+        Set<String> unvisited = new HashSet<>();
         Set<String> stack = new HashSet<>();
+        List<String> visited = new ArrayList<>();
         for (String node: adjacencyList.keySet()) {
-            if (!(visited.contains(node))){
-                if (depthFirstSearch(node, visited, stack, adjacencyList)) {
+            if (unvisited.contains(node)) {
+                if (depthFirstSearch(node, unvisited, visited, stack, adjacencyList)) {
                     return true;
                 }
             }
@@ -52,21 +55,24 @@ public class DependencyGraph {
     /**
      * Performs dfs on graph to check for cycles
      * @param node next node to check
-     * @param visited set of visited nodes
+     * @param unvisited set of unvisited nodes
      * @param stack set of nodes in current path. Used to check cycles
      * @param adjacencyList
      * @return true is there is a cycle
      */
-    private boolean depthFirstSearch(String node, Set<String> visited, Set<String> stack, Map<String,
-            Set<String>> adjacencyList) {
+    private boolean depthFirstSearch(String node, Set<String> unvisited, List<String> visited, Set<String> stack,
+                                     Map<String, Set<String>> adjacencyList) {
         if (stack.contains(node)) {
             return true;
         }
-        visited.add(node);
+        if (unvisited.contains(node)){
+            visited.add(node);
+        }
+        unvisited.remove(node);
         stack.add(node);
         Set<String> edges = adjacencyList.getOrDefault(node, new HashSet<String>());
         for (String nextNode: edges) {
-            if (depthFirstSearch(nextNode, visited, stack, adjacencyList)) {
+            if (depthFirstSearch(nextNode, unvisited, stack, adjacencyList)) {
                 return true;
             }
         }
