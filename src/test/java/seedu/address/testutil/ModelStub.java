2a95fe737bcb45c3c74077239b0d958f5538cc55
Derek-Hardy <derekzxj@outlook.com>
2018-10-16 20:06:35 +0800

    fix wrong import path in ModelStub

--- src/test/java/seedu/address/testutil/ModelStub.java
+++ src/test/java/seedu/address/testutil/ModelStub.java
@@ -8,7 +8,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.group.Group;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.PersonPropertyComparator;
+import seedu.address.model.person.util.PersonPropertyComparator;
 
 
 /**
