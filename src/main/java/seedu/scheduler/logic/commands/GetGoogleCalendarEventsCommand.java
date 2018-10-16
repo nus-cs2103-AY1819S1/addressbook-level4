c23e5a9a5c6719d2b674f576758a49156e443115
YezhongZ <39483787+YezhongZ@users.noreply.github.com>
2018-10-17 00:36:01 +0800

    Establish connection to Google

--- src/main/java/seedu/scheduler/logic/commands/GetGoogleCalendarEventsCommand.java
+++ src/main/java/seedu/scheduler/logic/commands/GetGoogleCalendarEventsCommand.java
@@ -1,19 +1,29 @@
 package seedu.scheduler.logic.commands;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.calendar.CalendarScopes;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+
 import seedu.scheduler.logic.CommandHistory;
 import seedu.scheduler.logic.commands.exceptions.CommandException;
 import seedu.scheduler.model.Model;
+
 
 /**
  * Get events from google calendar.
@@ -41,18 +51,58 @@ public class GetGoogleCalendarEventsCommand extends Command {
         return new CommandResult(MESSAGE_GGEVENTS_SUCCESS);
     }
 
+    private Calendar getCalendar() {
+        NetHttpTransport httpTransport = getNetHttpTransport();
+
+        // Build Google calender service object.
+        Calendar service = null;
+        try {
+            service = new Calendar.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
+                    .setApplicationName("iScheduler Xs Max")
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return service;
+    }
+
     private NetHttpTransport getNetHttpTransport() {
         // Build a new authorized API client service.
-        NetHttpTransport HTTP_TRANSPORT = null;
+        NetHttpTransport httpTransport = null;
         try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         } catch (GeneralSecurityException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return HTTP_TRANSPORT;
+        return httpTransport;
     }
 
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param httpTransport The network HTTP Transport.
+     *
+     * @return An authorized Credential object.
+     *
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport httpTransport) throws IOException {
+        GoogleClientSecrets clientSecrets = getGoogleClientSecrets();
 
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+                .setAccessType("offline")
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+
+    private static GoogleClientSecrets getGoogleClientSecrets() throws IOException {
+        // Load client secrets.
+        InputStream in = GetGoogleCalendarEventsCommand.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    }
 }
