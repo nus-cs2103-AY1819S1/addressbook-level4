2002591d825b0c255f69dd81f8323a3dfcdaa5cc
remo5000 <vigneshshankar@gmail.com>
2018-10-16 16:47:13 +0800

    Add alternate constructor for ListCommand

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,8 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
 
+import java.util.function.Predicate;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.task.Task;
 
 /**
  * Lists all tasks in the task manager to the user.
@@ -14,6 +16,25 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all tasks";
+
+    private final Predicate<Task> predicate;
+
+    public enum ListFilter {
+            DUE_TODAY;
+    }
+
+    public ListCommand() { this.predicate = PREDICATE_SHOW_ALL_TASKS; }
+
+    public ListCommand(ListFilter listFilter) {
+        switch (listFilter) {
+            case DUE_TODAY:
+                this.predicate = PREDICATE_SHOW_ALL_TASKS;
+                break;
+            default:
+                this.predicate = null;
+                break;
+        }
+    }
 
 
     @Override
