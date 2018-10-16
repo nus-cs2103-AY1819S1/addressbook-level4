4049ea397cf45391d0f4069f39a9b09acd687184
NEW-DGU39GCO4DB\teoji <e0175930@u.nus.edu>
2018-10-17 01:02:46 +0800

    Fix MainWindow.java

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -126,7 +126,6 @@ public class MainWindow extends UiPart<Stage> {
     void fillInnerParts() {
         browserPanel = new BrowserPanel();
         browserPlaceholder.getChildren().add(browserPanel.getRoot());
-        
         rideListPanel = new RideListPanel(logic.getFilteredRideList());
         personListPanelPlaceholder.getChildren().add(rideListPanel.getRoot());
 
