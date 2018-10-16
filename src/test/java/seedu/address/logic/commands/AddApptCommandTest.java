d11111b40b75711b67641748d1a8094350903fbb
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-16 11:28:27 +0800

    Add AddApptCommand test.

---
+++ src/test/java/seedu/address/logic/commands/AddApptCommandTest.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalPersons.AMY_APPT;
+import static seedu.address.testutil.TypicalPersons.CARL_APPT;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.analytics.Analytics;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.testutil.AppointmentBuilder;
+
+public class AddApptCommandTest {
+    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private CommandHistory commandHistory = new CommandHistory();
+    private Analytics analytics = new Analytics();
+
+    @Test
+    public void constructor_nullAppointment_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AddApptCommand(null);
+    }
+
+    @Test
+    public void equals() {
+        Appointment amy = new AppointmentBuilder(AMY_APPT).build();
+        Appointment carl = new AppointmentBuilder(CARL_APPT).build();
+        AddApptCommand addApptAmyCommand = new AddApptCommand(amy);
+        AddApptCommand addApptCarlCommand = new AddApptCommand(carl);
+
+        // same object -> returns true
+        assertTrue(addApptAmyCommand.equals(addApptAmyCommand));
+
+        // same values -> returns false because clash
+        AddApptCommand addApptAmyCommandCopy = new AddApptCommand(amy);
+        assertTrue(addApptAmyCommand.equals(addApptAmyCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addApptAmyCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addApptAmyCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(addApptAmyCommand.equals(addApptCarlCommand));
+    }
+}
