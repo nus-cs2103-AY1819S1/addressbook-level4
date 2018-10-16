3f706553b4233da6f12fabe628098fdc6b7b3cab
NEW-DGU39GCO4DB\teoji <e0175930@u.nus.edu>
2018-10-17 00:55:11 +0800

    Fix MainWindow.java

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -126,14 +126,9 @@ public class MainWindow extends UiPart<Stage> {
     void fillInnerParts() {
         browserPanel = new BrowserPanel();
         browserPlaceholder.getChildren().add(browserPanel.getRoot());
-
-<<<<<<< HEAD
-        personListPanel = new PersonListPanel(logic.getFilteredRideList());
-        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
-=======
-        rideListPanel = new RideListPanel(logic.getFilteredPersonList());
+        
+        rideListPanel = new RideListPanel(logic.getFilteredRideList());
         personListPanelPlaceholder.getChildren().add(rideListPanel.getRoot());
->>>>>>> 12db7ead8b33d4491556cc0158e55fbc95bb99b3
 
         ResultDisplay resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
