10eed7dc8e6748b36b7ab4e565b980c738d8cfab
gingivitiss <39242221+gingivitiss@users.noreply.github.com>
2018-10-16 11:57:50 +0800

    Fix code style.

--- src/main/java/seedu/address/model/appointment/Date.java
+++ src/main/java/seedu/address/model/appointment/Date.java
@@ -88,16 +88,7 @@ public class Date {
      * @param year The year to check if leap year.
      */
     public static boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
-            return false;
-        }
-        if (year % 400 == 0) {
-            return true;
-        }
-        if (year % 100 == 0) {
-            return false;
-        }
-        return true;
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 
     /**
