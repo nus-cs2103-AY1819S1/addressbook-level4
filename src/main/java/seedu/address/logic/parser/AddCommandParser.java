47aca4892fc91d855538a1e32940aa01c73868d4
Ong Jun Jie <>
2018-10-16 15:45:58 +0800

    correct checkstyle

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -35,7 +35,7 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                                            PREFIX_ADDRESS,PREFIX_EDUCATION, PREFIX_GRADES, PREFIX_TAG);
+                                            PREFIX_ADDRESS, PREFIX_EDUCATION, PREFIX_GRADES, PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_GRADES, PREFIX_EDUCATION)
                 || !argMultimap.getPreamble().isEmpty()) {
