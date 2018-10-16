1ace26b6109faf645353107a7b3c7d86f8bfc8af
leong eng sea <leongengsea@gmail.com>
2018-10-16 12:59:35 +0800

    Fix stylecheck issues.

--- src/main/java/seedu/address/logic/parser/ThaneParkParser.java
+++ src/main/java/seedu/address/logic/parser/ThaneParkParser.java
@@ -10,7 +10,6 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.UpdateCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.commands.FindCommand;
@@ -20,6 +19,7 @@ import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.SuggestCommand;
 import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.UpdateCommand;
 import seedu.address.logic.commands.ViewAllCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
