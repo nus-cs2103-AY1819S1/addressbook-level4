49d007854a29e4bbe83a916cd8638de0890b33cd
YezhongZ <39483787+YezhongZ@users.noreply.github.com>
2018-10-16 12:23:53 +0800

    Fix style

--- src/test/java/seedu/scheduler/logic/parser/SchedulerParserTest.java
+++ src/test/java/seedu/scheduler/logic/parser/SchedulerParserTest.java
@@ -78,7 +78,7 @@ public class SchedulerParserTest {
 
     @Test
     public void parseCommand_find() throws Exception {
-        List <String> keywords = Arrays.asList("foo", "bar", "baz");
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
         FindCommand command = (FindCommand) parser.parseCommand(
                 FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
         assertEquals(new FindCommand(new EventNameContainsKeywordsPredicate(keywords)), command);
