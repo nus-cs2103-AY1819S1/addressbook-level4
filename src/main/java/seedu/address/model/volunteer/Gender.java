0e6de463b5ba7436cf2643f449e4794f7593f0b2
Afiq Lattif <muhdafiqlattif@gmail.com>
2018-10-16 18:00:13 +0800

    model/volunteer: Added Gender and Birthday classes for volunteer

---
+++ src/main/java/seedu/address/model/volunteer/Gender.java
@@ -0,0 +1,58 @@
+package seedu.address.model.volunteer;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Volunteer's gender in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidGender(String)}
+ */
+public class Gender {
+
+    public static final String MESSAGE_GENDER_CONSTRAINTS =
+            "Gender should only contain 'male' or 'female', and it should not be blank";
+
+    /*
+     * Gender must only be male or female
+     */
+    public static final String GENDER_MALE_VALIDATION_REGEX = "male";
+    public static final String GENDER_FEMALE_VALIDATION_REGEX = "female";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Gender}.
+     *
+     * @param gender A valid gender.
+     */
+    public Gender(String gender) {
+        requireNonNull(gender);
+        checkArgument(isValidGender(gender), MESSAGE_GENDER_CONSTRAINTS);
+        value = gender;
+    }
+
+    /**
+     * Returns true if a given string is a valid gender.
+     */
+    public static boolean isValidGender(String test) {
+        return test.matches(GENDER_MALE_VALIDATION_REGEX)
+                || test.matches(GENDER_FEMALE_VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Gender // instanceof handles nulls
+                && value.equals(((Gender) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+
+}
--- src/main/java/seedu/address/model/volunteer/Volunteer.java
+++ src/main/java/seedu/address/model/volunteer/Volunteer.java
@@ -20,6 +20,8 @@ public class Volunteer {
     // Identity fields
     private final VolunteerId volunteerId;
     private final Name name;
+    private final Gender gender;
+    private final Birthday birthday;
     private final Phone phone;
     private final Email email;
 
@@ -30,13 +32,15 @@ public class Volunteer {
     /**
      * Used when creating new Volunteer. Every field must be present and not null.
      */
-    public Volunteer(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Volunteer(Name name, Gender gender, Birthday birthday, Phone phone, Email email, Address address, Set<Tag> tags) {
+        requireAllNonNull(name, gender, birthday, phone, email, address, tags);
 
         incrementMaxId();
         this.volunteerId = new VolunteerId(maxId);
 
         this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
         this.phone = phone;
         this.email = email;
         this.address = address;
@@ -47,7 +51,7 @@ public class Volunteer {
      * Used when loading data from XML and editing Volunteer.
      * Every field must be present and not null.
      */
-    public Volunteer(VolunteerId volunteerId, Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Volunteer(VolunteerId volunteerId, Name name, Gender gender, Birthday birthday, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(volunteerId, name, phone, email, address, tags);
 
         if (isVolunteerIdGreaterThanMaxId(volunteerId.id)) {
@@ -55,6 +59,8 @@ public class Volunteer {
         }
         this.volunteerId = volunteerId;
         this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
         this.phone = phone;
         this.email = email;
         this.address = address;
@@ -93,6 +99,10 @@ public class Volunteer {
     public Name getName() {
         return name;
     }
+
+    public Gender getGender() { return gender; }
+
+    public Birthday getBirthday() { return  birthday; }
 
     public Phone getPhone() {
         return phone;
@@ -144,6 +154,8 @@ public class Volunteer {
 
         seedu.address.model.volunteer.Volunteer otherVolunteer = (seedu.address.model.volunteer.Volunteer) other;
         return otherVolunteer.getName().equals(getName())
+                && otherVolunteer.getGender().equals(getGender())
+                && otherVolunteer.getBirthday().equals(getBirthday())
                 && otherVolunteer.getPhone().equals(getPhone())
                 && otherVolunteer.getEmail().equals(getEmail())
                 && otherVolunteer.getAddress().equals(getAddress())
@@ -153,13 +165,17 @@ public class Volunteer {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, gender, birthday, phone, email, address, tags);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
+                .append(" Gender: ")
+                .append(getGender())
+                .append(" Birthday: ")
+                .append(getBirthday())
                 .append(" Phone: ")
                 .append(getPhone())
                 .append(" Email: ")
