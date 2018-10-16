3d71b25b533df707f67a501a65b146f43cfbe2ed
SleepySanjinLi <sanjin980223@gmail.com>
2018-10-16 16:39:09 +0800

    finishe the todoList in model part.

---
+++ src/main/java/seedu/address/model/todolist/Priority.java
@@ -0,0 +1,63 @@
+package seedu.address.model.todolist;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the priority of a calendar event.
+ * Guarantees: immutable; is valid as declared in {@link #isValid(String)}
+ * Setting Rule: user can set 1 for High; 2 for Medium; 3 for Low
+ */
+
+public class Priority {
+
+    public static final String DEFAULT_PRIORITY = "1"; // default field name
+    public static final String MESSAGE_CONSTRAINTS =
+            "The valid user input for priority is p/[priority]." +
+                    "While [priority] is a integer from 1 to 3," +
+                    "and 1 for high priority, 2 for medium priority, 3 for low priority";
+
+    /*
+     * There should be only one integer, and the integer should be 1, 2 or 3.
+     */
+    public static final String VALIDATION_REGEX = "\\d";
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Priority}.
+     *
+     * @param priority A valid priority.
+     */
+    public Priority(String priority) {
+        requireNonNull(priority);
+        checkArgument(isValid(priority), MESSAGE_CONSTRAINTS);
+        value = priority;
+    }
+
+    /**
+     * Returns true if a given string is a valid priority.
+     */
+    public static boolean isValid(String priority) {
+        return priority.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof seedu.address.model.todolist.Priority // instanceof handles nulls
+                && value.equals(((seedu.address.model.todolist.Priority) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/todolist/TodoListEvent.java
@@ -0,0 +1,92 @@
+package seedu.address.model.todolist;
+
+import seedu.address.model.calendarevent.Description;
+import seedu.address.model.calendarevent.Title;
+
+import java.util.Objects;
+
+/**
+ * Represents an event in todoList.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+
+public class TodoListEvent {
+
+    // Identity fields
+    private final Title title;
+    private final Description description;
+
+    //Data fields
+    private final Priority priority;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public TodoListEvent(Title title, Description description, Priority priority){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public Priority getPriority() { return priority; }
+
+    /**
+     * Returns true if both todoList events of the same title have one other identity field that is the same.
+     * This defines a weaker notion of equality between two calendar events.
+     */
+    public boolean isSameCalendarEvent(TodoListEvent otherTodoListEvent) {
+        if (otherTodoListEvent == this) {
+            return true;
+        }
+
+        return otherTodoListEvent != null
+                && otherTodoListEvent.getTitle().equals(getTitle())
+                && otherTodoListEvent.getDescription().equals(getDescription());
+    }
+
+    /**
+     * Returns true if both todoList events have the same identity fields and data fields.
+     * This defines a stronger notion of equality between two todoList events.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof TodoListEvent)) {
+            return false;
+        }
+
+        TodoListEvent otherTodoListEvent = (TodoListEvent) other;
+        return otherTodoListEvent.getTitle().equals(getTitle())
+                && otherTodoListEvent.getDescription().equals(getDescription())
+                && otherTodoListEvent.getPriority().equals(getPriority());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(title, description, priority);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(" Title: ")
+                .append(getTitle())
+                .append(" Description: ")
+                .append(getDescription())
+                .append(" Priority: ")
+                .append(getPriority());
+        return builder.toString();
+    }
+}
