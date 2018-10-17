207a98a624a1fcae56dcd83d5484f3be82526aae
Dominic Chong <freedomzelda_dc@hotmail.com>
2018-10-17 12:17:53 +0800

    checkstyle: XmlAdaptedWish.java

--- src/main/java/seedu/address/storage/XmlAdaptedWish.java
+++ src/main/java/seedu/address/storage/XmlAdaptedWish.java
@@ -1,6 +1,11 @@
 package seedu.address.storage;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -131,7 +136,8 @@ public class XmlAdaptedWish {
         final SavedAmount modelSavedAmount = new SavedAmount(this.savedAmount);
 
         final Set<Tag> modelTags = new HashSet<>(wishTags);
-        return new Wish(modelName, modelPrice, modelEmail, modelUrl, modelSavedAmount, modelRemark, modelTags, new LinkedList<>());
+        return new Wish(modelName, modelPrice, modelEmail, modelUrl, modelSavedAmount, modelRemark, modelTags,
+                new LinkedList<>());
     }
 
     @Override
