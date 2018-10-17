a2e055f144ac73d644dadb126e4136ff2b3ecd30
leongshengmin <35290582+leongshengmin@users.noreply.github.com>
2018-10-17 12:22:21 +0800

    Revert "Update dev guide"

--- src/main/java/seedu/address/logic/commands/SaveCommand.java
+++ src/main/java/seedu/address/logic/commands/SaveCommand.java
@@ -69,7 +69,7 @@ public class SaveCommand extends Command {
 
             Amount wishSavedDifference = editedWish.getSavedAmountToPriceDifference();
 
-            if (wishSavedDifference.value > 0) {
+            if (wishSavedDifference.value < 0) {
                 differenceString = String.format(MESSAGE_SAVE_EXCESS, wishSavedDifference);
             } else {
                 differenceString = String.format(MESSAGE_SAVE_DIFFERENCE, wishSavedDifference.getAbsoluteAmount());
