f90f27e8a5e18dcd7bdb29a2ca74889094397de9
Jeremy Choo <ChooJeremy@Ymail.com>
2018-10-17 22:39:09 +0800

    added LogoutCommandSystemTest to test if logout works

---
+++ src/test/java/systemtests/LogoutCommandSystemTest.java
@@ -0,0 +1,29 @@
+package systemtests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import guitests.GuiRobot;
+import seedu.address.logic.commands.LogoutCommand;
+
+/**
+ * A system test class for the help window, which contains interaction with other UI components.
+ */
+public class LogoutCommandSystemTest extends AddressBookSystemTest {
+    private final GuiRobot guiRobot = new GuiRobot();
+
+    @Test
+    public void attemptLogout() {
+        //use command box
+        executeCommand(LogoutCommand.COMMAND_WORD);
+        assertLogout();
+
+    }
+
+    private void assertLogout() {
+        assert getBrowserPlaceholder().getChildren().size() == 0;
+    }
+
+}
