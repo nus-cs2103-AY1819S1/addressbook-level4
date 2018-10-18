e327533ab95d33f060b9ba626dc795a248d26730
RnardHa <ranardha@gmail.com>
2018-10-18 20:15:49 +0800

    Fixed

--- src/main/java/seedu/address/model/project/ProjectName.java
+++ src/main/java/seedu/address/model/project/ProjectName.java
@@ -3,6 +3,10 @@ package seedu.address.model.project;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents a Project's name in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
 public class ProjectName {
 
     public static final String MESSAGE_PROJECT_NAME_CONSTRAINTS = "Project names should not be blank";
