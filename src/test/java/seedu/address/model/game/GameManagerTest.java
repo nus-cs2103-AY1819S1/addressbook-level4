051dfead7b195ee5a70d40f67eb37b5089e27033
chikchengyao <chikben@gmail.com>
2018-10-18 20:11:05 +0800

    Add blank GameManagerTest.java

---
+++ src/test/java/seedu/address/model/game/GameManagerTest.java
@@ -0,0 +1,18 @@
+package seedu.address.model.game;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameManagerTest {
+
+    @Test
+    public void appraiseTaskXp() {
+    }
+
+    @Test
+    public void forecastTaskXp() {
+    }
+}
