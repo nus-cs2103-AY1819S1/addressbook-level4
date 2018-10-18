cf6de525449100b6ca2b2e00db3ec50a33768c53
Snajef <40816755+snajef@users.noreply.github.com>
2018-10-18 23:48:15 +0800

    Refactor Duration to use LocalDate instead of Calendar for internal date representations (#82)
    Refactor XmlAdapted* classes to follow the Law of Demeter
    Modify tests to fit changes to existing classes

--- src/main/java/seedu/address/storage/XmlAdaptedPrescription.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPrescription.java
@@ -1,20 +1,14 @@
 package seedu.address.storage;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.medicine.Dose;
-import seedu.address.model.medicine.Duration;
 import seedu.address.model.medicine.Prescription;
 
 //@@author snajef
 /**
- * JAXB-friendly adapted version of the Prescription.
+ * JAXB-friendly adapted version of the Prescription class.
  */
 
 @XmlRootElement
@@ -25,22 +19,10 @@ public class XmlAdaptedPrescription {
     private String drugName;
 
     @XmlElement(required = true)
-    private double dosage;
+    private XmlAdaptedDose dose;
 
     @XmlElement(required = true)
-    private String doseUnit;
-
-    @XmlElement(required = true)
-    private int dosesPerDay;
-
-    @XmlElement(required = true)
-    private String startDate;
-
-    @XmlElement(required = true)
-    private String endDate;
-
-    @XmlElement(required = true)
-    private double durationInMilliseconds;
+    private XmlAdaptedDuration duration;
 
     /**
      * Constructs an XmlAdaptedPrescription.
@@ -52,34 +34,11 @@ public class XmlAdaptedPrescription {
      * Constructs a {@code XmlAdaptedPrescription} with the given details.
      */
     public XmlAdaptedPrescription(String drugName, double dosage, String doseUnit, int dosesPerDay, String startDate,
-        String endDate, double durationInMilliseconds) throws IllegalValueException {
+        String endDate, int durationInDays) throws IllegalValueException {
+
         this.drugName = drugName;
-        this.dosage = dosage;
-        this.doseUnit = doseUnit;
-        this.dosesPerDay = dosesPerDay;
-
-        try {
-            Calendar startCalendar = Calendar.getInstance();
-            Calendar endCalendar = Calendar.getInstance();
-
-            startCalendar.setTime(Duration.DATE_FORMAT.parse(startDate));
-            endCalendar.setTime(Duration.DATE_FORMAT.parse(endDate));
-
-            if (endCalendar.getTimeInMillis() - startCalendar.getTimeInMillis() < 0) {
-                throw new IllegalValueException(MESSAGE_END_DATE_MUST_BE_AFTER_START_DATE);
-            }
-        } catch (ParseException e) {
-            throw new IllegalValueException(e.toString());
-        }
-
-        this.startDate = startDate;
-        this.endDate = endDate;
-
-        if (durationInMilliseconds <= 0) {
-            throw new IllegalValueException(Duration.MESSAGE_DURATION_MUST_BE_POSITIVE);
-        }
-
-        this.durationInMilliseconds = durationInMilliseconds;
+        dose = new XmlAdaptedDose(dosage, doseUnit, dosesPerDay);
+        duration = new XmlAdaptedDuration(startDate, endDate, durationInDays);
     }
 
     /**
@@ -87,12 +46,8 @@ public class XmlAdaptedPrescription {
      */
     public XmlAdaptedPrescription(Prescription source) {
         drugName = source.getDrugName();
-        dosage = source.getDose().getDose();
-        doseUnit = source.getDose().getDoseUnit();
-        dosesPerDay = source.getDose().getDosesPerDay();
-        startDate = source.getDuration().getStartDateAsString();
-        endDate = source.getDuration().getEndDateAsString();
-        durationInMilliseconds = source.getDuration().getDurationInMilliseconds();
+        dose = new XmlAdaptedDose(source.getDose());
+        duration = new XmlAdaptedDuration(source.getDuration());
     }
 
     /**
@@ -101,23 +56,7 @@ public class XmlAdaptedPrescription {
      * @throws IllegalValueException if there were any data constraints violated in the adapted Prescription.
      */
     public Prescription toModelType() throws IllegalValueException {
-        Dose dose = new Dose(dosage, doseUnit, dosesPerDay);
-
-        SimpleDateFormat dateFormat = Duration.DATE_FORMAT;
-        Calendar startDate = Calendar.getInstance();
-        Calendar endDate = Calendar.getInstance();
-
-        try {
-            startDate.setTime(dateFormat.parse(this.startDate));
-            endDate.setTime(dateFormat.parse(this.endDate));
-        } catch (ParseException e) {
-            throw new IllegalValueException(e.toString());
-        }
-
-        Duration duration = new Duration(durationInMilliseconds);
-        duration.shiftDateRange(startDate);
-
-        return new Prescription(drugName, dose, duration);
+        return new Prescription(drugName, dose.toModelType(), duration.toModelType());
     }
 
     @Override
@@ -130,32 +69,9 @@ public class XmlAdaptedPrescription {
             return false;
         }
 
-        return drugName.equals(((XmlAdaptedPrescription) other).drugName)
-            && dosage == ((XmlAdaptedPrescription) other).dosage
-            && doseUnit.equals(((XmlAdaptedPrescription) other).doseUnit)
-            && dosesPerDay == ((XmlAdaptedPrescription) other).dosesPerDay
-            && areDatesEqual(startDate, (((XmlAdaptedPrescription) other).startDate))
-            && areDatesEqual(endDate, (((XmlAdaptedPrescription) other).endDate));
-        // Note we don't test for equality of duration in milliseconds, because it tends
-        // to get truncated and rounded when we convert from duration in days and back.
-    }
-
-    /**
-     * Helper method to check if two dates (in String format dd-MM-yyyy) are identical.
-     * @param date in String format.
-     * @param anotherDate in String format.
-     * @return
-     */
-    public static boolean areDatesEqual(String date, String anotherDate) {
-        try {
-            Calendar calendarDate = Calendar.getInstance();
-            Calendar anotherCalendarDate = Calendar.getInstance();
-            calendarDate.setTime(Duration.DATE_FORMAT.parse(date));
-            anotherCalendarDate.setTime(Duration.DATE_FORMAT.parse(anotherDate));
-
-            return Duration.areDatesEqual(calendarDate, anotherCalendarDate);
-        } catch (ParseException e) {
-            return false;
-        }
+        XmlAdaptedPrescription xap = (XmlAdaptedPrescription) other;
+        return drugName.equals(xap.drugName)
+            && dose.equals(xap.dose)
+            && duration.equals(xap.duration);
     }
 }
