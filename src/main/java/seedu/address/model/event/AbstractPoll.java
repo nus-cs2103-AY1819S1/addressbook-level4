b553695c0b2349e5947b98c22816db6579f1dd70
KJ-LT\kengj <kengji.c>
2018-10-18 08:11:52 +0800

    add TimePoll

--- src/main/java/seedu/address/model/event/AbstractPoll.java
+++ src/main/java/seedu/address/model/event/AbstractPoll.java
@@ -1,4 +1,84 @@
 package seedu.address.model.event;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+
 public class AbstractPoll {
+    private int id;
+    private String pollName;
+    private HashMap<String, UniquePersonList> pollData;
+
+    /**
+     * Adds the vote of a user into an option
+     */
+    public void addVote(String option, Person person) throws IllegalArgumentException, DuplicatePersonException {
+        if (!pollData.containsKey(option)) {
+            throw new IllegalArgumentException();
+        }
+        pollData.get(option).add(person);
+    }
+
+    /**
+     * Updates the person in the poll votes.
+     */
+    public void updatePerson(Person target, Person editedPerson) {
+        pollData.forEach((k, v) -> v.setPerson(target, editedPerson));
+    }
+
+    /**
+     * Retrieves most popular options by number of votes.
+     */
+    public LinkedList<String> getPopularOptions() {
+        TreeMap<Integer, LinkedList<String>> frequency = new TreeMap<>();
+        pollData.forEach((k, v) -> {
+            if (!frequency.containsKey(v.size())) {
+                LinkedList<String> options = new LinkedList<>();
+                options.add(k);
+                frequency.put(v.size(), options);
+            } else {
+                frequency.get(v.size()).add(k);
+            }
+        });
+        return frequency.lastEntry().getValue();
+    }
+
+    /**
+     * Returns a string representation of the poll
+     */
+    public String displayPoll() {
+        String title = String.format("Poll %1$s: %2$s", Integer.toString(id), pollName);
+        String mostPopularEntries = "";
+        if (!pollData.isEmpty()) {
+            mostPopularEntries = "Most popular options:\n" + getPopularOptions().toString();
+        }
+        String data = displayPollData();
+        return title + "\n" + mostPopularEntries + "\n\n" + data;
+    }
+
+    /**
+     * Returns the poll data as a string identifying people by their names.
+     */
+    public String displayPollData() {
+        HashMap<String, List<String>> displayData = new HashMap<>();
+        pollData.forEach((k, v) -> {
+            List<String> nameList = v.asUnmodifiableObservableList()
+                    .stream()
+                    .map(person -> person.getName().toString())
+                    .collect(Collectors.toList());
+            displayData.put(k, nameList);
+        });
+        String result = "";
+        for (Map.Entry<String, List<String>> entry : displayData.entrySet()) {
+            result += entry.getKey() + ":\n" + entry.getValue().toString() + "\n";
+        }
+        return result;
+    }
 }
--- src/main/java/seedu/address/model/event/Poll.java
+++ src/main/java/seedu/address/model/event/Poll.java
@@ -15,7 +15,7 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
 /**
  * Represents a poll associated with an event.
  */
-public class Poll {
+public class Poll extends AbstractPoll {
     private int id;
     private String pollName;
     private HashMap<String, UniquePersonList> pollData;
@@ -58,71 +58,5 @@ public class Poll {
     public void addOption(String option) {
         UniquePersonList personList = new UniquePersonList();
         pollData.put(option, personList);
-    }
-
-    /**
-     * Adds the vote of a user into an option
-     */
-    public void addVote(String option, Person person) throws IllegalArgumentException, DuplicatePersonException {
-        if (!pollData.containsKey(option)) {
-            throw new IllegalArgumentException();
-        }
-        pollData.get(option).add(person);
-    }
-
-    /**
-     * Updates the person in the poll votes.
-     */
-    public void updatePerson(Person target, Person editedPerson) {
-        pollData.forEach((k, v) -> v.setPerson(target, editedPerson));
-    }
-
-    /**
-     * Retrieves most popular options by number of votes.
-     */
-    public LinkedList<String> getPopularOptions() {
-        TreeMap<Integer, LinkedList<String>> frequency = new TreeMap<>();
-        pollData.forEach((k, v) -> {
-            if (!frequency.containsKey(v.size())) {
-                LinkedList<String> options = new LinkedList<>();
-                options.add(k);
-                frequency.put(v.size(), options);
-            } else {
-                frequency.get(v.size()).add(k);
-            }
-        });
-        return frequency.lastEntry().getValue();
-    }
-
-    /**
-     * Returns a string representation of the poll
-     */
-    public String displayPoll() {
-        String title = String.format("Poll %1$s: %2$s", Integer.toString(id), pollName);
-        String mostPopularEntries = "";
-        if (!pollData.isEmpty()) {
-            mostPopularEntries = "Most popular options:\n" + getPopularOptions().toString();
-        }
-        String data = displayPollData();
-        return title + "\n" + mostPopularEntries + "\n\n" + data;
-    }
-
-    /**
-     * Returns the poll data as a string identifying people by their names.
-     */
-    public String displayPollData() {
-        HashMap<String, List<String>> displayData = new HashMap<>();
-        pollData.forEach((k, v) -> {
-            List<String> nameList = v.asUnmodifiableObservableList()
-                    .stream()
-                    .map(person -> person.getName().toString())
-                    .collect(Collectors.toList());
-            displayData.put(k, nameList);
-        });
-        String result = "";
-        for (Map.Entry<String, List<String>> entry : displayData.entrySet()) {
-            result += entry.getKey() + ":\n" + entry.getValue().toString() + "\n";
-        }
-        return result;
     }
 }
