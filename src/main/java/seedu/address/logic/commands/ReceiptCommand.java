5b86bfd7bb0cb8583a4a970ab6ce40306225a6c7
blewjy <39292809+blewjy@users.noreply.github.com>
2018-10-19 16:59:51 +0800

    Update ReceiptCommand
    fix travis

--- src/main/java/seedu/address/logic/commands/ReceiptCommand.java
+++ src/main/java/seedu/address/logic/commands/ReceiptCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.events.ui.ShowPatientListEvent;
