59d4d92efbea62042e872d332973e79c21b76598
elroyhaw <elroyhaw@gmail.com>
2018-10-19 21:16:05 +0800

    CommandBox: Add line in between import statements

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
