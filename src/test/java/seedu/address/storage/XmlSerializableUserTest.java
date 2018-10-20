f7ae8f72bc50ee7c7c4c3d688882a9e5bd12162d
etjk <edricteojk@hotmail.com>
2018-10-21 00:51:29 +0800

    Add test for XmlSerializableUser

---
+++ src/test/java/seedu/address/storage/XmlSerializableUserTest.java
@@ -0,0 +1,26 @@
+package seedu.address.storage;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seedu.address.commons.util.XmlUtil;
+
+public class XmlSerializableUserTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test",
+            "data", "XmlSerializableUserTest");
+    private static final Path VALID_USER_DATA_FILE =
+            TEST_DATA_FOLDER.resolve("validUserData.xml");
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void toModelType_validUserDataFile_Success() throws Exception {
+        XmlSerializableUser dataFromFile = XmlUtil.getDataFromFile(VALID_USER_DATA_FILE,
+                        XmlSerializableUser.class);
+        dataFromFile.toModelType();
+    }
+}
