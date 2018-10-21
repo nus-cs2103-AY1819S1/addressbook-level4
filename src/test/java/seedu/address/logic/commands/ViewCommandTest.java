bf8773aa9945929504c54ced73d04578ce1fe7e1
snajef <40816755+snajef@users.noreply.github.com>
2018-10-21 11:58:07 +0800

    Add more tests for ViewCommand

--- src/test/java/seedu/address/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -1,24 +1,48 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.CommandTestUtil.ModelStub;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.ui.SwappablePanelName;
 
 public class ViewCommandTest {
+    private SwappablePanelName panelName = SwappablePanelName.BLANK;
     private ViewCommand command;
 
     @Before
     public void setup() {
-        command = new ViewCommand(SwappablePanelName.BLANK);
+        command = new ViewCommand(panelName);
     }
 
     @Test(expected = NullPointerException.class)
     public void constructor_nullColIdx_throwsException() {
         new ViewCommand(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void execute_nullModel_throwsNullPointerException() throws CommandException {
+        command.execute(null, new CommandHistory());
+    }
+
+    @Test
+    public void execute_normalScenario_successMessage() throws CommandException {
+        CommandResult result = command.execute(new ModelStub(), new CommandHistory());
+        
+        String formattedPanelName = panelName.toString()
+                                             .substring(0, 1)
+                                             .toUpperCase()
+            + panelName.toString()
+                       .toLowerCase()
+                       .substring(1);
+        assertEquals(result.feedbackToUser,
+            String.format(ViewCommand.MESSAGE_VIEW_CHANGED_SUCCESS, formattedPanelName));
     }
 
     @Test
