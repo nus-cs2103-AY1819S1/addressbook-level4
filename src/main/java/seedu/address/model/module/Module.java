df2013196a3244e5a4879cd22ed844cf790a8634
alistairong <ongalistair@gmail.com>
2018-10-21 23:10:32 +0800

    Changes to module and test

--- src/main/java/seedu/address/model/module/Module.java
+++ src/main/java/seedu/address/model/module/Module.java
@@ -2,13 +2,12 @@ package seedu.address.model.module;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import seedu.address.commons.util.TypeUtil;
 import seedu.address.model.entity.Entity;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.UniquePersonList;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -22,6 +21,7 @@ public class Module extends Entity {
     private final ModuleTitle moduleTitle;
     private final AcademicYear academicYear;
     private final Semester semester;
+    private final UniquePersonList students;
 
     // Data fields
     private final Set<Tag> tags = new HashSet<>();
@@ -29,12 +29,13 @@ public class Module extends Entity {
      * Every field must be present and not null.
      */
     public Module(ModuleCode moduleCode, ModuleTitle moduleTitle, AcademicYear academicYear,
-                  Semester semester, Set<Tag> tags, TypeUtil type) {
+                  Semester semester, UniquePersonList students, Set<Tag> tags, TypeUtil type) {
         requireAllNonNull(moduleCode, moduleTitle, academicYear, semester, tags, type);
         this.moduleCode = moduleCode;
         this.moduleTitle = moduleTitle;
         this.academicYear = academicYear;
         this.semester = semester;
+        this.students = students;
         this.tags.addAll(tags);
         this.type = type;
     }
@@ -54,6 +55,8 @@ public class Module extends Entity {
     public Semester getSemester() {
         return semester;
     }
+
+    public UniquePersonList getStudents() { return students; }
 
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
@@ -82,7 +85,8 @@ public class Module extends Entity {
         return otherModule.getModuleCode().equals(getModuleCode())
                 && otherModule.getModuleTitle().equals(getModuleTitle())
                 && otherModule.getAcademicYear().equals(getAcademicYear())
-                && otherModule.getSemester().equals(getSemester());
+                && otherModule.getSemester().equals(getSemester())
+                && otherModule.getStudents().equals(getStudents());
     }
 
 
