917243968e57712002461b23209b69436c6aeff5
Chester Sng <chester_nutty@hotmail.com>
2018-10-22 00:36:48 +0800

    Update QueueCommandTestUtil

--- src/test/java/seedu/address/logic/commands/DisplayServedPatientListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DisplayServedPatientListCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-import static seedu.address.logic.commands.QueueCommandTestUtil.generatePatientQueue;
 import static seedu.address.logic.commands.QueueCommandTestUtil.generateServedPatientList;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
