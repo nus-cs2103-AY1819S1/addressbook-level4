3d341761265f9fd15db1294c941c197ced692a73
lancelotwillow <lancelotwillow@gmail.com>
2018-10-22 15:55:36 +0800

    finish the convert for mac, single command

---
+++ src/main/java/seedu/address/logic/parser/ConvertCommandParser.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import seedu.address.logic.commands.ConvertCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.transformation.Transformation;
+
+/**
+ * Parses input arguments and creates a new ExampleCommand object
+ */
+public class ConvertCommandParser implements Parser<ConvertCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ExampleCommand
+     * and returns an ExampleCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ConvertCommand parse(String args) throws ParseException {
+        try {
+            String[] all = args.split(" ");
+            String operation = all[1];
+            String[] cmds = new String[0];
+            Path filePath;
+            if (all.length >= 1) {
+                cmds = Arrays.copyOfRange(all, 2, all.length);
+                filePath = ConvertCommand.SINGLE_COMMAND_TEMPLATE_PATH;
+            } else {
+                filePath = ConvertCommand.SINGLE_COMMAND_PATH;
+            }
+            return new ConvertCommand(filePath, new Transformation(operation, cmds));
+        } catch (ParseException | IOException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ConvertCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
