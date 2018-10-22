b74ac20a226a6b792afc5641a84cf21f3dcca747
GAOJIAXIN <38589312+GAOJIAXIN666@users.noreply.github.com>
2018-10-22 17:18:20 +0800

    refactoring

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -219,7 +219,7 @@ public class Person {
         return otherPerson != null && otherPerson.getNric().equals(getNric())
                 && otherPerson.getName().equals(getName())
                 && (otherPerson.getPhone().equals(getPhone())
-                || otherPerson.getEmail().equals(getEmail()));
+                        || otherPerson.getEmail().equals(getEmail()));
     }
 
     /**
