6dd62e30aae90cbfb8dac1f9d11f6fb1a5642325
Yu Jia Tay <yujiatay@gmail.com>
2018-10-23 17:02:04 +0800

    Add assert checks for decks and cards

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -3,66 +3,101 @@ package seedu.address.ui.testutil;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import guitests.guihandles.PersonCardHandle;
-import guitests.guihandles.PersonListPanelHandle;
+import guitests.guihandles.CardCardHandle;
+import guitests.guihandles.CardListPanelHandle;
+import guitests.guihandles.DeckCardHandle;
+import guitests.guihandles.DeckListPanelHandle;
 import guitests.guihandles.ResultDisplayHandle;
-import seedu.address.model.person.Person;
+import seedu.address.model.anakindeck.AnakinCard;
+import seedu.address.model.anakindeck.AnakinDeck;
 
 /**
  * A set of assertion methods useful for writing GUI tests.
  */
 public class GuiTestAssert {
     /**
-     * Asserts that {@code actualCard} displays the same values as {@code expectedCard}.
+     * Asserts that {@code actualDeckCard} displays the same values as {@code expectedDeckCard}.
      */
-    public static void assertCardEquals(PersonCardHandle expectedCard, PersonCardHandle actualCard) {
-        assertEquals(expectedCard.getId(), actualCard.getId());
-        assertEquals(expectedCard.getAddress(), actualCard.getAddress());
-        assertEquals(expectedCard.getEmail(), actualCard.getEmail());
-        assertEquals(expectedCard.getName(), actualCard.getName());
-        assertEquals(expectedCard.getPhone(), actualCard.getPhone());
-        assertEquals(expectedCard.getTags(), actualCard.getTags());
+    public static void assertDeckCardEquals(DeckCardHandle expectedDeckCard, DeckCardHandle actualDeckCard) {
+        assertEquals(expectedDeckCard.getId(), actualDeckCard.getId());
+        assertEquals(expectedDeckCard.getName(), actualDeckCard.getName());
     }
 
     /**
-     * Asserts that {@code actualCard} displays the details of {@code expectedPerson}.
+     * Asserts that {@code actualDeckCard} displays the details of {@code expectedDeck}.
      */
-    public static void assertCardDisplaysPerson(Person expectedPerson, PersonCardHandle actualCard) {
-        assertEquals(expectedPerson.getName().fullName, actualCard.getName());
-        assertEquals(expectedPerson.getPhone().value, actualCard.getPhone());
-        assertEquals(expectedPerson.getEmail().value, actualCard.getEmail());
-        assertEquals(expectedPerson.getAddress().value, actualCard.getAddress());
-        assertEquals(expectedPerson.getTags().stream().map(tag -> tag.tagName).collect(Collectors.toList()),
-                actualCard.getTags());
+    public static void assertDeckCardDisplaysDeck(AnakinDeck expectedDeck, DeckCardHandle actualCard) {
+        assertEquals(expectedDeck.getName().fullName, actualCard.getName());
     }
 
     /**
-     * Asserts that the list in {@code personListPanelHandle} displays the details of {@code persons} correctly and
+     * Asserts that the list in {@code deckListPanelHandle} displays the details of {@code decks} correctly and
      * in the correct order.
      */
-    public static void assertListMatching(PersonListPanelHandle personListPanelHandle, Person... persons) {
-        for (int i = 0; i < persons.length; i++) {
-            personListPanelHandle.navigateToCard(i);
-            assertCardDisplaysPerson(persons[i], personListPanelHandle.getPersonCardHandle(i));
+    public static void assertDeckListMatching(DeckListPanelHandle deckListPanelHandle, AnakinDeck... decks) {
+        for (int i = 0; i < decks.length; i++) {
+            deckListPanelHandle.navigateToCard(i);
+            assertDeckCardDisplaysDeck(decks[i], deckListPanelHandle.getDeckCardHandle(i));
         }
     }
 
     /**
-     * Asserts that the list in {@code personListPanelHandle} displays the details of {@code persons} correctly and
+     * Asserts that the list in {@code deckListPanelHandle} displays the details of {@code decks} correctly and
      * in the correct order.
      */
-    public static void assertListMatching(PersonListPanelHandle personListPanelHandle, List<Person> persons) {
-        assertListMatching(personListPanelHandle, persons.toArray(new Person[0]));
+    public static void assertDeckListMatching(DeckListPanelHandle deckListPanelHandle, List<AnakinDeck> decks) {
+        assertDeckListMatching(deckListPanelHandle, decks.toArray(new AnakinDeck[0]));
     }
 
     /**
-     * Asserts the size of the list in {@code personListPanelHandle} equals to {@code size}.
+     * Asserts the size of the list in {@code deckListPanelHandle} equals to {@code size}.
      */
-    public static void assertListSize(PersonListPanelHandle personListPanelHandle, int size) {
-        int numberOfPeople = personListPanelHandle.getListSize();
-        assertEquals(size, numberOfPeople);
+    public static void assertDeckListSize(DeckListPanelHandle deckListPanelHandle, int size) {
+        int numberOfDecks = deckListPanelHandle.getListSize();
+        assertEquals(size, numberOfDecks);
+    }
+
+    /**
+     * Asserts that {@code actualCardCard} displays the same values as {@code expectedCardCard}.
+     */
+    public static void assertCardCardEquals(CardCardHandle expectedCard, CardCardHandle actualCard) {
+        assertEquals(expectedCard.getId(), actualCard.getId());
+        assertEquals(expectedCard.getQuestion(), actualCard.getQuestion());
+    }
+
+    /**
+     * Asserts that {@code actualCardCard} displays the details of {@code expectedCard}.
+     */
+    public static void assertCardCardDisplaysCard(AnakinCard expectedCard, CardCardHandle actualCard) {
+        assertEquals(expectedCard.getQuestion().fullQuestion, actualCard.getQuestion());
+    }
+
+    /**
+     * Asserts that the list in {@code cardListPanelHandle} displays the details of {@code cards} correctly and
+     * in the correct order.
+     */
+    public static void assertCardListMatching(CardListPanelHandle cardListPanelHandle, AnakinCard... cards) {
+        for (int i = 0; i < cards.length; i++) {
+            cardListPanelHandle.navigateToCard(i);
+            assertCardCardDisplaysCard(cards[i], cardListPanelHandle.getCardCardHandle(i));
+        }
+    }
+
+    /**
+     * Asserts that the list in {@code cardListPanelHandle} displays the details of {@code cards} correctly and
+     * in the correct order.
+     */
+    public static void assertCardListMatching(CardListPanelHandle cardListPanelHandle, List<AnakinCard> cards) {
+        assertCardListMatching(cardListPanelHandle, cards.toArray(new AnakinCard[0]));
+    }
+
+    /**
+     * Asserts the size of the list in {@code cardListPanelHandle} equals to {@code size}.
+     */
+    public static void assertCardListSize(CardListPanelHandle cardListPanelHandle, int size) {
+        int numberOfCards = cardListPanelHandle.getListSize();
+        assertEquals(size, numberOfCards);
     }
 
     /**
