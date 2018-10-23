ecc11ce3ce3e38ec402f1f36ccdf242476e5d4d8
ryantanwk <ryantan2103@gmail.com>
2018-10-23 19:29:36 +0800

    implement favourite command with stubs

--- src/main/java/seedu/address/logic/commands/FavouriteCommand.java
+++ src/main/java/seedu/address/logic/commands/FavouriteCommand.java
@@ -2,10 +2,18 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.event.Event;
 
 /**
  * Favourite an event for it to show up as a notification on startup
@@ -32,13 +40,15 @@ public class FavouriteCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        //List<Event> uniqueEventsList = model.getFilteredPersonList();
+        /*ObservableList<Event> eventsList = AddressBook.getEventList();
 
-        /*if (targetIndex.getZeroBased() >= filteredPersonList.size()) {
+        if (targetIndex.getZeroBased() >= eventsList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
-        }
+        } */
 
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));*/
+        ModelManager.updateFavourite("new Favourite");
+
+        //EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
         return new CommandResult(String.format(MESSAGE_FAVOURITE_EVENT_SUCCESS, targetIndex.getOneBased()));
     }
 
