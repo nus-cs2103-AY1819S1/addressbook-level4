11377a865f258f1806e45708d734f82b837e715e
GAO_JIAXIN <enochgao7@gmail.com>
2018-10-23 15:25:48 +0800

    add more test for coverage error

--- src/test/java/seedu/address/model/visitor/VisitorListTest.java
+++ src/test/java/seedu/address/model/visitor/VisitorListTest.java
@@ -1,5 +1,8 @@
 package seedu.address.model.visitor;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -12,6 +15,17 @@ public class VisitorListTest {
     @Test(expected = NullPointerException.class)
     public void constructor_nullList_throwsNullPointerException() {
         new VisitorList((ArrayList<Visitor>) null);
+    }
+
+    @Test
+    public void equals_emptyVisitorListAndNull_returnsFalse() {
+        assertFalse(new VisitorList().equals(null));
+    }
+
+    @Test
+    public void equals_objectEqualsItself_returnsTrue() {
+        VisitorList visitorList = new VisitorList();
+        assertTrue(visitorList.equals(visitorList));
     }
     //Todo: cant add further tests, needs to modify visitorList structure;
 }
