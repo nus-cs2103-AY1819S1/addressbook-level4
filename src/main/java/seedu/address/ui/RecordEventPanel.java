c11a6c5e34b788d0ffbfa45b657f272c2e1497d1
iMarbles <amabel_yeo@hotmail.com>
2018-10-23 16:42:17 +0800

    AddRecordCommand: Allow users to add records

--- src/main/java/seedu/address/ui/RecordEventPanel.java
+++ src/main/java/seedu/address/ui/RecordEventPanel.java
@@ -4,15 +4,10 @@ import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.ObjectBinding;
-import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
