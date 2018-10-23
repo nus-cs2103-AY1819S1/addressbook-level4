9212c945ed73c602e7bcf969febbfca2ad725135
CJuanvip <42432423+CJuanvip@users.noreply.github.com>
2018-10-24 00:41:32 +0800

    Update FilterByFeeCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/FilterByFeeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterByFeeCommandParserTest.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.FilterByFeeCommand;
-
-import org.junit.Test;
-
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import seedu.address.logic.commands.FilterByFeeCommand;
+
+import org.junit.Test;
 
 public class FilterByFeeCommandParserTest {
 
@@ -14,18 +14,17 @@ public class FilterByFeeCommandParserTest {
 
     @Test
     public void parseEmptyArgThrowsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterByFeeCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " ", String.format(
+            MESSAGE_INVALID_COMMAND_FORMAT, FilterByFeeCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parseValidArgsReturnsFindCommand() {
         // no leading and trailing whitespaces
         FilterByFeeCommand expectedFindCommand =
-                new FilterByFeeCommand( "20");
+                new FilterByFeeCommand("20");
         assertParseSuccess(parser, "20", expectedFindCommand);
 
-
     }
-
 
 }
