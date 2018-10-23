c5b18d320c097406678bbd02b92f4a8031512aa3
Ong Jun Jie <>
2018-10-23 19:43:24 +0800

    enhance TimeAddCommand to catch more exceptions

--- src/main/java/seedu/address/logic/commands/TimeAddCommand.java
+++ src/main/java/seedu/address/logic/commands/TimeAddCommand.java
@@ -31,6 +31,8 @@ public class TimeAddCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Time slot successfully added";
     public static final String MESSAGE_PERSON_NOT_FOUND = "This person does not exists in the address book";
     public static final String MESSAGE_TIME_IS_NOT_AVAILABLE = "The time has already been taken";
+    public static final String MESSAGE_INVALID_START_END_TIME = "Start time must be earlier than end time";
+    public static final String MESSAGE_TIME_CLASH = "There is a clash with other tuition timing in the address book";
 
     private String[] personToFind;
 
@@ -50,9 +52,15 @@ public class TimeAddCommand extends Command {
         requireNonNull(model);
 
         ArrayList<Time> allTimeSlot = new ArrayList();
+        ArrayList<Time> sameDay = new ArrayList();
 
         for (Person ppl : model.getFilteredPersonList()) {
             allTimeSlot.addAll(ppl.getTime());
+        }
+        for (Time time : allTimeSlot) {
+            if (time.getDay() == toAdd.getDay()) {
+                sameDay.add(time);
+            }
         }
         model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(personToFind)));
 
@@ -63,6 +71,20 @@ public class TimeAddCommand extends Command {
         }
         if (!model.hasPerson(targetPerson)) {
             throw new CommandException(MESSAGE_PERSON_NOT_FOUND);
+        }
+        if (toAdd.getStartTime() > toAdd.getEndTime()) {
+            throw new CommandException(MESSAGE_INVALID_START_END_TIME);
+        }
+        for (Time time : sameDay) {
+            if (toAdd.getStartTime() >= time.getStartTime() && toAdd.getStartTime() < time.getEndTime() ) {
+                throw new CommandException(MESSAGE_TIME_CLASH);
+            }
+            if (toAdd.getEndTime() > time.getStartTime() && toAdd.getEndTime() <= time.getEndTime()) {
+                throw new CommandException(MESSAGE_TIME_CLASH);
+            }
+            if (toAdd.getStartTime() <= time.getStartTime() && toAdd.getEndTime() >= time.getEndTime()) {
+                throw new CommandException(MESSAGE_TIME_CLASH);
+            }
         }
         model.addTime(targetPerson, toAdd);
         model.commitAddressBook();
