16a8b7640e23e56159ba568176b4fb5d8ae55142
xueyantian <daisytianxueyan@gmail.com>
2018-10-23 11:15:28 +0800

    Update addOccasionCommand.

--- src/main/java/seedu/address/logic/commands/AddOccasionCommand.java
+++ src/main/java/seedu/address/logic/commands/AddOccasionCommand.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_OCCASION_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_OCCASION_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ORGANIZER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.CommandHistory;
@@ -23,12 +23,12 @@ public class AddOccasionCommand extends Command {
             + "Parameters: "
             + PREFIX_OCCASION_NAME + "OCCASION_NAME "
             + PREFIX_OCCASION_DATE + "OCCASION_DATE "
-            + PREFIX_ORGANIZER + "ORGANIZER "
+            + PREFIX_LOCATION + "LOCATION "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_OCCASION_NAME + "neighborhood party "
             + PREFIX_OCCASION_DATE + "2018-01-01 "
-            + PREFIX_ORGANIZER + "John Doe"
+            + PREFIX_LOCATION + "Cinnamon College"
             + PREFIX_TAG + "food "
             + PREFIX_TAG + "games";
 
