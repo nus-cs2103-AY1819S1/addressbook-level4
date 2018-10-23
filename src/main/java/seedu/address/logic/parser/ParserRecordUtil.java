07ca78d82eed7aac9831ce89545c3e16dc2f9782
iMarbles <amabel_yeo@hotmail.com>
2018-10-23 12:57:15 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/parser/ParserRecordUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserRecordUtil.java
@@ -2,21 +2,11 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.Context;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
 import seedu.address.model.record.Hour;
 import seedu.address.model.record.Remark;
-import seedu.address.model.tag.Tag;
 
 /**
  * Contains utility methods used for parsing strings in the various *Parser classes.
