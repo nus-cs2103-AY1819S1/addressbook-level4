859a618949a5233c158dd11fea0442a9ad8fb196
iMarbles <amabel_yeo@hotmail.com>
2018-10-24 11:09:52 +0800

    DeleteCommand.java: Added codes to cleanly delete the records

--- src/main/java/seedu/address/model/record/RecordContainsVolunteerIdPredicate.java
+++ src/main/java/seedu/address/model/record/RecordContainsVolunteerIdPredicate.java
@@ -2,28 +2,28 @@ package seedu.address.model.record;
 
 import java.util.function.Predicate;
 
-import seedu.address.model.person.PersonId;
+import seedu.address.model.volunteer.VolunteerId;
 
 /**
  * Tests that a {@code Record}'s {@code personId} matches the given PersonId.
  */
 public class RecordContainsVolunteerIdPredicate implements Predicate<Record> {
-    private final PersonId personId;
+    private final VolunteerId volunteerId;
 
-    public RecordContainsVolunteerIdPredicate(PersonId personId) {
-        this.personId = personId;
+    public RecordContainsVolunteerIdPredicate(VolunteerId volunteerId) {
+        this.volunteerId = volunteerId;
     }
 
     @Override
     public boolean test(Record record) {
-        return record.getVolunteerId().id == personId.id;
+        return record.getVolunteerId().id == volunteerId.id;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof RecordContainsVolunteerIdPredicate // instanceof handles nulls
-                && personId == ((RecordContainsVolunteerIdPredicate) other).personId); // state check
+                && volunteerId == ((RecordContainsVolunteerIdPredicate) other).volunteerId); // state check
     }
 
 }
