7fe2f5d82844c81e718d67506ac1659544941432
xueyantian <daisytianxueyan@gmail.com>
2018-10-24 21:01:03 +0800

    Remove Unused inport.

--- src/main/java/seedu/address/logic/parser/AddOccasionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddOccasionCommandParser.java
@@ -10,7 +10,6 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.commons.util.TypeUtil;
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddOccasionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.occasion.Occasion;
