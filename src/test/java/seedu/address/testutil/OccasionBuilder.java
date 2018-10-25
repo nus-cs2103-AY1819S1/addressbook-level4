a91ae728406cb84be1e2961304817f69eca298e2
waytan <fiboway@yahoo.com.sg>
2018-10-25 22:27:02 +0800

    Build Occasion test classes, fix Module test issues

--- src/test/java/seedu/address/testutil/OccasionBuilder.java
+++ src/test/java/seedu/address/testutil/OccasionBuilder.java
@@ -12,37 +12,37 @@ import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
 /**
- * A utility class to help with building Module objects.
+ * A utility class to help with building Occasion objects.
  */
 public class OccasionBuilder {
     public static final String DEFAULT_OCCASIONNAME = "meeting";
     public static final String DEFAULT_OCCASIONDATE = "2018-01-01".trim();
-    public static final String DEFAULT_LOCATION = "SoC";
+    public static final String DEFAULT_OCCASIONLOCATION = "SoC";
 
     private OccasionName occasionName;
     private OccasionDate occasionDate;
-    private OccasionLocation location;
+    private OccasionLocation occasionLocation;
     private Set<Tag> tags;
 
     public OccasionBuilder() {
         occasionName = new OccasionName(DEFAULT_OCCASIONNAME);
         occasionDate = new OccasionDate(DEFAULT_OCCASIONDATE);
-        location = new OccasionLocation(DEFAULT_LOCATION);
+        occasionLocation = new OccasionLocation(DEFAULT_OCCASIONLOCATION);
         tags = new HashSet<>();
     }
 
     public OccasionBuilder(Occasion occasionToCopy) {
         occasionName = occasionToCopy.getOccasionName();
         occasionDate = occasionToCopy.getOccasionDate();
-        location = occasionToCopy.getLocation();
+        occasionLocation = occasionToCopy.getLocation();
         tags = occasionToCopy.getTags();
     }
 
     /**
      * Sets the {@code occasionName} of the {@code Occasion} that we are building.
      */
-    public OccasionBuilder withOccasionNanme(String occasionNanme) {
-        this.occasionName = new OccasionName(occasionNanme);
+    public OccasionBuilder withOccasionName(String occasionName) {
+        this.occasionName = new OccasionName(occasionName);
         return this;
     }
 
@@ -57,8 +57,8 @@ public class OccasionBuilder {
     /**
      * Sets the {@code location} of the {@code Occasion} that we are building.
      */
-    public OccasionBuilder withLocation(OccasionLocation location) {
-        this.location = location;
+    public OccasionBuilder withOccasionLocation(String occasionLocation) {
+        this.occasionLocation = new OccasionLocation(occasionLocation);
         return this;
     }
 
@@ -71,6 +71,6 @@ public class OccasionBuilder {
     }
 
     public Occasion build() {
-        return new Occasion(occasionName, occasionDate, location, tags, TypeUtil.OCCASION);
+        return new Occasion(occasionName, occasionDate, occasionLocation, tags, TypeUtil.OCCASION);
     }
 }
---
+++ src/test/java/seedu/address/testutil/TypicalOccasions.java
@@ -0,0 +1,51 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.occasion.Occasion;
+
+/**
+ * A utility class containing a list of {@code Occasions} objects to be used in tests.
+ */
+public class TypicalOccasions {
+
+    public static final Occasion TYPICAL_OCCASION_ONE = new OccasionBuilder()
+            .withOccasionName("Project Meeting").withOccasionDate("2018-10-29")
+            .withOccasionLocation("UTown").withTags("Needs Prep").build();
+    public static final Occasion TYPICAL_OCCASION_TWO = new OccasionBuilder()
+            .withOccasionName("Next CS2103 Tutorial").withOccasionDate("2018-10-31")
+            .withOccasionLocation("Active Learning Room").withTags("Work Due").build();
+    public static final Occasion TYPICAL_OCCASION_THREE = new OccasionBuilder()
+            .withOccasionName("Final Project Submission").withOccasionDate("2018-11-13")
+            .withOccasionLocation("NIL").withTags("Important").build();
+    public static final Occasion TYPICAL_OCCASION_FOUR = new OccasionBuilder()
+            .withOccasionName("Christmas Day").withOccasionDate("2018-12-25")
+            .withOccasionLocation("NIL").withTags("With Family", "Holiday").build();
+    public static final Occasion TYPICAL_OCCASION_FIVE = new OccasionBuilder()
+            .withOccasionName("Hiking Trip").withOccasionDate("2019-01-03")
+            .withOccasionLocation("Mongolia").build();
+    public static final Occasion TYPICAL_OCCASION_SIX = new OccasionBuilder()
+            .withOccasionName("Combined Birthday Celebration").withOccasionDate("2019-01-10")
+            .withOccasionLocation("TBC").withTags("School Friends").build();
+
+    private TypicalOccasions() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical occasions.
+     */
+    public static AddressBook getTypicalOccasionsAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Occasion occasion : getTypicalOccasions()) {
+            ab.addOccasion(occasion);
+        }
+        return ab;
+    }
+
+    public static List<Occasion> getTypicalOccasions() {
+        return new ArrayList<>(Arrays.asList(TYPICAL_OCCASION_ONE, TYPICAL_OCCASION_TWO,
+                TYPICAL_OCCASION_THREE, TYPICAL_OCCASION_FOUR, TYPICAL_OCCASION_FIVE, TYPICAL_OCCASION_SIX));
+    }
+}
