130e6f50a1e34a758745beb6d8230e0bae605208
Jeffry Lum <jeffry.lum@ifinancialsingapore.com>
2018-10-25 20:18:09 +0800

    Added IllegalOperationException.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,11 +11,13 @@ public class Messages {
     public static final String MESSAGE_INDEX_END_OF_IMAGE_LIST = "Exceeded number of images in current directory";
     public static final String MESSAGE_INDEX_EXCEED_MAX_BATCH_SIZE = "The index provided exceeds batch size";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+
+    /* Error messages for Google OAuth */
     public static final String MESSAGE_INVALID_ALBUM_REQUESTED = "The album name provided is invalid";
     public static final String MESSAGE_GOOGLE_INVALID_FORMAT = "Please enter a valid google command format.";
     public static final String MESSAGE_LOGIN_FAILURE = "Login unsuccessful";
     public static final String MESSAGE_CONNECTION_FAILURE = "Error connecting to Google Photos, please try again.";
 
-
-
+    /* Error messages canvas and layers. */
+    public static final String MESSAGE_LAYER_SWAP_INVALID_INDEX = "Please specify valid layer indexes!";
 }
