da148f3925f977bcd0594a37f8100ef241b9cb2f
CrimsonJacket <danieltanjiawang@gmail.com>
2018-10-25 20:44:14 +0800

    ui: created new UserLoginEvent

---
+++ src/main/java/seedu/modsuni/commons/events/ui/UserLoginEvent.java
@@ -0,0 +1,21 @@
+package seedu.modsuni.commons.events.ui;
+
+import seedu.modsuni.commons.events.BaseEvent;
+import seedu.modsuni.model.user.User;
+
+/**
+ * An event indicating a user has logged in.
+ */
+public class UserLoginEvent extends BaseEvent {
+
+    public final User currentUser;
+
+    public UserLoginEvent(User user) {
+        currentUser = user;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+}
