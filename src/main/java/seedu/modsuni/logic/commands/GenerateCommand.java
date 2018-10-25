f9e0ee501e1294c1c6771e187738c23f1fa4ae30
rtjl <reubentjl@gmail.com>
2018-10-25 13:38:30 +0800

    Update UI when Generate command executed

--- src/main/java/seedu/modsuni/logic/commands/GenerateCommand.java
+++ src/main/java/seedu/modsuni/logic/commands/GenerateCommand.java
@@ -2,12 +2,16 @@ package seedu.modsuni.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.modsuni.commons.core.EventsCenter;
+import seedu.modsuni.commons.events.ui.NewCommandResultAvailableEvent;
+import seedu.modsuni.commons.events.ui.NewGenerateResultAvailableEvent;
 import seedu.modsuni.logic.CommandHistory;
 import seedu.modsuni.logic.Generate;
 import seedu.modsuni.logic.commands.exceptions.CommandException;
 import seedu.modsuni.model.Model;
 import seedu.modsuni.model.semester.SemesterList;
 import seedu.modsuni.model.user.student.Student;
+import seedu.modsuni.ui.GenerateDisplay;
 
 /**
  * Generates a schedule for the current student user.
@@ -20,10 +24,15 @@ public class GenerateCommand extends Command {
             + " with a student account and try again";
     public static final String MESSAGE_NO_MODULES = "Ensure that you have added module(s) that you "
             + "would like to take before running the generate command";
+    public static final String MESSAGE_ERROR = "Unable to save. Please ensure that you are registered or logged in.";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
+
+        if (model.getCurrentUser() == null) {
+            throw new CommandException(MESSAGE_ERROR);
+        }
 
         if (!model.isStudent()) {
             throw new CommandException(MESSAGE_INVALID_ROLE);
@@ -38,6 +47,13 @@ public class GenerateCommand extends Command {
         Generate generate = new Generate(currentStudent);
         SemesterList semesterList = generate.getSchedule();
         System.out.println(semesterList.toString());
+
+        NewCommandResultAvailableEvent newCommandResultAvailableEvent =
+                new NewCommandResultAvailableEvent();
+        newCommandResultAvailableEvent.toBeDisplayed = new GenerateDisplay();
+        EventsCenter.getInstance().post(newCommandResultAvailableEvent);
+
+        EventsCenter.getInstance().post(new NewGenerateResultAvailableEvent(semesterList));
 
         return new CommandResult(MESSAGE_SUCCESS + "\n" + semesterList.toString());
     }
