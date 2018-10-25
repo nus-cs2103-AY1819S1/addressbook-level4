0be492e1149b872a8380f6a90b392e5fb44a5bf2
CrimsonJacket <danieltanjiawang@gmail.com>
2018-10-25 20:47:49 +0800

    UserTab.java: updated FXML elements and included login event handler

--- src/main/java/seedu/modsuni/ui/UserTab.java
+++ src/main/java/seedu/modsuni/ui/UserTab.java
@@ -1,14 +1,23 @@
 package seedu.modsuni.ui;
 
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.util.Date;
 import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
 import seedu.modsuni.commons.core.LogsCenter;
+import seedu.modsuni.commons.events.ui.UserLoginEvent;
+import seedu.modsuni.model.user.Role;
+import seedu.modsuni.model.user.student.Student;
 
 /**
  * The User tab of the Application.
@@ -17,14 +26,60 @@ public class UserTab extends UiPart<Region> {
 
     private static final Logger logger = LogsCenter.getLogger(UserTab.class);
     private static final String FXML = "UserTab.fxml";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
     private final StringProperty displayed = new SimpleStringProperty("");
 
+    private static Clock clock = Clock.systemDefaultZone();
+
     @FXML
-    private TextArea test;
+    private Label nameLabel;
+
+    @FXML
+    private Text userNameText;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private Text dateText;
+
+    @FXML
+    private Label userDetailLabel1;
+
+    @FXML
+    private Text userDetailText1;
+
+    @FXML
+    private Label userDetailLabel2;
+
+    @FXML
+    private Text userDetailText2;
+
+    @FXML
+    private Label lastSavedLabel;
+
+    @FXML
+    private Text lastSavedText;
+
 
     public UserTab() {
         super(FXML);
-        test.textProperty().bind(displayed);
+        registerAsAnEventHandler(this);
+    }
+
+    @Subscribe
+    public void handleUserLoginEvent(UserLoginEvent event) {
+
+        userNameText.setText(event.currentUser.getName().fullName);
+
+        if (event.currentUser.getRole() == Role.STUDENT) {
+            dateText.setText(((Student) event.currentUser).getEnrollmentDate().enrollmentDate);
+            userDetailText1.setText(((Student) event.currentUser).getMajor().toString());
+            userDetailText2.setText(((Student) event.currentUser).getMinor().toString());
+        }
+
+        lastSavedText.setText(dateFormat.format(new Date(clock.millis())));
+
     }
 }
