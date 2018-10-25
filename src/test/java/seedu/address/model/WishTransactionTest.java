393cebb5045c05e095b306336e8f18256974fd5c
leongshengmin <leongshengmin@gmail.com>
2018-10-25 09:19:57 +0800

    fix import order in WishTransactionTest

--- src/test/java/seedu/address/model/WishTransactionTest.java
+++ src/test/java/seedu/address/model/WishTransactionTest.java
@@ -14,6 +14,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 import seedu.address.model.tag.Tag;
 import seedu.address.model.wish.Date;
 import seedu.address.model.wish.Name;
