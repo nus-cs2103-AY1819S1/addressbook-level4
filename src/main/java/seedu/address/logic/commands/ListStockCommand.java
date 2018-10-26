9884195d165bf3d3589295393addf835eda34685
blewjy <blewjy@gmail.com>
2018-10-27 01:54:44 +0800

    Bug fixes for XmlSerializableAddressBookTest

--- src/main/java/seedu/address/logic/commands/ListStockCommand.java
+++ src/main/java/seedu/address/logic/commands/ListStockCommand.java
@@ -3,12 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_MEDICINES;
 
-import javafx.collections.ObservableList;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ShowMedicineListEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
-import seedu.address.model.medicine.Medicine;
 
 /**
  * Lists all medicines in the records to the user.
