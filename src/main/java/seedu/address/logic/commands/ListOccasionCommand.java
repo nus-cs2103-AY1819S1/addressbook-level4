1970f5d9d60b0eb0ff475881c41ec5e10c270dca
waytan <fiboway@yahoo.com.sg>
2018-10-26 15:53:39 +0800

    Update List Command & Tests, Update Documentation, Correct Typos

---
+++ src/main/java/seedu/address/logic/commands/ListOccasionCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_OCCASIONS;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Lists all occasions in the address book to the user.
+ */
+public class ListOccasionCommand extends Command {
+
+    public static final String COMMAND_WORD = "listoccasion";
+
+    public static final String MESSAGE_SUCCESS = "Listed all occasions";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredOccasionList(PREDICATE_SHOW_ALL_OCCASIONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
