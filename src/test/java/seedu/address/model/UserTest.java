b95d080d8419456b54be9fa904c79822f617b9d6
Jeremy Choo <ChooJeremy@Ymail.com>
2018-10-27 20:07:09 +0800

    Reindent code and added tests for equals and toString

--- src/test/java/seedu/address/model/UserTest.java
+++ src/test/java/seedu/address/model/UserTest.java
@@ -10,22 +10,25 @@ public class UserTest {
 
     @Test
     public void assert_person_parameters() {
-         Person p = TypicalPersons.AMY;
-         User u = new User(p);
-         assert u.getPerson() == p;
-         assert u.getName().equals(p.getName());
-         assert u.getPhone().equals(p.getPhone());
-         assert u.getEmail().equals(p.getEmail());
-         assert u.getAddress().equals(p.getAddress());
-         assert u.getSalary().equals(p.getSalary());
-         assert u.getProfilePic().equals(p.getProfilePic());
-         assert u.getProjects().equals(p.getProjects());
-         assert u.getLeaveApplications().equals(p.getLeaveApplications());
-         assert u.getPermissionSet().equals(p.getPermissionSet());
-         assert u.getUsername().equals(p.getUsername());
-         assert u.getPassword().equals(p.getPassword());
+        Person p = TypicalPersons.AMY;
+        User u = new User(p);
+        assert u.getPerson() == p;
+        assert u.getName().equals(p.getName());
+        assert u.getPhone().equals(p.getPhone());
+        assert u.getEmail().equals(p.getEmail());
+        assert u.getAddress().equals(p.getAddress());
+        assert u.getSalary().equals(p.getSalary());
+        assert u.getProfilePic().equals(p.getProfilePic());
+        assert u.getProjects().equals(p.getProjects());
+        assert u.getLeaveApplications().equals(p.getLeaveApplications());
+        assert u.getPermissionSet().equals(p.getPermissionSet());
+        assert u.getUsername().equals(p.getUsername());
+        assert u.getPassword().equals(p.getPassword());
 
-         assert !u.isAdminUser();
+        assert !u.isAdminUser();
+
+        assert !u.equals(User.getAdminUser());
+        assert u.toString().contains(p.toString());
     }
 
     @Test
@@ -46,5 +49,7 @@ public class UserTest {
         assert u.getPassword().equals(p.getPassword());
 
         assert u.isAdminUser();
+
+        assert u.equals(User.getAdminUser());
     }
 }
