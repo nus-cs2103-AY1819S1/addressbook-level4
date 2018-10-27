a27db888ca4246e9f1e0a7826714c2180e6fe6e5
zioul123 <zioul.rsmod123@gmail.com>
2018-10-27 11:17:03 +0800

    Add the DeleteByNameCommand functionality.

--- src/main/java/seedu/address/logic/commands/DeleteByNameCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteByNameCommand.java
@@ -1,6 +1,13 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.PersonFinderUtil;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 import static java.util.Objects.requireNonNull;
 
@@ -17,5 +24,28 @@ public class DeleteByNameCommand extends DeleteCommand {
         requireNonNull(personIdentifier);
 
         this.personIdentifier = personIdentifier;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        Person personToDelete;
+        try {
+            personToDelete = PersonFinderUtil.findPerson(model, personIdentifier);
+        } catch (ParseException pe) {
+            throw new CommandException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        }
+
+        model.deletePerson(personToDelete);
+        model.commitAddressBook();
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteByNameCommand // instanceof handles nulls
+                && personIdentifier.equals(((DeleteByNameCommand) other).personIdentifier)); // state check
     }
 }
