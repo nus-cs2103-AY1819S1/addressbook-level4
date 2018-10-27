865480aa8edce324a28ba51c7b65f66fecc40001
denzelchung <denzelchung@hotmail.com>
2018-10-27 21:03:51 +0800

    Add test cases.

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE_LOCATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
@@ -19,6 +20,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Picture;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
 /**
@@ -42,6 +44,7 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_CAM = "Block 132, Cammy Street 2";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_PICTURE_PATH = Picture.DEFAULT_PICTURE_URL.getPath();
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
@@ -57,12 +60,14 @@ public class CommandTestUtil {
     public static final String ADDRESS_DESC_CAM = " " + PREFIX_ADDRESS + VALID_ADDRESS_CAM;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
+    public static final String PICTURE_PATH = " " + PREFIX_FILE_LOCATION + VALID_PICTURE_PATH;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+    public static final String INVALID_PICTURE_PATH = " " + PREFIX_FILE_LOCATION + "johndoe.jpg";
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
--- src/test/java/seedu/address/logic/parser/PictureCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PictureCommandParserTest.java
@@ -1,12 +1,19 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_PICTURE_PATH;
+import static seedu.address.logic.commands.CommandTestUtil.PICTURE_PATH;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Test;
 
 import seedu.address.logic.commands.PictureCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Picture;
 
 //@@author denzelchung
 public class PictureCommandParserTest {
@@ -15,16 +22,36 @@ public class PictureCommandParserTest {
         String.format(MESSAGE_INVALID_COMMAND_FORMAT, PictureCommand.MESSAGE_USAGE);
 
     private PictureCommandParser parser = new PictureCommandParser();
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void parse_missingParts_failure() {
-        // no name specified
-        assertParseFailure(parser, "l//images/johndoe.jpg", MESSAGE_INVALID_FORMAT);
+        // no index specified
+        assertParseFailure(parser, PICTURE_PATH, MESSAGE_INVALID_FORMAT);
 
         // no file location specified
-        assertParseFailure(parser, NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, INDEX_FIRST_PERSON.getOneBased() + "", MESSAGE_INVALID_FORMAT);
 
         // no name and no file location specified
         assertParseFailure(parser, "", MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void parse_invalidPreamble_failure() {
+        // negative index
+        assertParseFailure(parser, "-5" + INDEX_FIRST_PERSON, MESSAGE_INVALID_FORMAT);
+
+        // zero index
+        assertParseFailure(parser, "0" + INDEX_FIRST_PERSON, MESSAGE_INVALID_FORMAT);
+
+        // positive index with a plus sign
+        assertParseFailure(parser, "+5" + INDEX_FIRST_PERSON, MESSAGE_INVALID_FORMAT);
+    }
+
+    @Test
+    public void runTest_invalidValue_failure() {
+        // invalid image
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + INVALID_PICTURE_PATH;
+        assertParseFailure(parser, userInput, Picture.MESSAGE_PICTURE_CONSTRAINTS);
     }
 }
