4622462b1e4ccafcf8139607e55efe1748829a23
rtjl <reubentjl@gmail.com>
2018-10-27 17:05:42 +0800

    Add #canGenerate for Generate
    To be used for checking before producing a schedule.

--- src/main/java/seedu/modsuni/logic/Generate.java
+++ src/main/java/seedu/modsuni/logic/Generate.java
@@ -48,6 +48,26 @@ public class Generate {
     }
 
     /**
+     * Checks if a schedule can be generated or not.
+     * @return
+     */
+    public static Optional<List<Code>> canGenerate(Student student) {
+        List<Code> cannotTakeCode = new ArrayList<>();
+        List<Code> codeChecklist = student.getTakenAndStageCode();
+        UniqueModuleList modulesStaged = student.getModulesStaged();
+        for (Module module : modulesStaged) {
+            if (!module.checkPrereq(codeChecklist)) {
+                cannotTakeCode.add(module.getCode());
+            }
+        }
+        if (cannotTakeCode.size() == 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(cannotTakeCode);
+        }
+    }
+
+    /**
      * Creates an edge from a module code to another module code.
      */
     public void addEdge(Code fromCode, Code toCode) {
