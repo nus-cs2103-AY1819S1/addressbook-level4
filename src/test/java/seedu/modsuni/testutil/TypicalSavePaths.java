aa30d3a6c2dca02db4ced89989fd0ffe8c360170
CrimsonJacket <danieltanjiawang@gmail.com>
2018-10-27 12:30:26 +0800

    test: updated test cases for new RegisterCommand & incl. TypicalSavePaths.java

---
+++ src/test/java/seedu/modsuni/testutil/TypicalSavePaths.java
@@ -0,0 +1,17 @@
+package seedu.modsuni.testutil;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * A utility class containing a list of {@code Path} objects to be used
+ * in tests.
+ */
+public class TypicalSavePaths {
+
+    public static final Path PATH_USERDATA_MAX =
+        Paths.get("max33.xml");
+
+    public static final Path PATH_USERDATA_SEB =
+        Paths.get("seb5.xml");
+}
