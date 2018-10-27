cadf45be6f94fe6289b5979d2f18d3904b0e792a
NancyQuris <liuyingnan1999@gmail.com>
2018-10-27 23:19:42 +0800

    fix coding style error

--- src/main/java/seedu/jxmusic/ui/MainWindow.java
+++ src/main/java/seedu/jxmusic/ui/MainWindow.java
@@ -203,7 +203,7 @@ public class MainWindow extends UiPart<Stage> {
     }
 
     //void releaseResources() {
-        //browserPanel.freeResources();
+    //browserPanel.freeResources();
     //}
 
     @Subscribe
