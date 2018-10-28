980208bd41addd437aed17f9a8b0574b866a2227
Jeremy Choo <ChooJeremy@Ymail.com>
2018-10-28 22:47:20 +0800

    Added basics of password command

---
+++ src/main/java/seedu/address/logic/commands/PasswordCommand.java
@@ -0,0 +1,25 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.LogoutEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Command to change the password
+ */
+public class PasswordCommand extends Command {
+
+    public static final String COMMAND_WORD = "Password";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Change your password\n"
+        + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_LOGOUT_MESSAGE = "Password changed!";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        EventsCenter.getInstance().post(new LogoutEvent());
+        return new CommandResult(SHOWING_LOGOUT_MESSAGE);
+    }
+}
