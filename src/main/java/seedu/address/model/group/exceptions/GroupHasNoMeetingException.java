14f237b82b2ed28bb14a729a03af90f40911c1de
Zheng Wei <7553066+Zenious@users.noreply.github.com>
2018-10-29 03:53:29 +0800

    Update src/main/java/seedu/address/model/group/exceptions/GroupHasNoMeetingException.java
    Co-Authored-By: NyxF4ll <vosk1093@gmail.com>

--- src/main/java/seedu/address/model/group/exceptions/GroupHasNoMeetingException.java
+++ src/main/java/seedu/address/model/group/exceptions/GroupHasNoMeetingException.java
@@ -3,6 +3,5 @@ package seedu.address.model.group.exceptions;
 /**
  * Signals that the group has no meeting.
  *
- * {@author NyxF4ll}
  */
 public class GroupHasNoMeetingException extends RuntimeException {}
