fa47cd3cc206f4ced38b116316602fb20ff6462b
Kok Yuan <warfear@hotmail.com>
2018-10-28 15:18:51 +0800

    Added tests to DeleteEmailCommandTest

--- src/test/java/seedu/address/logic/commands/DeleteEmailCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteEmailCommandTest.java
@@ -1,17 +1,22 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalEmails.getTypicalExistingEmails;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.simplejavamail.email.Email;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.BudgetBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.email.Subject;
+import seedu.address.testutil.DefaultEmailBuilder;
 
 //@@author EatOrBeEaten
 public class DeleteEmailCommandTest {
@@ -29,4 +34,36 @@ public class DeleteEmailCommandTest {
         thrown.expect(NullPointerException.class);
         new DeleteEmailCommand(null);
     }
+
+    @Test
+    public void execute_validSubject_success() {
+        Email emailToDelete = new DefaultEmailBuilder().build();
+        DeleteEmailCommand deleteEmailCommand = new DeleteEmailCommand(new Subject(emailToDelete.getSubject()));
+
+        ModelManager modelWithEmailToDelete = new ModelManager(model.getAddressBook(), new BudgetBook(),
+                new UserPrefs(), model.getExistingEmails());
+        modelWithEmailToDelete.saveComposedEmail(emailToDelete);
+
+        String expectedMessage = String.format(DeleteEmailCommand.MESSAGE_SUCCESS, emailToDelete.getSubject());
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new BudgetBook(),
+                new UserPrefs(), model.getExistingEmails());
+
+        assertCommandSuccess(deleteEmailCommand, modelWithEmailToDelete,
+                commandHistory, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidSubject_success() {
+        Email emailToDelete = new DefaultEmailBuilder().build();
+        DeleteEmailCommand deleteEmailCommand = new DeleteEmailCommand(new Subject(emailToDelete.getSubject()));
+
+        ModelManager modelWithoutEmailToDelete = new ModelManager(model.getAddressBook(), new BudgetBook(),
+                new UserPrefs(), model.getExistingEmails());
+
+        String expectedMessage = String.format(DeleteEmailCommand.MESSAGE_EMAIL_DOES_NOT_EXIST, emailToDelete.getSubject());
+
+        assertCommandFailure(deleteEmailCommand, modelWithoutEmailToDelete, commandHistory, expectedMessage);
+    }
+
 }
