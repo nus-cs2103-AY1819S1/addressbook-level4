c98ff441ac7014914ecc863f1f0e292c29d3f2d3
leong eng sea <leongengsea@gmail.com>
2018-10-28 20:17:33 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/logic/parser/OpenCommandParser.java
+++ src/main/java/seedu/address/logic/parser/OpenCommandParser.java
@@ -12,7 +12,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new OpenCommand object
  */
 
-public class OpenCommandParser implements Parser<OpenCommand>{
+public class OpenCommandParser implements Parser<OpenCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the OpenCommand
      * and returns an OpenCommand object for execution.
