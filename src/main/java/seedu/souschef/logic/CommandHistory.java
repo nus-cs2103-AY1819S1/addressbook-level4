4b2728bc60ae55ea947cd9ba0b17a0aab11399e7
barkhyehyeon <barkhyehyeon@gmail.com>
2018-10-29 01:08:40 +0800

    Add getKeyword to history and update commands accordingly

--- src/main/java/seedu/souschef/logic/CommandHistory.java
+++ src/main/java/seedu/souschef/logic/CommandHistory.java
@@ -32,6 +32,13 @@ public class CommandHistory {
         return context;
     }
 
+    public String getKeyword() {
+        if (context.equals(Context.CROSS)) {
+            return "recipe";
+        }
+        return context.toString().toLowerCase();
+    }
+
     public void setContext(Context context) {
         this.context = context;
     }
