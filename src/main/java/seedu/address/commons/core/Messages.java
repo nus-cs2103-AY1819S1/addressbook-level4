c95b1d81f267f790db39700e8d579e4f2a004e64
bannified <freedomzelda_dc@hotmail.com>
2018-10-28 11:48:23 +0800

    Add MESSAGE_INVALID_PERIOD_FORMAT

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,6 +9,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command FORMAT! \n%1$s";
     public static final String MESSAGE_DOUBLE_DATE_FORMAT = "Both Period and Dates are stated. " +
                                                                 "Only one can be used!\n";
+    public static final String MESSAGE_INVALID_PERIOD_FORMAT = "Invalid Period FORMAT! \n%1$s" +
+            "\nShould be [years]y[months]m[days]d! Take note of the order!";
     public static final String MESSAGE_INVALID_WISH_DISPLAYED_INDEX = "The wish index provided is invalid";
     public static final String MESSAGE_WISHES_LISTED_OVERVIEW = "%1$d wishes listed!";
     public static final String MESSAGE_INVALID_AMOUNT = "Invalid amount! \n%1$s";
