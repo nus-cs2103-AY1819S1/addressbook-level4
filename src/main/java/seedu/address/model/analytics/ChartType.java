a1f05297031e1a424d316370a9d3df1a0acfe8e7
Arsaln Cheema <arsalanc.v2@gmail.com>
2018-10-29 00:21:07 +0800

    add enums

---
+++ src/main/java/seedu/address/model/analytics/ChartType.java
@@ -0,0 +1,23 @@
+package seedu.address.model.analytics;
+
+/**
+ *
+ */
+public enum ChartType {
+    VERTICAL_BAR("vbar"),
+    HORIZONTAL_BAR("hbar"),
+    STACKED_BAR("stackedbar"),
+    LINE("line"),
+    AREA("area");
+
+    private final String type;
+
+    ChartType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
