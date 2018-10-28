ae29f4cbc323ecea8fd4b83663307d488e81e0c4
LAPTOP-E3JOI16D\clint <clawyq@hotmail.com>
2018-10-28 19:44:12 +0800

    Update Document and Receipt classes to further decouple them and changed DocumentContentAdd to ensure that the number of MC days is a positive integer.

--- src/main/java/seedu/address/model/document/Receipt.java
+++ src/main/java/seedu/address/model/document/Receipt.java
@@ -2,7 +2,11 @@ package seedu.address.model.document;
 
 import java.util.ArrayList;
 
+import java.util.Map;
+import seedu.address.model.medicine.Medicine;
+import seedu.address.model.medicine.QuantityToDispense;
 import seedu.address.model.person.ServedPatient;
+import seedu.address.model.services.Service;
 
 /**
  * Represents the receipt for the served patients. This class is responsible for extracting information that is
@@ -10,6 +14,10 @@ import seedu.address.model.person.ServedPatient;
  */
 public class Receipt extends Document {
     public static final String FILE_TYPE = "Receipt";
+
+    private float totalPrice = 0;
+    private Map<Medicine, QuantityToDispense> allocatedMedicine;
+    private ArrayList<Service> servicesRendered;
 
     /**
      * Creates a receipt object for the specified servedPatient.
@@ -19,7 +27,23 @@ public class Receipt extends Document {
         setFileType(FILE_TYPE);
         setName(servedPatient.getName());
         setIcNumber(servedPatient.getIcNumber());
-        setAllocatedMedicine(servedPatient.getMedicineAllocated());
-        setServicesRendered(new ArrayList<>());
+        allocatedMedicine = servedPatient.getMedicineAllocated();
+        servicesRendered = new ArrayList<>();
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void increaseTotalPriceBy(int increment) {
+        totalPrice += increment;
+    }
+
+    public Map<Medicine, QuantityToDispense> getAllocatedMedicine() {
+        return allocatedMedicine;
+    }
+
+    public ArrayList<Service> getServicesRendered() {
+        return servicesRendered;
     }
 }
