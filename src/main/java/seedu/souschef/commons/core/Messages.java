cea72ebf442582579c44853323bc2eb4b67d0d16
junrong09 <jun_rong5@hotmail.com>
2018-10-29 15:17:04 +0800

    Messages.java: Add invalid instruction index.

--- src/main/java/seedu/souschef/commons/core/Messages.java
+++ src/main/java/seedu/souschef/commons/core/Messages.java
@@ -28,6 +28,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_RECIPE_DISPLAYED_INDEX = "The recipe index provided is invalid";
+    public static final String MESSAGE_INVALID_INSTRUCTION_INDEX = "The instruction index provided is invalid";
     public static final String MESSAGE_LISTED_OVERVIEW = "%1$d %2$ss listed!";
     public static final String MESSAGE_DUPLICATE = "This %1$s already exists.";
     public static final String MESSAGE_NO_ELEMENT = "There is no matching result";
