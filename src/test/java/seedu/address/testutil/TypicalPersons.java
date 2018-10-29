d1d7f9648de4da8e5909e16929d123dcafdc04aa
Auyok Sean <sean.auyok@gmail.com>
2018-10-29 19:04:01 +0800

    Update typicalPersonsAddressBook to a functioning contact list

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -35,12 +35,14 @@ public class TypicalPersons {
             .withPicture("/images/placeholder_image.jpg").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com").withAddress("wall street")
+            .withTags("Singaporean", "OCBC")
             .withPicture("/images/placeholder_image.jpg").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends")
             .withPicture("/images/placeholder_image.jpg").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withAddress("michegan ave")
+            .withTags("Singaporean")
             .withPicture("/images/placeholder_image.jpg").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
             .withEmail("lydia@example.com").withAddress("little tokyo")
@@ -48,11 +50,13 @@ public class TypicalPersons {
             .withPicture("/images/placeholder_image.jpg").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street")
+            .withTags("OCBC")
             .withPicture("/images/placeholder_image.jpg").build();
     // Without certain fields (still typical)
     // Without phone
     public static final Person HENRY = new PersonBuilder().withName("Henry Golding").withoutPhone()
             .withEmail("henry@example.com").withAddress("Crazy Rich Street")
+            .withTags("Singaporean", "POSB")
             .withPicture("/images/placeholder_image.jpg").build();
     // Without email
     public static final Person IANNA = new PersonBuilder().withName("Ianna Cluse").withPhone("83848586")
@@ -61,39 +65,6 @@ public class TypicalPersons {
     // Without address
     public static final Person JENNY = new PersonBuilder().withName("Jenny Khiu").withPhone("91234523")
             .withEmail("jenny@example.com").withoutAddress()
-            .withPicture("/images/placeholder_image.jpg").build();
-    // Contacts with tags to be deleted
-    public static final Person KENNETH = new PersonBuilder().withName("Kenneth Koh").withPhone("92345085")
-            .withEmail("kenneth@example.com").withAddress("1st Avenue")
-            .withTags("Korean", "Important")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person LEONARD = new PersonBuilder().withName("Leonard Low").withPhone("84325731")
-            .withEmail("leonard@example.com").withAddress("2nd Avenue")
-            .withTags("Japanese", "Important")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person MICHAEL = new PersonBuilder().withName("Michael Moore").withPhone("94325165")
-            .withEmail("michael@example.com").withAddress("3rd Avenue")
-            .withTags("Chinese", "Colleague")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person NOEL = new PersonBuilder().withName("Noel Tan").withPhone("85743754")
-            .withEmail("noel@example.com").withAddress("4th Avenue")
-            .withTags("Rich", "Client")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person OPENHEIMER = new PersonBuilder().withName("Openheimer Ang").withPhone("93424521")
-            .withEmail("openheimer@example.com").withAddress("5th Avenue")
-            .withTags("Singaporean", "OCBC")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person PERCY = new PersonBuilder().withName("Percy Jackson").withPhone("83242542")
-            .withEmail("percy@example.com").withAddress("6th Avenue")
-            .withTags("Singaporean")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person QUINE = new PersonBuilder().withName("Quine Ang").withPhone("94346903")
-            .withEmail("quine@example.com").withAddress("7th Avenue")
-            .withTags("OCBC")
-            .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person RICHARD = new PersonBuilder().withName("Richard Lim").withPhone("87779042")
-            .withEmail("richard@example.com").withAddress("8th Avenue")
-            .withTags("Singaporean", "POSB")
             .withPicture("/images/placeholder_image.jpg").build();
 
     // Manually added
@@ -110,20 +81,23 @@ public class TypicalPersons {
             .build();
 
     // Untagged persons
-    public static final Person OPENHEIMER_UNTAGGED = new PersonBuilder().withName("Openheimer Ang")
-            .withPhone("93424521")
-            .withEmail("openheimer@example.com").withAddress("5th Avenue")
+    public static final Person CARL_UNTAGGED = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
+            .withEmail("heinz@example.com").withAddress("wall street")
             .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person PERCY_UNTAGGED = new PersonBuilder().withName("Percy Jackson").withPhone("83242542")
-            .withEmail("percy@example.com").withAddress("6th Avenue")
+    public static final Person ELLE_UNTAGGED = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
+            .withEmail("werner@example.com").withAddress("michegan ave")
             .withPicture("/images/placeholder_image.jpg").build();
-    public static final Person QUINE_UNTAGGED = new PersonBuilder().withName("Quine Ang").withPhone("94346903")
-            .withEmail("quine@example.com").withAddress("7th Avenue")
-            .withPicture("/images/place_holder_image.jpg").build();
-    public static final Person RICHARD_UNTAGGED = new PersonBuilder().withName("Richard Lim").withPhone("87779042")
-            .withEmail("richard@example.com").withAddress("8th Avenue")
+    public static final Person GEORGE_UNTAGGED = new PersonBuilder().withName("George Best").withPhone("9482442")
+            .withEmail("anna@example.com").withAddress("4th street")
+            .withPicture("/images/placeholder_image.jpg").build();
+    public static final Person HENRY_SINGAPOREAN = new PersonBuilder().withName("Henry Golding").withoutPhone()
+            .withEmail("henry@example.com").withAddress("Crazy Rich Street")
+            .withTags("Singaporean")
+            .withPicture("/images/placeholder_image.jpg").build();
+    public static final Person HENRY_POSB = new PersonBuilder().withName("Henry Golding").withoutPhone()
+            .withEmail("henry@example.com").withAddress("Crazy Rich Street")
             .withTags("POSB")
-            .withPicture("/images/place_holder_image.jpg").build();
+            .withPicture("/images/placeholder_image.jpg").build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
@@ -141,7 +115,6 @@ public class TypicalPersons {
     }
 
     public static List<Person> getTypicalPersons() {
-        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE, HENRY, IANNA, JENNY,
-                OPENHEIMER, PERCY, QUINE, RICHARD));
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE, HENRY, IANNA, JENNY));
     }
 }
