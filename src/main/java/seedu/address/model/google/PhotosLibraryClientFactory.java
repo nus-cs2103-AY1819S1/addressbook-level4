f49085a7114048e7b1e47bcbcb8867d13115c914
chivent <e0191350@u.nus.edu>
2018-10-30 01:45:50 +0800

    check style fix

--- src/main/java/seedu/address/model/google/PhotosLibraryClientFactory.java
+++ src/main/java/seedu/address/model/google/PhotosLibraryClientFactory.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.List;
