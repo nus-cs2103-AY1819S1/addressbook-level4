4c402e9c5b1b5c207f6154379a29117e27f606d7
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-30 01:59:43 +0800

    Fix checkstyle errors
    - Remove trailing whitespace.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/clinicio/logic/commands/AddPatientCommand.java
+++ src/main/java/seedu/clinicio/logic/commands/AddPatientCommand.java
@@ -2,17 +2,18 @@ package seedu.clinicio.logic.commands;
 
 import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_IC;
 import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.clinicio.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.clinicio.logic.CommandHistory;
 import seedu.clinicio.logic.commands.exceptions.CommandException;
+
 import seedu.clinicio.model.Model;
 import seedu.clinicio.model.analytics.Analytics;
 
 //@@author jjlee050
-
 /**
  * Adds a patient to ClinicIO.
  */
@@ -23,12 +24,14 @@ public class AddPatientCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a patient to the ClinicIO. "
             + "Parameters: "
             + PREFIX_NAME + "NAME "
+            + PREFIX_IC + " IC "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
+            + PREFIX_IC + " S1234567A "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
@@ -36,7 +39,7 @@ public class AddPatientCommand extends Command {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New patient added: %1$s";
-    
+
     @Override
     public CommandResult execute(Model model, CommandHistory history, Analytics analytics)
             throws CommandException {
