6d510cf97c502cf44987917a91959a00219a8de7
chantca95 <chantca95@gmail.com>
2018-10-29 11:37:00 +0800

    checkStyle 1

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -21,8 +21,8 @@ public class ExportCommandTest {
     private CommandHistory commandHistory;
     private ExportCommand exportCommand = new ExportCommand(EXPORTED_FILE_NAME);
 
-    private static final String EXPECTED_FILE_NAME = "expectedExport.csv";
     private static final String EXPORTED_FILE_NAME = "exportCommandTest.csv";
+    private static final String EXPECTED_FILE_NAME = "expectedExport.csv";
 
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -35,27 +35,30 @@ public class ExportCommandTest {
         CommandResult result = exportCommand.execute(model, commandHistory);
         File produced = new File(EXPORTED_FILE_NAME);
         File expected = new File(EXPECTED_FILE_NAME);
-        boolean isTwoEqual = CompareFilesbyByte(produced, expected);
+        boolean isTwoEqual = compareFiles(produced, expected);
         assert(isTwoEqual);
     }
 
-    public static boolean CompareFilesbyByte(File produced, File expected) throws IOException {
+    /**
+     * Helper method to check if the exported file is the same as the expected one
+     */
+    public static boolean compareFiles(File produced, File expected) throws IOException {
         boolean isIdentical = true;
         FileInputStream fis1 = new FileInputStream (produced);
         FileInputStream fis2 = new FileInputStream (expected);
         if (produced.length() == expected.length()) {
-            int n=0;
+            int n = 0;
             byte[] b1;
             byte[] b2;
             while ((n = fis1.available()) > 0) {
-                if (n>80) {
-                    n=80;
+                if (n > 80) {
+                    n = 80;
                 }
                 b1 = new byte[n];
                 b2 = new byte[n];
                 fis1.read(b1);
                 fis2.read(b2);
-                if (Arrays.equals(b1,b2)==false) {
+                if (Arrays.equals(b1, b2) == false) {
                     System.out.println(produced + " :\n\n " + new String(b1));
                     System.out.println();
                     System.out.println(expected + " : \n\n" + new String(b2));
@@ -63,11 +66,13 @@ public class ExportCommandTest {
                 }
             }
         } else {
-            isIdentical = false;// length is not matched. 
+            //if length does not match.
+            isIdentical = false;
         }
         fis1.close();
         fis2.close();
-        produced.delete(); // delete the generated file so that the test can be rerun
+        // delete the generated file so that the test can be rerun
+        produced.delete();
         return isIdentical;
     }
 }
