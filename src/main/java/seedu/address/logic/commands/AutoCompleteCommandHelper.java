819aec57fe429ed8a19a1a3c417d884aa477cbf0
Kok Jian Yu <kokjianyu@gmail.com>
2018-10-29 22:33:42 +0800

    Add 'viewpermission' to autocomplete list

--- src/main/java/seedu/address/logic/commands/AutoCompleteCommandHelper.java
+++ src/main/java/seedu/address/logic/commands/AutoCompleteCommandHelper.java
@@ -5,23 +5,25 @@ package seedu.address.logic.commands;
  */
 public class AutoCompleteCommandHelper {
     private static String[] commandWordList = {
-        AddCommand.COMMAND_WORD,
-        ClearCommand.COMMAND_WORD,
-        DeleteCommand.COMMAND_WORD,
-        EditCommand.COMMAND_WORD,
-        ExitCommand.COMMAND_WORD,
-        FindCommand.COMMAND_WORD,
-        HelpCommand.COMMAND_WORD,
-        HistoryCommand.COMMAND_WORD,
-        ListCommand.COMMAND_WORD,
-        RedoCommand.COMMAND_WORD,
-        SelectCommand.COMMAND_WORD,
-        UndoCommand.COMMAND_WORD,
-        ModifyPermissionCommand.COMMAND_WORD
+            AddCommand.COMMAND_WORD,
+            ClearCommand.COMMAND_WORD,
+            DeleteCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD,
+            ExitCommand.COMMAND_WORD,
+            FindCommand.COMMAND_WORD,
+            HelpCommand.COMMAND_WORD,
+            HistoryCommand.COMMAND_WORD,
+            ListCommand.COMMAND_WORD,
+            RedoCommand.COMMAND_WORD,
+            SelectCommand.COMMAND_WORD,
+            UndoCommand.COMMAND_WORD,
+            ModifyPermissionCommand.COMMAND_WORD,
+            ViewPermissionCommand.COMMAND_WORD
     };
 
     /**
      * This method predicts the command the user is entering.
+     *
      * @param partialWord The current characters available in command box.
      * @return The predicted command.
      */
