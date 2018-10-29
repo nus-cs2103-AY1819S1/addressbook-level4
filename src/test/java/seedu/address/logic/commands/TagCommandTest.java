9a2ff9a636a0a2ee16152995843b90fc87d0b308
Auyok Sean <sean.auyok@gmail.com>
2018-10-29 19:04:13 +0800

    Update TagCommandTest

--- src/test/java/seedu/address/logic/commands/TagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TagCommandTest.java
@@ -4,16 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_TAGGED_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.commons.core.Messages.MESSAGE_TAG_DELETED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.OPENHEIMER;
-import static seedu.address.testutil.TypicalPersons.OPENHEIMER_UNTAGGED;
-import static seedu.address.testutil.TypicalPersons.PERCY;
-import static seedu.address.testutil.TypicalPersons.PERCY_UNTAGGED;
-import static seedu.address.testutil.TypicalPersons.QUINE;
-import static seedu.address.testutil.TypicalPersons.QUINE_UNTAGGED;
-import static seedu.address.testutil.TypicalPersons.RICHARD;
-import static seedu.address.testutil.TypicalPersons.RICHARD_UNTAGGED;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.ELLE;
+import static seedu.address.testutil.TypicalPersons.GEORGE;
+import static seedu.address.testutil.TypicalPersons.HENRY;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
@@ -44,35 +39,23 @@ public class TagCommandTest {
                 Collections.singletonList("first"));
         TagCommand findSecondCommand = new TagCommand(secondPredicate, TagCommand.Action.FIND,
                 Collections.singletonList("second"));
-        TagCommand deleteFirstCommand = new TagCommand(firstPredicate, TagCommand.Action.DELETE,
-                Collections.singletonList("first"));
-        TagCommand deleteSecondCommand = new TagCommand(secondPredicate, TagCommand.Action.DELETE,
-                Collections.singletonList("second"));
 
         // same object -> returns true
         assertTrue(findFirstCommand.equals(findFirstCommand));
-        assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
 
         // same values -> returns true
         TagCommand findFirstCommandCopy = new TagCommand(firstPredicate, TagCommand.Action.FIND,
                 Collections.singletonList("first"));
         assertTrue(findFirstCommand.equals(findFirstCommandCopy));
-        TagCommand deleteFirstCommandCopy = new TagCommand(firstPredicate, TagCommand.Action.DELETE,
-                Collections.singletonList("first"));
-        assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
 
         // different types -> returns false
         assertFalse(findFirstCommand.equals(1));
-        assertFalse(deleteFirstCommand.equals(1));
 
         // null -> returns false
         assertFalse(findFirstCommand.equals(null));
-        assertFalse(deleteFirstCommand.equals(null));
 
         // different tag -> returns false
         assertFalse(findFirstCommand.equals(findSecondCommand));
-        assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
-        assertTrue(findFirstCommand.equals(deleteFirstCommand));
     }
 
     @Test
@@ -93,37 +76,7 @@ public class TagCommandTest {
                 "important"));
         expectedModel.updateFilteredPersonList(predicate);
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(OPENHEIMER, PERCY, QUINE, RICHARD), model.getFilteredPersonList());
-    }
-
-    // @Test
-    /**
-     * Test to be implemented
-     * @throws Exception
-     */
-    private void execute_zeroKeywords_noTagDeleted() {
-        String expectedMessage = String.format(MESSAGE_TAG_DELETED_OVERVIEW, 0);
-        PersonContainsTagPredicate predicate = preparePredicate(" ");
-        TagCommand command = new TagCommand(predicate, TagCommand.Action.DELETE, Collections.singletonList(" "));
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel);
-        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
-    }
-
-    // @Test
-    /**
-     * Test to be implemented
-     * @throws Exception
-     */
-    private void execute_multipleKeywords_multipleTagsDeleted() {
-        String expectedMessage = String.format(MESSAGE_TAG_DELETED_OVERVIEW, 4);
-        PersonContainsTagPredicate predicate = preparePredicate("Singaporean OCBC important");
-        TagCommand command = new TagCommand(predicate, TagCommand.Action.DELETE, Arrays.asList("friends", "owesMoney",
-                "important"));
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(OPENHEIMER_UNTAGGED, PERCY_UNTAGGED, QUINE_UNTAGGED, RICHARD_UNTAGGED),
-                model.getFilteredPersonList());
+        assertEquals(Arrays.asList(CARL, ELLE, GEORGE, HENRY), model.getFilteredPersonList());
     }
 
     /**
