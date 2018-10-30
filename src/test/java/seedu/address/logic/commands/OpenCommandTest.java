830c393baef015bb52fe21c1a827d69886eeb14c
leong eng sea <leongengsea@gmail.com>
2018-10-30 18:00:29 +0800

    Update OpenCommandTest.java.

--- src/test/java/seedu/address/logic/commands/OpenCommandTest.java
+++ src/test/java/seedu/address/logic/commands/OpenCommandTest.java
@@ -2,9 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_RIDE;
@@ -15,14 +13,9 @@ import org.junit.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.UpdateCommand.UpdateRideDescriptor;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.ThanePark;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.ride.Ride;
-import seedu.address.testutil.RideBuilder;
-import seedu.address.testutil.UpdateRideDescriptorBuilder;
 
 /**
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
@@ -32,37 +25,6 @@ public class OpenCommandTest {
 
     private Model model = new ModelManager(getTypicalThanePark(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
-
-    @Test
-    public void execute_noFieldSpecifiedUnfilteredList_success() {
-        UpdateCommand editCommand = new UpdateCommand(INDEX_FIRST_PERSON, new UpdateRideDescriptor());
-        Ride editedRide = model.getFilteredRideList().get(INDEX_FIRST_PERSON.getZeroBased());
-
-        String expectedMessage = String.format(UpdateCommand.MESSAGE_UPDATE_RIDE_SUCCESS, editedRide);
-
-        Model expectedModel = new ModelManager(new ThanePark(model.getAddressBook()), new UserPrefs());
-        expectedModel.commitAddressBook();
-
-        assertCommandSuccess(editCommand, model, commandHistory, expectedMessage, expectedModel);
-    }
-
-    @Test
-    public void execute_filteredList_success() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-
-        Ride rideInFilteredList = model.getFilteredRideList().get(INDEX_FIRST_PERSON.getZeroBased());
-        Ride editedRide = new RideBuilder(rideInFilteredList).withName(VALID_NAME_BOB).build();
-        UpdateCommand editCommand = new UpdateCommand(INDEX_FIRST_PERSON,
-                new UpdateRideDescriptorBuilder().withName(VALID_NAME_BOB).build());
-
-        String expectedMessage = String.format(UpdateCommand.MESSAGE_UPDATE_RIDE_SUCCESS, editedRide);
-
-        Model expectedModel = new ModelManager(new ThanePark(model.getAddressBook()), new UserPrefs());
-        expectedModel.updatePerson(model.getFilteredRideList().get(0), editedRide);
-        expectedModel.commitAddressBook();
-
-        assertCommandSuccess(editCommand, model, commandHistory, expectedMessage, expectedModel);
-    }
 
     @Test
     public void execute_duplicateRideUnfilteredList_failure() {
@@ -75,7 +37,7 @@ public class OpenCommandTest {
     public void execute_duplicateRideFilteredList_failure() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        // edit ride in filtered list into a duplicate in address book
+        //open ride in filtered list into a duplicate in address book
         OpenCommand openCommand = new OpenCommand(INDEX_FIRST_PERSON);
 
         assertCommandFailure(openCommand, model, commandHistory, OpenCommand.MESSAGE_DUPLICATE_RIDE);
