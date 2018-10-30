4cf3a1c8b43a1a8cddae59455eb303ee21265566
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-30 20:21:44 +0800

    Update MainApp.java
    Move version to v1.3

--- src/main/java/seedu/clinicio/MainApp.java
+++ src/main/java/seedu/clinicio/MainApp.java
@@ -40,7 +40,7 @@ import seedu.clinicio.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 2, 1, false);
+    public static final Version VERSION = new Version(1, 3, 0, false);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
