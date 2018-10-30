4c31bdfd87bcb8d7b07cf42e1e39c3ed5c9f8fd4
pratyayj <pratyay.jaidev@gmail.com>
2018-10-30 20:03:58 +0800

    Add GenerateLocationCommand and GenerateLocationCommandParser but do not do perform any action yet.

--- src/main/java/seedu/address/logic/commands/GenerateLocationCommand.java
+++ src/main/java/seedu/address/logic/commands/GenerateLocationCommand.java
@@ -1,9 +1,12 @@
 package seedu.address.logic.commands;
 
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.event.Event;
+import seedu.address.model.event.EventName;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
@@ -23,33 +26,47 @@ public class GenerateLocationCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Meeting location generated!";
 
-    private final Event meetingLocationEvent;
+
+
+    private final EventName meetingLocationEventName;
 
     /**
      * Creates an AddCommand to add the specified {@code Person}
      */
-    public GenerateLocationCommand(Event event) {
-        requireNonNull(event);
-        meetingLocationEvent = event;
+    public GenerateLocationCommand(EventName eventName) {
+        requireNonNull(eventName);
+        meetingLocationEventName = eventName;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        if (!model.hasEvent(meetingLocationEvent)) {
+        ObservableList<Event> eventList = model.getFilteredEventList();
+        boolean isEventFound = false;
+
+        for (Event event : eventList) {
+            if (event.getEventName().equals(meetingLocationEventName)) {
+                isEventFound = true;
+                break;
+            }
+        }
+
+        if (isEventFound == false) {
             throw new CommandException(MESSAGE_EVENT_DOES_NOT_EXIST);
         }
 
+
+
         // TODO - to work on what to do after command
-        model.commitAddressBook();
-        return new CommandResult(String.format(MESSAGE_SUCCESS, meetingLocationEvent));
+        // model.commitAddressBook();
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof GenerateLocationCommand // instanceof handles nulls
-                && meetingLocationEvent.equals(((GenerateLocationCommand) other).meetingLocationEvent));
+                && meetingLocationEventName.equals(((GenerateLocationCommand) other).meetingLocationEventName));
     }
 }
