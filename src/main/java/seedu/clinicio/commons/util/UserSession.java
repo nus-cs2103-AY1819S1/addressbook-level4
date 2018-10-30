4694d80de156181a96e2f81dfd3b22500fbe3312
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-30 11:28:27 +0800

    UserSession.java: Update session class
    - Add new methods for UserSession.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/clinicio/commons/util/UserSession.java
+++ src/main/java/seedu/clinicio/commons/util/UserSession.java
@@ -2,7 +2,33 @@ package seedu.clinicio.commons.util;
 
 import seedu.clinicio.model.staff.Staff;
 
+//@@author jjlee050
+/**
+ * A utility class to handle user session in ClinicIO
+ */
 public class UserSession {
     private static Staff currentUser;
-    
+
+    /**
+     * Create a new user session for ClinicIO.
+     * @param user Authenticated user to add into user session
+     */
+    public static void createSession(Staff user) {
+        currentUser = user;
+    }
+
+    /**
+     * Delete the user session from ClinicIO.
+     */
+    public static void destorySession() {
+        currentUser = null;
+    }
+
+    /**
+     * Check if user is login to ClinicIO.
+     * @return The status of any user session.
+     */
+    public static boolean isLogin() {
+        return currentUser != null;
+    }
 }
