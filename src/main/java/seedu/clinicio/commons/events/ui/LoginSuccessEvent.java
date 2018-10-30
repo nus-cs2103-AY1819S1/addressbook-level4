b8ed053603a72e5bf5abac50db532a40efd73766
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-10-30 12:18:26 +0800

    LoginSuccessEvent.java: Handle login event.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/clinicio/commons/events/ui/LoginSuccessEvent.java
+++ src/main/java/seedu/clinicio/commons/events/ui/LoginSuccessEvent.java
@@ -1,5 +1,21 @@
 package seedu.clinicio.commons.events.ui;
 
-public class LoginSuccessEvent {
+import seedu.clinicio.commons.events.BaseEvent;
+import seedu.clinicio.commons.util.UserSession;
 
+/**
+ * Indicate the login is successful.
+ */
+public class LoginSuccessEvent extends BaseEvent {
+
+    private UserSession userSession;
+    
+    public LoginSuccessEvent(UserSession userSession) {
+        this.userSession = userSession;
+    }
+    
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
 }
