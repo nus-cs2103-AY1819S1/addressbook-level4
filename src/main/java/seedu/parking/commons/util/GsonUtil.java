6ded13d367db8b5b438b7ac7de04f82b96bbdae9
Keith Fong <fongzhizhong@gmail.com>
2018-10-30 16:49:59 +0800

    Removed unused imports

--- src/main/java/seedu/parking/commons/util/GsonUtil.java
+++ src/main/java/seedu/parking/commons/util/GsonUtil.java
@@ -24,7 +24,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import seedu.parking.MainApp;
 import seedu.parking.commons.core.LogsCenter;
 
 /**
