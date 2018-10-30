38de3d3194ccead2a42bc834cb6a5c0159326357
betakuwe <33549490+betakuwe@users.noreply.github.com>
2018-10-30 12:12:45 +0800

    Update FindCommandSystemTest

--- src/test/java/systemtests/FindCommandSystemTest.java
+++ src/test/java/systemtests/FindCommandSystemTest.java
@@ -22,7 +22,6 @@ import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
 
-//TODO need to update this and add sortcommand
 public class FindCommandSystemTest extends AddressBookSystemTest {
 
     @Test
