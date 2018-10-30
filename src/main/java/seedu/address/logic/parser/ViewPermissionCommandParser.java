bba5e1d88e216549e35398949fd6e32447deae91
Kok Jian Yu <kokjianyu@gmail.com>
2018-10-30 18:25:28 +0800

    Add test for ViewPermissionCommandParser

--- src/main/java/seedu/address/logic/parser/ViewPermissionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewPermissionCommandParser.java
@@ -9,11 +9,12 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new ViewPermissionCommand object
  */
-public class ViewPermissionCommandParser {
+public class ViewPermissionCommandParser implements Parser<ViewPermissionCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the ViewPermissionCommand
      * and returns an ViewPermissionCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewPermissionCommand parse(String args) throws ParseException {
