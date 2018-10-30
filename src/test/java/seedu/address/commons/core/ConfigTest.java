5197924ea3bdea2f137c672948736376ef861faf
omegafishy <zhiyulling@gmail.com>
2018-10-30 17:12:08 +0800

    Update application title

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -14,7 +14,7 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : Address App\n"
+        String defaultConfigAsString = "App title : Hospital Management System 2K18\n"
                 + "Current log level : INFO\n"
                 + "Preference file Location : preferences.json";
 
