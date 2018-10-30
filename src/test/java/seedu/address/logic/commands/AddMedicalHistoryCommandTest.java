1a94a02f5b760a55962a515669e76bca508e6659
YangYafei1998 <faye.yangyafei@gmail.com>
2018-10-30 10:40:50 +0800

    test for add-medical-history

---
+++ src/test/java/seedu/address/logic/commands/AddMedicalHistoryCommandTest.java
@@ -0,0 +1,143 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ALLERGY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_CONDITION;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPatientsAndDoctors.getTypicalAddressBookWithPatientAndDoctor;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.TagContainsDoctorPredicate;
+import seedu.address.model.tag.TagContainsPatientPredicate;
+import seedu.address.testutil.PatientBuilder;
+
+/**
+ * Contains integration tests and unit tests for AddMedicalHistoryCommand.
+ */
+public class AddMedicalHistoryCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBookWithPatientAndDoctor(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_addMedicalHistory_success(){
+        final TagContainsPatientPredicate predicate = new TagContainsPatientPredicate();
+        model.updateFilteredPersonList(predicate);
+        Patient firstPatient = (Patient) model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPatient =
+                new PatientBuilder(firstPatient).withMedicalHistory(VALID_ALLERGY, VALID_CONDITION).build();
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, VALID_ALLERGY, VALID_CONDITION);
+        String expectedMessage = String.format(AddMedicalHistoryCommand.MESSAGE_ADD_MEDICAL_HISTORY_SUCCESS,
+                editedPatient);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.updatePerson(firstPatient, editedPatient);
+        expectedModel.commitAddressBook();
+
+        assertCommandSuccess(addMedicalHistoryCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndex_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(outOfBoundIndex, VALID_ALLERGY, VALID_CONDITION);
+
+        assertCommandFailure(addMedicalHistoryCommand,
+                model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_duplicateAllergy_failure() {
+        final TagContainsPatientPredicate predicate = new TagContainsPatientPredicate();
+        model.updateFilteredPersonList(predicate);
+        Patient firstPatient = (Patient) model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        String duplicateAllergy = new String(firstPatient.getMedicalHistory().getAllergies().get(0));
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, duplicateAllergy, VALID_CONDITION);
+
+        assertCommandFailure(addMedicalHistoryCommand, model, commandHistory,
+                duplicateAllergy
+                        + AddMedicalHistoryCommand.MESSAGE_INVALID_ADD_MEDICAL_HISTORY_DUPLICATE);
+
+    }
+
+    @Test
+    public void execute_duplicateCondition_failure() {
+        final TagContainsPatientPredicate predicate = new TagContainsPatientPredicate();
+        model.updateFilteredPersonList(predicate);
+        Patient firstPatient = (Patient) model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        String duplicateCondition = new String(firstPatient.getMedicalHistory().getConditions().get(0));
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, VALID_ALLERGY, duplicateCondition);
+
+        assertCommandFailure(addMedicalHistoryCommand, model, commandHistory,
+                duplicateCondition
+                        + AddMedicalHistoryCommand.MESSAGE_INVALID_ADD_MEDICAL_HISTORY_DUPLICATE);
+
+    }
+
+    @Test
+    public void execute_invalidType_failure() {
+        final TagContainsDoctorPredicate predicate = new TagContainsDoctorPredicate();
+        model.updateFilteredPersonList(predicate);
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, VALID_ALLERGY, VALID_CONDITION);
+        assertCommandFailure(addMedicalHistoryCommand, model, commandHistory,
+                AddMedicalHistoryCommand.MESSAGE_INVALID_ADD_MEDICAL_HISTORY);
+
+    }
+
+    @Test
+    public void execute_blankInput_failure() {
+        final TagContainsPatientPredicate predicate = new TagContainsPatientPredicate();
+        model.updateFilteredPersonList(predicate);
+        AddMedicalHistoryCommand addMedicalHistoryCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, "", "");
+        assertCommandFailure(addMedicalHistoryCommand, model, commandHistory,
+                AddMedicalHistoryCommand.MESSAGE_INVALID_ADD_MEDICAL_HISTORY_NO_INFO);
+    }
+
+    @Test
+    public void equals() {
+        final AddMedicalHistoryCommand standardCommand =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, VALID_ALLERGY, VALID_CONDITION);
+
+        // same values -> returns true
+        AddMedicalHistoryCommand commandWithSameValues =
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, VALID_ALLERGY, VALID_CONDITION);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(
+                new AddMedicalHistoryCommand(INDEX_SECOND_PERSON, VALID_ALLERGY, VALID_CONDITION)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(
+                new AddMedicalHistoryCommand(INDEX_FIRST_PERSON, "", "")));
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/PatientBuilder.java
@@ -0,0 +1,145 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.patient.MedicalHistory;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+
+public class PatientBuilder {
+    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final String DEFAULT_PHONE = "85355255";
+    public static final String DEFAULT_EMAIL = "alice@gmail.com";
+    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_TAG = "Patient";
+    public static final String DEFAULT_REMARK = "Likes to swim";
+    public static final String DEFAULT_TELEGRAM_ID = "1234";
+    public static final String VALID_ALLERGY = "milk";
+    public static final String VALID_CONDITION = "sub-health";
+
+
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
+    private Remark remark;
+    private Set<Tag> tags;
+    private String telegramId;
+    private MedicalHistory medicalHistory;
+    private PriorityQueue<Appointment> upcomingAppointments;
+    private List<Appointment> pastAppointments;
+
+
+
+    public PatientBuilder() {
+        name = new Name(DEFAULT_NAME);
+        phone = new Phone(DEFAULT_PHONE);
+        email = new Email(DEFAULT_EMAIL);
+        address = new Address(DEFAULT_ADDRESS);
+        remark = new Remark(DEFAULT_REMARK);
+        tags = new HashSet<>();
+        tags.add(new Tag(DEFAULT_TAG));
+        telegramId = DEFAULT_TELEGRAM_ID;
+        medicalHistory = new MedicalHistory(new ArrayList<String>(Arrays.asList(VALID_ALLERGY)),
+                new ArrayList<String>(Arrays.asList(VALID_CONDITION)));
+        upcomingAppointments = new PriorityQueue<>();
+        pastAppointments = new ArrayList<>();
+    }
+
+    /**
+     * Initializes the PatientBuilder with the data of {@code personToCopy}.
+     */
+    public PatientBuilder(Patient patientToCopy) {
+        name = patientToCopy.getName();
+        phone = patientToCopy.getPhone();
+        email = patientToCopy.getEmail();
+        address = patientToCopy.getAddress();
+        remark = patientToCopy.getRemark();
+        tags = new HashSet<>(patientToCopy.getTags());
+        medicalHistory = patientToCopy.getMedicalHistory();
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Patient} that we are building.
+     */
+    public PatientBuilder withTags(String ... tags) {
+        this.tags = SampleDataUtil.getTagSet(tags);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withAddress(String address) {
+        this.address = new Address(address);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withPhone(String phone) {
+        this.phone = new Phone(phone);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withEmail(String email) {
+        this.email = new Email(email);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Remark} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withRemark(String remark) {
+        this.remark = new Remark(remark);
+        return this;
+    }
+
+    /**
+     * Sets the {@code MedicalHistory} of the {@code Patient} that we are building.
+     */
+    public PatientBuilder withMedicalHistory(String allergy, String condition) {
+        if (allergy.equals("") && condition.equals("")) {
+            this.medicalHistory = new MedicalHistory();
+        }else if (!(allergy.equals("")) && condition.equals("")) {
+            this.medicalHistory = new MedicalHistory(new ArrayList<String>(Arrays.asList(allergy)), new ArrayList<>());
+        }else if ((allergy.equals("")) && !(condition.equals(""))) {
+            this.medicalHistory = new MedicalHistory(new ArrayList<>(),
+                    new ArrayList<String>(Arrays.asList(condition)));
+        }else{
+            this.medicalHistory = new MedicalHistory(new ArrayList<String>(Arrays.asList(allergy)),
+                    new ArrayList<String>(Arrays.asList(condition)));
+        }
+
+        return this;
+    }
+
+    public Patient build() {
+        return new Patient(name, phone, email, address, remark, tags, telegramId,
+                upcomingAppointments, pastAppointments, medicalHistory);
+    }
+}
