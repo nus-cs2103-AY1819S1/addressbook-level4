9025df80685153720459ff445621e152ba4c4306
Javen <javenseow@gmail.com>
2018-10-30 23:51:44 +0800

    added "this" to budget window in mainwindow.java

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -89,7 +89,7 @@ public class MainWindow extends UiPart<Stage> {
         registerAsAnEventHandler(this);
 
         helpWindow = new HelpWindow();
-        budgetWindow = new BudgetWindow(logic, prefs);
+        budgetWindow = new BudgetWindow(logic, prefs, this);
     }
 
     public Stage getPrimaryStage() {
