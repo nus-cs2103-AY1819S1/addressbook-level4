133356a367aefb997d9068266f9d07a23a0bfd17
barkhyehyeon <barkhyehyeon@gmail.com>
2018-10-30 21:22:46 +0800

    Fix mistake in sorting ingredient list

--- src/main/java/seedu/souschef/logic/IngredientDateComparator.java
+++ src/main/java/seedu/souschef/logic/IngredientDateComparator.java
@@ -2,14 +2,14 @@ package seedu.souschef.logic;
 
 import java.util.Comparator;
 
-import seedu.souschef.model.ingredient.IngredientDate;
+import seedu.souschef.model.ingredient.Ingredient;
 
 /**
  * Comparator class for Day which compares Day objects by LocalDate date.
  */
-public class IngredientDateComparator implements Comparator<IngredientDate> {
+public class IngredientDateComparator implements Comparator<Ingredient> {
     @Override
-    public int compare(IngredientDate a, IngredientDate b) {
-        return a.getDate().compareTo(b.getDate());
+    public int compare(Ingredient a, Ingredient b) {
+        return a.getDate().getValue().compareTo(b.getDate().getValue());
     }
 }
