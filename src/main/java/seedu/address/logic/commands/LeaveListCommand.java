ec85e622b1d4b578e21bb3948d26188c5a4df7ff
Elliot Tan <elliottan@gmail.com>
2018-11-01 03:36:08 +0800

    Add required permission to leave list command

--- src/main/java/seedu/address/logic/commands/LeaveListCommand.java
+++ src/main/java/seedu/address/logic/commands/LeaveListCommand.java
@@ -1,12 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_LEAVEAPPLICATIONS;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.LeaveListEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.permission.Permission;
 
 /**
  * Lists all leave applications that employees have made to the user.
@@ -16,6 +18,10 @@ public class LeaveListCommand extends Command {
     public static final String COMMAND_WORD = "leavelist";
 
     public static final String MESSAGE_SUCCESS = "Listed all leave applications";
+
+    public LeaveListCommand() {
+        requiredPermission.addPermissions(Permission.VIEW_EMPLOYEE_LEAVE);
+    }
 
     @Override
     public CommandResult runBody(Model model, CommandHistory history) {
