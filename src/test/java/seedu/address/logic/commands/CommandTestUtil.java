0399840674354dfbf00b6f87b417b72a41ffe45f
Elliot Tan <elliottan@gmail.com>
2018-10-31 17:02:05 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -21,7 +21,6 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
-import seedu.address.model.leaveapplication.LeaveApplication;
 import seedu.address.model.leaveapplication.LeaveApplicationWithEmployee;
 import seedu.address.model.leaveapplication.LeaveContainsKeywordsPredicate;
 import seedu.address.model.leaveapplication.StatusEnum;
@@ -189,7 +188,8 @@ public class CommandTestUtil {
     public static void showLeaveApplicationAtIndex(Model model, Index targetIndex) {
         assertTrue(targetIndex.getZeroBased() < model.getFilteredLeaveApplicationList().size());
 
-        LeaveApplicationWithEmployee leaveApplication = model.getFilteredLeaveApplicationList().get(targetIndex.getZeroBased());
+        LeaveApplicationWithEmployee leaveApplication = model.getFilteredLeaveApplicationList()
+                .get(targetIndex.getZeroBased());
         final String[] splitDesc = leaveApplication.getDescription().value.split("\\s+");
         model.updateFilteredLeaveApplicationList(new LeaveContainsKeywordsPredicate(Arrays.asList(splitDesc[0])));
 
--- src/test/java/seedu/address/logic/commands/LeaveListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LeaveListCommandTest.java
@@ -2,12 +2,12 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showLeaveApplicationAtIndex;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
