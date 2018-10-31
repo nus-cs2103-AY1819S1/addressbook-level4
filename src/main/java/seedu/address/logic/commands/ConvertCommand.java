c06a990291b64338b48bf919490f7e27ff3411f2
lancelotwillow <lancelotwillow@gmail.com>
2018-10-31 23:23:54 +0800

    modify the UG

--- src/main/java/seedu/address/logic/commands/ConvertCommand.java
+++ src/main/java/seedu/address/logic/commands/ConvertCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.awt.image.BufferedImage;
-import java.net.URL;
 
 import javafx.embed.swing.SwingFXUtils;
 import seedu.address.commons.core.EventsCenter;
@@ -12,7 +11,6 @@ import seedu.address.commons.events.ui.TransformationEvent;
 import seedu.address.commons.util.ImageMagickUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.transformation.Transformation;
 
@@ -28,27 +26,13 @@ public class ConvertCommand extends Command {
             + ": do the operation to the image.\n"
             + "Parameters: operationName argument1 argument2 ...\n"
             + "Example: " + COMMAND_WORD + " blur 1x8";
-    //the path of the json file containing the arguments of the convert command
-    public static final URL SINGLE_COMMAND_TEMPLATE_PATH =
-            ImageMagickUtil.class.getResource("/imageMagic/commandTemplates");
-    private URL fileUrl;
     private Transformation transformation;
     /**
      * the constructor take the path of the JSON file of the detail of the convert operation
-     * @param fileUrl the path to the JSON file
      * @param transformation contains the operation to be processed to the image
      */
-    public ConvertCommand(URL fileUrl, Transformation transformation) throws ParseException {
-        if (!isFileExist(fileUrl)) {
-            throw new ParseException("no file found");
-        }
-        this.fileUrl = fileUrl;
+    public ConvertCommand(Transformation transformation) {
         this.transformation = transformation;
-    }
-
-    private static boolean isFileExist(URL fileUrl) {
-        /*return new File(fileUrl.toString()).exists();*/
-        return true;
     }
 
     /**
