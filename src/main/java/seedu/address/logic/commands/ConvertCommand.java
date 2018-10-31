125f37a6ff98c0b04cc20e73f811f0c5aa12e5ce
lancelotwillow <lancelotwillow@gmail.com>
2018-10-31 23:32:04 +0800

    fix error for mac

--- src/main/java/seedu/address/logic/commands/ConvertCommand.java
+++ src/main/java/seedu/address/logic/commands/ConvertCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.awt.image.BufferedImage;
+import java.net.URL;
 
 import javafx.embed.swing.SwingFXUtils;
 import seedu.address.commons.core.EventsCenter;
@@ -11,6 +12,7 @@ import seedu.address.commons.events.ui.TransformationEvent;
 import seedu.address.commons.util.ImageMagickUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.transformation.Transformation;
 
@@ -26,13 +28,23 @@ public class ConvertCommand extends Command {
             + ": do the operation to the image.\n"
             + "Parameters: operationName argument1 argument2 ...\n"
             + "Example: " + COMMAND_WORD + " blur 1x8";
+    //the path of the json file containing the arguments of the convert command
     private Transformation transformation;
     /**
      * the constructor take the path of the JSON file of the detail of the convert operation
+     * @param fileUrl the path to the JSON file
      * @param transformation contains the operation to be processed to the image
      */
-    public ConvertCommand(Transformation transformation) {
+    public ConvertCommand(URL fileUrl, Transformation transformation) throws ParseException {
+        if (!isFileExist(fileUrl)) {
+            throw new ParseException("no file found");
+        }
         this.transformation = transformation;
+    }
+
+    private static boolean isFileExist(URL fileUrl) {
+        /*return new File(fileUrl.toString()).exists();*/
+        return true;
     }
 
     /**
