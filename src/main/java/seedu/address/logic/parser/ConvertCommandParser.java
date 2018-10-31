52f403d4507950ad5223ea9ad3ffa35f1bc06c04
lancelotwillow <lancelotwillow@gmail.com>
2018-10-31 23:32:04 +0800

    fix error for mac

--- src/main/java/seedu/address/logic/parser/ConvertCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ConvertCommandParser.java
@@ -3,6 +3,10 @@ package seedu.address.logic.parser;
 //@@author lancelotwilow
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.net.URL;
+import java.util.Arrays;
+
+import seedu.address.commons.util.ImageMagickUtil;
 import seedu.address.logic.commands.ConvertCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.transformation.Transformation;
@@ -20,12 +24,16 @@ public class ConvertCommandParser implements Parser<ConvertCommand> {
     public ConvertCommand parse(String args) throws ParseException {
         try {
             String[] all = args.split(" ");
-            if (all.length < 2) {
-                throw new ParseException("invalid argument");
-            }
             String operation = all[1];
             String[] cmds = new String[0];
-            return new ConvertCommand(new Transformation(operation, cmds));
+            URL fileUrl;
+            if (all.length >= 1) {
+                cmds = Arrays.copyOfRange(all, 2, all.length);
+                fileUrl = ImageMagickUtil.SINGLE_COMMAND_TEMPLATE_PATH;
+            } else {
+                fileUrl = ImageMagickUtil.SINGLE_COMMAND_TEMPLATE_PATH;
+            }
+            return new ConvertCommand(fileUrl, new Transformation(operation, cmds));
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ConvertCommand.MESSAGE_USAGE), pe);
