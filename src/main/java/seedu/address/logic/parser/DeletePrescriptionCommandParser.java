23305a23283f7e4ec57e05f1555a7933d3d93438
florafong97 <e0042072@u.nus.edu>
2018-10-31 20:28:38 +0800

    DeletePrescriptionCommandParser

---
+++ src/main/java/seedu/address/logic/parser/DeletePrescriptionCommandParser.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DeletePrescriptionCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.MedicineName;
+
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICINE_NAME;
+
+/**
+ * Parses input arguments and creates a new DeletePrescriptionCommand object
+ */
+public class DeletePrescriptionCommandParser implements Parser<DeletePrescriptionCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeletePrescriptionCommand
+     * and returns a DeleteAppointmentCommand object for execution
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeletePrescriptionCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultiMap = ArgumentTokenizer.tokenize(args, PREFIX_INDEX, PREFIX_MEDICINE_NAME);
+
+        if (!arePrefixesPresent(argMultiMap, PREFIX_INDEX, PREFIX_MEDICINE_NAME)
+                || !argMultiMap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeletePrescriptionCommand.MESSAGE_USAGE));
+        }
+
+        int id = ParserUtil.parseId(argMultiMap.getValue(PREFIX_INDEX).get());
+        MedicineName medicineName = ParserUtil.parseMedicineName(argMultiMap.getValue(PREFIX_MEDICINE_NAME).get());
+
+        return new DeletePrescriptionCommand(id, medicineName);
+    }
+
+
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
+
