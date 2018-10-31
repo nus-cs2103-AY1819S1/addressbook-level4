deddfd79ce06681e4fd5663b555960f169804ebf
LAPTOP-AF5DLL52\Harry Qu <haoxuanqu@gmail.com>
2018-10-31 17:58:09 +0800

    groupadd command test case

--- src/test/java/seedu/learnvocabulary/logic/commands/GroupaddCommandTest.java
+++ src/test/java/seedu/learnvocabulary/logic/commands/GroupaddCommandTest.java
@@ -1,7 +1,47 @@
 package seedu.learnvocabulary.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static seedu.learnvocabulary.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.learnvocabulary.testutil.TypicalWords.getTypicalLearnVocabulary;
 
-class GroupaddCommandTest {
+import org.junit.Test;
 
+import seedu.learnvocabulary.logic.CommandHistory;
+import seedu.learnvocabulary.model.Model;
+import seedu.learnvocabulary.model.ModelManager;
+import seedu.learnvocabulary.model.UserPrefs;
+import seedu.learnvocabulary.model.tag.Tag;
+
+public class GroupaddCommandTest {
+    private Model model = new ModelManager(getTypicalLearnVocabulary(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalLearnVocabulary(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_duplicate() {
+        GroupaddCommand command = new GroupaddCommand(new Tag("toLearn"));
+        assertCommandFailure(command, model, commandHistory, GroupaddCommand.MESSAGE_NO_GROUP);
+    }
+
+    @Test
+    public void execute_normal() throws Exception {
+        GroupaddCommand command = new GroupaddCommand(new Tag("test"));
+        command.execute(model, commandHistory);
+        expectedModel.addGroup(new Tag("test"));
+        assertEquals(model.getTags(), expectedModel.getTags());
+    }
+    @Test
+    public void execute_undo() throws Exception {
+        GroupaddCommand command = new GroupaddCommand(new Tag("test"));
+        command.execute(model, commandHistory);
+        model.undoLearnVocabulary();
+        assertEquals(model.getTags(), expectedModel.getTags());
+    }
+    @Test
+    public void execute_clear() throws Exception {
+        GroupaddCommand command = new GroupaddCommand(new Tag("test"));
+        command.execute(model, commandHistory);
+        model.resetData(getTypicalLearnVocabulary());
+        assertEquals(model.getTags(), expectedModel.getTags());
+    }
 }
