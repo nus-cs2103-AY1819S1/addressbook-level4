0b5f0e18ffdde5b147d1725793d0e966f9e88202
chivent <e0191350@u.nus.edu>
2018-10-31 22:32:24 +0800

    Added ui-tests for StatusBarFooter and HistoryListPanel

--- src/test/java/guitests/guihandles/StatusBarFooterHandle.java
+++ src/test/java/guitests/guihandles/StatusBarFooterHandle.java
@@ -11,15 +11,19 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
     public static final String STATUS_BAR_PLACEHOLDER = "#statusbarPlaceholder";
 
     private static final String LOGIN_STATUS_ID = "#loginStatus";
+    private static final String DIRECTORY_DISPLAY_ID = "#directoryDisplay";
 
     private final StatusBar loginStatusNode;
+    private final StatusBar directoryNode;
 
     private String lastRememberedLoginStatus;
+    private String lastRememberedDirectoryDisplay;
 
     public StatusBarFooterHandle(Node statusBarFooterNode) {
         super(statusBarFooterNode);
 
         loginStatusNode = getChildNode(LOGIN_STATUS_ID);
+        directoryNode = getChildNode(DIRECTORY_DISPLAY_ID);
     }
 
     /**
@@ -30,10 +34,24 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
     }
 
     /**
+     * Returns the text of the directory display portion of the status bar.
+     */
+    public String getDirectoryDisplay() {
+        return directoryNode.getText();
+    }
+
+    /**
      * Remembers the content of the login status portion of the status bar.
      */
     public void rememberLoginStatus() {
         lastRememberedLoginStatus = getLoginStatus();
+    }
+
+    /**
+     * Remembers the content of the directory display portion of the status bar.
+     */
+    public void rememberDirectoryDisplay() {
+        lastRememberedDirectoryDisplay = getDirectoryDisplay();
     }
 
     /**
@@ -42,5 +60,13 @@ public class StatusBarFooterHandle extends NodeHandle<Node> {
      */
     public boolean isLoginStatusChanged() {
         return !lastRememberedLoginStatus.equals(getLoginStatus());
+    }
+
+    /**
+     * Returns true if the current content of the directory display is different from the value remembered by the most recent
+     * {@code rememberDirectoryStatus()} call.
+     */
+    public boolean isDirectoryChanged() {
+        return !lastRememberedDirectoryDisplay.equals(getLoginStatus());
     }
 }
