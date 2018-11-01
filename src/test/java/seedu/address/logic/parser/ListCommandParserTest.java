5131d27aa9e88b1800f454c284b94a66006bad77
remo5000 <vigneshshankar@gmail.com>
2018-11-01 17:46:58 +0800

    ListCommandParserTest: Use constants in test

--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -13,19 +13,21 @@ import seedu.address.logic.commands.ListCommand;
  */
 public class ListCommandParserTest {
 
+    private String filterPrefix = " " + ListCommandParser.PREFIX_FILTER;
+
     private ListCommandParser parser = new ListCommandParser();
 
     @Test
     public void parse_validArgs_returnsListCommand() {
         assertParseSuccess(parser, "", new ListCommand());
-        assertParseSuccess(parser, " b/today", new ListCommand(ListCommand.ListFilter.DUE_TODAY));
-        assertParseSuccess(parser, " b/week", new ListCommand(ListCommand.ListFilter.DUE_END_OF_WEEK));
-        assertParseSuccess(parser, " b/month", new ListCommand(ListCommand.ListFilter.DUE_END_OF_MONTH));
+        assertParseSuccess(parser, filterPrefix + "today", new ListCommand(ListCommand.ListFilter.DUE_TODAY));
+        assertParseSuccess(parser, filterPrefix + "week", new ListCommand(ListCommand.ListFilter.DUE_END_OF_WEEK));
+        assertParseSuccess(parser, filterPrefix + "month", new ListCommand(ListCommand.ListFilter.DUE_END_OF_MONTH));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, " b/sunImplodes", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        assertParseFailure(parser, filterPrefix + "sunImplodes", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 ListCommand.MESSAGE_USAGE));
     }
 }
