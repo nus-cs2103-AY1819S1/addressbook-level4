238f64cdae480f45a48711c092c4dcaf56f5326d
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-11-01 15:18:57 +0800

    Update LabelMatchesKeywordPredicate #test
    This predicate will now return true if there's a case-insenstive match
    of the interested keyLabel against the set of labels in the task

--- src/main/java/seedu/address/model/task/LabelMatchesKeywordPredicate.java
+++ src/main/java/seedu/address/model/task/LabelMatchesKeywordPredicate.java
@@ -17,9 +17,23 @@ public class LabelMatchesKeywordPredicate implements Predicate<Task> {
         this.keyLabel = keyLabel;
     }
 
+    /**
+     * Precondition: The predicate's keylabel should be in lowercase in order to
+     * perform a case insensitive check.
+     *
+     * Checks the {@code Task}'s set of labels for a case-insensitive match.
+     *
+     * @param task to check for the interested label
+     * @return True is the task contains the predicate's keyLabel
+     */
     @Override
     public boolean test(Task task) {
-        return task.getLabels().contains(keyLabel);
+        return task
+            .getLabels()
+            .stream()
+            .anyMatch((label)->
+                label.labelName.toLowerCase()
+                    .equals(keyLabel.labelName));
     }
 
     @Override
