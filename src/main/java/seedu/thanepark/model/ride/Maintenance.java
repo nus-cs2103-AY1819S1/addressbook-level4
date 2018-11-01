d26659b803378c61a5d7eb952f5de83eda1e5634
jiajunteo <e0175930@u.nus.edu>
2018-11-01 11:39:24 +0800

    Revert "Replace integer that stores day of last maintenance with localdate that stores date of last maintenance"
    This reverts commit f6e0114e5bc66108e6386379b7cd1988d428e3c7.

--- src/main/java/seedu/thanepark/model/ride/Maintenance.java
+++ src/main/java/seedu/thanepark/model/ride/Maintenance.java
@@ -3,9 +3,6 @@ package seedu.thanepark.model.ride;
 import static java.util.Objects.requireNonNull;
 import static seedu.thanepark.commons.util.AppUtil.checkArgument;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 /**
  * Represents a Ride's days since last maintenance in the park management app.
  */
@@ -14,7 +11,7 @@ public class Maintenance implements NumericAttribute {
     public static final String MESSAGE_MAINTENANCE_CONSTRAINTS =
             "Maintenance should only contain numbers with at least 1 digit long.";
     public static final String MAINTENANCE_VALIDATION_REGEX = "\\d+";
-    private LocalDate lastMaintenanceDate;
+    private int value;
 
     /**
      * Constructs a {@code Maintenance}.
@@ -24,8 +21,7 @@ public class Maintenance implements NumericAttribute {
     public Maintenance(String daysSinceMaintenanceString) {
         requireNonNull(daysSinceMaintenanceString);
         checkArgument(isValidMaintenance(daysSinceMaintenanceString), MESSAGE_MAINTENANCE_CONSTRAINTS);
-        int value = Integer.parseInt(daysSinceMaintenanceString);
-        lastMaintenanceDate = LocalDate.now().minusDays((long)value);
+        value = Integer.parseInt(daysSinceMaintenanceString);
     }
 
     /**
@@ -33,16 +29,15 @@ public class Maintenance implements NumericAttribute {
      * @param daysSinceMaintenance Days since last maintenance.
      */
     public Maintenance(int daysSinceMaintenance) {
-        lastMaintenanceDate = LocalDate.now().minusDays(daysSinceMaintenance);
+        value = daysSinceMaintenance;
     }
 
     public int getValue() {
-        Period period = lastMaintenanceDate.until(LocalDate.now());
-        return period.getDays();
+        return value;
     }
 
     public void setValue(int value) {
-        this.lastMaintenanceDate = LocalDate.now().minusDays((long)value);
+        this.value = value;
     }
 
     /**
@@ -54,19 +49,19 @@ public class Maintenance implements NumericAttribute {
 
     @Override
     public String toString() {
-        return String.valueOf(getValue()) + " days";
+        return String.valueOf(value) + " days";
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
             || (other instanceof Maintenance // instanceof handles nulls
-            && lastMaintenanceDate == ((Maintenance) other).lastMaintenanceDate); // state check
+            && value == ((Maintenance) other).value); // state check
     }
 
     @Override
     public int hashCode() {
-        return getValue();
+        return value;
     }
 
 }
