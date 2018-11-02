7465a95889a3836094aeca23f670773594acf83d
lancelotwillow <lancelotwillow@gmail.com>
2018-11-02 09:24:10 +0800

    comment removing cache folder

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -5,7 +5,6 @@ import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.commons.util.ImageMagickUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
-import seedu.address.model.PreviewImage;
 
 /**
  * Terminates the program.
@@ -19,7 +18,7 @@ public class ExitCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         ImageMagickUtil.getTempFolderPath().toFile().delete();
-        PreviewImage.getCacheFolder().toFile().delete();
+        //PreviewImage.getCacheFolder().toFile().delete();
         EventsCenter.getInstance().post(new ExitAppRequestEvent());
         return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT);
     }
