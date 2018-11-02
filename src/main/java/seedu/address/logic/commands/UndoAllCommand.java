f6db128c7f87ea6385d6a565b41c188f368af12f
ihwk1996 <ihwk1996@gmail.com>
2018-11-02 18:57:45 +0800

    added undo-all and redo-all commands changed undo and redo success and failure messages

--- src/main/java/seedu/address/logic/commands/UndoAllCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoAllCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 //@@author ihwk1996
-import static java.util.Objects.requireNonNull;
 
 import javafx.embed.swing.SwingFXUtils;
 import seedu.address.commons.core.EventsCenter;
@@ -10,27 +9,30 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-/**
- * Reverts the {@code model}'s current layer's previewImage to its previously undone state.
- */
-public class RedoCommand extends Command {
+import static java.util.Objects.requireNonNull;
 
-    public static final String COMMAND_WORD = "redo";
-    public static final String MESSAGE_SUCCESS = "Transformation successfully redone";
-    public static final String MESSAGE_FAILURE = "No more transformations to redo";
+/**
+ * Reverts the {@code model}'s previewImageManager to its previous state.
+ */
+public class UndoAllCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo-all";
+    public static final String MESSAGE_SUCCESS = "All transformations successfully undone";
+    public static final String MESSAGE_FAILURE = "No more transformations to undo";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        if (!model.canRedoPreviewImage()) {
+        if (!model.canUndoPreviewImage()) {
             throw new CommandException(MESSAGE_FAILURE);
         }
 
-        model.redoPreviewImage();
+        model.undoAllPreviewImage();
         EventsCenter.getInstance().post(
                 new ChangeImageEvent(SwingFXUtils.toFXImage(
                         model.getCurrentPreviewImage().getImage(), null), "preview"));
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
