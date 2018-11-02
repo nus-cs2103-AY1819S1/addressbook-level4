5e2c4c2821d6deed749d56cbf955d222e54041ed
chivent <e0191350@u.nus.edu>
2018-11-02 15:36:43 +0800

    Updated tests for StatusBarFooter and HistoryListPanel UI - Maxed coverage of methods for tests

--- src/main/java/seedu/address/ui/HistoryListPanel.java
+++ src/main/java/seedu/address/ui/HistoryListPanel.java
@@ -20,11 +20,16 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.ClearHistoryEvent;
 import seedu.address.commons.events.ui.TransformationEvent;
 
+
 /**
  * Panel containing the list of past transformations.
  */
 public class HistoryListPanel extends UiPart<Region> {
+    public static final String SELECTED_STYLE = "-fx-background-color: #a3a3a3;  -fx-text-fill: #1f1f1f;";
+
     private static final String FXML = "HistoryListPanel.fxml";
+    private static final String DEFAULT_STYLE = "-fx-background-color: transparent;  -fx-text-fill: #6e6e6e;";
+
     private final Logger logger = LogsCenter.getLogger(getClass());
     private ObservableList<String> items = FXCollections.observableArrayList();
 
@@ -108,12 +113,12 @@ public class HistoryListPanel extends UiPart<Region> {
         protected void updateItem(String transformation, boolean empty) {
             super.updateItem(transformation, empty);
 
-            setStyle("-fx-background-color: transparent;  -fx-text-fill: #6e6e6e;");
+            setStyle(DEFAULT_STYLE);
             setGraphic(null);
             setText(empty ? null : transformation);
 
             if (!empty && getIndex() == (getListView().getItems().size() - 1)) {
-                setStyle("-fx-background-color: #a3a3a3;  -fx-text-fill: #1f1f1f;");
+                setStyle(SELECTED_STYLE);
             }
         }
     }
