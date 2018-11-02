1b9d1117e1bd5baa373ffa738ccaea7935a3aa9f
florafong97 <e0042072@u.nus.edu>
2018-11-02 12:29:16 +0800

    checkstyle

--- src/main/java/seedu/address/model/patient/Patient.java
+++ src/main/java/seedu/address/model/patient/Patient.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.appointment.Appointment;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
