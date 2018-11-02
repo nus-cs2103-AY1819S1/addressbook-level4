bec15173ae4f7a82f8d8b5abc5dea0eb0d4c54d2
florafong97 <e0042072@u.nus.edu>
2018-11-02 08:21:16 +0800

    TypicalPatientsAndDoctor Included a few appointments for testing

--- src/test/java/seedu/address/testutil/TypicalPatientsAndDoctors.java
+++ src/test/java/seedu/address/testutil/TypicalPatientsAndDoctors.java
@@ -25,16 +25,38 @@ import seedu.address.model.patient.Patient;
 import seedu.address.model.person.Person;
 
 /**
- * A utility class containing a list of {@code Person} objects to be used in tests.
+ * A utility class containing a list of {@code Person} and {@code Appointment} objects to be used in tests.
  */
 public class TypicalPatientsAndDoctors {
 
+    // Appointment objects
     public static final Appointment FIRST = new AppointmentBuilder().withAppointmentId(10000)
             .withDoctor("Fiona Kunz").withPatient("Alice Pauline").withDateTime("2018-12-11 12:00")
             .withComments("Heart check up")
             .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
                     .withAppointmentId(10000).withMedicineName("Aspirin").build()))).build();
+    public static final Appointment SECOND = new AppointmentBuilder().withAppointmentId(10001)
+            .withDoctor("George Best").withPatient("Benson Meier").withDateTime("2018-12-12 12:00")
+            .withComments("Flu check up")
+            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
+                    .withAppointmentId(10001).withMedicineName("Tamiflu").build()))).build();
+    public static final Appointment THIRD = new AppointmentBuilder().withAppointmentId(10002)
+            .withDoctor("Fiona Kunz").withPatient("Carl Kurz").withDateTime("2018-12-13 12:00")
+            .withComments("Flu check up")
+            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
+                    .withAppointmentId(10002).withMedicineName("Tamiflu").build()))).build();
+    public static final Appointment FOURTH = new AppointmentBuilder().withAppointmentId(10003)
+            .withDoctor("George Best").withPatient("Daniel Meier").withDateTime("2018-12-14 12:00")
+            .withComments("Cough check up")
+            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
+                    .withAppointmentId(10003).withMedicineName("Dextromethorphan").build()))).build();
+    public static final Appointment FIFTH = new AppointmentBuilder().withAppointmentId(10004)
+            .withDoctor("Fiona Kunz").withPatient("Elle Meyer").withDateTime("2018-12-15 12:00")
+            .withComments("Heart check up")
+            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
+                    .withAppointmentId(10004).withMedicineName("Aspirin").build()))).build();
 
+    // Persons objects
     public static final Patient ALICE = new PatientBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
@@ -48,28 +70,32 @@ public class TypicalPatientsAndDoctors {
             .withEmail("johnd@example.com").withPhone("98765432")
             .withRemark("")
             .withTags("Patient", "friends")
-            .withMedicalHistory("", "having cold").build();
+            .withMedicalHistory("", "having cold")
+            .withAppointment(SECOND).build();
     public static final Patient CARL = new PatientBuilder().withName("Carl Kurz")
             .withPhone("95352563")
             .withEmail("heinz@example.com")
             .withAddress("wall street")
             .withRemark("")
             .withTags("Patient")
-            .withMedicalHistory("fish", "").build();
+            .withMedicalHistory("fish", "")
+            .withAppointment(THIRD).build();
     public static final Patient DANIEL = new PatientBuilder().withName("Daniel Meier")
             .withPhone("87652533")
             .withEmail("cornelia@example.com")
             .withAddress("10th street")
             .withRemark("")
             .withTags("Patient", "friends")
-            .withMedicalHistory("", "").build();
+            .withMedicalHistory("", "")
+            .withAppointment(FOURTH).build();
     public static final Patient ELLE = new PatientBuilder()
             .withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com")
             .withAddress("michegan ave")
             .withRemark("")
             .withTags("Patient")
-            .withMedicalHistory("", "").build();
+            .withMedicalHistory("", "")
+            .withAppointment(FIFTH).build();
     public static final Doctor FIONA = new DoctorBuilder()
             .withName("Fiona Kunz")
             .withPhone("9482427")
@@ -77,14 +103,15 @@ public class TypicalPatientsAndDoctors {
             .withAddress("little tokyo")
             .withRemark("")
             .withTags("Doctor")
-            .withAppointment(FIRST).build();
+            .withAppointment(FIRST, THIRD, FIFTH).build();
     public static final Doctor GEORGE = new DoctorBuilder()
             .withName("George Best")
             .withPhone("9482442")
             .withEmail("anna@example.com")
             .withAddress("4th street")
             .withRemark("")
-            .withTags("Doctor").build();
+            .withTags("Doctor")
+            .withAppointment(SECOND, FOURTH).build();
 
     // Manually added
     public static final Patient HOON = new PatientBuilder()
@@ -122,35 +149,6 @@ public class TypicalPatientsAndDoctors {
             .withMedicalHistory(VALID_ALLERGY, VALID_CONDITION).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
-
-
-    public static final Appointment SECOND = new AppointmentBuilder().withAppointmentId(10001)
-            .withDoctor("George Best").withPatient("Benson Meier").withDateTime("2018-12-12 12:00")
-            .withComments("Flu check up")
-            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
-                    .withAppointmentId(10001).withMedicineName("Tamiflu").build()))).build();
-
-
-    public static final Appointment THIRD = new AppointmentBuilder().withAppointmentId(10002)
-            .withDoctor("Fiona Kunz").withPatient("Carl Kurz").withDateTime("2018-12-13 12:00")
-            .withComments("Flu check up")
-            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
-                    .withAppointmentId(10002).withMedicineName("Tamiflu").build()))).build();
-
-
-    public static final Appointment FOURTH = new AppointmentBuilder().withAppointmentId(10003)
-            .withDoctor("George Best").withPatient("Daniel Meier").withDateTime("2018-12-14 12:00")
-            .withComments("Cough check up")
-            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
-                    .withAppointmentId(10003).withMedicineName("Dextromethorphan").build()))).build();
-
-
-    public static final Appointment FIFTH = new AppointmentBuilder().withAppointmentId(10004)
-            .withDoctor("Fiona Kunz").withPatient("Elle Meyer").withDateTime("2018-12-15 12:00")
-            .withComments("Heart check up")
-            .withPrescriptions(new ArrayList<>(Arrays.asList(new PrescriptionBuilder()
-                    .withAppointmentId(10004).withMedicineName("Aspirin").build()))).build();
-
 
     private TypicalPatientsAndDoctors() {} // prevents instantiation
 
