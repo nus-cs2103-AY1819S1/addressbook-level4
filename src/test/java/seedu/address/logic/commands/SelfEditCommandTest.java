36c7f556a6c163b2f59d138a66f012c5fa3f20ce
Jeremy Choo <ChooJeremy@Ymail.com>
2018-11-03 15:12:31 +0800

    updated undo/redo test

--- src/test/java/seedu/address/logic/commands/SelfEditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SelfEditCommandTest.java
@@ -120,7 +120,6 @@ public class SelfEditCommandTest {
 
         setUpAsAdmin();
 
-        PersonBuilder personInList = new PersonBuilder(lastPerson);
         SelfEditCommand selfEditCommand = null;
         try {
             SelfEditCommandParserBuilder selfEditCommandParserBuilder = new SelfEditCommandParserBuilder()
@@ -134,37 +133,33 @@ public class SelfEditCommandTest {
         assertCommandFailure(selfEditCommand, model, commandHistory, expectedMessage);
     }
 
-    /**
-     * Edit filtered list where index is larger than size of filtered list,
-     * but smaller than size of address book
-     */
     @Test
-    public void execute_invalidPersonIndexFilteredList_failure() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        Index outOfBoundIndex = INDEX_SECOND_PERSON;
-        // ensures that outOfBoundIndex is still in bounds of address book list
-        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+    public void executeUndoRedo_success() throws Exception {
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
 
-        EditCommand editCommand = new EditCommand(outOfBoundIndex,
-            new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
+        setUpAsUser(indexLastPerson.getZeroBased());
 
-        assertCommandFailure(editCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-    }
+        PersonBuilder personInList = new PersonBuilder(lastPerson);
+        Person editedPerson = personInList.withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
+            .build();
 
-    @Test
-    public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
-        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        Person editedPerson = new PersonBuilder().withUsername(personToEdit.getUsername().username)
-            .withPassword(personToEdit.getPassword().password).build();
-        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
-        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
+        SelfEditCommand selfEditCommand = null;
+        try {
+            SelfEditCommandParserBuilder selfEditCommandParserBuilder = new SelfEditCommandParserBuilder()
+                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB);
+            selfEditCommand = selfEditCommandParserBuilder.getCommand();
+        } catch(ParseException pe) {
+            throw new AssertionError("Failed to build appropiate SelfEditCommand! ", pe);
+        }
+
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
             model.getArchiveList(), new UserPrefs());
-        expectedModel.updatePerson(personToEdit, editedPerson);
+        expectedModel.updatePerson(lastPerson, editedPerson);
         expectedModel.commitAddressBook();
 
-        // edit -> first person edited
-        editCommand.execute(model, commandHistory);
+        // edit -> modify person data
+        selfEditCommand.execute(model, commandHistory);
 
         // undo -> reverts addressbook back to previous state and filtered person list to show all persons
         expectedModel.undoAddressBook();
