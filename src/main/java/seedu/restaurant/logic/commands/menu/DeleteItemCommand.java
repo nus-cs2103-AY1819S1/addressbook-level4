8674f96b039e38a7abd8c5d0a3ae3048c8cf5e40
yican95 <yican95@gmail.com>
2018-11-03 11:25:19 +0800

    DeleteItemCommand.java: Update message usage

--- src/main/java/seedu/restaurant/logic/commands/menu/DeleteItemCommand.java
+++ src/main/java/seedu/restaurant/logic/commands/menu/DeleteItemCommand.java
@@ -1,5 +1,7 @@
 package seedu.restaurant.logic.commands.menu;
 
+import static seedu.restaurant.logic.parser.util.CliSyntax.PREFIX_ENDING_INDEX;
+
 import seedu.restaurant.logic.CommandHistory;
 import seedu.restaurant.logic.commands.Command;
 import seedu.restaurant.logic.commands.CommandResult;
@@ -17,7 +19,9 @@ public abstract class DeleteItemCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the item identified by the index number used in the displayed item list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX (must be a positive integer, starting index) "
+            + "[" + PREFIX_ENDING_INDEX + "INDEX](can not be smaller than the starting index)\n"
+            + "or Parameters: NAME\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_ITEM_SUCCESS = "Deleted %1$d items";
