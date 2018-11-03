5557ab20bfb06e953a3d9194b01417aa7ab15f85
Jeremy Choo <ChooJeremy@Ymail.com>
2018-11-03 15:14:18 +0800

    Updated undo redo failure test

--- src/test/java/seedu/address/logic/commands/SelfEditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SelfEditCommandTest.java
@@ -115,9 +115,6 @@ public class SelfEditCommandTest {
 
     @Test
     public void execute_admin_failure() {
-        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
-        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
-
         setUpAsAdmin();
 
         SelfEditCommand selfEditCommand = null;
@@ -171,13 +168,8 @@ public class SelfEditCommandTest {
     }
 
     @Test
-    public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
-        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
-
-        // execution failed -> address book state not added into model
-        assertCommandFailure(editCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    public void executeUndoRedo_admin_failure() {
+        execute_admin_failure();
 
         // single address book state in model -> undoCommand and redoCommand fail
         assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
