5d6daefcde32699426f23718eddf80dea3ff3684
yican95 <yican95@gmail.com>
2018-11-03 10:11:14 +0800

    DeleteItemCommandParser.java: Update to support mass delete

--- src/main/java/seedu/restaurant/logic/parser/menu/DeleteItemCommandParser.java
+++ src/main/java/seedu/restaurant/logic/parser/menu/DeleteItemCommandParser.java
@@ -1,11 +1,15 @@
 package seedu.restaurant.logic.parser.menu;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.restaurant.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.restaurant.logic.parser.util.CliSyntax.PREFIX_ENDING_INDEX;
 
 import seedu.restaurant.commons.core.index.Index;
 import seedu.restaurant.logic.commands.menu.DeleteItemCommand;
 import seedu.restaurant.logic.parser.Parser;
 import seedu.restaurant.logic.parser.exceptions.ParseException;
+import seedu.restaurant.logic.parser.util.ArgumentMultimap;
+import seedu.restaurant.logic.parser.util.ArgumentTokenizer;
 import seedu.restaurant.logic.parser.util.ParserUtil;
 
 /**
@@ -19,13 +23,33 @@ public class DeleteItemCommandParser implements Parser<DeleteItemCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteItemCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ENDING_INDEX);
+
+        Index index;
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteItemCommand(index);
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteItemCommand.MESSAGE_USAGE), pe);
         }
+
+        Index endingIndex = index;
+        if (argMultimap.getValue(PREFIX_ENDING_INDEX).isPresent()) {
+            try {
+                endingIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_ENDING_INDEX).get());
+            } catch (ParseException pe) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteItemCommand.MESSAGE_USAGE), pe);
+            }
+        }
+
+        if (endingIndex.getZeroBased() < index.getZeroBased()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteItemCommand.MESSAGE_USAGE));
+        }
+
+        return new DeleteItemCommand(index, endingIndex);
     }
 
 }
