311d46cb9cd47a213a6f2b7486d3a2236c7c2830
agendazhang <e0202838@u.nus.edu>
2018-11-04 12:43:19 +0800

    New SuggestFriendsByInterests command

--- src/main/java/seedu/address/logic/commands/personcommands/SuggestFriendsByInterestsCommand.java
+++ src/main/java/seedu/address/logic/commands/personcommands/SuggestFriendsByInterestsCommand.java
@@ -24,8 +24,10 @@ public class SuggestFriendsByInterestsCommand extends Command {
     public static final String COMMAND_WORD = "suggestFriendsByInterests";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Shows a list of persons with at least one similar interest with the target user"
-            + " identified by the index number used in the displayed person list.\n"
+            + ": Shows a list of persons (including target user) with at least one similar interest"
+            + " with respect to the target user"
+            + " identified by the index number used in the displayed person list"
+            + " and are not yet friends.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
