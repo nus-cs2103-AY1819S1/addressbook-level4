d8bce269bcf9748fa1a1ff99fdad46c30dbf5593
yican95 <yican95@gmail.com>
2018-11-04 18:57:08 +0800

    PriceTest.java: Add JUnit test for max price

--- src/test/java/seedu/restaurant/model/menu/PriceTest.java
+++ src/test/java/seedu/restaurant/model/menu/PriceTest.java
@@ -25,7 +25,7 @@ public class PriceTest {
         // null price
         Assert.assertThrows(NullPointerException.class, () -> Price.isValidPrice(null));
 
-        // invalid phone numbers
+        // invalid price numbers
         assertFalse(Price.isValidPrice("")); // empty string
         assertFalse(Price.isValidPrice(" ")); // spaces only
         assertFalse(Price.isValidPrice("phone")); // non-numeric
@@ -33,10 +33,12 @@ public class PriceTest {
         assertFalse(Price.isValidPrice("9312 1534")); // spaces within digits
         assertFalse(Price.isValidPrice("2.55555")); // more than 2 decimal places
         assertFalse(Price.isValidPrice("2.55.55.5")); // multiple dots
+        assertFalse(Price.isValidPrice("-888")); // negative price
+        assertFalse(Price.isValidPrice("2147483647.01")); // Greater than max price
 
-        // valid phone numbers
+        // valid price numbers
         assertTrue(Price.isValidPrice("911")); // no decimal places
         assertTrue(Price.isValidPrice("2.50")); // at most 2 decimal places
-        assertTrue(Price.isValidPrice("124293842033123")); // long price numbers
+        assertTrue(Price.isValidPrice("2147483647")); // max price
     }
 }
