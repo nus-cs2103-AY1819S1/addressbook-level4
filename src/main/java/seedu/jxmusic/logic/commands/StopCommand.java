18d8548dc6f5af59d9654698f9866aab1866a409
David Choo <davidchoo16@gmail.com>
2018-11-04 22:44:17 +0800

    Add validation checks for Play, Pause, Stop

--- src/main/java/seedu/jxmusic/logic/commands/StopCommand.java
+++ src/main/java/seedu/jxmusic/logic/commands/StopCommand.java
@@ -1,7 +1,8 @@
 package seedu.jxmusic.logic.commands;
 
+import seedu.jxmusic.logic.commands.exceptions.CommandException;
 import seedu.jxmusic.model.Model;
-import seedu.jxmusic.player.PlayerManager;
+import seedu.jxmusic.player.Playable;
 
 /**
  * Lists all persons in the jxmusic book to the user.
@@ -13,10 +14,14 @@ public class StopCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Stop the play of current track";
 
+    public static final String MESSAGE_NO_TRACK = "No track to stop";
 
     @Override
-    public CommandResult execute(Model model) {
-        PlayerManager.getInstance().stop();
+    public CommandResult execute(Model model) throws CommandException {
+        if (player.getStatus() == Playable.Status.UNINITIALIZED) {
+            throw new CommandException(MESSAGE_NO_TRACK);
+        }
+        player.stop();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
