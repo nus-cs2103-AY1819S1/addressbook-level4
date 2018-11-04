4f4995ab4970fc65f5cdf45040dfc30467bee8da
yican95 <yican95@gmail.com>
2018-11-04 18:29:43 +0800

    Model: Fix bug that allows adding item with same name and different price

--- src/main/java/seedu/restaurant/model/menu/Name.java
+++ src/main/java/seedu/restaurant/model/menu/Name.java
@@ -48,7 +48,7 @@ public class Name {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Name // instanceof handles nulls
-                    && fullName.equals(((Name) other).toString())); // state check
+                    && fullName.equalsIgnoreCase(((Name) other).toString())); // state check
     }
 
     @Override
