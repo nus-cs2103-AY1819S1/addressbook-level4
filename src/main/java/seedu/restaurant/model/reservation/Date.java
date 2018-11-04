c26c3c97fa7112b122502925868f6a9e812e3392
m4dkip <ongmingxian@gmail.com>
2018-11-04 22:25:25 +0800

    Model: Update Date to parse using NATTY

--- src/main/java/seedu/restaurant/model/reservation/Date.java
+++ src/main/java/seedu/restaurant/model/reservation/Date.java
@@ -4,9 +4,15 @@ import static java.util.Objects.requireNonNull;
 import static seedu.restaurant.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
+import java.util.List;
+import java.util.TimeZone;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
 
 //@@author m4dkip
 /**
@@ -25,25 +31,71 @@ public class Date {
     /**
      * Constructs a {@code date}.
      *
+     * !!!!!!!!!!!! IMPORTANT NOTE !!!!!!!!!!!!!
+     * Firstly checks if the date is able to be regularly parsed in DD-MM-YYYY format without using NATTY. This is
+     * necessary as NATTY cannot be configured to interpret dates in DD-MM-YYYY format, and will default to MM-DD-YYYY.
+     *
+     * i.e. NATTY will think 03-12-2018 means "12th of March" instead of "3rd of December"
+     *
      * @param date A valid date.
      */
     public Date(String date) {
         requireNonNull(date);
+
         checkArgument(isValidDate(date), MESSAGE_DATE_CONSTRAINTS);
-        DateTimeFormatter validFormat =
-                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT);
-        this.date = LocalDate.parse(date, validFormat);
+
+        if (Date.canStrictParse(date)) {
+            DateTimeFormatter validFormat =
+                    DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT);
+            this.date = LocalDate.parse(date, validFormat);
+        } else {
+            Parser parser = new Parser(TimeZone.getTimeZone("GMT+8:00"));
+            List<DateGroup> dateGroupList = parser.parse(date);
+
+            this.date = dateGroupList.get(0).getDates().get(0)
+                    .toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        }
     }
 
     /**
      * Returns true if a given date string is a valid date.
      */
     public static boolean isValidDate(String test) {
+        if (Date.canStrictParse(test) || Date.canNattyParse(test)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if a given date string can be regularly parsed.
+     */
+    public static boolean canStrictParse(String test) {
         DateTimeFormatter validFormat =
                 DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT);
         try {
             LocalDate.parse(test, validFormat);
         } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if a given date string can be parsed by Natty.
+     */
+    public static boolean canNattyParse(String test) {
+        Parser parser = new Parser(TimeZone.getTimeZone("GMT+8:00"));
+        List<DateGroup> dateGroupList = parser.parse(test);
+
+        if (dateGroupList.isEmpty()) {
+            return false;
+        }
+
+        List<java.util.Date> dates = dateGroupList.get(0).getDates();
+
+        if (dates.isEmpty()) {
             return false;
         }
         return true;
@@ -56,7 +108,7 @@ public class Date {
 
     @Override
     public String toString() {
-        return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT));
+        return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
     }
 
     @Override
