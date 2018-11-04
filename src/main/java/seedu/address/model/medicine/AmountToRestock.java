1d6245349c308cd96dc52d7478bf76ce5f8dc1c3
DESKTOP-NSV5A3C\Lee Yu Cong <yu_cong96@hotmail.com>
2018-11-04 16:02:03 +0800

    Checkstock and restock and dispense medicine can only take java integer values

---
+++ src/main/java/seedu/address/model/medicine/AmountToRestock.java
@@ -0,0 +1,52 @@
+package seedu.address.model.medicine;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represent the quantity of medicine to be restocked.
+ */
+public class AmountToRestock {
+
+    public final Integer value;
+
+    /**
+     * Constructs a {@code AmountToRestock}
+     *
+     * @param value A positive integer.
+     */
+    public AmountToRestock(Integer value) {
+        requireNonNull(value);
+        this.value = value;
+    }
+
+    /**
+     * Returns true if a given amount is a valid amount to restock.
+     */
+    public static boolean isValidAmountToRestock(AmountToRestock test) {
+        return test.getValue() > 0;
+    }
+
+    /**
+     * Getter for the value of the amount to restock.
+     */
+    public Integer getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof AmountToRestock
+                && value.equals(((AmountToRestock) other).getValue()));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
