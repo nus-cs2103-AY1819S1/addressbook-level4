83b31bc33770fe5fa4d91d41d7cb2810bf34e607
CrimsonAng <e0176842@u.nus.edu>
2018-11-05 21:28:32 +0800

    update: RangeOfWeekTest.java

--- src/test/java/ssp/scheduleplanner/model/rangeofweek/RangeOfWeekTest.java
+++ src/test/java/ssp/scheduleplanner/model/rangeofweek/RangeOfWeekTest.java
@@ -1,7 +1,6 @@
 package ssp.scheduleplanner.model.rangeofweek;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
