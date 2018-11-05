d59aacfd167831a53643cbb215b6fd152763014a
iMarbles <amabel_yeo@hotmail.com>
2018-11-05 09:56:02 +0800

    Overview: Added charts

--- src/main/java/seedu/address/ui/OverviewPanel.java
+++ src/main/java/seedu/address/ui/OverviewPanel.java
@@ -1,15 +1,23 @@
 package seedu.address.ui;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.OverviewPanelChangedEvent;
+import seedu.address.model.Overview;
 import seedu.address.model.event.Event;
 import seedu.address.model.record.Record;
 import seedu.address.model.volunteer.Volunteer;
@@ -27,15 +35,27 @@ public class OverviewPanel extends UiPart<Region> {
     private Label ongoingLabel;
     @FXML
     private Label completedLabel;
+    @FXML
+    private PieChart genderPieChart;
+    @FXML
+    private BarChart ageBarChart;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
 
     private ObservableList<Volunteer> volunteerList;
     private ObservableList<Event> eventList;
     private ObservableList<Record> recordList;
 
+    private Overview overview;
 
     public OverviewPanel(ObservableList<Volunteer> volunteerList, ObservableList<Event> eventList,
-                                                                    ObservableList<Record> recordList) {
+                         ObservableList<Record> recordList) {
         super(FXML);
+        Overview overview = new Overview(volunteerList, eventList, recordList);
+        this.overview = overview;
+
         this.volunteerList = volunteerList;
         this.eventList = eventList;
         this.recordList = recordList;
@@ -46,9 +66,46 @@ public class OverviewPanel extends UiPart<Region> {
         upcomingLabel.setText("Number of events: " + Integer.toString(eventList.size()));
     }
 
+    /**
+     * This method creates a pie chart which shows the gender ratio.
+     */
+    private void createGenderPieChart() {
+        String male = "Male (" + overview.getNumOfMale() + ")";
+        String female = "Female (" + overview.getNumOfFemale() + ")";
+
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data(male, overview.getNumOfMale()),
+                new PieChart.Data(female, overview.getNumOfFemale()));
+
+        genderPieChart.setData(pieChartData);
+        genderPieChart.setTitle("Gender Ratio");
+        genderPieChart.setLabelsVisible(true);
+    }
+
+    /**
+     * This method creates a bar chart which shows the age distribution.
+     */
+    private void createAgeBarChart() {
+        xAxis.setCategories(FXCollections.
+                observableArrayList(Arrays.asList("14 and Below", "15 to 24", "24 to 64", "65 and Above")));
+        yAxis.setLabel("Number");
+
+        //Prepare XYChart.Series objects by setting data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("14 and Below", overview.getNumOfChildren()));
+        series.getData().add(new XYChart.Data<>("15 to 24", overview.getNumOfYouth()));
+        series.getData().add(new XYChart.Data<>("24 to 64", overview.getNumOfAdult()));
+        series.getData().add(new XYChart.Data<>("65 and Above", overview.getNumOfSenior()));
+
+        ageBarChart.setTitle("Age Distribution of Volunteers");
+        ageBarChart.getData().add(series);
+    }
+
     @Subscribe
     private void handleOverviewPanelSelectionChangedEvent(OverviewPanelChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         setLabelText();
+        createGenderPieChart();
+        createAgeBarChart();
     }
 }
