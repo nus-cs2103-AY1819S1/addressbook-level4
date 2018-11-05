30265861bed43e72006a2cdd0fdba204cbac979c
hotarumin <>
2018-11-05 21:51:21 +0800

    add display tags

--- src/main/java/ssp/scheduleplanner/ui/SidebarPanel.java
+++ src/main/java/ssp/scheduleplanner/ui/SidebarPanel.java
@@ -1,24 +1,32 @@
 package ssp.scheduleplanner.ui;
 
+import java.util.Iterator;
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import ssp.scheduleplanner.commons.core.LogsCenter;
+import ssp.scheduleplanner.commons.events.model.SchedulePlannerChangedEvent;
 import ssp.scheduleplanner.commons.events.ui.NewResultAvailableEvent;
 import ssp.scheduleplanner.logic.ListElementPointer;
 import ssp.scheduleplanner.logic.Logic;
 import ssp.scheduleplanner.logic.commands.CommandResult;
 import ssp.scheduleplanner.logic.commands.exceptions.CommandException;
 import ssp.scheduleplanner.logic.parser.exceptions.ParseException;
+import ssp.scheduleplanner.model.category.Category;
+import ssp.scheduleplanner.model.tag.Tag;
 
 /**
  * The Sidebar Panel of the App.
@@ -38,7 +46,7 @@ public class SidebarPanel extends UiPart<Region> {
     private ToggleButton week;
 
     @FXML
-    private Accordion modules;
+    private Accordion accordion;
 
     @FXML
     private Text categorieslabel;
@@ -47,6 +55,8 @@ public class SidebarPanel extends UiPart<Region> {
 
     public SidebarPanel(Logic logic) {
         super(FXML);
+        registerAsAnEventHandler(this);
+
         this.logic = logic;
         this.historySnapshot = logic.getHistorySnapshot();
         this.group = new ToggleGroup();
@@ -55,6 +65,8 @@ public class SidebarPanel extends UiPart<Region> {
         categorieslabel.setFill(Color.WHITE);
         today.setAlignment(Pos.BASELINE_LEFT);
         week.setAlignment(Pos.BASELINE_LEFT);
+
+        setTags(logic.getCategoryList());
     }
 
     /**
@@ -101,4 +113,30 @@ public class SidebarPanel extends UiPart<Region> {
         historySnapshot.add("");
     }
 
+    @Subscribe
+    public void handleSchedulePlannerChangedEvent(SchedulePlannerChangedEvent e) {
+        setTags(e.data.getCategoryList());
+    }
+
+    private void setTags(ObservableList<Category> categories) {
+        accordion.getPanes().removeAll();
+        System.out.println(categories.size());
+        for (Category cat : categories) {
+            String catName = cat.getName();
+            System.out.println(catName);
+            TitledPane catPane = new TitledPane();
+            catPane.setText(catName);
+            accordion.getPanes().add(catPane);
+            ObservableList<Tag> tags = cat.getTags();
+            System.out.println(tags.size());
+            for (Tag tag : tags) {
+                System.out.println("adding tags");
+                String tagName = tag.getTagName();
+                ToggleButton tagButton = new ToggleButton(tagName);
+                tagButton.setToggleGroup(group);
+                System.out.println(tagName);
+                catPane.setContent(tagButton);
+            }
+        }
+    }
 }
