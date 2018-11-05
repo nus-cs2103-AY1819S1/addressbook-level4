5b4450319d43e019622cd2df25894a4af4c05a52
Scrubbius <ice_devil1995@hotmail.com>
2018-11-06 01:46:44 +0800

    Import commands added

--- src/main/java/seedu/address/logic/parser/ImportVolunteerCsvCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportVolunteerCsvCommandParser.java
@@ -1,12 +1,16 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.logic.commands.ExportVolunteerCsvCommand;
+import seedu.address.logic.commands.ImportVolunteerCsvCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-public class ImportVolunteerCsvCommandParser {
+
+public class ImportVolunteerCsvCommandParser implements Parser<ImportVolunteerCsvCommand>{
 
     /**
      * Parses the given {@code String} of arguments in the context of the {@code ImportVolunteerCsvCommand}
@@ -15,10 +19,15 @@ public class ImportVolunteerCsvCommandParser {
      * @return ImportVolunteerCsvCommand object
      * @throws ParseException if the user input does not abide by the expected format
      */
-    public ExportVolunteerCsvCommand parse(String userInput) throws ParseException {
+    public ImportVolunteerCsvCommand parse(String userInput) throws ParseException {
         requireNonNull(userInput);
 
-
+        try {
+            FileReader csvFile = new FileReader(userInput);
+            return new ImportVolunteerCsvCommand(csvFile);
+        } catch (FileNotFoundException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportVolunteerCsvCommand.FILE_ERROR));
+        }
         
     }
 }
