0f80e1b216a460408d2886bf8336798559e2a8ae
Arsaln Cheema <arsalanc.v2@gmail.com>
2018-11-06 17:56:29 +0800

    add files

---
+++ src/main/java/seedu/clinicio/model/analytics/data/CircularList.java
@@ -0,0 +1,61 @@
+package seedu.clinicio.model.analytics.data;
+
+//@@author arsalanc-v2
+
+import java.util.ArrayList;
+
+/**
+ * A circular list to cycle forwards and backwards through data.
+ * Makes use of the existing java ArrayList implementation.
+ * Elements cannot be removed.
+ */
+public class CircularList<T> {
+
+    private ArrayList<T> lst;
+    private int index;
+
+    public CircularList() {
+        lst = new ArrayList<>();
+        index = -1;
+    }
+
+    public void add(T val) {
+        lst.add(val);
+    }
+
+    public T getFirst() {
+        if (getSize() == 0) {
+            return null;
+        } else {
+            return lst.get(0);
+        }
+    }
+
+    public T getLast() {
+        if (getSize() == 0) {
+            return null;
+        } else {
+            return lst.get(getSize() - 1);
+        }
+    }
+
+    public T getNext() {
+        index += 1;
+        if (index > getSize() - 1) {
+            index = 0;
+        }
+        return lst.get(index);
+    }
+
+    public T getPrevious() {
+        index -= 1;
+        if (index < 0) {
+            index = getSize() - 1;
+        }
+       return lst.get(index);
+    }
+
+    public int getSize() {
+        return lst.size();
+    }
+}
---
+++ src/main/java/seedu/clinicio/model/analytics/util/TimeUtil.java
@@ -0,0 +1,99 @@
+package seedu.clinicio.model.analytics.util;
+
+//@@author arsalanc-v2
+
+import static java.lang.Math.toIntExact;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.clinicio.model.analytics.data.Tuple;
+import seedu.clinicio.model.appointment.Time;
+
+/**
+ * Contains utility methods for computing ocurrences and retrieving time values.
+ */
+public class TimeUtil {
+
+    /**
+     * stores periods of time in a list, each as a tuple with start and end time
+     * the periods make up the hours in a working clinic day: 9AM - 9PM
+     */
+    private static final List<Tuple<Time, Time>> timePeriods = Arrays.asList(
+        new Tuple<Time, Time>(new Time(9, 0), new Time(11, 59)),
+        new Tuple<Time, Time>(new Time(12, 0), new Time(14, 59)),
+        new Tuple<Time, Time>(new Time(15, 0), new Time(17, 59)),
+        new Tuple<Time, Time>(new Time(18, 0), new Time(20, 59))
+    );
+
+
+    /**
+     * @return the number of occurrences for each time period.
+     */
+    public static List<Tuple<Tuple<Time, Time>, Integer>> getTimeGroupsCount(List<Time> times) {
+
+        List<Tuple<Tuple<Time, Time>, Integer>> timeGroupsCount = new ArrayList<>();
+        for (Tuple<Time, Time> timePeriod : timePeriods) {
+            long count = times.stream()
+                .filter(time -> isAfterAndBeforeInclusive(time, timePeriod.getKey(), timePeriod.getValue()))
+                .count();
+
+            timeGroupsCount.add(new Tuple<Tuple<Time, Time>, Integer>(timePeriod, toIntExact(count)));
+        }
+
+        return timeGroupsCount;
+    }
+
+    /**
+     * Checks if {@code time} occurs before or equal to {@code before} AND occurs after or equal to {@code after}
+     */
+    public static boolean isAfterAndBeforeInclusive(Time time, Time after, Time before) {
+        return isAfterOrEqual(time, after) && isBeforeOrEqual(time, before);
+    }
+
+    /**
+     * Checks if {@code time1} occurs after {@code time2} or is the same.
+     * Assumes both times are from the same date.
+     */
+    public static boolean isAfterOrEqual(Time time1, Time time2) {
+        if (time1.equals(time2)) {
+            return true;
+        }
+
+        if (time1.getHour() < time2.getHour()) {
+            return false;
+        } else if (time1.getHour() > time2.getHour()) {
+            return true;
+        } else {
+            if (time1.getMinute() < time2.getMinute()) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    /**
+     * Checks if {@code time1} occurs before {@code time2} or is the same.
+     * Assumes both times are from the same date.
+     */
+    public static boolean isBeforeOrEqual(Time time1, Time time2) {
+        if (time1.equals(time2)) {
+            return true;
+        }
+
+        if (time1.getHour() > time2.getHour()) {
+            return false;
+        } else if (time1.getHour() < time2.getHour()) {
+            return true;
+        } else {
+            if (time1.getMinute() > time2.getMinute()) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+}
