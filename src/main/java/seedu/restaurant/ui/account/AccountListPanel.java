7f8846b74d611e6316c9e3b6a7232dc4b559f95e
AZhiKai <ang-zhikai@outlook.com>
2018-11-07 00:51:33 +0800

    AccountListPanel: Handle jump event

--- src/main/java/seedu/restaurant/ui/account/AccountListPanel.java
+++ src/main/java/seedu/restaurant/ui/account/AccountListPanel.java
@@ -1,10 +1,17 @@
 package seedu.restaurant.ui.account;
 
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import seedu.restaurant.commons.core.LogsCenter;
+import seedu.restaurant.commons.events.ui.accounts.JumpToAccountListRequestEvent;
 import seedu.restaurant.model.account.Account;
 import seedu.restaurant.ui.UiPart;
 
@@ -15,6 +22,7 @@ import seedu.restaurant.ui.UiPart;
 public class AccountListPanel extends UiPart<Region> {
 
     private static final String FXML = "AccountListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(AccountListPanel.class);
 
     @FXML
     private ListView<Account> accountListView;
@@ -25,9 +33,31 @@ public class AccountListPanel extends UiPart<Region> {
         registerAsAnEventHandler(this);
     }
 
+    @FXML
+    private void handleMouseClick() {
+        Account account = accountListView.getSelectionModel().getSelectedItem();
+        logger.fine("Selection in account list panel changed to : '" + account + "'");
+    }
+
     private void setConnections(ObservableList<Account> itemList) {
         accountListView.setItems(itemList);
         accountListView.setCellFactory(listView -> new AccountListViewCell());
+    }
+
+    /**
+     * Scrolls to the {@code AccountCard} at the {@code index} and selects it.
+     */
+    private void scrollTo(int index) {
+        Platform.runLater(() -> {
+            accountListView.scrollTo(index);
+            accountListView.getSelectionModel().clearAndSelect(index);
+        });
+    }
+
+    @Subscribe
+    private void handleJumpToAccountListRequestEvent(JumpToAccountListRequestEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        scrollTo(event.targetIndex);
     }
 
     /**
