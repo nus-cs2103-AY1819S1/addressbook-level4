e524f7d117a0c23aacc458d4d5af8f5972405c39
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-07 00:39:29 +0800

    Update LogicManagerTest.java
    Add test cases for getAllPatientsInQueue.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/logic/LogicManagerTest.java
+++ src/test/java/seedu/clinicio/logic/LogicManagerTest.java
@@ -64,6 +64,12 @@ public class LogicManagerTest {
         logic.getFilteredStaffList().remove(0);
     }
 
+    @Test
+    public void getAllPatientsInQueue_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        logic.getAllPatientsInQueue().remove(0);
+    }
+
     /**
      * Executes the command, confirms that no exceptions are thrown and that the result message is correct.
      * Also confirms that {@code expectedModel} is as specified.
