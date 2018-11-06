8697737ae2fa95acd8f29c116f0a3a0c572c3401
elroyhaw <elroyhaw@gmail.com>
2018-11-06 20:15:32 +0800

    Remove whitespaces

--- src/main/java/seedu/address/logic/commands/RegisterPatientCommand.java
+++ src/main/java/seedu/address/logic/commands/RegisterPatientCommand.java
@@ -34,7 +34,7 @@ public class RegisterPatientCommand extends Command {
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 ";
 
     public static final String MESSAGE_SUCCESS = "New patient registered: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = 
+    public static final String MESSAGE_DUPLICATE_PERSON =
             "There exists a patient/doctor with this name in the HealthBook";
 
     private final Patient patientToRegister;
