a1acda3a543999e4ae389341e64f7afea20ebf84
ryantanwk <ryantan2103@gmail.com>
2018-11-06 20:48:29 +0800

    merge issues

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -6,10 +6,13 @@ import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.CLASHING_EVENT_END_TIME_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.CLASHING_EVENT_START_TIME_DOCTORAPPT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_APPOINTMENT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.ImportContactsPersons.getImportContactsAddressBook;
 import static seedu.address.testutil.TypicalEvents.DOCTORAPPT;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalTags.APPOINTMENT_TAG;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -25,8 +28,12 @@ import javafx.collections.ObservableList;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.exceptions.DuplicateEventException;
 import seedu.address.model.event.exceptions.EventClashException;
+import seedu.address.model.filereader.FileReader;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.exceptions.DuplicateTagException;
+import seedu.address.testutil.FileReaderBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.ScheduledEventBuilder;
 
@@ -67,7 +74,8 @@ public class AddressBookTest {
         AddressBookStub newData = new AddressBookStub(newPersons, newEvents);
 
         thrown.expect(DuplicatePersonException.class);
-        addressBook.resetData(newData);
+        List<Tag> newTags = Arrays.asList(APPOINTMENT_TAG);	        AddressBookStub newData = new AddressBookStub(newPersons, newEvents);
+        AddressBookStub newData = new AddressBookStub(newPersons, newEvents, newTags);
     }
 
     @Test
@@ -79,7 +87,8 @@ public class AddressBookTest {
         Event duplicateEvent =
                 new ScheduledEventBuilder(DOCTORAPPT).build();
         List<Event> newEvents = Arrays.asList(DOCTORAPPT, duplicateEvent);
-        AddressBookStub newData = new AddressBookStub(newPersons, newEvents);
+        List<Tag> newTags = Arrays.asList(APPOINTMENT_TAG);
+        AddressBookStub newData = new AddressBookStub(newPersons, newEvents, newTags);
 
         thrown.expect(DuplicateEventException.class);
         addressBook.resetData(newData);
@@ -97,9 +106,22 @@ public class AddressBookTest {
                         .withEventEndTime(CLASHING_EVENT_END_TIME_DOCTORAPPT)
                         .build();
         List<Event> newEvents = Arrays.asList(DOCTORAPPT, clashingEvent);
-        AddressBookStub newData = new AddressBookStub(newPersons, newEvents);
+        List<Tag> newTags = Arrays.asList(APPOINTMENT_TAG);	        AddressBookStub newData = new AddressBookStub(newPersons, newEvents);
+        AddressBookStub newData = new AddressBookStub(newPersons, newEvents, newTags);
 
         thrown.expect(EventClashException.class);
+        addressBook.resetData(newData);
+    }
+
+    @Test
+    public void resetData_withDuplicateEventTags_throwsDuplicateTagException() {
+        List<Person> newPersons = Arrays.asList(ALICE);
+        List<Event> newEvents = Arrays.asList(DOCTORAPPT);
+        // Two event tags with the same values (case-insensitive)
+        Tag duplicateTag = new Tag(VALID_TAG_APPOINTMENT.toUpperCase());
+        List<Tag> newTags = Arrays.asList(APPOINTMENT_TAG, duplicateTag);
+        AddressBookStub newData = new AddressBookStub(newPersons, newEvents, newTags);
+        thrown.expect(DuplicateTagException.class);
         addressBook.resetData(newData);
     }
 
@@ -107,6 +129,12 @@ public class AddressBookTest {
     public void hasPerson_nullPerson_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
         addressBook.hasPerson(null);
+    }
+
+    @Test
+    public void hasEventTag_nullTag_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        addressBook.hasEventTag(null);
     }
 
     @Test
@@ -118,6 +146,11 @@ public class AddressBookTest {
     @Test
     public void hasPerson_personNotInAddressBook_returnsFalse() {
         assertFalse(addressBook.hasPerson(ALICE));
+    }
+
+    @Test
+    public void hasEventTag_tagNotInAddressBook_returnsFalse() {
+        assertFalse(addressBook.hasEventTag(APPOINTMENT_TAG));
     }
 
     @Test
@@ -138,18 +171,26 @@ public class AddressBookTest {
 
     @Test
     public void hasEvent_eventInAddressBook_returnsTrue() {
+        DOCTORAPPT.getEventTags().forEach(addressBook::addEventTag);
         addressBook.addEvent(DOCTORAPPT);
         assertTrue(addressBook.hasEvent(DOCTORAPPT));
     }
 
     @Test
     public void hasClashingEvent_clashingEventInAddressBook_returnsTrue() {
+        DOCTORAPPT.getEventTags().forEach(addressBook::addEventTag);
         addressBook.addEvent(DOCTORAPPT);
         Event clashingEvent = new ScheduledEventBuilder(DOCTORAPPT)
                 .withEventStartTime(CLASHING_EVENT_START_TIME_DOCTORAPPT)
                 .withEventEndTime(CLASHING_EVENT_END_TIME_DOCTORAPPT)
                 .build();
         assertTrue(addressBook.hasClashingEvent(clashingEvent));
+    }
+
+    @Test
+    public void hasEventTag_tagInAddressBook_returnsTrue() {
+        addressBook.addEventTag(APPOINTMENT_TAG);
+        assertTrue(addressBook.hasEventTag(APPOINTMENT_TAG));
     }
 
     @Test
@@ -162,15 +203,37 @@ public class AddressBookTest {
 
     @Test
     public void hasEvent_eventWithSameIdentityFieldsInAddressBook_returnsTrue() {
+        DOCTORAPPT.getEventTags().forEach(addressBook::addEventTag);
         addressBook.addEvent(DOCTORAPPT);
         Event duplicateEvent = new ScheduledEventBuilder(DOCTORAPPT).build();
         assertTrue(addressBook.hasEvent(duplicateEvent));
     }
 
     @Test
+    public void hasEventTag_tagWithSameValuesInAddressBook_returnsTrue() {
+        addressBook.addEventTag(APPOINTMENT_TAG);
+        Tag duplicateEventTag = new Tag(VALID_TAG_APPOINTMENT);
+        assertTrue(addressBook.hasEventTag(duplicateEventTag));
+    }
+
+    @Test
     public void getPersonList_modifyList_throwsUnsupportedOperationException() {
         thrown.expect(UnsupportedOperationException.class);
         addressBook.getPersonList().remove(0);
+    }
+
+    @Test
+    public void getEventTagList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        addressBook.getEventTagList().add(APPOINTMENT_TAG);
+    }
+    @Test
+    public void hasPersonsImported() {
+        FileReader fileReader = new FileReaderBuilder().build();
+        AddressBook importAddressBook = getImportContactsAddressBook();
+        AddressBook newAddressBook = new AddressBook();
+        newAddressBook.importContacts(fileReader);
+        assertEquals(importAddressBook, newAddressBook);
     }
 
     @Test
@@ -185,13 +248,20 @@ public class AddressBookTest {
     private static class AddressBookStub implements ReadOnlyAddressBook {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
         private final ObservableList<Event> events = FXCollections.observableArrayList();
+        private final ObservableList<Tag> eventTags = FXCollections.observableArrayList();
         private final boolean notificationPref = true;
         private final String favourite = null;
 
-        AddressBookStub(Collection<Person> persons, Collection<Event> events) {
-            this.persons.setAll(persons);
-            this.events.setAll(events);
+        AddressBookStub(Collection<Person> persons, Collection<Event> events, Collection<Tag> eventTags) {	        AddressBookStub(Collection<Person> persons, Collection<Event> events) {
+            this.persons.setAll(persons);	            this.persons.setAll(persons);
+            this.events.setAll(events);	            this.events.setAll(events);
+            this.eventTags.setAll(eventTags);
         }
+
+        @Override
+        public ObservableList<Tag> getEventList() {
+            return eventTags;
+            }
 
         @Override
         public ObservableList<Person> getPersonList() {
