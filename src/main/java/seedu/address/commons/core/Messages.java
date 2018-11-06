3a4c0b0d14d576357cc33dc4a113de7f81d3ccb2
ryantanwk <ryantan2103@gmail.com>
2018-11-06 19:50:48 +0800

    error

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,6 +11,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_EVENT_DISPLAYED_INDEX = "The event index provided is invalid";
     public static final String MESSAGE_INVALID_EVENT_DISPLAYED_DATE = "The event date provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    //public static final String MESSAGE_INVALID_EVENT_DISPLAYED_DATE = "The event date provided is invalid";
-
+    public static final String MESSAGE_NONEXISTENT_TAG = "Event tag %1$s does not exist in the address book";
+    public static final String MESSAGE_FILE_READER_INVALID_FORMAT = "Wrong file format or wrong header format! \n%1$s";
 }
