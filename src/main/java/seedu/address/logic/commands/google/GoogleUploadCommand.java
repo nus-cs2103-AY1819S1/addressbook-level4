0fafcab9979be12d04b1fd7d24b49cc3ff7adb1e
chivent <e0191350@u.nus.edu>
2018-11-06 13:03:22 +0800

    Adding more tests - Tests for GoogleCommandParser - Tests for PiconsoParser - Tests for PhotoHandler - Restored test for ParserUtilTest

--- src/main/java/seedu/address/logic/commands/google/GoogleUploadCommand.java
+++ src/main/java/seedu/address/logic/commands/google/GoogleUploadCommand.java
@@ -15,20 +15,20 @@ import seedu.address.model.Model;
  * Handles download and upload (to be added) of files to Google Photos
  */
 public class GoogleUploadCommand extends GoogleCommand {
+    public static final String TYPE = "ul";
+    public static final String FULL_CMD = COMMAND_WORD + " " + TYPE;
+    public static final String MESSAGE_USAGE = "Usage of google upload (requires an internet connection): "
+            + "\n- " + FULL_CMD + " <IMAGE_NAME>: Uploads specified image to Google Photos"
+            + "\n\tExample: " + FULL_CMD + " <mountain.png>, usage inclusive of <> "
+            + "\n- " + FULL_CMD + " all: Uploads all images in current directory to Google Photos, "
+            + "takes a longer amount of time depending on number of images to upload. \n\n"
+            + "!!NOTE: All photos uploaded from Piconso can be found in album 'Piconso Uploads'";
+
     public static final String MESSAGE_ALL_DUPLICATE = "Failure to upload. %s already exist(s) in Google Photos.";
     private static final String ADVICE = "\n\nYou'll need to use `g refresh` before you can see it by ls!";
     public static final String MESSAGE_SUCCESS = "Successfully uploaded to Google Photos: \n%s" + ADVICE;
     public static final String MESSAGE_DUPLICATE = "Upload success. Some of the images in the selected folder are "
             + "duplicates, only the following were uploaded: \n%s" + ADVICE;
-
-    private static final String TYPE = COMMAND_WORD + " ul";
-    public static final String MESSAGE_USAGE = "Usage of google upload (requires an internet connection): "
-            + "\n- " + TYPE + " <IMAGE_NAME>: Uploads specified image to Google Photos"
-            + "\n\tExample: " + TYPE + " <mountain.png>, usage inclusive of <> "
-            + "\n- " + TYPE + " all: Uploads all images in current directory to Google Photos, "
-            + "takes a longer amount of time depending on number of images to upload. \n\n"
-            + "!!NOTE: All photos uploaded from Piconso can be found in album 'Piconso Uploads'";
-
     public static final String MESSAGE_FAILURE = "%s failed to upload." + "\n\n" + MESSAGE_USAGE;
 
     public GoogleUploadCommand(String parameter) {
@@ -78,5 +78,16 @@ public class GoogleUploadCommand extends GoogleCommand {
         } else {
             return new CommandResult(String.format(MESSAGE_DUPLICATE, uploaded));
         }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof GoogleUploadCommand)) { //this handles null as well.
+            return false;
+        }
+        return ((GoogleUploadCommand) other).parameter.equals(this.parameter);
     }
 }
