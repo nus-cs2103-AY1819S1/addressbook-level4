78bafa919bc77d75f4533cdfae1d5ac92c0a4444
Kok Yuan <warfear@hotmail.com>
2018-11-07 00:06:08 +0800

    Added unit tests to EmailDirStorageTest

--- src/test/java/seedu/address/storage/EmailDirStorageTest.java
+++ src/test/java/seedu/address/storage/EmailDirStorageTest.java
@@ -1,13 +1,20 @@
 package seedu.address.storage;
 
+import static org.junit.Assert.assertEquals;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
+import org.simplejavamail.email.Email;
+
+import seedu.address.model.EmailModel;
+import seedu.address.testutil.DefaultEmailBuilder;
 
 //@@author EatOrBeEaten
 public class EmailDirStorageTest {
@@ -26,7 +33,32 @@ public class EmailDirStorageTest {
         readEmailFiles(null);
     }
 
+    @Test
+    public void readEmailFiles_missingDirectory_returnsEmptySet() {
+        assertEquals(readEmailFiles("nonExistentDirectory"), new HashSet<>());
+    }
+
+    @Test
+    public void readEmailFiles_emptyDirectory_returnsEmptySet() {
+        String filePath = testFolder.getRoot().toPath().toString();
+        assertEquals(readEmailFiles(filePath), new HashSet<>());
+    }
+
     private Set<String> readEmailFiles(String filePath) {
         return new EmailDirStorage(Paths.get(filePath)).readEmailFiles(Paths.get(filePath));
     }
+
+//    @Test
+//    public void loadEmail_allInOrder_success() throws Exception {
+//        Email validEmail = new DefaultEmailBuilder().build();
+//        EmailModel emailModel = new EmailModel();
+//        emailModel.saveComposedEmail(validEmail);
+//        Path filePath = testFolder.getRoot().toPath();
+//        EmailDirStorage emailDirStorage = new EmailDirStorage(filePath);
+//
+//        // Save in a new email and read back
+//        emailDirStorage.saveEmail(emailModel);
+//        Email readBack = emailDirStorage.loadEmail(validEmail.getSubject());
+//        assertEquals(validEmail, readBack);
+//    }
 }
