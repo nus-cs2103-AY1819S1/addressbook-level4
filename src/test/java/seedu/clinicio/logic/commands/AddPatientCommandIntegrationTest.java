1a78317f1a429e71ae00887f5271e3e2529f86af
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-07 00:07:08 +0800

    Add test cases for add patient
    Add Unit tests and integration tests for AddPatientCommand.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

---
+++ src/test/java/seedu/clinicio/logic/commands/AddPatientCommandIntegrationTest.java
@@ -0,0 +1,53 @@
+package seedu.clinicio.logic.commands;
+
+import static seedu.clinicio.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.clinicio.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.clinicio.testutil.TypicalPersons.getTypicalClinicIo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.clinicio.logic.CommandHistory;
+
+import seedu.clinicio.model.Model;
+import seedu.clinicio.model.ModelManager;
+import seedu.clinicio.model.UserPrefs;
+import seedu.clinicio.model.analytics.Analytics;
+import seedu.clinicio.model.patient.Patient;
+
+import seedu.clinicio.testutil.PatientBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code AddPatientCommand}.
+ */
+public class AddPatientCommandIntegrationTest {
+
+    private Model model;
+    private CommandHistory commandHistory = new CommandHistory();
+    private Analytics analytics = new Analytics();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalClinicIo(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newPatient_success() {
+        Patient validPatient = new PatientBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getClinicIo(), new UserPrefs());
+        expectedModel.addPatient(validPatient);
+        expectedModel.commitClinicIo();
+
+        assertCommandSuccess(new AddPatientCommand(validPatient), model, commandHistory,
+                String.format(AddPatientCommand.MESSAGE_SUCCESS, validPatient), expectedModel, analytics);
+    }
+
+    @Test
+    public void execute_duplicatePatient_throwsCommandException() {
+        Patient patientInList = model.getClinicIo().getPatientList().get(0);
+        assertCommandFailure(new AddPatientCommand(patientInList), model, commandHistory,
+                analytics, AddPatientCommand.MESSAGE_DUPLICATE_PATIENT);
+    }
+
+}
--- src/test/java/seedu/clinicio/testutil/PatientBuilder.java
+++ src/test/java/seedu/clinicio/testutil/PatientBuilder.java
@@ -1,8 +1,16 @@
 package seedu.clinicio.testutil;
 
+import java.util.HashSet;
 import java.util.Optional;
 
+import java.util.Set;
+
 import seedu.clinicio.model.appointment.Appointment;
+
+import seedu.clinicio.model.patient.Allergy;
+import seedu.clinicio.model.patient.MedicalProblem;
+import seedu.clinicio.model.patient.Medication;
+import seedu.clinicio.model.patient.Nric;
 import seedu.clinicio.model.patient.Patient;
 import seedu.clinicio.model.person.Person;
 import seedu.clinicio.model.staff.Staff;
@@ -12,11 +20,21 @@ import seedu.clinicio.model.staff.Staff;
  */
 public class PatientBuilder extends PersonBuilder {
 
+    public static final String DEFAULT_NRIC = "S1234567A";
+
+    private Nric nric;
+    private Set<MedicalProblem> medicalProblems;
+    private Set<Medication> medications;
+    private Set<Allergy> allergies;
     private Optional<Staff> preferredDoctor;
     private Optional<Appointment> appointment;
 
     public PatientBuilder() {
         super();
+        nric = new Nric(DEFAULT_NRIC);
+        medicalProblems = new HashSet<>();
+        medications = new HashSet<>();
+        allergies = new HashSet<>();
         preferredDoctor = Optional.empty();
         appointment = Optional.empty();
     }
@@ -26,6 +44,10 @@ public class PatientBuilder extends PersonBuilder {
      */
     public PatientBuilder(Patient patientToCopy) {
         super(patientToCopy);
+        nric = patientToCopy.getNric();
+        medicalProblems = new HashSet<>(patientToCopy.getMedicalProblems());
+        medications = new HashSet<>(patientToCopy.getMedications());
+        allergies = new HashSet<>(patientToCopy.getAllergies());
         preferredDoctor = patientToCopy.getPreferredDoctor();
         appointment = patientToCopy.getAppointment();
     }
@@ -38,6 +60,51 @@ public class PatientBuilder extends PersonBuilder {
     public static PatientBuilder buildFromPerson(Person person) {
         Patient patient = Patient.buildFromPerson(person);
         return new PatientBuilder(patient);
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Patient} that we are building.
+     * @return a PatientBuilder
+     */
+    public PatientBuilder withName(String name) {
+        super.withName(name);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code Patient} that we are building.
+     * @return a PatientBuilder
+     */
+    public PatientBuilder withAddress(String address) {
+        super.withAddress(address);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Patient} that we are building.
+     * @return a PatientBuilder
+     */
+    public PatientBuilder withPhone(String phone) {
+        super.withPhone(phone);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code Patient} that we are building.
+     * @return a PatientBuilder
+     */
+    public PatientBuilder withEmail(String email) {
+        super.withEmail(email);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Nric} of the {@code Patient} that we are building.
+     * @return a PatientBuilder
+     */
+    public PatientBuilder withNric(String nric) {
+        this.nric = new Nric(nric);
+        return this;
     }
 
     /**
