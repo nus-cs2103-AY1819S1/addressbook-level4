5209e132456f1de6804d1b58478e836d5d409746
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-06 23:15:48 +0800

    Add methods for LoginCommandTest.java
    Add test cases to LoginCommandTest to cater for receptionist.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/logic/commands/LoginCommandTest.java
+++ src/test/java/seedu/clinicio/logic/commands/LoginCommandTest.java
@@ -4,15 +4,20 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import static seedu.clinicio.logic.commands.CommandTestUtil.VALID_PASSWORD_ADAM;
-import static seedu.clinicio.logic.commands.CommandTestUtil.VALID_PASSWORD_BEN;
+import static seedu.clinicio.logic.commands.CommandTestUtil.VALID_PASSWORD_ALAN;
 import static seedu.clinicio.logic.commands.CommandTestUtil.VALID_PASSWORD_CAT;
+import static seedu.clinicio.logic.commands.CommandTestUtil.VALID_PASSWORD_DAISY;
 import static seedu.clinicio.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.clinicio.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import static seedu.clinicio.model.staff.Role.DOCTOR;
+import static seedu.clinicio.model.staff.Role.RECEPTIONIST;
 import static seedu.clinicio.testutil.TypicalPersons.ADAM;
+import static seedu.clinicio.testutil.TypicalPersons.ALAN;
 import static seedu.clinicio.testutil.TypicalPersons.BEN;
 import static seedu.clinicio.testutil.TypicalPersons.CAT;
+import static seedu.clinicio.testutil.TypicalPersons.DAISY;
+import static seedu.clinicio.testutil.TypicalPersons.FRANK;
 import static seedu.clinicio.testutil.TypicalPersons.getTypicalClinicIo;
 
 import org.junit.Rule;
@@ -20,6 +25,7 @@ import org.junit.Test;
 
 import org.junit.rules.ExpectedException;
 
+import seedu.clinicio.commons.core.UserSession;
 import seedu.clinicio.logic.CommandHistory;
 
 import seedu.clinicio.model.Model;
@@ -42,42 +48,44 @@ public class LoginCommandTest {
 
     @Test
     public void execute_validCredentials_returnTrue() {
-        LoginCommand command = new LoginCommand(
-                new Staff(DOCTOR, ADAM.getName(), new Password("doctor1", false)));
         String expectedMessage = LoginCommand.MESSAGE_SUCCESS;
 
+        LoginCommand command = new LoginCommand(
+                new Staff(DOCTOR, ADAM.getName(), new Password(VALID_PASSWORD_ADAM, false)));
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
 
-        // TODO: Receptionist
+        UserSession.destorySession();
+        
+        command = new LoginCommand(
+                new Staff(RECEPTIONIST, ALAN.getName(), new Password(VALID_PASSWORD_ALAN, false)));
+        assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
+
     }
 
     @Test
     public void execute_invalidCredentials_returnFalse() {
         String expectedMessage = LoginCommand.MESSAGE_FAILURE;
 
-        // different name
         LoginCommand command = new LoginCommand(
                 new Staff(DOCTOR, BEN.getName(), new Password(VALID_PASSWORD_ADAM, false)));
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
 
-        // different password
         command = new LoginCommand(
-                new Staff(DOCTOR, ADAM.getName(), new Password(VALID_PASSWORD_BEN, false)));
+                new Staff(RECEPTIONIST, FRANK.getName(), new Password(VALID_PASSWORD_ALAN, false)));
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel, analytics);
-
-        // TODO: Receptionist
     }
 
     @Test
     public void execute_staffNotInClinicIO_throwsCommandException() {
         String expectedMessage = LoginCommand.MESSAGE_NO_RECORD_FOUND;
 
-        // Staff not inside ClinicIO
         LoginCommand command = new LoginCommand(
                 new Staff(DOCTOR, CAT.getName(), new Password(VALID_PASSWORD_CAT, false)));
         assertCommandFailure(command, model, commandHistory, analytics, expectedMessage);
 
-        // TODO: Receptionist
+        command = new LoginCommand(
+                new Staff(RECEPTIONIST, DAISY.getName(), new Password(VALID_PASSWORD_DAISY, false)));
+        assertCommandFailure(command, model, commandHistory, analytics, expectedMessage);
     }
 
     @Test
@@ -85,7 +93,7 @@ public class LoginCommandTest {
         LoginCommand loginFirstCommand = new LoginCommand(
                 new Staff(DOCTOR, ADAM.getName(), ADAM.getPassword()));
         LoginCommand loginSecondCommand = new LoginCommand(
-                new Staff(DOCTOR, BEN.getName(), BEN.getPassword()));
+                new Staff(RECEPTIONIST, BEN.getName(), BEN.getPassword()));
 
         // same object -> returns true
         assertTrue(loginFirstCommand.equals(loginFirstCommand));
