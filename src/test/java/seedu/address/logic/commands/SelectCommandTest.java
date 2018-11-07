1a8951bd74f44648c5face27c5efcf19f2cda586
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2018-11-07 18:27:47 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/commands/SelectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SelectCommandTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.FilmReelSelectionChangeEvent;
+//import seedu.address.commons.events.ui.FilmReelSelectionChangeEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.ui.testutil.EventsCollectorRule;
