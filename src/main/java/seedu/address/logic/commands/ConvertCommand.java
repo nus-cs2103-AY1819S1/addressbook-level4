68018aee536fb5cadd962439e87e05a8a40e32ae
lancelotwillow <lancelotwillow@gmail.com>
2018-11-08 01:50:11 +0800

    remove unused import

--- src/main/java/seedu/address/logic/commands/ConvertCommand.java
+++ src/main/java/seedu/address/logic/commands/ConvertCommand.java
@@ -10,7 +10,6 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.ImageMagickUtil;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.transformation.Transformation;
 
