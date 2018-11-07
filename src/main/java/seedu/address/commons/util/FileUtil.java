fa74c2fa9ebd9e4328fcda66a214b1250e2e6178
KongZijin <e0253754@u.nus.edu>
2018-11-07 21:32:29 +0800

    move redundant code

--- src/main/java/seedu/address/commons/util/FileUtil.java
+++ src/main/java/seedu/address/commons/util/FileUtil.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
 
 /**
  * Writes and reads files
