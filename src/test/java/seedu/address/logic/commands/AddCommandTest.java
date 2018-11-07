b52f364460dc1ba7065e2bc701c72a7dcffd452e
kaitingpeck <kaiting_95@hotmail.com>
2018-11-07 23:45:18 +0800

    add interface methods into stubs

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -162,6 +162,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void updateEvent(Event target, Event editedEvent) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
--- src/test/java/seedu/address/logic/commands/ImportContactsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportContactsCommandTest.java
@@ -114,6 +114,11 @@ public class ImportContactsCommandTest {
         }
 
         @Override
+        public void updateEvent(Event target, Event editedEvent) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
