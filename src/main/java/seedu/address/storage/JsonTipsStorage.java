af71825077115ca741f611ff83fc4873f95a5259
Winson Heng Yong Seng <winsonheng@hotmail.com>
2018-11-08 12:05:04 +0800

    Adding an expense being overbudget now shows a notification instead of a warning (#176)
    * Changed to warning notification
    
    * Merged main
    
    * Jaime's code
    
    * Added setCategoryBudget on existing expense test
    
    * Fixed XMlAdaptation of CategoryBudget
    
    * Checkstyle
    
    * Moved +
    
    * Removed all debug statements

--- src/main/java/seedu/address/storage/JsonTipsStorage.java
+++ src/main/java/seedu/address/storage/JsonTipsStorage.java
@@ -2,6 +2,7 @@ package seedu.address.storage;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -18,8 +19,15 @@ import seedu.address.model.notification.Tip;
 public class JsonTipsStorage implements TipsStorage {
 
     private static InputStream fileStream = JsonTipsStorage.class.getResourceAsStream("/json/tips.json");
-    private static Path filePath = Paths.get(JsonTipsStorage.class.getResource("/json/tips.json").getPath());
+    private static Path filePath;
 
+    static {
+        try {
+            filePath = Paths.get(JsonTipsStorage.class.getResource("/json/tips.json").toURI());
+        } catch (URISyntaxException e) {
+            System.err.println(e.getMessage());
+        }
+    }
     @Override
     public Path getTipsFilePath() {
         return filePath;
--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -18,6 +18,7 @@ import seedu.address.model.ExpenseTracker;
 import seedu.address.model.ReadOnlyExpenseTracker;
 import seedu.address.model.budget.TotalBudget;
 import seedu.address.model.expense.Expense;
+import seedu.address.model.notification.Notification;
 import seedu.address.model.user.Username;
 import seedu.address.testutil.ExpenseBuilder;
 import seedu.address.testutil.ModelStub;
@@ -55,7 +56,7 @@ public class AddCommandTest {
         Expense validExpense = new ExpenseBuilder().build();
         CommandResult commandResult = new AddCommand(validExpense).execute(modelStub, commandHistory);
 
-        assertEquals(AddCommand.MESSAGE_BUDGET_EXCEED_WARNING, commandResult.feedbackToUser);
+        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, validExpense), commandResult.feedbackToUser);
         assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
         assertTrue(modelStub.addWarningNotification());
     }
@@ -188,6 +189,10 @@ public class AddCommandTest {
         @Override
         public void commitExpenseTracker() {
             // called by {@code AddCommand#execute()}
+        }
+        @Override
+        public void addGeneralNotification(Notification notif) {
+            // called by (@code AddCommand#execute()}
         }
         @Override
         public boolean addExpense(Expense expense) {
