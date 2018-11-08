467c9a57c0f63d97041478a4c04026f1e13d3a4f
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-08 21:31:04 +0800

    MedicalProblemTest.java: Add test cases for isValidMedProb
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/model/patient/MedicalProblemTest.java
+++ src/test/java/seedu/clinicio/model/patient/MedicalProblemTest.java
@@ -1,5 +1,8 @@
 package seedu.clinicio.model.patient;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import seedu.clinicio.testutil.Assert;
@@ -13,7 +16,7 @@ public class MedicalProblemTest {
 
     @Test
     public void constructor_invalidMedicalProblem_throwsIllegalArgumentException() {
-        String invalidMedicalProblem = "";
+        String invalidMedicalProblem = "@";
         Assert.assertThrows(IllegalArgumentException.class, () -> new MedicalProblem(invalidMedicalProblem));
     }
 
@@ -23,8 +26,11 @@ public class MedicalProblemTest {
         Assert.assertThrows(NullPointerException.class, () -> MedicalProblem.isValidMedProb(null));
 
         //invalid medical problem
-
+        assertFalse(MedicalProblem.isValidMedProb("@")); // Only non-alphanumeric characters
+        
         //valid medical problem
+        assertTrue(MedicalProblem.isValidMedProb("Asthma")); // 1 Words
+        assertTrue(MedicalProblem.isValidMedProb("High Blood Pressure")); // Multiple words
 
     }
 }
