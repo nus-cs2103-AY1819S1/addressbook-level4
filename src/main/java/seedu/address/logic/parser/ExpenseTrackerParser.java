e5be16143786c4fe4aa0cd616cda2e52bea33e0a
Jason Chong <jasontjakra@gmail.com>
2018-11-08 10:02:28 +0800

    Add userguide section for encrypt/decrypt and change app icon/title

--- src/main/java/seedu/address/logic/parser/ExpenseTrackerParser.java
+++ src/main/java/seedu/address/logic/parser/ExpenseTrackerParser.java
@@ -57,6 +57,7 @@ public class ExpenseTrackerParser {
 
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
+        System.out.println(arguments);
         switch (commandWord) {
 
         case AddCommand.COMMAND_ALIAS:
