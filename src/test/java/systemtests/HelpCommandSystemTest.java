02cae5cb42f90cca2f07207baea3b0e037b0ff70
LZYAndy <>
2018-11-08 14:52:37 +0800

    further change

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -1,9 +1,7 @@
 package systemtests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.ui.testutil.GuiTestAssert.assertListMatching;
 
