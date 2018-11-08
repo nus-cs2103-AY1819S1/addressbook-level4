a43659050c7f7cb1eb0b561b471b3cd12e41006c
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-08 17:49:44 +0800

    NricTest.java: Edit imports.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/model/patient/NricTest.java
+++ src/test/java/seedu/clinicio/model/patient/NricTest.java
@@ -1,8 +1,7 @@
 package seedu.clinicio.model.patient;
 
-
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
