07315e80db0d269dbfc09547c9d030118d931313
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-08 20:25:34 +0800

    LogoutCommandTest.java: Remove test cases
    Remove execute logout not success.
    
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/logic/commands/LogoutCommandTest.java
+++ src/test/java/seedu/clinicio/logic/commands/LogoutCommandTest.java
@@ -3,7 +3,6 @@ package seedu.clinicio.logic.commands;
 import static org.junit.Assert.assertTrue;
 
 import static seedu.clinicio.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.clinicio.logic.commands.LogoutCommand.MESSAGE_NO_USER_LOGIN;
 import static seedu.clinicio.logic.commands.LogoutCommand.MESSAGE_SUCCESS;
 import static seedu.clinicio.testutil.TypicalPersons.ADAM;
 
@@ -15,7 +14,6 @@ import org.junit.rules.ExpectedException;
 import seedu.clinicio.commons.core.UserSession;
 import seedu.clinicio.commons.events.ui.LogoutClinicIoEvent;
 import seedu.clinicio.logic.CommandHistory;
-import seedu.clinicio.logic.commands.exceptions.CommandException;
 import seedu.clinicio.model.Model;
 import seedu.clinicio.model.ModelManager;
 import seedu.clinicio.model.analytics.Analytics;
@@ -40,12 +38,5 @@ public class LogoutCommandTest {
         assertCommandSuccess(new LogoutCommand(), model, commandHistory, MESSAGE_SUCCESS, expectedModel, analytics);
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof LogoutClinicIoEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
-    }
-
-    @Test
-    public void execute_logout_failure() throws CommandException {
-        thrown.expect(CommandException.class);
-        thrown.expectMessage(MESSAGE_NO_USER_LOGIN);
-        new LogoutCommand().execute(model, commandHistory);
     }
 }
