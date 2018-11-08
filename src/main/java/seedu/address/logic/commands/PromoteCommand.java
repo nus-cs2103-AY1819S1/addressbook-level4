4a45f4e38f187a7f0d6dec608e2d88f59ae02ac9
chenlu-chua <chenlu.ccl@gmail.com>
2018-11-08 14:46:32 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/commands/PromoteCommand.java
+++ src/main/java/seedu/address/logic/commands/PromoteCommand.java
@@ -10,7 +10,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Education;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Time;
 import seedu.address.model.tag.Tag;
 
 /**
