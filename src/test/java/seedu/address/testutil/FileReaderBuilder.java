2e6a02cac03a0c9e2ec0f9625fa99da281a0e27e
cjiefeng <cjiefeng@live.com>
2018-11-09 15:53:57 +0800

    Update test case for import contacts parser

--- src/test/java/seedu/address/testutil/FileReaderBuilder.java
+++ src/test/java/seedu/address/testutil/FileReaderBuilder.java
@@ -20,6 +20,11 @@ public class FileReaderBuilder {
             .resolve("EmptyImportContacts.csv")
             .toFile()
             .getAbsolutePath();
+    public static final String HEADER_ONLY_CSV_FILE_PATH = Paths
+            .get("src", "test", "data", "ImportContactsTest")
+            .resolve("HeaderOnlyContacts.csv")
+            .toFile()
+            .getAbsolutePath();
     public static final String INVALID_FILE = Paths
             .get("src", "test", "data", "ImportContactsTest")
             .resolve("InvalidImportContacts.csv")
@@ -40,10 +45,19 @@ public class FileReaderBuilder {
     }
 
     /**
-     * Sets the {@code FileReader} of the {@code FileReader} that we are building.
+     * Sets the {@code FileReader} of the {@code FileReader} that we are building to empty file.
      */
     public FileReaderBuilder empty() {
         FilePath filePath = new FilePath(EMPTY_CSV_FILE_PATH);
+        this.fileReader = new FileReader(filePath);
+        return this;
+    }
+
+    /**
+     * Sets the {@code FileReader} of the {@code FileReader} that we are building to header only file.
+     */
+    public FileReaderBuilder headerOnly() {
+        FilePath filePath = new FilePath(HEADER_ONLY_CSV_FILE_PATH);
         this.fileReader = new FileReader(filePath);
         return this;
     }
