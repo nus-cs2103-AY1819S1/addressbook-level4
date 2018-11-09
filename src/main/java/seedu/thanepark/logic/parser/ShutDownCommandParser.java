d951590ed56cc02559194183b9b4c7c2d4331493
jiajunteo <e0175930@u.nus.edu>
2018-11-09 15:01:28 +0800

    Fix checkstyle violations

--- src/main/java/seedu/thanepark/logic/parser/ShutDownCommandParser.java
+++ src/main/java/seedu/thanepark/logic/parser/ShutDownCommandParser.java
@@ -13,10 +13,10 @@ import seedu.thanepark.logic.parser.exceptions.ParseException;
 
 public class ShutDownCommandParser implements Parser<ShutDownCommand> {
      /**
-     * Parses the given {@code String} of arguments in the context of the ShutDownCommand
-     * and returns an ShutDownCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
-     */
+      * Parses the given {@code String} of arguments in the context of the ShutDownCommand
+      * and returns an ShutDownCommand object for execution.
+      * @throws ParseException if the user input does not conform the expected format
+      */
     @Override
     public ShutDownCommand parse(String args) throws ParseException {
         requireNonNull(args);
