f2bd5335681728d28f55693b5a858c1bf15529b6
Daniel Koh <danielkoh554@hotmail.com>
2018-11-09 15:57:06 +0800

    Updated developer guide

--- src/main/java/seedu/learnvocabulary/logic/commands/TriviaCommand.java
+++ src/main/java/seedu/learnvocabulary/logic/commands/TriviaCommand.java
@@ -35,7 +35,7 @@ public class TriviaCommand extends Command {
         }
 
         model.toggleTriviaMode();
-        model.setTriviaList();O
+        model.setTriviaList();
         Word triviaQ = model.getTrivia();
         Meaning qMeaning = triviaQ.getMeaning();
 
