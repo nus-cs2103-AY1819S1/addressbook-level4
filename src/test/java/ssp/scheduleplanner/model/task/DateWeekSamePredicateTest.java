4cd626b2761dca20035964a24e6f56237abd8db9
CrimsonAng <e0176842@u.nus.edu>
2018-11-10 05:01:48 +0800

    add: test for DateWeekSamePredicateTest

--- src/test/java/ssp/scheduleplanner/model/task/DateWeekSamePredicateTest.java
+++ src/test/java/ssp/scheduleplanner/model/task/DateWeekSamePredicateTest.java
@@ -1,6 +1,7 @@
 package ssp.scheduleplanner.model.task;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.text.SimpleDateFormat;
@@ -13,14 +14,30 @@ import org.junit.Test;
 public class DateWeekSamePredicateTest {
 
     @Test
-    public void dateWeekSamePredicate_equal_success() {
+    public void dateWeekSamePredicate_equal() {
         List<String> dateList = new ArrayList<String>();
         List<String> dateListEqual = dateList;
-        Calendar c = Calendar.getInstance();
-        dateList.add(new SimpleDateFormat("ddMMyy").format(c.getTime()));
+        List<String> dateListNotEqual = new ArrayList<>();
+        dateList.add("130818");
+        dateListNotEqual.add("130818");
+        dateListNotEqual.add("140818");
 
+        DateWeekSamePredicate dateWeekSamePredicate1 = new DateWeekSamePredicate(dateList);
+        DateWeekSamePredicate dateWeekSamePredicate2 = new DateWeekSamePredicate(dateListNotEqual);
+
+        //equal values -> return true
         assertEquals(dateList, dateListEqual);
         assertTrue(dateList.equals(dateListEqual));
         assertTrue(dateListEqual.equals(dateList));
+
+        //same object -> returns true
+        assertTrue(dateWeekSamePredicate1.equals(dateWeekSamePredicate1));
+
+        //different value -> returns false
+        assertFalse(dateWeekSamePredicate1.equals(null));
+        assertFalse(dateWeekSamePredicate1.equals(dateWeekSamePredicate2));
+        assertFalse(dateWeekSamePredicate2.equals(dateWeekSamePredicate1));
+
+
     }
 }
