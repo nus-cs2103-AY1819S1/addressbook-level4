b42efc3932e53136e7094798c9a820ca093f5288
jeremiah-ang <jeremiahang94@gmail.com>
2018-11-09 21:54:19 +0800

    Add more tests to transcript parser

--- src/test/java/seedu/address/logic/parser/TranscriptParserTest.java
+++ src/test/java/seedu/address/logic/parser/TranscriptParserTest.java
@@ -2,21 +2,34 @@ package seedu.address.logic.parser;
 
 import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.TypicalModules.ASKING_QUESTIONS;
 import static seedu.address.testutil.TypicalModules.DATA_STRUCTURES;
 import static seedu.address.testutil.TypicalModules.DISCRETE_MATH;
 
 import java.util.EnumMap;
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import org.junit.rules.ExpectedException;
 import seedu.address.logic.commands.AddModuleCommand;
+import seedu.address.logic.commands.AdjustCommand;
 import seedu.address.logic.commands.DeleteModuleCommand;
 import seedu.address.logic.commands.EditModuleCommand;
+import seedu.address.logic.commands.GoalCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.RedoModuleCommand;
+import seedu.address.logic.commands.UndoModuleCommand;
 import seedu.address.logic.parser.arguments.EditArgument;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.module.Module;
 import seedu.address.testutil.Assert;
 import seedu.address.testutil.ModuleUtil;
+import seedu.address.testutil.TypicalModules;
 
 //@@author alexkmj
 /**
@@ -33,6 +46,10 @@ import seedu.address.testutil.ModuleUtil;
  * </ul>
  */
 public class TranscriptParserTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     /**
      * {@code TranscriptParser} used for all tests in {@TranscriptParserTest}.
      */
@@ -112,5 +129,68 @@ public class TranscriptParserTest {
         EditModuleCommand command = (EditModuleCommand) parser
                 .parseCommand(editModuleString);
         assertEquals(editModuleCommand, command);
+    }
+
+    //@@author jeremiah-ang
+    @Test
+    public void parseCommandAdjustModuleSuccess() throws ParseException {
+        Module target = TypicalModules.DATA_STRUCTURES;
+        AdjustCommand adjustCommand = new AdjustCommand(
+                target.getCode(), target.getYear(), target.getSemester(), target.getGrade());
+        AdjustCommand command = (AdjustCommand) parser.parseCommand(
+                ModuleUtil.getAdjustModuleCommand(
+                        target.getCode(), target.getYear(), target.getSemester(), target.getGrade()));
+        assertTrue(command instanceof  AdjustCommand);
+        assertEquals(adjustCommand, command);
+    }
+
+    @Test
+    public void parseCommandGoalModuleSuccess() throws ParseException {
+        assertTrue(parser.parseCommand(GoalCommand.COMMAND_WORD + " 3") instanceof GoalCommand);
+    }
+
+    @Test
+    public void parseCommand_help() throws Exception {
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+    }
+
+    @Test
+    public void parseCommand_history() throws Exception {
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
+        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
+
+        try {
+            parser.parseCommand("histories");
+            throw new AssertionError("The expected ParseException was not thrown.");
+        } catch (ParseException pe) {
+            assertEquals(MESSAGE_UNKNOWN_COMMAND, pe.getMessage());
+        }
+    }
+
+    @Test
+    public void parseCommand_redoCommandWord_returnsRedoCommand() throws Exception {
+        assertTrue(parser.parseCommand(RedoModuleCommand.COMMAND_WORD) instanceof RedoModuleCommand);
+        assertTrue(parser.parseCommand("redo 1") instanceof RedoModuleCommand);
+    }
+
+    @Test
+    public void parseCommand_undoCommandWord_returnsUndoCommand() throws Exception {
+        assertTrue(parser.parseCommand(UndoModuleCommand.COMMAND_WORD) instanceof UndoModuleCommand);
+        assertTrue(parser.parseCommand("undo 3") instanceof UndoModuleCommand);
+    }
+
+    @Test
+    public void parseCommand_unrecognisedInput_throwsParseException() throws Exception {
+        thrown.expect(ParseException.class);
+        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        parser.parseCommand("");
+    }
+
+    @Test
+    public void parseCommand_unknownCommand_throwsParseException() throws Exception {
+        thrown.expect(ParseException.class);
+        thrown.expectMessage(MESSAGE_UNKNOWN_COMMAND);
+        parser.parseCommand("unknownCommand");
     }
 }
