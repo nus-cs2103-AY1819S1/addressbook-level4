8c8f2ba89cca7e7b0a7198e4d26d370c0a27311b
CrimsonAng <e0176842@u.nus.edu>
2018-11-10 05:08:44 +0800

    fix: checkstyle

--- src/test/java/ssp/scheduleplanner/model/task/DateWeekSamePredicateTest.java
+++ src/test/java/ssp/scheduleplanner/model/task/DateWeekSamePredicateTest.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Test;
