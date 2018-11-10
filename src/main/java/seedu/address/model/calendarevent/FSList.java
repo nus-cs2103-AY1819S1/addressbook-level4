e584a505ad170e54034c99c243268dfcd8a4f071
pangjiahao <dontaskmestuff@gmail.com>
2018-11-10 15:46:17 +0800

    add FSList.java. it is a class that allows filtering by multiple predicates and sorting.

--- src/main/java/seedu/address/model/calendarevent/FSList.java
+++ src/main/java/seedu/address/model/calendarevent/FSList.java
@@ -9,9 +9,10 @@ import java.util.Comparator;
 import java.util.function.Predicate;
 
 /**
- * A List that combines SortedList and FilteredList
+ * Filtered-and-Sorted List. A List of {@code CalendarEvents} that combines SortedList and FilteredList.
+ * Also allows for multiple predicates to be used for filtering.
  *
- * FilteredList wrapped with SortedList
+ * Implemented using a SortedList wrapped wit a FilteredList.
  */
 public class FSList {
 
@@ -20,6 +21,12 @@ public class FSList {
     private SortedList<CalendarEvent> sortedList;
     private FilteredList<CalendarEvent> filteredList;
 
+    /**
+     * Creates a new FSList wrapped around the backingList.
+     * The FSList has no predicates and no comparators.
+     *
+     * @param backingList list of CalendarEvents to be filtered and sorted.
+     */
     public FSList(ObservableList<CalendarEvent> backingList) {
         predicates = new ArrayList<>();
         calendarEvents = backingList;
@@ -27,13 +34,34 @@ public class FSList {
         filteredList = new FilteredList<>(sortedList);
     }
 
+    /**
+     * @return the filtered-and-sorted list.
+     */
     public FilteredList<CalendarEvent> getFSList() {
         return filteredList;
     }
 
     /**
-     * Set a single predicate for the filtered list
-     * @param predicate predicate for filtering
+     * Returns a new FSList wrapping backingList.
+     * The new FSList uses the same predicates and comparator as the copied FSList.
+     *
+     * @param backingList the list to be filted-and-sorted.
+     */
+    public FSList copy(ObservableList<CalendarEvent> backingList) {
+        FSList copy = new FSList(backingList);
+
+        // copy predicates and comparator
+        copy.predicates.addAll(this.predicates);
+        copy.setComparator(getComparator());
+        copy.combineAndApplyPredicates();
+        return copy;
+    }
+
+    /**
+     * Sets the predicate for filtering.
+     * Clears all existing predicates from FSList.
+     *
+     * @param predicate for filtering FSList.
      */
     public void setPredicate(Predicate<CalendarEvent> predicate) {
         clearPredicates();
@@ -41,7 +69,17 @@ public class FSList {
     }
 
     /**
-     * Clears all predicates so no filtering is done
+     * Add a predicate to the filtered list, on top of existing predicates.
+     *
+     * @param predicate additional predicate to filter FSList.
+     */
+    public void addPredicate(Predicate<CalendarEvent> predicate) {
+        predicates.add(predicate);
+        combineAndApplyPredicates();
+    }
+
+    /**
+     * Clears all predicates from FSList.
      */
     public void clearPredicates() {
         predicates.clear();
@@ -49,23 +87,29 @@ public class FSList {
     }
 
     /**
-     * Add another predicate to the filtered list
-     * The added predicate will be applied to the list immediately
-     * @param predicate additional predicate to filter
+     * Combines all the predicates in {@code predicates} into a new Predicate that is a logical AND of all predicates.
+     *
+     * @return the combined predicate.
      */
-    public void addPredicate(Predicate<CalendarEvent> predicate) {
-        predicates.add(predicate);
-        filteredList.setPredicate(getCombinedPredicate());
-    }
-
-    /**
-     * Combines all the predicates in {@code predicates} into a new Predicate that is a logical AND of all predicates
-     * @return the combined predicate
-     */
-    public Predicate<CalendarEvent> getCombinedPredicate() {
+    private Predicate<CalendarEvent> getCombinedPredicate() {
         return predicates.stream().reduce(Predicate::and).orElse(null);
     }
 
+    /**
+     * Convenience method
+     */
+    private void combineAndApplyPredicates() {
+        filteredList.setPredicate(getCombinedPredicate());
+    }
+
+    // TODO: fix this warning
+    private Comparator<CalendarEvent> getComparator() {
+        return (Comparator<CalendarEvent>) sortedList.getComparator();
+    }
+
+    /**
+     * Set the comparator for sorting the FSList.
+     */
     public void setComparator(Comparator<CalendarEvent> comparator) {
         sortedList.setComparator(comparator);
     }
