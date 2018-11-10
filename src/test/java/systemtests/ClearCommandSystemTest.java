a99e7882860c9c670bdc710257c7b8f0a876b04f
kengwoon <shannalimkengwoon@gmail.com>
2018-11-11 02:11:59 +0800

    Checkstyle changes

--- src/test/java/systemtests/ClearCommandSystemTest.java
+++ src/test/java/systemtests/ClearCommandSystemTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.UndoCommand;
