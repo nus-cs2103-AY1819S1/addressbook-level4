6f85e4407c5b533b14127f35a2c9f2da05f7287b
afiqlattif <muhdafiqlattif@gmail.com>
2018-11-10 23:33:32 +0800

    Updated Volunteer Class with new VolunteerId format

--- src/main/java/seedu/address/model/volunteer/VolunteerId.java
+++ src/main/java/seedu/address/model/volunteer/VolunteerId.java
@@ -1,45 +1,48 @@
 package seedu.address.model.volunteer;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.util.Objects;
 
 /**
  * Represents an Volunteer's Id in the application. Volunteer Id is used in identifying record entries.
- * Guarantees: immutable; is valid as declared in {@link #isValidId(int)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidId(String)}
  */
 public class VolunteerId {
-    public static final String MESSAGE_NAME_CONSTRAINTS =
-            "Id should be more than zero, and it should not be blank";
+    public static final String MESSAGE_ID_CONSTRAINTS =
+            "Id should only start with letters S, T, F or G, followed by 7 digits, ending with an alphabet, and it should not be blank";
+    public static final String ID_VALIDATION_REGEX = "(?i)^[STFG]\\d{7}[A-Z]$(?-i)";
 
-    public final int id;
+    public final String id;
     /**
      * Constructs an {@code id}.
      *
      * @param id A valid id.
      */
-    public VolunteerId(int id) {
+    public VolunteerId(String id) {
         requireNonNull(id);
-        this.id = id;
+        checkArgument(isValidId(id), MESSAGE_ID_CONSTRAINTS);
+        this.id = id.toUpperCase();
     }
 
     /**
-     * Returns true if a given string is a valid name.
+     * Returns true if a given string is a valid Id.
      */
-    public static boolean isValidId(int test) {
-        return test > 0;
+    public static boolean isValidId(String test) {
+        return test.matches(ID_VALIDATION_REGEX);
     }
 
     @Override
     public String toString() {
-        return Integer.toString(id);
+        return id;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof VolunteerId // instanceof handles nulls
-                && id == ((VolunteerId) other).id); // state check
+                && id.equals(((VolunteerId) other).id)); // state check
     }
 
     @Override
