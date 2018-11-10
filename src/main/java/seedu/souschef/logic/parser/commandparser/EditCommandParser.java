9b6b9d2d6affd5a5ff36c652602e0b3edb298492
junrong09 <jun_rong5@hotmail.com>
2018-11-10 21:43:14 +0800

    EditCommandParser.java: Enforce Model generic type <Recipe> for parseRecipe().

--- src/main/java/seedu/souschef/logic/parser/commandparser/EditCommandParser.java
+++ src/main/java/seedu/souschef/logic/parser/commandparser/EditCommandParser.java
@@ -71,7 +71,7 @@ public class EditCommandParser implements CommandParser<EditCommand> {
      * and returns an EditCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public EditCommand<Recipe> parseRecipe(Model model, String args) throws ParseException {
+    public EditCommand<Recipe> parseRecipe(Model<Recipe> model, String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_DIFFICULTY, PREFIX_COOKTIME,
