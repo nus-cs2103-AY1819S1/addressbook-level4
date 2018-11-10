c5f743442164a308ff6cbe4553f16d620465a271
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-11 00:24:33 +0800

    Remove system test java files.
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/main/java/seedu/clinicio/logic/commands/ListCommand.java
+++ src/main/java/seedu/clinicio/logic/commands/ListCommand.java
@@ -1,25 +1,25 @@
 package seedu.clinicio.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.clinicio.model.Model.PREDICATE_SHOW_ALL_PATIENTS;
+import static seedu.clinicio.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.clinicio.logic.CommandHistory;
 import seedu.clinicio.model.Model;
 
 /**
- * Lists all patients in the ClinicIO to the user.
+ * Lists all persons in the ClinicIO to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all patients";
+    public static final String MESSAGE_SUCCESS = "Listed all persons";
 
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredPatientList(PREDICATE_SHOW_ALL_PATIENTS);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
