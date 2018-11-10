3282edcfb90c949b9228b2006a7b811c3edcdb08
He Xinyi <hexinyi51@gmail.com>
2018-11-10 22:11:44 +0800

    Change delay time

--- src/main/java/seedu/scheduler/ui/BrowserPanel.java
+++ src/main/java/seedu/scheduler/ui/BrowserPanel.java
@@ -51,7 +51,7 @@ public class BrowserPanel extends UiPart<Region> {
                         runnable.run();
                     }
                 },
-                9999000
+                999999000
         );
     }
 
