955239dd879ba3f96929cd5607bc931bd6644314
YezhongZ <39483787+YezhongZ@users.noreply.github.com>
2018-11-10 19:04:59 +0800

    Add test case, supply credential with write permission

--- src/test/java/seedu/scheduler/commons/web/ConnectToGoogleCalendarTest.java
+++ src/test/java/seedu/scheduler/commons/web/ConnectToGoogleCalendarTest.java
@@ -3,18 +3,37 @@ package seedu.scheduler.commons.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.scheduler.testutil.TypicalEvents.getTypicalScheduler;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import com.google.api.services.calendar.Calendar;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.testing.auth.oauth2.MockTokenServerTransport;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.http.LowLevelHttpResponse;
+import com.google.api.client.json.Json;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.testing.http.HttpTesting;
+import com.google.api.client.testing.http.MockHttpTransport;
+import com.google.api.client.testing.http.MockLowLevelHttpRequest;
+import com.google.api.client.testing.http.MockLowLevelHttpResponse;
+import com.google.api.services.calendar.model.Calendar;
 
 import seedu.scheduler.logic.CommandHistory;
 import seedu.scheduler.logic.commands.ClearCommand;
@@ -22,9 +41,14 @@ import seedu.scheduler.model.Model;
 import seedu.scheduler.model.ModelManager;
 import seedu.scheduler.model.UserPrefs;
 
+/**
+ * Contains tests for ConnectToGoogleCalendar.
+ */
 class ConnectToGoogleCalendarTest {
     private static final String CALENDAR_NAME = "primary";
+    private static final String SAMPLE = "123\u05D9\u05e0\u05D9\u05D1";
     private static final String TEMP_EVENT_NAME = "tempEventName";
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
     private CommandHistory commandHistory = new CommandHistory();
     private Model model = new ModelManager(getTypicalScheduler(), new UserPrefs());
 
@@ -55,21 +79,27 @@ class ConnectToGoogleCalendarTest {
     }
 
     @Test
-    void isGoogleCalendarEnabled() {
+    public void isGoogleCalendarEnabled() {
+        //Status is "Enabled" -> ok
         enable();
         assertTrue(ConnectToGoogleCalendar.isGoogleCalendarEnabled());
+        //Status is not "Enabled" -> fail
         disable();
         assertFalse(ConnectToGoogleCalendar.isGoogleCalendarEnabled());
     }
 
     @Test
-    void isGoogleCalendarDisabled() {
+    public void isGoogleCalendarDisabled() {
+        //Status is not "Disabled" -> fail
+        enable();
+        assertFalse(ConnectToGoogleCalendar.isGoogleCalendarDisabled());
+        //Status is "Disabled" -> ok
         disable();
         assertTrue(ConnectToGoogleCalendar.isGoogleCalendarDisabled());
     }
 
     @Test
-    void checkStatus() {
+    public void checkStatus() {
         //Prepare the test file where the status indicates Enabled
         enable();
         //Test whether returns Enabled
@@ -89,7 +119,8 @@ class ConnectToGoogleCalendarTest {
         enable();
         final ConnectToGoogleCalendar connectToGoogleCalendar =
                 new ConnectToGoogleCalendar();
-        Calendar service = connectToGoogleCalendar.getCalendar();
+        com.google.api.services.calendar.Calendar service =
+                connectToGoogleCalendar.getCalendar();
 
         //create an event in Google Calender
         try {
@@ -123,5 +154,129 @@ class ConnectToGoogleCalendarTest {
         assertEquals(0, size);
         //clean up
         disable();
+    }
+
+    @Test
+    public void setGoogleCalendarEnabled() {
+        //execute the command
+        ConnectToGoogleCalendar.setGoogleCalendarEnabled();
+        //get the result from the status file
+        String contents = getModeContent();
+        //check
+        assertNotNull(contents);
+        assertEquals("Enabled" + System.getProperty("line.separator"), contents);
+        //clean up test
+        disable();
+    }
+    @Test
+    public void setGoogleCalendarDisabled() {
+        //execute the command
+        ConnectToGoogleCalendar.setGoogleCalendarDisabled();
+        //get the result from the mode.txt
+        String contents = getModeContent();
+        //check
+        assertNotNull(contents);
+        assertEquals("Disabled" + System.getProperty("line.separator"), contents);
+    }
+    /**
+     * Retrieves an status for testing purpose.
+     *
+     * @return String in the status file
+     */
+    private String getModeContent() {
+        //Get the file
+        File file = new File("./tokens/mode.txt");
+        //Read the content
+        StringBuilder contents = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String text;
+            String lineSeparator = System.getProperty("line.separator");
+            while ((text = reader.readLine()) != null) {
+                contents.append(text).append(lineSeparator);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //convert to string and return
+        return contents.toString();
+    }
+    @Test
+    public void getCalendar() throws IOException {
+        //get a connection to Google
+        final ConnectToGoogleCalendar connectToGoogleCalendar =
+                new ConnectToGoogleCalendar();
+        //get a service
+        com.google.api.services.calendar.Calendar service =
+                connectToGoogleCalendar.getCalendar();
+        //create a calendar for testing
+        Calendar expectedCalendar = new Calendar();
+        expectedCalendar.setSummary("testCalendar");
+        Calendar createdCalendar = service.calendars().insert(expectedCalendar).execute();
+        //try to get the test calender from Google
+        Calendar getFromGoogleCalendar =
+                service.calendars().get(createdCalendar.getId()).execute();
+        //check whether this is the test calender
+        assertEquals(createdCalendar.getId(), getFromGoogleCalendar.getId());
+        //remove the test calender from Google Calender
+        service.calendars().delete(createdCalendar.getId()).execute();
+        //create an expected exception
+        //Try to retrieve the deleted exception
+        //Confirm the an exception will be thrown
+        assertThrows(
+                com.google.api.client.googleapis.json.GoogleJsonResponseException.class, (
+                ) -> service.calendars().get(createdCalendar.getId()).execute());
+    }
+    /**
+     * Tests {@link GoogleCredential}.
+     *
+     * @author Yaniv Inbar
+     */
+    @Test
+    public void getNetHttpTransport() throws Exception {
+        //Test Download
+        HttpTransport transport = new MockHttpTransport() {
+            @Override
+            public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
+                return new MockLowLevelHttpRequest() {
+                    @Override
+                    public LowLevelHttpResponse execute() throws IOException {
+                        MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+                        result.setContentType(Json.MEDIA_TYPE);
+                        result.setContent(SAMPLE);
+                        return result;
+                    }
+                };
+            }
+        };
+        HttpRequest request =
+                transport.createRequestFactory().buildGetRequest(HttpTesting.SIMPLE_GENERIC_URL);
+        HttpResponse response = request.execute();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        response.download(outputStream);
+        assertEquals(SAMPLE, outputStream.toString("UTF-8"));
+    }
+    /**
+     * Tests {@link GoogleCredential}.
+     *
+     * @author Yaniv Inbar
+     */
+    @Test
+    public void getCredentials() throws IOException {
+        //testRefreshToken_User()
+        final String accessToken = "1/MkSJoj1xsli0AccessToken_NKPY2";
+        final String clientSecret = "jakuaL9YyieakhECKL2SwZcu";
+        final String clientId = "ya29.1.AADtN_UtlxN3PuGAxrN2XQnZTVRvDyVWnYq4I6dws";
+        final String refreshToken = "1/Tl6awhpFjkMkSJoj1xsli0H2eL5YsMgU_NKPY2TyGWY";
+        MockTokenServerTransport transport = new MockTokenServerTransport();
+        transport.addClient(clientId, clientSecret);
+        transport.addRefreshToken(refreshToken, accessToken);
+        GoogleCredential credential = new GoogleCredential.Builder()
+                .setClientSecrets(clientId, clientSecret)
+                .setTransport(transport)
+                .setJsonFactory(JSON_FACTORY)
+                .build();
+        credential.setRefreshToken(refreshToken);
+        assertTrue(credential.refreshToken());
+        assertEquals(accessToken, credential.getAccessToken());
     }
 }
