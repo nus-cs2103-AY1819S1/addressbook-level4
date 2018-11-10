1dd987f52d6fd1b2bcc1a05f5235aa4e4d897bbf
Joseph Lee Zhuo Xun <josephlee050@gmail.com>
2018-11-10 22:22:25 +0800

    PasswordPrefixFormatterTest.java: Fix test cases
    Signed-off-by: jjlee050 <josephlee050@gmail.com>

--- src/test/java/seedu/clinicio/ui/util/PasswordPrefixFormatterTest.java
+++ src/test/java/seedu/clinicio/ui/util/PasswordPrefixFormatterTest.java
@@ -11,35 +11,16 @@ import static seedu.clinicio.testutil.TypicalPersons.ADAM;
 import org.junit.Before;
 import org.junit.Test;
 
-import guitests.guihandles.CommandBoxHandle;
-
-import javafx.scene.input.KeyCode;
-
-import seedu.clinicio.logic.Logic;
-import seedu.clinicio.logic.LogicManager;
 import seedu.clinicio.logic.commands.LoginCommand;
-import seedu.clinicio.model.Model;
-import seedu.clinicio.model.ModelManager;
-import seedu.clinicio.ui.CommandBox;
-import seedu.clinicio.ui.GuiUnitTest;
 
-public class PasswordPrefixFormatterTest extends GuiUnitTest {
+public class PasswordPrefixFormatterTest {
 
     private static final String WHITESPACE = " ";
 
     private PasswordPrefixFormatter formatter;
-    private CommandBoxHandle commandBoxHandle;
 
     @Before
     public void setUp() {
-        Model model = new ModelManager();
-        Logic logic = new LogicManager(model);
-
-        CommandBox commandBox = new CommandBox(logic);
-        commandBoxHandle = new CommandBoxHandle(getChildNode(commandBox.getRoot(),
-                CommandBoxHandle.COMMAND_INPUT_FIELD_ID));
-        uiPartRule.setUiPart(commandBox);
-
         formatter = new PasswordPrefixFormatter();
     }
 
@@ -53,33 +34,33 @@ public class PasswordPrefixFormatterTest extends GuiUnitTest {
                 + WHITESPACE + PREFIX_PASSWORD + "-------2";
 
         // IsHistory -> false, IsBackspace -> false
-        commandBoxHandle.run(text);
-        assertEquals(expectedText, formatter.maskPassword(commandBoxHandle.getInput(), false, false) + "2");
+        assertEquals(expectedText, formatter.maskPassword(text, false, false) + "2");
 
         // IsHistory -> false, IsBackspace -> true
-        guiRobot.type(KeyCode.BACK_SPACE);
+        text = LoginCommand.COMMAND_WORD + WHITESPACE + PREFIX_ROLE + "doctor"
+                + WHITESPACE + PREFIX_NAME + ADAM.getName().fullName
+                + WHITESPACE + PREFIX_PASSWORD + VALID_PASSWORD_ADAM;
         expectedText = LoginCommand.COMMAND_WORD + WHITESPACE + PREFIX_ROLE + "doctor"
                 + WHITESPACE + PREFIX_NAME + ADAM.getName().fullName
                 + WHITESPACE + PREFIX_PASSWORD + "------1";
-        assertEquals(expectedText, formatter.maskPassword(commandBoxHandle.getInput(), false, true));
+
+        assertEquals(expectedText, formatter.maskPassword(text, false, true));
 
         // IsHistory -> true, IsBackspace -> false
-        guiRobot.type(KeyCode.UP);
-        assertEquals(expectedText, formatter.maskPassword(commandBoxHandle.getInput(), true, false));
+        assertEquals(expectedText, formatter.maskPassword(text, true, false));
     }
 
     @Test
     public void unmaskPassword() {
         String text = LoginCommand.COMMAND_WORD + WHITESPACE + PREFIX_ROLE + "doctor"
                 + WHITESPACE + PREFIX_NAME + ADAM.getName().fullName
-                + WHITESPACE + PREFIX_PASSWORD + "doctor1";
+                + WHITESPACE + PREFIX_PASSWORD + "doctor";
         String expectedText = LoginCommand.COMMAND_WORD + WHITESPACE + PREFIX_ROLE + "doctor"
                 + WHITESPACE + PREFIX_NAME + ADAM.getName().fullName
                 + WHITESPACE + PREFIX_PASSWORD + VALID_PASSWORD_ADAM;
 
         // IsHistory -> false, IsBackspace -> false
-        commandBoxHandle.run(text);
-        String formattedText = formatter.maskPassword(commandBoxHandle.getInput(), false, false);
+        String formattedText = formatter.maskPassword(text, false, false) + "1";
         assertEquals(expectedText, formatter.unmaskPassword(formattedText));
     }
 }
