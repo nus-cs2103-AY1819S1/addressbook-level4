5babe7d74fa5d9ed53e0bdc454dd895e4876a119
alexkmj <alex_koh_@live.com>
2018-11-10 16:01:41 +0800

    Update javadocs in AddModuleCommand

--- src/main/java/seedu/address/logic/commands/AddModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/AddModuleCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Objects;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -11,8 +12,16 @@ import seedu.address.model.module.Module;
  * Adds a module to the transcript.
  */
 public class AddModuleCommand extends Command {
+    /**
+     * Command word for {@code AddModuleCommand}.
+     */
     public static final String COMMAND_WORD = "add";
 
+    /**
+     * Usage of <b>add</b>.
+     * <p>
+     * Provides the description and syntax of <b>add</b>.
+     */
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": "
             + "Adds a module.\n"
             + "Parameters: "
@@ -22,11 +31,20 @@ public class AddModuleCommand extends Command {
             + "-c CREDIT "
             + "[-g GRADE]";
 
-    // Constants for CommandException.
+    /**
+     * Message that informs that new module is added successfully.
+     */
     public static final String MESSAGE_ADD_SUCCESS = "New module add: %1$s";
+
+    /**
+     * Message that informs that new module already exist.
+     */
     public static final String MESSAGE_MODULE_ALREADY_EXIST = "New module"
             + " already exist.";
 
+    /**
+     * Module to be added.
+     */
     private final Module toAdd;
 
     /**
@@ -38,6 +56,17 @@ public class AddModuleCommand extends Command {
         toAdd = module;
     }
 
+    /**
+     * Adds a module into the module list of transcript.
+     * <p>
+     * Throws {@code CommandException} when module to be added already exist.
+     *
+     * @param model {@code Model} that the command operates on.
+     * @param history {@code CommandHistory} that the command operates on.
+     * @return result of the command
+     * @throws CommandException thrown when command cannot be executed
+     * successfully
+     */
     @Override
     public CommandResult execute(Model model, CommandHistory history)
             throws CommandException {
@@ -71,10 +100,26 @@ public class AddModuleCommand extends Command {
         }
     }
 
+    /**
+     * Returns true if all {@code toAdd} matches.
+     *
+     * @param other the other object compared against
+     * @return true if all field matches
+     */
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof AddModuleCommand // instanceof handles nulls
-                && toAdd.equals(((AddModuleCommand) other).toAdd));
+        // Short circuit if same object.
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls.
+        if (!(other instanceof AddModuleCommand)) {
+            return false;
+        }
+
+        // State check.
+        AddModuleCommand e = (AddModuleCommand) other;
+        return Objects.equals(toAdd, e.toAdd);
     }
 }
