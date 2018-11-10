ab656d7ec80585adc81fe403df91a94ce9bb009c
remo5000 <vigneshshankar@gmail.com>
2018-11-10 20:05:06 +0800

    Remove address field from TaskCard tests

--- src/test/java/guitests/guihandles/TaskCardHandle.java
+++ src/test/java/guitests/guihandles/TaskCardHandle.java
@@ -16,14 +16,12 @@ import seedu.address.model.task.Task;
 public class TaskCardHandle extends NodeHandle<Node> {
     private static final String ID_FIELD_ID = "#id";
     private static final String NAME_FIELD_ID = "#name";
-    private static final String ADDRESS_FIELD_ID = "#description";
     private static final String PHONE_FIELD_ID = "#dueDate";
     private static final String EMAIL_FIELD_ID = "#priorityValue";
     private static final String TAGS_FIELD_ID = "#tags";
 
     private final Label idLabel;
     private final Label nameLabel;
-    private final Label addressLabel;
     private final Label phoneLabel;
     private final Label emailLabel;
     private final List<Label> tagLabels;
@@ -33,7 +31,6 @@ public class TaskCardHandle extends NodeHandle<Node> {
 
         idLabel = getChildNode(ID_FIELD_ID);
         nameLabel = getChildNode(NAME_FIELD_ID);
-        addressLabel = getChildNode(ADDRESS_FIELD_ID);
         phoneLabel = getChildNode(PHONE_FIELD_ID);
         emailLabel = getChildNode(EMAIL_FIELD_ID);
 
@@ -51,10 +48,6 @@ public class TaskCardHandle extends NodeHandle<Node> {
 
     public String getName() {
         return nameLabel.getText();
-    }
-
-    public String getAddress() {
-        return addressLabel.getText();
     }
 
     public String getPhone() {
@@ -77,7 +70,6 @@ public class TaskCardHandle extends NodeHandle<Node> {
      */
     public boolean equals(Task task) {
         return getName().equals(task.getName().fullName)
-                && getAddress().equals(task.getDescription().value)
                 && getPhone().equals(task.getDueDate().value)
                 && getEmail().equals(task.getPriorityValue().value)
                 && ImmutableMultiset.copyOf(getTags()).equals(ImmutableMultiset.copyOf(task.getLabels().stream()
--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -20,7 +20,6 @@ public class GuiTestAssert {
      */
     public static void assertCardEquals(TaskCardHandle expectedCard, TaskCardHandle actualCard) {
         assertEquals(expectedCard.getId(), actualCard.getId());
-        assertEquals(expectedCard.getAddress(), actualCard.getAddress());
         assertEquals(expectedCard.getEmail(), actualCard.getEmail());
         assertEquals(expectedCard.getName(), actualCard.getName());
         assertEquals(expectedCard.getPhone(), actualCard.getPhone());
@@ -34,7 +33,6 @@ public class GuiTestAssert {
         assertEquals(expectedTask.getName().fullName, actualCard.getName());
         assertEquals(expectedTask.getDueDate().value, actualCard.getPhone());
         assertEquals(expectedTask.getPriorityValue().value, actualCard.getEmail());
-        assertEquals(expectedTask.getDescription().value, actualCard.getAddress());
         assertEquals(expectedTask.getLabels().stream().map(tag -> tag.labelName).collect(Collectors.toList()),
                 actualCard.getTags());
     }
