c52243a96475a666abba99cc79e325aebe3d24ed
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-11-11 16:28:17 +0800

    Remove extra blank lines

--- src/test/java/seedu/address/model/task/LabelMatchesAnyKeywordPredicateTest.java
+++ src/test/java/seedu/address/model/task/LabelMatchesAnyKeywordPredicateTest.java
@@ -102,6 +102,4 @@ public class LabelMatchesAnyKeywordPredicateTest {
         LabelMatchesAnyKeywordPredicate predicate = new LabelMatchesAnyKeywordPredicate(createLabelsFromKeywords(""));
     }
 
-
-
 }
