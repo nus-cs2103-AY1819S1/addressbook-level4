fe2a7780e7c8cc8486f161f7c3f73606a1d368da
He Xinyi <hexinyi51@gmail.com>
2018-11-11 23:07:58 +0800

    Delete unused function defined

--- src/main/java/seedu/scheduler/model/event/EventPopUpInfo.java
+++ src/main/java/seedu/scheduler/model/event/EventPopUpInfo.java
@@ -86,17 +86,6 @@ public class EventPopUpInfo implements Comparable<EventPopUpInfo> {
     }
 
     /**
-     * get Display for events that are past
-     * @return
-     */
-    public String getPastPopUpDisplay() {
-        String result = "";
-        result = result + "Event Name: " + eventName.toString() + "\n" + startDateTime.getPrettyString() + " - "
-                + endDateTime.getPrettyString();
-        return result;
-    }
-
-    /**
      * get Display for events
      * @return
      */
