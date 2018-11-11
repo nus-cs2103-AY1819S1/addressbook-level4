0e6ba4b2079b4446976f6ddf7b3707ad64a5df0e
pratyayj <pratyay.jaidev@gmail.com>
2018-11-11 21:44:21 +0800

    Add more tests

--- src/test/java/seedu/address/logic/EmbedGoogleMapsTest.java
+++ src/test/java/seedu/address/logic/EmbedGoogleMapsTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,9 +21,17 @@ class EmbedGoogleMapsTest {
         assertEquals("ChIJfyddcgMa2jER6C0WKzLdP6w", EmbedGoogleMaps.getPlaceId("FOL"));
         assertEquals("ChIJSejKW_Ya2jER9bHfrr9sA-c", EmbedGoogleMaps.getPlaceId("YSTCOM"));
         assertEquals("ChIJx_aDLFEa2jERvhbP1ACc2t8", EmbedGoogleMaps.getPlaceId("YLLSOM"));
+        assertEquals("ChIJW-fkx_ga2jERSjkkKeJjaUM", EmbedGoogleMaps.getPlaceId("SOC"));
+        assertEquals("ChIJSfKZ6VYa2jERnuNsDKZ8moU", EmbedGoogleMaps.getPlaceId("FOS"));
+        assertEquals("ChIJQ91DPvca2jERARhJO1i77Cg", EmbedGoogleMaps.getPlaceId("FOE"));
+        assertEquals("ChIJk315cv8a2jERyoLIHsKS40Y", EmbedGoogleMaps.getPlaceId("BIZ"));
+        assertEquals("ChIJadpVLvka2jERHq8cT_xdGVQ", EmbedGoogleMaps.getPlaceId("FASS"));
+        assertEquals("ChIJcUayaFEa2jERVJ-bhaQxjmk", EmbedGoogleMaps.getPlaceId("FOD"));
+
     }
 
     @Test
     void getMeetingPlaceId() {
+        assertNotEquals(null, EmbedGoogleMaps.getMeetingPlaceId());
     }
 }
