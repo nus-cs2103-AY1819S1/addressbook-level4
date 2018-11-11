70986357c301b356b42e738e8d2de0ae40ad0225
Woodnsuns <pollylin0617@gmail.com>
2018-11-11 16:25:51 +0800

    Updated developer guide for features by Woodnsuns

--- src/main/java/ssp/scheduleplanner/model/category/Category.java
+++ src/main/java/ssp/scheduleplanner/model/category/Category.java
@@ -39,14 +39,6 @@ public class Category {
         return test.matches(CATEGORY_VALIDATION_REGEX);
     }
 
-
-    /**
-     * Change name of this category.
-     * @param name
-     */
-    public void editName(String name) {
-        this.name = name;
-    }
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
