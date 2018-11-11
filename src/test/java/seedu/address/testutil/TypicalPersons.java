828266d433c49f98256d5e2028be0e87f0f64722
xueyantian <daisytianxueyan@gmail.com>
2018-11-11 11:01:34 +0800

    Delete AddModule system test. Fix Select optional person bug.

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -10,14 +10,12 @@ import static seedu.address.logic.commands.CommandPersonTestUtil.VALID_PHONE_AMY
 import static seedu.address.logic.commands.CommandPersonTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandPersonTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandPersonTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.testutil.TypicalModules.getOneTypicalModule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
-import seedu.address.model.module.Module;
 import seedu.address.model.person.Person;
 
 /**
@@ -57,10 +55,6 @@ public class TypicalPersons {
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
-
-    // Manually added - Person with Module list
-    public static final List<Module> modules = getOneTypicalModule();
-    public static final Person FIONA_WITH_MODULES = new PersonBuilder(FIONA).withModuleList(modules).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
