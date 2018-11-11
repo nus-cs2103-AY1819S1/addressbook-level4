28e6ce7c1c62f6f3794e551682b09390efe6d4c2
junrong09 <jun_rong5@hotmail.com>
2018-11-11 19:30:48 +0800

    Update recipe attributes test cases.

--- src/test/java/seedu/souschef/model/recipe/DifficultyTest.java
+++ src/test/java/seedu/souschef/model/recipe/DifficultyTest.java
@@ -28,14 +28,14 @@ public class DifficultyTest {
         // invalid difficulty
         assertFalse(Difficulty.isValidDifficulty("")); // empty string
         assertFalse(Difficulty.isValidDifficulty(" ")); // spaces only
-        assertFalse(Difficulty.isValidDifficulty("91")); // less than 3 numbers
-        assertFalse(Difficulty.isValidDifficulty("phone")); // non-numeric
-        assertFalse(Difficulty.isValidDifficulty("9011p041")); // alphabets within digits
-        assertFalse(Difficulty.isValidDifficulty("9312 1534")); // spaces within digits
+        assertFalse(Difficulty.isValidDifficulty("6")); // above 5
+        assertFalse(Difficulty.isValidDifficulty("0")); // below 1
 
-        // valid difficulty
-        assertTrue(Difficulty.isValidDifficulty("3")); // exactly 3 numbers
+        // valid difficulty (all combination)
         assertTrue(Difficulty.isValidDifficulty("1"));
-        assertTrue(Difficulty.isValidDifficulty("5")); // max
+        assertTrue(Difficulty.isValidDifficulty("2"));
+        assertTrue(Difficulty.isValidDifficulty("3"));
+        assertTrue(Difficulty.isValidDifficulty("4"));
+        assertTrue(Difficulty.isValidDifficulty("5"));
     }
 }
