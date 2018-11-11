711d16f7fc0b086432374f5fc7104898c8c65825
DESKTOP-NSV5A3C\Lee Yu Cong <yu_cong96@hotmail.com>
2018-11-12 02:48:34 +0800

    Add Checkstock Test

--- src/main/java/seedu/address/logic/commands/CheckStockCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckStockCommand.java
@@ -2,11 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Predicate;
+
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ShowMedicineListEvent;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.medicine.Medicine;
 
 /**
  * Lists all the stocks that do not have the adequate supply, which is to say
@@ -18,13 +20,14 @@ public class CheckStockCommand extends Command {
     public static final String COMMAND_ALIAS = "cs";
     public static final String MESSAGE_SUCCESS = "The following medicines listed in the medicine panel "
             + "are low in supply.";
+    public static final Predicate<Medicine> MEDICINE_LOW_IN_SUPPLY_PREDICATE =
+        medicine -> medicine.getStockValue() <= medicine.getMsqValue();
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+    public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
 
-        model.updateFilteredMedicineList(medicine ->
-                medicine.getStockValue() <= medicine.getMsqValue());
+        model.updateFilteredMedicineList(MEDICINE_LOW_IN_SUPPLY_PREDICATE);
 
         EventsCenter.getInstance().post(new ShowMedicineListEvent());
 
