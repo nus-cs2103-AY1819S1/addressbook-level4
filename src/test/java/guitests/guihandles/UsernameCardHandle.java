99a1e39024601f5ff47c674bdacf4ecc2f47a850
deming16 <neverggyay@gmail.com>
2018-11-11 11:47:01 +0800

    Update UsernameCardTest

---
+++ src/test/java/guitests/guihandles/UsernameCardHandle.java
@@ -0,0 +1,36 @@
+package guitests.guihandles;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import seedu.modsuni.model.credential.Username;
+
+public class UsernameCardHandle extends NodeHandle<Node> {
+    public static final String ID_FIELD_ID = "#id";
+    public static final String USERNAME_FIELD_ID = "#username";
+
+    private final Label idLabel;
+    private final Label usernameLabel;
+
+    public UsernameCardHandle(Node cardNode) {
+        super(cardNode);
+
+        idLabel = getChildNode(ID_FIELD_ID);
+        usernameLabel = getChildNode(USERNAME_FIELD_ID);
+    }
+
+    public String getId() {
+        return idLabel.getText();
+    }
+
+    public String getUsername() {
+        return usernameLabel.getText();
+    }
+
+
+    /**
+     * Returns true if this handle contains {@code username}.
+     */
+    public boolean equals(Username username) {
+        return getUsername().equals(username.getUsername());
+    }
+}
