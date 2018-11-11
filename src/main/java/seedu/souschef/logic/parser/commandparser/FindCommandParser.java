c84358c6ef0fd8e01e281bfc0cf3142bab101fe8
Noobscrub95 <smjlim2@msn.com>
2018-11-11 12:13:58 +0800

    Resolve merge conflict

--- src/main/java/seedu/souschef/logic/parser/commandparser/FindCommandParser.java
+++ src/main/java/seedu/souschef/logic/parser/commandparser/FindCommandParser.java
@@ -1,43 +1,36 @@
 package seedu.souschef.logic.parser.commandparser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.souschef.commons.core.Messages.MESSAGE_FIND_FAVOURITES_USAGE;
+import static seedu.souschef.commons.core.Messages.MESSAGE_FIND_INGREDIENT_USAGE;
+import static seedu.souschef.commons.core.Messages.MESSAGE_FIND_MEALPLAN_USAGE;
 import static seedu.souschef.commons.core.Messages.MESSAGE_FIND_RECIPE_USAGE;
 import static seedu.souschef.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
 
 import seedu.souschef.logic.commands.FindCommand;
 import seedu.souschef.logic.parser.exceptions.ParseException;
 import seedu.souschef.model.Model;
-import seedu.souschef.model.favourite.Favourites;
 import seedu.souschef.model.ingredient.Ingredient;
-import seedu.souschef.model.recipe.FavouriteNameContainsKeywordsPredicate;
-import seedu.souschef.model.recipe.IngredientNameContainsKeywordsPredicate;
-import seedu.souschef.model.recipe.NameContainsKeywordsPredicate;
+import seedu.souschef.model.ingredient.IngredientNameContainsKeywordsPredicate;
+import seedu.souschef.model.planner.Day;
+import seedu.souschef.model.planner.MealPlanContainsDatePredicate;
 import seedu.souschef.model.recipe.Recipe;
+import seedu.souschef.model.recipe.RecipeContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
-public class FindCommandParser implements CommandParser<FindCommand> {
-
+public class FindCommandParser {
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public FindCommand<Recipe> parseRecipe(Model model, String args) throws ParseException {
-        requireNonNull(model);
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_FIND_RECIPE_USAGE));
-        }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand<>(model, new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    public FindCommand<Recipe> parseRecipe(Model<Recipe> model, String args) throws ParseException {
+        return new FindCommand<>(model,
+                new RecipeContainsKeywordsPredicate(parse(model, args, MESSAGE_FIND_RECIPE_USAGE)));
     }
 
     /**
