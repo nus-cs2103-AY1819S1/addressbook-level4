0d94917fecf072935e86f3c7fa0bbaf12a55e9ad
Javen <javenseow@gmail.com>
2018-11-11 09:55:23 +0800

    Corrected checkstyle errors in Search Command System Test

--- src/test/java/systemtests/SearchCommandSystemTest.java
+++ src/test/java/systemtests/SearchCommandSystemTest.java
@@ -4,9 +4,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIE
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
-import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.ELLE;
-import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.GEORGE;
 import static seedu.address.testutil.TypicalPersons.KEYWORD_MATCHING_D111;
 import static seedu.address.testutil.TypicalPersons.KEYWORD_MATCHING_ENGINE;
