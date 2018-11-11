50d3ec49a7fb3a4b432e900c61ba4bd8ad9dd422
pratyayj <pratyay.jaidev@gmail.com>
2018-11-12 00:35:27 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/GenerateLocationCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GenerateLocationCommandTest.java
@@ -1,18 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.RandomMeetingLocationGeneratedEvent;
-import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.event.EventDate;
-import seedu.address.ui.testutil.EventsCollectorRule;
-
-
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -23,6 +10,18 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.RandomMeetingLocationGeneratedEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.event.EventDate;
+import seedu.address.ui.testutil.EventsCollectorRule;
 
 class GenerateLocationCommandTest {
 
