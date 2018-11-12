d656d85c53b95332c6dfc984be38aa1c0ce8da66
barkhyehyeon <barkhyehyeon@gmail.com>
2018-11-12 10:31:24 +0800

    Create test cases for ingredient models

---
+++ src/test/java/seedu/souschef/model/ingredient/IngredientDefinitionTest.java
@@ -0,0 +1,34 @@
+package seedu.souschef.model.ingredient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IngredientDefinitionTest {
+    private IngredientDefinition onion = new IngredientDefinition(new IngredientName("onion"));
+    private IngredientDefinition otherOnion = new IngredientDefinition("onion");
+    private IngredientDefinition carrot = new IngredientDefinition("carrot");
+
+    @Test
+    public void getNameTest() {
+        assertEquals(onion.getName(), otherOnion.getName());
+        assertEquals(onion.getName(), carrot.getName());
+    }
+
+    @Test
+    public void isSameTest() {
+        assertTrue(onion.isSame(onion));
+        assertTrue(onion.isSame(otherOnion));
+        assertFalse(onion.isSame(carrot));
+    }
+
+    @Test
+    public void equalsTest() {
+        assertEquals(onion, onion);
+        assertEquals(onion, otherOnion);
+        assertNotEquals(onion, carrot);
+    }
+}
---
+++ src/test/java/seedu/souschef/model/ingredient/IngredientTest.java
@@ -0,0 +1,79 @@
+package seedu.souschef.model.ingredient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.Test;
+
+public class IngredientTest {
+    private Ingredient onion;
+    private Ingredient otherOnion;
+    private Ingredient carrot;
+    {
+        try {
+            onion = new Ingredient(new IngredientName("onion"),
+                    new IngredientAmount(200.0), new IngredientServingUnit("gram"),
+                    new IngredientDate(new SimpleDateFormat("MM-dd-yyyy").parse("11-12-2018")));
+            otherOnion = new Ingredient("onion", "gram", 100.0,
+                    new SimpleDateFormat("MM-dd-yyyy").parse("11-12-2018"));
+            carrot = new Ingredient("carrot", "kg", 1.0,
+                    new SimpleDateFormat("MM-dd-yyyy").parse("11-13-2018"));
+        } catch (ParseException e) {
+            System.out.println("Wrong Date Format!");
+        }
+    }
+
+
+    @Test
+    public void getNameTest() {
+        assertEquals(onion.getName(), otherOnion.getName());
+        assertNotEquals(onion.getName(), carrot.getName());
+    }
+
+    @Test
+    public void getUnitTest() {
+        assertEquals(onion.getUnit(), otherOnion.getUnit());
+        assertNotEquals(otherOnion.getUnit(), carrot.getUnit());
+    }
+
+    @Test
+    public void getAmountTest() {
+        assertNotEquals(onion.getAmount(), otherOnion.getAmount());
+        assertEquals(otherOnion.getAmount(), carrot.getAmount());
+    }
+
+    @Test
+    public void getDateTest() {
+        assertEquals(onion.getDate(), otherOnion.getDate());
+        assertNotEquals(otherOnion.getDate(), carrot.getDate());
+    }
+
+    @Test
+    public void convertToCommonUnitTest() {
+        Ingredient convertedOnion = onion.convertToCommonUnit();
+        Ingredient convertedCarrot = carrot.convertToCommonUnit();
+        assertEquals(convertedOnion.getUnit(), new IngredientServingUnit("gram"));
+        assertEquals(convertedCarrot.getUnit(), new IngredientServingUnit("gram"));
+        assertEquals(200.0, convertedOnion.getAmount().getValue(), 0.0);
+        assertEquals(1000.0, convertedCarrot.getAmount().getValue(), 0.0);
+    }
+
+    @Test
+    public void isSameTest() {
+        assertTrue(onion.isSame(onion));
+        assertFalse(onion.isSame(otherOnion));
+        assertFalse(onion.isSame(carrot));
+    }
+
+    @Test
+    public void equalsTest() {
+        assertEquals(onion, onion);
+        assertNotEquals(onion, otherOnion);
+        assertNotEquals(onion, carrot);
+    }
+}
