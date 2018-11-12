be6a822e67e74b0eb587f179b60838adbe56267a
Swiftkage <lim.yu.xi@hotmail.com>
2018-11-12 22:34:06 +0800

    Add test cases for predicates

--- src/test/java/seedu/lostandfound/logic/parser/ResolveCommandParserTest.java
+++ src/test/java/seedu/lostandfound/logic/parser/ResolveCommandParserTest.java
@@ -1,13 +1,23 @@
 package seedu.lostandfound.logic.parser;
 
 import static seedu.lostandfound.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.lostandfound.logic.commands.CommandTestUtil.INVALID_OWNER_DESC;
 import static seedu.lostandfound.logic.commands.CommandTestUtil.NAME_DESC_POWERBANK;
+import static seedu.lostandfound.logic.commands.CommandTestUtil.OWNER_DESC_MOUSE;
+import static seedu.lostandfound.logic.commands.CommandTestUtil.OWNER_DESC_POWERBANK;
 import static seedu.lostandfound.logic.commands.CommandTestUtil.VALID_NAME_POWERBANK;
+import static seedu.lostandfound.logic.commands.CommandTestUtil.VALID_OWNER_MOUSE;
+import static seedu.lostandfound.logic.commands.CommandTestUtil.VALID_OWNER_POWERBANK;
 import static seedu.lostandfound.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.lostandfound.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.lostandfound.testutil.TypicalIndexes.INDEX_FIRST_ARTICLE;
+import static seedu.lostandfound.testutil.TypicalIndexes.INDEX_THIRD_ARTICLE;
 
 import org.junit.Test;
 
+import seedu.lostandfound.commons.core.index.Index;
 import seedu.lostandfound.logic.commands.ResolveCommand;
+import seedu.lostandfound.model.article.Name;
 
 public class ResolveCommandParserTest {
     private static final String MESSAGE_INVALID_FORMAT =
@@ -42,4 +52,44 @@ public class ResolveCommandParserTest {
         assertParseFailure(parser, "1 i/ string", MESSAGE_INVALID_FORMAT);
     }
 
+    @Test
+    public void parse_invalidValue_failure() {
+        assertParseFailure(parser, "1" + INVALID_OWNER_DESC, Name.MESSAGE_CONSTRAINTS); // invalid finder
+
+        // valid owner followed by invalid owner. The test case for invalid owner followed by valid owner
+        // is tested at {@code parse_invalidValueFollowedByValidValue_success()}
+        assertParseFailure(parser, "1" + OWNER_DESC_MOUSE + INVALID_OWNER_DESC, Name.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_fieldSpecified_success() {
+        // owner
+        Index targetIndex = INDEX_THIRD_ARTICLE;
+        String userInput = targetIndex.getOneBased() + OWNER_DESC_POWERBANK;
+        Name owner = new Name(VALID_OWNER_POWERBANK);
+        ResolveCommand expectedCommand = new ResolveCommand(targetIndex, owner);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_multipleRepeatedField_acceptsLast() {
+        Index targetIndex = INDEX_FIRST_ARTICLE;
+        String userInput = targetIndex.getOneBased() + OWNER_DESC_POWERBANK + OWNER_DESC_POWERBANK
+                + OWNER_DESC_MOUSE;
+
+        Name owner = new Name(VALID_OWNER_MOUSE);
+        ResolveCommand expectedCommand = new ResolveCommand(targetIndex, owner);
+
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidValueFollowedByValidValue_success() {
+        // no other valid values specified
+        Index targetIndex = INDEX_FIRST_ARTICLE;
+        String userInput = targetIndex.getOneBased() + INVALID_OWNER_DESC + OWNER_DESC_MOUSE;
+        Name owner = new Name(VALID_OWNER_MOUSE);
+        ResolveCommand expectedCommand = new ResolveCommand(targetIndex, owner);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
 }
