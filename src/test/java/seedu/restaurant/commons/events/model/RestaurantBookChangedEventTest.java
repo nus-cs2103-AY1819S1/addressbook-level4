503c2723cc96e4d677e51e066c3b96f0155d07a5
AZhiKai <ang-zhikai@outlook.com>
2018-11-12 17:33:00 +0800

    SampleDataUtil: Add method to get sample data
    Currently, the sample data only contains the root account and nothing else.
    
    This might be an issue when it comes to PE or product demonstration.
    
    Let's update the file by including a method that populates the storage with decent amount of data. In addition, update related test cases (mainly the Clear-related cases) to use the method that generates only the root account and not the sample data.

--- src/test/java/seedu/restaurant/commons/events/model/RestaurantBookChangedEventTest.java
+++ src/test/java/seedu/restaurant/commons/events/model/RestaurantBookChangedEventTest.java
@@ -17,9 +17,16 @@ public class RestaurantBookChangedEventTest {
     }
 
     @Test
-    public void createEvent_withStockRestaurantBook_success() {
-        BaseEvent event = new RestaurantBookChangedEvent(SampleDataUtil.getSampleRestaurantBook());
+    public void createEvent_withDefaultRestaurantBook_success() {
+        BaseEvent event = new RestaurantBookChangedEvent(SampleDataUtil.getDefaultRestaurantBook());
         assertEquals("number of accounts 1, number of items 0, number of reservations 0, "
                 + "number of sales record 0, number of ingredients 0", event.toString());
+    }
+
+    @Test
+    public void createEvent_withSampleRestaurantBook_success() {
+        BaseEvent event = new RestaurantBookChangedEvent(SampleDataUtil.getSampleRestaurantBook());
+        assertEquals("number of accounts 1, number of items 2, number of reservations 2, "
+                + "number of sales record 6, number of ingredients 5", event.toString());
     }
 }
