a31def0f8170d556eb69b2addb7e4c21d6a32dcb
Woodnsuns <pollylin0617@gmail.com>
2018-11-12 11:41:00 +0800

    Added RemoveCategoryCommandTest

--- src/main/java/ssp/scheduleplanner/commons/core/Messages.java
+++ src/main/java/ssp/scheduleplanner/commons/core/Messages.java
@@ -9,5 +9,10 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
+    public static final String MESSAGE_DEFAULT_CATEGORY =
+            "Default categories (Modules and Others) cannot be removed. \n"
+                    + "(But you can clear the tags using clear command)";
+    public static final String MESSAGE_NONEXISTENT_CATEGORY =
+            "This category does not exist your schedule planner T_T";
 
 }
