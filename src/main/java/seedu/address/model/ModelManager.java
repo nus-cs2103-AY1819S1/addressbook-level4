7814b1201f0fd53db41ec366b258a7ff73b4fb62
yao-feng <yaofeng96@gmail.com>
2018-11-12 09:12:01 +0800

    Update documents

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -69,7 +69,7 @@ public class ModelManager extends ComponentManager implements Model {
      * @param person the User to be set as the current user.
      */
     public void setCurrentUser(Person person) {
-        
+        assert person != null;
         this.currentUser = person;
         indicateUserLoginStatusChanged();
     }
