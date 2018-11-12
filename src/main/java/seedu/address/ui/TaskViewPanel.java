b451ee144524c15a20f79b12845519ffde79c12c
pengnam <seanngpengnam@gmail.com>
2018-11-12 09:07:58 +0800

    Amend docs

--- src/main/java/seedu/address/ui/TaskViewPanel.java
+++ src/main/java/seedu/address/ui/TaskViewPanel.java
@@ -137,8 +137,9 @@ public class TaskViewPanel extends UiPart<Region> {
     }
     @Subscribe
     public void handleTaskManagerChangedEvent(TaskManagerChangedEvent tmce) {
-        //Try catch clause used here as both complete and dependency has several intermediate states of the task
-        //manager that might trigger this event and throw and exception.
+        //Try catch clause used here as both complete and dependency has intermediate states in an operation
+        //that are invalid. This try catch clause is to preemptively catch these exceptions thrown from these
+        //states
         try {
             earliestTimeOfChildren.setText(tmce.data.getEarliestDependentTimeForNode(this.displayedTask.get()).value);
         } catch (Exception e) {
