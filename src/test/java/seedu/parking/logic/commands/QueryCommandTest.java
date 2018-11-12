b3ed08a5a6aaf4e7c258b2a31b862a345f954962
ZhuanYu <senauh_ryo@hotmail.com>
2018-11-12 23:25:48 +0800

    fix

--- src/test/java/seedu/parking/logic/commands/QueryCommandTest.java
+++ src/test/java/seedu/parking/logic/commands/QueryCommandTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 import guitests.guihandles.MainWindowHandle;
