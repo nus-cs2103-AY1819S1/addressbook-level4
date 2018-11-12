f0e26e857e4ed9a3501908eec6efe55288de60a8
Jack Wong <jackwong3101@yahoo.com>
2018-11-12 21:50:43 +0800

    fix typo

--- src/main/java/seedu/clinicio/ui/MainWindow.java
+++ src/main/java/seedu/clinicio/ui/MainWindow.java
@@ -278,7 +278,7 @@ public class MainWindow extends UiPart<Stage> {
      */
     public void switchTab(int index) {
         for (int i = 0; i < tabLists.getTabs().size(); i++) {
-            tabLists.getSelectionModel().clearAndSelect(login r/receptionist n/Bell pass/recep1login r/receptionist n/Bell pass/recep1i);
+            tabLists.getSelectionModel().clearAndSelect(i);
         }
         tabLists.getSelectionModel().clearAndSelect(index);
     }
