b166171627ca557add7584b93c36105137b6c2b4
ZhuanYu <senauh_ryo@hotmail.com>
2018-11-12 18:31:18 +0800

    Checking for null pointer

--- src/test/java/seedu/parking/commons/util/GsonUtilTest.java
+++ src/test/java/seedu/parking/commons/util/GsonUtilTest.java
@@ -3,21 +3,20 @@ package seedu.parking.commons.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class GsonUtilTest extends GsonUtil {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void fetchAllCarparkInfo_getAllCarparks_notNullReturned() throws Exception {
-        assertNotNull(fetchAllCarparkInfo());
+
+        try {
+            assertNotNull(fetchAllCarparkInfo());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -28,11 +27,5 @@ public class GsonUtilTest extends GsonUtil {
     @Test
     public void getSelectedCarparkInfo_getEmptyList_notNullReturned() throws Exception {
         assertEquals(getSelectedCarparkInfo(""), new ArrayList<String>());
-    }
-
-    @Test
-    public void fetchIndivudlaCarparkPostalCode () throws IOException {
-        //System.out.println("test: " + getCarparkPostalData("15758.7046", "34458.9721"));
-        //assertEquals("610138", getCarparkPostalData("15758.7046", "34458.9721"));
     }
 }
