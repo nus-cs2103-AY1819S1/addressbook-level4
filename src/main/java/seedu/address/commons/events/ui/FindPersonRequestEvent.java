3f1d0973d0bc3d64baba003736f8ddc59e3cd9db
xueyantian <daisytianxueyan@gmail.com>
2018-11-12 17:36:52 +0800

    Update style.

--- src/main/java/seedu/address/commons/events/ui/FindPersonRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/FindPersonRequestEvent.java
@@ -2,6 +2,9 @@ package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
 
+/**
+ * Indicates a request for Find Person
+ */
 public class FindPersonRequestEvent extends BaseEvent {
 
     @Override
