6599997cb4f4845551aea1a0f8f8efb8f8977799
deming16 <neverggyay@gmail.com>
2018-11-12 12:58:23 +0800

    Add test cases for Code

--- src/test/java/seedu/modsuni/model/module/CodeTest.java
+++ src/test/java/seedu/modsuni/model/module/CodeTest.java
@@ -4,11 +4,33 @@ import org.junit.Test;
 
 import seedu.modsuni.testutil.Assert;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class CodeTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
         Assert.assertThrows(NullPointerException.class, () -> new Code(null));
+    }
+
+    @Test
+    public void constructor_invalidCode_throwsIllegalArgumentException() {
+        String invalidCode = "#cs1010";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Code(invalidCode));
+    }
+
+    @Test
+    public void isValidSalary() {
+        // null code
+        Assert.assertThrows(NullPointerException.class, () -> Code.isValidCode(null));
+
+        //invalid code
+        assertFalse(Code.isValidCode("#CS1010")); // special character
+        assertFalse(Code.isValidCode("")); // empty string
+
+        //valid code
+        assertTrue(Code.isValidCode("CS1010"));
     }
 
 }
