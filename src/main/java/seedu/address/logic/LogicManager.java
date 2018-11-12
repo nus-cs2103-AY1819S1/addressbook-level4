6b35b4bdf7493d13b4dfd43a9a0d6385a4df8094
Jefferson Sie <39295219+jeffypie369@users.noreply.github.com>
2018-11-12 21:20:07 +0800

    Jeff's UG, DG, PPP (#215)
    * Make changes to UG and PPP
    
    * Make changes to UG and PPP
    
    * Make changes to DG
    
    * Refactor AddApptCommand to better suit Sequence Diagram
    
    * Update sequence of addappt in DG
    
    * Fix sequence of addappt in DG
    
    * Edit AddapptSequenceDiagram
    
    * Fix DG
    
    * Edit ppp
    
    * Update UGDG
    
    * Add screenshots for UG
    
    * Update AddapptSequenceDiagram
    
    * Add addapptactivitydiagram
    
    * Make changes to PPP
    
    * Update DG
    
    * resolve conflict
    
    * Add test instructions in DG
    
    * Edit UG
    
    * Edit UGDGPPP

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -33,7 +33,7 @@ public class LogicManager extends ComponentManager implements Logic {
     public CommandResult execute(String commandText) throws CommandException, ParseException {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
         try {
-            Command command = healthBaseParser.parseCommand(commandText);
+            Command command = healthBaseParser.parseCommand(commandText); // <1>
             return command.execute(model, history);
         } finally {
             history.add(commandText);
