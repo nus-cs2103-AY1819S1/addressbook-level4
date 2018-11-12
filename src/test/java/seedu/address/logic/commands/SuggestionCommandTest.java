ccedff1ea3f3bda5f7515f447f559fa3d9fcf248
CJuanvip <42432423+CJuanvip@users.noreply.github.com>
2018-11-12 16:04:15 +0800

    Update SuggestionCommandTest.java

--- src/test/java/seedu/address/logic/commands/SuggestionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SuggestionCommandTest.java
@@ -2,17 +2,10 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 import java.util.Arrays;
 
 import org.junit.Test;
 
-import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 
